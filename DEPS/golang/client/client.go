07538e673c70519bfbd66f452226b78fcd0e995e
sdk-team <sdk-team@alibabacloud.com>
2021-01-20 15:27:49 +0800

    Generated SDK for DEPS.

--- DEPS/golang/client/client.go
+++ DEPS/golang/client/client.go
@@ -37003,7 +37003,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("3.2.0"),
+				"sdk_version":      tea.String("3.2.1"),
 			}
 			if !tea.BoolValue(util.Empty(client.SecurityToken)) {
 				request_.Query["security_token"] = client.SecurityToken
