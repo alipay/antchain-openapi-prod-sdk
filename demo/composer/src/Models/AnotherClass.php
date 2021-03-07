449f58037e840ce397a7973a154f3e6fba3bbf0f
sdk-team <sdk-team@alibabacloud.com>
2021-03-07 09:19:21 +0000

    Generated SDK for DEMO.

---
+++ demo/composer/src/Models/AnotherClass.php
@@ -0,0 +1,94 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AntChain\DEMO\Models;
+
+use AlibabaCloud\Tea\Model;
+
+class AnotherClass extends Model
+{
+    // 测试字段
+    /**
+     * @example some string
+     *
+     * @var string
+     */
+    public $bar;
+
+    // 引用字段
+    /**
+     * @example
+     *
+     * @var DemoClass
+     */
+    public $ref;
+
+    // 列表引用Struct
+    /**
+     * @example
+     *
+     * @var DemoClass[]
+     */
+    public $refList;
+    protected $_name = [
+        'bar'     => 'bar',
+        'ref'     => 'ref',
+        'refList' => 'refList',
+    ];
+
+    public function validate()
+    {
+        Model::validateRequired('bar', $this->bar, true);
+        Model::validateRequired('ref', $this->ref, true);
+    }
+
+    public function toMap()
+    {
+        $res = [];
+        if (null !== $this->bar) {
+            $res['bar'] = $this->bar;
+        }
+        if (null !== $this->ref) {
+            $res['ref'] = null !== $this->ref ? $this->ref->toMap() : null;
+        }
+        if (null !== $this->refList) {
+            $res['refList'] = [];
+            if (null !== $this->refList && \is_array($this->refList)) {
+                $n = 0;
+                foreach ($this->refList as $item) {
+                    $res['refList'][$n++] = null !== $item ? $item->toMap() : $item;
+                }
+            }
+        }
+
+        return $res;
+    }
+
+    /**
+     * @param array $map
+     *
+     * @return AnotherClass
+     */
+    public static function fromMap($map = [])
+    {
+        $model = new self();
+        if (isset($map['bar'])) {
+            $model->bar = $map['bar'];
+        }
+        if (isset($map['ref'])) {
+            $model->ref = DemoClass::fromMap($map['ref']);
+        }
+        if (isset($map['refList'])) {
+            if (!empty($map['refList'])) {
+                $model->refList = [];
+                $n              = 0;
+                foreach ($map['refList'] as $item) {
+                    $model->refList[$n++] = null !== $item ? DemoClass::fromMap($item) : $item;
+                }
+            }
+        }
+
+        return $model;
+    }
+}
---
+++ demo/composer/src/Models/EchoGatewayCheckResponse.php
@@ -0,0 +1,110 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AntChain\DEMO\Models;
+
+use AlibabaCloud\Tea\Model;
+
+class EchoGatewayCheckResponse extends Model
+{
+    /**
+     * @var string
+     */
+    public $reqMsgId;
+
+    /**
+     * @var string
+     */
+    public $resultCode;
+
+    /**
+     * @var string
+     */
+    public $resultMsg;
+
+    // output_demo
+    /**
+     * @var DemoClass
+     */
+    public $outputDemo;
+
+    // output_string
+    /**
+     * @var string
+     */
+    public $outputString;
+
+    // file_url
+    /**
+     * @var string
+     */
+    public $fileUrl;
+    protected $_name = [
+        'reqMsgId'     => 'req_msg_id',
+        'resultCode'   => 'result_code',
+        'resultMsg'    => 'result_msg',
+        'outputDemo'   => 'output_demo',
+        'outputString' => 'output_string',
+        'fileUrl'      => 'file_url',
+    ];
+
+    public function validate()
+    {
+    }
+
+    public function toMap()
+    {
+        $res = [];
+        if (null !== $this->reqMsgId) {
+            $res['req_msg_id'] = $this->reqMsgId;
+        }
+        if (null !== $this->resultCode) {
+            $res['result_code'] = $this->resultCode;
+        }
+        if (null !== $this->resultMsg) {
+            $res['result_msg'] = $this->resultMsg;
+        }
+        if (null !== $this->outputDemo) {
+            $res['output_demo'] = null !== $this->outputDemo ? $this->outputDemo->toMap() : null;
+        }
+        if (null !== $this->outputString) {
+            $res['output_string'] = $this->outputString;
+        }
+        if (null !== $this->fileUrl) {
+            $res['file_url'] = $this->fileUrl;
+        }
+
+        return $res;
+    }
+
+    /**
+     * @param array $map
+     *
+     * @return EchoGatewayCheckResponse
+     */
+    public static function fromMap($map = [])
+    {
+        $model = new self();
+        if (isset($map['req_msg_id'])) {
+            $model->reqMsgId = $map['req_msg_id'];
+        }
+        if (isset($map['result_code'])) {
+            $model->resultCode = $map['result_code'];
+        }
+        if (isset($map['result_msg'])) {
+            $model->resultMsg = $map['result_msg'];
+        }
+        if (isset($map['output_demo'])) {
+            $model->outputDemo = DemoClass::fromMap($map['output_demo']);
+        }
+        if (isset($map['output_string'])) {
+            $model->outputString = $map['output_string'];
+        }
+        if (isset($map['file_url'])) {
+            $model->fileUrl = $map['file_url'];
+        }
+
+        return $model;
+    }
+}
