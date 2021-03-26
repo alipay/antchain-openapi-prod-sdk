44bfdf408b69e129661d0a4dcf5d39ce8829585b
sdk-team <sdk-team@alibabacloud.com>
2021-03-26 02:22:23 +0000

    Generated SDK for TWC.

--- twc/java/src/main/java/com/antgroup/antchain/openapi/twc/Client.java
+++ twc/java/src/main/java/com/antgroup/antchain/openapi/twc/Client.java
@@ -110,7 +110,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.4.143")
+                    new TeaPair("sdk_version", "1.4.144")
                 );
                 if (!com.aliyun.teautil.Common.empty(_securityToken)) {
                     request_.query.put("security_token", _securityToken);
@@ -1396,6 +1396,25 @@ public class Client {
     public DeleteContractSignerResponse deleteContractSignerEx(DeleteContractSignerRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "twc.notary.contract.signer.delete", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new DeleteContractSignerResponse());
+    }
+
+    /**
+     * Description: 获取区块链合同存证证明
+     * Summary: 获取区块链合同存证证明
+     */
+    public GetContractCertificateResponse getContractCertificate(GetContractCertificateRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.getContractCertificateEx(request, headers, runtime);
+    }
+
+    /**
+     * Description: 获取区块链合同存证证明
+     * Summary: 获取区块链合同存证证明
+     */
+    public GetContractCertificateResponse getContractCertificateEx(GetContractCertificateRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "twc.notary.contract.certificate.get", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new GetContractCertificateResponse());
     }
 
     /**
@@ -3163,25 +3182,6 @@ public class Client {
     public CreateLeaseZftagreementunsignResponse createLeaseZftagreementunsignEx(CreateLeaseZftagreementunsignRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "twc.notary.lease.zftagreementunsign.create", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new CreateLeaseZftagreementunsignResponse());
-    }
-
-    /**
-     * Description: 获取区块链合同存证证明
-     * Summary: 获取区块链合同存证证明
-     */
-    public GetContractCertificateResponse getContractCertificate(GetContractCertificateRequest request) throws Exception {
-        RuntimeOptions runtime = new RuntimeOptions();
-        java.util.Map<String, String> headers = new java.util.HashMap<>();
-        return this.getContractCertificateEx(request, headers, runtime);
-    }
-
-    /**
-     * Description: 获取区块链合同存证证明
-     * Summary: 获取区块链合同存证证明
-     */
-    public GetContractCertificateResponse getContractCertificateEx(GetContractCertificateRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
-        com.aliyun.teautil.Common.validateModel(request);
-        return TeaModel.toModel(this.doRequest("1.0", "twc.notary.contract.certificate.get", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new GetContractCertificateResponse());
     }
 
     /**
