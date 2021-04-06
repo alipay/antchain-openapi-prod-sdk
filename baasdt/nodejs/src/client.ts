2f9ebab5f7b109e87e5c4d6b12ef6ce2be55c632
sdk-team <sdk-team@alibabacloud.com>
2021-04-06 09:53:17 +0000

    Generated SDK for BAASDT.

--- baasdt/nodejs/src/client.ts
+++ baasdt/nodejs/src/client.ts
@@ -21124,7 +21124,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.2.19",
+          sdk_version: "1.2.20",
         };
         if (!Util.empty(this._securityToken)) {
           request_.query["security_token"] = this._securityToken;
