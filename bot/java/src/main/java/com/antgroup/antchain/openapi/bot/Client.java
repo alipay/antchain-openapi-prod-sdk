89909b5a0689cb9fc0c9afe910eecf05ba7f7713
sdk-team <sdk-team@alibabacloud.com>
2021-04-23 11:29:54 +0000

    Generated SDK for BOT.

--- bot/java/src/main/java/com/antgroup/antchain/openapi/bot/Client.java
+++ bot/java/src/main/java/com/antgroup/antchain/openapi/bot/Client.java
@@ -110,7 +110,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.3.15")
+                    new TeaPair("sdk_version", "1.3.16")
                 );
                 if (!com.aliyun.teautil.Common.empty(_securityToken)) {
                     request_.query.put("security_token", _securityToken);
@@ -157,6 +157,25 @@ public class Client {
         }
 
         throw new TeaUnretryableException(_lastRequest, _lastException);
+    }
+
+    /**
+     * Description: 根据设备串号查询采购设备
+     * Summary: 根据设备串号查询采购设备
+     */
+    public QueryIotplatformPurchaseorderResponse queryIotplatformPurchaseorder(QueryIotplatformPurchaseorderRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.queryIotplatformPurchaseorderEx(request, headers, runtime);
+    }
+
+    /**
+     * Description: 根据设备串号查询采购设备
+     * Summary: 根据设备串号查询采购设备
+     */
+    public QueryIotplatformPurchaseorderResponse queryIotplatformPurchaseorderEx(QueryIotplatformPurchaseorderRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "blockchain.bot.iotplatform.purchaseorder.query", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new QueryIotplatformPurchaseorderResponse());
     }
 
     /**
