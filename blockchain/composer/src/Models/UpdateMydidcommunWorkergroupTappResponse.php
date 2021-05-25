f9600787983063c56cb481232351ae514dad051f
sdk-team <sdk-team@alibabacloud.com>
2021-05-25 06:44:41 +0000

    Generated SDK for BLOCKCHAIN.

--- blockchain/composer/src/Models/UpdateMydidcommunWorkergroupTappResponse.php
+++ blockchain/composer/src/Models/UpdateMydidcommunWorkergroupTappResponse.php
@@ -25,17 +25,10 @@ class UpdateMydidcommunWorkergroupTappResponse extends Model
      * @var string
      */
     public $resultMsg;
-
-    // 是否成功 1 成功 2 失败
-    /**
-     * @var string
-     */
-    public $data;
     protected $_name = [
         'reqMsgId'   => 'req_msg_id',
         'resultCode' => 'result_code',
         'resultMsg'  => 'result_msg',
-        'data'       => 'data',
     ];
 
     public function validate()
@@ -53,9 +46,6 @@ class UpdateMydidcommunWorkergroupTappResponse extends Model
         }
         if (null !== $this->resultMsg) {
             $res['result_msg'] = $this->resultMsg;
-        }
-        if (null !== $this->data) {
-            $res['data'] = $this->data;
         }
 
         return $res;
@@ -77,9 +67,6 @@ class UpdateMydidcommunWorkergroupTappResponse extends Model
         }
         if (isset($map['result_msg'])) {
             $model->resultMsg = $map['result_msg'];
-        }
-        if (isset($map['data'])) {
-            $model->data = $map['data'];
         }
 
         return $model;
