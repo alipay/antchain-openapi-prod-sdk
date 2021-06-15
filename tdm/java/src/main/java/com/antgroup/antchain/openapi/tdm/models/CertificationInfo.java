0f22b3734cfd15ee35d9f2ed924b8d10ab1581c7
sdk-team <sdk-team@alibabacloud.com>
2021-06-15 06:45:08 +0000

    Generated SDK for TDM.

--- tdm/java/src/main/java/com/antgroup/antchain/openapi/tdm/models/CertificationInfo.java
+++ tdm/java/src/main/java/com/antgroup/antchain/openapi/tdm/models/CertificationInfo.java
@@ -4,9 +4,21 @@ package com.antgroup.antchain.openapi.tdm.models;
 import com.aliyun.tea.*;
 
 public class CertificationInfo extends TeaModel {
+    // 是否授权
+    @NameInMap("certification_flag")
+    public Boolean certificationFlag;
+
     public static CertificationInfo build(java.util.Map<String, ?> map) throws Exception {
         CertificationInfo self = new CertificationInfo();
         return TeaModel.build(map, self);
+    }
+
+    public CertificationInfo setCertificationFlag(Boolean certificationFlag) {
+        this.certificationFlag = certificationFlag;
+        return this;
+    }
+    public Boolean getCertificationFlag() {
+        return this.certificationFlag;
     }
 
 }
