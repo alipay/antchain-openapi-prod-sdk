e1e0bced215f058d19ea3db82d3662eb448bcaf7
sdk-team <sdk-team@alibabacloud.com>
2021-07-01 08:07:51 +0000

    Generated SDK for TDM.

--- tdm/go/client/client.go
+++ tdm/go/client/client.go
@@ -3961,7 +3961,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.1.3"),
+				"sdk_version":      tea.String("1.1.4"),
 			}
 			if !tea.BoolValue(util.Empty(client.SecurityToken)) {
 				request_.Query["security_token"] = client.SecurityToken
