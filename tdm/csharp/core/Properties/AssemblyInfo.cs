a9e9c4f8b6d551c5753fa9aa330dcbed1f188a17
sdk-team <sdk-team@alibabacloud.com>
2021-07-01 08:08:00 +0000

    Generated SDK for TDM.

--- tdm/csharp/core/Properties/AssemblyInfo.cs
+++ tdm/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3653ff10-d8a5-11eb-a3fa-5bb29f3f8ef3")]
+[assembly: Guid("6e3c4640-da43-11eb-ada3-0b28338cc200")]
 
 // Version information for an assembly consists of the following four values:
 //
