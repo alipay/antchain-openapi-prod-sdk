fa8b1ed3f331e8dfe5acc7a603e491bc978f9383
sdk-team <sdk-team@alibabacloud.com>
2021-07-12 05:53:45 +0000

    Generated SDK for TWC.

--- twc/nodejs/src/client.ts
+++ twc/nodejs/src/client.ts
@@ -18334,7 +18334,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.5.12",
+          sdk_version: "1.5.13",
         };
         if (!Util.empty(this._securityToken)) {
           request_.query["security_token"] = this._securityToken;
