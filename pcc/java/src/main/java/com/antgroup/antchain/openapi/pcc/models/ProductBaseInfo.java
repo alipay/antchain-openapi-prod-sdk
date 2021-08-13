b9cdd60ac5446fb365d7c3224ae170a109fe4e99
sdk-team <sdk-team@alibabacloud.com>
2021-08-13 03:37:00 +0000

    Generated SDK for PCC.

--- pcc/java/src/main/java/com/antgroup/antchain/openapi/pcc/models/ProductBaseInfo.java
+++ pcc/java/src/main/java/com/antgroup/antchain/openapi/pcc/models/ProductBaseInfo.java
@@ -4,7 +4,7 @@ package com.antgroup.antchain.openapi.pcc.models;
 import com.aliyun.tea.*;
 
 public class ProductBaseInfo extends TeaModel {
-    // 产品码
+    // 产品码，系统内部识别
     @NameInMap("code")
     @Validation(required = true)
     public String code;
@@ -17,6 +17,11 @@ public class ProductBaseInfo extends TeaModel {
     // 产品英文名称
     @NameInMap("name_en")
     public String nameEn;
+
+    // 产品短码，管理维度识别
+    @NameInMap("short_code")
+    @Validation(required = true)
+    public String shortCode;
 
     public static ProductBaseInfo build(java.util.Map<String, ?> map) throws Exception {
         ProductBaseInfo self = new ProductBaseInfo();
@@ -45,6 +50,14 @@ public class ProductBaseInfo extends TeaModel {
     }
     public String getNameEn() {
         return this.nameEn;
+    }
+
+    public ProductBaseInfo setShortCode(String shortCode) {
+        this.shortCode = shortCode;
+        return this;
+    }
+    public String getShortCode() {
+        return this.shortCode;
     }
 
 }
