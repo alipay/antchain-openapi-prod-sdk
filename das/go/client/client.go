45a9f10c798220f6330f678c7473fffb05a22c47
sdk-team <sdk-team@alibabacloud.com>
2021-09-03 04:13:44 +0000

    Generated SDK for DAS.

--- das/go/client/client.go
+++ das/go/client/client.go
@@ -1731,7 +1731,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.1.4"),
+				"sdk_version":      tea.String("1.1.5"),
 			}
 			if !tea.BoolValue(util.Empty(client.SecurityToken)) {
 				request_.Query["security_token"] = client.SecurityToken
