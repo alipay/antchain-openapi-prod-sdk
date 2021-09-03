a20f168214f172090184b91fee3dfa1708d50d9c
sdk-team <sdk-team@alibabacloud.com>
2021-09-03 04:14:05 +0000

    Generated SDK for DAS.

--- das/nodejs/src/client.ts
+++ das/nodejs/src/client.ts
@@ -1353,7 +1353,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.1.4",
+          sdk_version: "1.1.5",
         };
         if (!Util.empty(this._securityToken)) {
           request_.query["security_token"] = this._securityToken;
