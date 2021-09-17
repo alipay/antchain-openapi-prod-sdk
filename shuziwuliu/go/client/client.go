2ad421841eb610b969ef691ffc1dce343309886e
sdk-team <sdk-team@alibabacloud.com>
2021-09-17 02:41:22 +0000

    Generated SDK for SHUZIWULIU.

--- shuziwuliu/go/client/client.go
+++ shuziwuliu/go/client/client.go
@@ -28669,7 +28669,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.3.127"),
+				"sdk_version":      tea.String("1.3.128"),
 			}
 			if !tea.BoolValue(util.Empty(client.SecurityToken)) {
 				request_.Query["security_token"] = client.SecurityToken
