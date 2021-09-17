04a7df4ef5d5e002c844006024fedfe4eac63b4f
sdk-team <sdk-team@alibabacloud.com>
2021-09-17 02:41:53 +0000

    Generated SDK for SHUZIWULIU.

--- shuziwuliu/nodejs/src/client.ts
+++ shuziwuliu/nodejs/src/client.ts
@@ -20277,7 +20277,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.3.127",
+          sdk_version: "1.3.128",
         };
         if (!Util.empty(this._securityToken)) {
           request_.query["security_token"] = this._securityToken;
