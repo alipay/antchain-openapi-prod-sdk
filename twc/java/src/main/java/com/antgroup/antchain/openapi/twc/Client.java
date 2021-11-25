89cd2dc1b9b801531609cf45ef3d34274784d044
sdk-team <sdk-team@alibabacloud.com>
2021-11-25 05:43:42 +0000

    Generated SDK for TWC.

--- twc/java/src/main/java/com/antgroup/antchain/openapi/twc/Client.java
+++ twc/java/src/main/java/com/antgroup/antchain/openapi/twc/Client.java
@@ -110,7 +110,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.7.9")
+                    new TeaPair("sdk_version", "1.7.12")
                 );
                 if (!com.aliyun.teautil.Common.empty(_securityToken)) {
                     request_.query.put("security_token", _securityToken);
@@ -4056,5 +4056,24 @@ public class Client {
     public QueryFlowPhaseResponse queryFlowPhaseEx(QueryFlowPhaseRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "twc.notary.flow.phase.query", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new QueryFlowPhaseResponse());
+    }
+
+    /**
+     * Description: 阶段存证数据详情
+     * Summary: 阶段存证数据详情
+     */
+    public DetailFlowPhaseResponse detailFlowPhase(DetailFlowPhaseRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.detailFlowPhaseEx(request, headers, runtime);
+    }
+
+    /**
+     * Description: 阶段存证数据详情
+     * Summary: 阶段存证数据详情
+     */
+    public DetailFlowPhaseResponse detailFlowPhaseEx(DetailFlowPhaseRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "twc.notary.flow.phase.detail", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new DetailFlowPhaseResponse());
     }
 }
---
+++ twc/java/src/main/java/com/antgroup/antchain/openapi/twc/models/DetailFlowPhaseResponse.java
@@ -0,0 +1,133 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.twc.models;
+
+import com.aliyun.tea.*;
+
+public class DetailFlowPhaseResponse extends TeaModel {
+    // 请求唯一ID，用于链路跟踪和问题排查
+    @NameInMap("req_msg_id")
+    public String reqMsgId;
+
+    // 结果码，一般OK表示调用成功
+    @NameInMap("result_code")
+    public String resultCode;
+
+    // 异常信息的文本描述
+    @NameInMap("result_msg")
+    public String resultMsg;
+
+    // 阶段存证交易hash
+    @NameInMap("tx_hash")
+    public String txHash;
+
+    // 模板字段类型，Hash/Structure，(Hash->哈希,Structure->结构化)
+    @NameInMap("data_type")
+    public String dataType;
+
+    // 阶段存证内容，如果模板数据类型定义是Hash(哈希)则返回存证时Hash，如果定义是Structure(结构化)，则返回所有字段json对象的字符串Base64后的值
+    @NameInMap("notary_content")
+    public String notaryContent;
+
+    // 结构化数据里面英文key对应的中文名称关系，json格式，key为字段英文名，value为字段中文名称
+    // 
+    @NameInMap("data_type_key")
+    public String dataTypeKey;
+
+    // 阶段存证内容csv下载地址，暂时预留，存证内容过大时采用文件形式输出，有效期1个小时
+    @NameInMap("url")
+    public String url;
+
+    // 交易所在的区块Hash
+    @NameInMap("block_hash")
+    public String blockHash;
+
+    // 交易所在的区块高
+    @NameInMap("block_height")
+    public String blockHeight;
+
+    public static DetailFlowPhaseResponse build(java.util.Map<String, ?> map) throws Exception {
+        DetailFlowPhaseResponse self = new DetailFlowPhaseResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public DetailFlowPhaseResponse setReqMsgId(String reqMsgId) {
+        this.reqMsgId = reqMsgId;
+        return this;
+    }
+    public String getReqMsgId() {
+        return this.reqMsgId;
+    }
+
+    public DetailFlowPhaseResponse setResultCode(String resultCode) {
+        this.resultCode = resultCode;
+        return this;
+    }
+    public String getResultCode() {
+        return this.resultCode;
+    }
+
+    public DetailFlowPhaseResponse setResultMsg(String resultMsg) {
+        this.resultMsg = resultMsg;
+        return this;
+    }
+    public String getResultMsg() {
+        return this.resultMsg;
+    }
+
+    public DetailFlowPhaseResponse setTxHash(String txHash) {
+        this.txHash = txHash;
+        return this;
+    }
+    public String getTxHash() {
+        return this.txHash;
+    }
+
+    public DetailFlowPhaseResponse setDataType(String dataType) {
+        this.dataType = dataType;
+        return this;
+    }
+    public String getDataType() {
+        return this.dataType;
+    }
+
+    public DetailFlowPhaseResponse setNotaryContent(String notaryContent) {
+        this.notaryContent = notaryContent;
+        return this;
+    }
+    public String getNotaryContent() {
+        return this.notaryContent;
+    }
+
+    public DetailFlowPhaseResponse setDataTypeKey(String dataTypeKey) {
+        this.dataTypeKey = dataTypeKey;
+        return this;
+    }
+    public String getDataTypeKey() {
+        return this.dataTypeKey;
+    }
+
+    public DetailFlowPhaseResponse setUrl(String url) {
+        this.url = url;
+        return this;
+    }
+    public String getUrl() {
+        return this.url;
+    }
+
+    public DetailFlowPhaseResponse setBlockHash(String blockHash) {
+        this.blockHash = blockHash;
+        return this;
+    }
+    public String getBlockHash() {
+        return this.blockHash;
+    }
+
+    public DetailFlowPhaseResponse setBlockHeight(String blockHeight) {
+        this.blockHeight = blockHeight;
+        return this;
+    }
+    public String getBlockHeight() {
+        return this.blockHeight;
+    }
+
+}
