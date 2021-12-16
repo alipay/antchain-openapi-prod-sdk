9366828ea71d577922a0933b80bda750f12b3fad
sdk-team <sdk-team@alibabacloud.com>
2021-12-16 08:25:36 +0000

    Generated SDK for MYTC.

--- mytc/csharp/core/Properties/AssemblyInfo.cs
+++ mytc/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("700a5610-5e46-11ec-a3a2-4fa4629df38f")]
+[assembly: Guid("b8bcd790-5e49-11ec-99b7-3f72a7c08e7b")]
 
 // Version information for an assembly consists of the following four values:
 //
