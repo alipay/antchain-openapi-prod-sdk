596d786afe9dd9737ce501b2ea16c4a7928e2755
sdk-team <sdk-team@alibabacloud.com>
2021-12-18 01:47:34 +0000

    Generated SDK for GOODSCHAIN.

--- goodschain/nodejs/src/client.ts
+++ goodschain/nodejs/src/client.ts
@@ -1387,6 +1387,128 @@ export class DisableGoodsResponse extends $tea.Model {
   }
 }
 
+export class GetHealthcodeRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  // 身份证号，使用蚂蚁侧提供的RSA公钥加密
+  certNo: string;
+  // 设备编码
+  deviceCode: string;
+  // 姓名
+  name: string;
+  // 是否需要健康码信息，不传默认不需要
+  // 1：需要
+  // 0：不需要
+  health?: number;
+  // 是否需要疫苗信息
+  // 1：需要，0：不需要，不传默认不需要
+  vaccination?: number;
+  // 是否需要核酸信息：
+  // 1.需要，0：不需要，不传默认不需要
+  nucleicAcid?: number;
+  // 是否需要行程码：
+  // 1.需要，0：不需要，不传默认不需要
+  travel?: number;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+      certNo: 'cert_no',
+      deviceCode: 'device_code',
+      name: 'name',
+      health: 'health',
+      vaccination: 'vaccination',
+      nucleicAcid: 'nucleic_acid',
+      travel: 'travel',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+      certNo: 'string',
+      deviceCode: 'string',
+      name: 'string',
+      health: 'number',
+      vaccination: 'number',
+      nucleicAcid: 'number',
+      travel: 'number',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class GetHealthcodeResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  // 1：绿色 2：黄色 3：红色
+  healthCode?: string;
+  // 健康码失败原因
+  healthFactor?: string;
+  // 行程码信息
+  // 1:没去过疫情区，绿码;
+  // 2:去过疫情区，红码;
+  // 3:其他，黄码;
+  // 0:行程信息不全;
+  // -1查询失败;
+  travelCode?: string;
+  // 检测类型
+  reportType?: string;
+  // 检测结果
+  reportResult?: string;
+  // 检测机构
+  reportOrganization?: string;
+  // 检测时间
+  reportTime?: string;
+  // 疫苗接种信息：
+  // 0查询失败 1未接种 2已接种一针 3完成接种
+  vaccinationCode?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+      healthCode: 'health_code',
+      healthFactor: 'health_factor',
+      travelCode: 'travel_code',
+      reportType: 'report_type',
+      reportResult: 'report_result',
+      reportOrganization: 'report_organization',
+      reportTime: 'report_time',
+      vaccinationCode: 'vaccination_code',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+      healthCode: 'string',
+      healthFactor: 'string',
+      travelCode: 'string',
+      reportType: 'string',
+      reportResult: 'string',
+      reportOrganization: 'string',
+      reportTime: 'string',
+      vaccinationCode: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 
 export default class Client {
   _endpoint: string;
@@ -1500,7 +1622,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.3",
+          sdk_version: "1.0.4",
         };
         if (!Util.empty(this._securityToken)) {
           request_.query["security_token"] = this._securityToken;
@@ -1886,6 +2008,25 @@ export default class Client {
   async disableGoodsEx(request: DisableGoodsRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<DisableGoodsResponse> {
     Util.validateModel(request);
     return $tea.cast<DisableGoodsResponse>(await this.doRequest("1.0", "antchain.goodschain.goods.disable", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new DisableGoodsResponse({}));
+  }
+
+  /**
+   * Description: 获取健康码、核酸、疫苗、行程码等信息
+   * Summary: 获取健康码、核酸、疫苗、行程码等信息
+   */
+  async getHealthcode(request: GetHealthcodeRequest): Promise<GetHealthcodeResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.getHealthcodeEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 获取健康码、核酸、疫苗、行程码等信息
+   * Summary: 获取健康码、核酸、疫苗、行程码等信息
+   */
+  async getHealthcodeEx(request: GetHealthcodeRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<GetHealthcodeResponse> {
+    Util.validateModel(request);
+    return $tea.cast<GetHealthcodeResponse>(await this.doRequest("1.0", "antchain.goodschain.healthcode.get", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new GetHealthcodeResponse({}));
   }
 
 }
