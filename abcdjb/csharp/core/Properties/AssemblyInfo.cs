cd341e71cdf0ef24a9ddf5d289f9c7d94f9ec6a4
sdk-team <sdk-team@alibabacloud.com>
2021-12-22 09:19:59 +0000

    Generated SDK for ABCDJB.

--- abcdjb/csharp/core/Properties/AssemblyInfo.cs
+++ abcdjb/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2cee35f0-5f3f-11ec-b47e-df737773413a")]
+[assembly: Guid("4feb6d80-6308-11ec-92f9-df5036c9bb1d")]
 
 // Version information for an assembly consists of the following four values:
 //
