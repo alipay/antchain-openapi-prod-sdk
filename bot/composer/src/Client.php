5858ac7c901744b285d63e5766ec760e5f503aa7
sdk-team <sdk-team@alibabacloud.com>
2021-12-29 07:44:37 +0000

    Generated SDK for BOT.

--- bot/composer/src/Client.php
+++ bot/composer/src/Client.php
@@ -356,7 +356,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.6.53',
+                    'sdk_version'      => '1.6.54',
                 ];
                 if (!Utils::empty_($this->_securityToken)) {
                     $_request->query['security_token'] = $this->_securityToken;
