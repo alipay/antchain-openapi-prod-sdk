081bbf61274bce72c62868df47b0ce661bbd1460
sdk-team <sdk-team@alibabacloud.com>
2022-02-16 08:47:59 +0000

    Generated SDK for SAAS.

---
+++ saas/csharp/core/Models/KeyValuePair.cs
@@ -0,0 +1,25 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.SAAS.Models
+{
+    // 键值对，兼容map用
+    public class KeyValuePair : TeaModel {
+        // key
+        [NameInMap("key")]
+        [Validation(Required=true)]
+        public string Key { get; set; }
+
+        // value
+        [NameInMap("value")]
+        [Validation(Required=false)]
+        public string Value { get; set; }
+
+    }
+
+}
