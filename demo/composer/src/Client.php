295e4aae6c35e7cb38935fd109ec5babc395328c
sdk-team <sdk-team@alibabacloud.com>
2022-03-29 07:44:37 +0000

    Generated SDK for DEMO.

--- demo/composer/src/Client.php
+++ demo/composer/src/Client.php
@@ -182,7 +182,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.0.57',
+                    'sdk_version'      => '1.0.70',
                 ];
                 if (!Utils::empty_($this->_securityToken)) {
                     $_request->query['security_token'] = $this->_securityToken;
