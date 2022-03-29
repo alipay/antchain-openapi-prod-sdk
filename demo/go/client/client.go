c662660ee72891e7d51882ace04d533a41a34356
sdk-team <sdk-team@alibabacloud.com>
2022-03-29 07:55:15 +0000

    Generated SDK for DEMO.

--- demo/go/client/client.go
+++ demo/go/client/client.go
@@ -1277,7 +1277,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.0.70"),
+				"sdk_version":      tea.String("1.0.71"),
 			}
 			if !tea.BoolValue(util.Empty(client.SecurityToken)) {
 				request_.Query["security_token"] = client.SecurityToken
