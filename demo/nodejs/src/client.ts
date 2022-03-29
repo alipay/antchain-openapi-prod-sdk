df6e12ce284a2d1d97207fb45567ff65349f05ba
sdk-team <sdk-team@alibabacloud.com>
2022-03-29 07:44:50 +0000

    Generated SDK for DEMO.

--- demo/nodejs/src/client.ts
+++ demo/nodejs/src/client.ts
@@ -115,7 +115,7 @@ export class DemoClass extends $tea.Model {
   }
 }
 
-// 阿松大
+// test
 export class TestClass extends $tea.Model {
   // 1
   test: string;
@@ -1043,7 +1043,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.57",
+          sdk_version: "1.0.70",
         };
         if (!Util.empty(this._securityToken)) {
           request_.query["security_token"] = this._securityToken;
