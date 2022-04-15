625edbd1865f6e575861ddfff98b40b494ed6986
sdk-team <sdk-team@alibabacloud.com>
2022-04-15 09:30:37 +0000

    Generated SDK for YUQING.

--- yuqing/nodejs/src/client.ts
+++ yuqing/nodejs/src/client.ts
@@ -1262,7 +1262,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.1.8",
+          sdk_version: "1.1.9",
         };
         if (!Util.empty(this._securityToken)) {
           request_.query["security_token"] = this._securityToken;
