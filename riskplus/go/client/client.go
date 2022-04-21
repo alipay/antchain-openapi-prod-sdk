05a237e7c22af6cc1f04305f644d7d6d5cf9bd11
sdk-team <sdk-team@alibabacloud.com>
2022-04-21 02:36:42 +0000

    Generated SDK for RISKPLUS.

--- riskplus/go/client/client.go
+++ riskplus/go/client/client.go
@@ -13135,10 +13135,6 @@ type SyncUmktRtEventresultResponse struct {
 	ResultCode *string `json:"result_code,omitempty" xml:"result_code,omitempty"`
 	// 异常信息的文本描述
 	ResultMsg *string `json:"result_msg,omitempty" xml:"result_msg,omitempty"`
-	// 处理是否成功
-	Success *bool `json:"success,omitempty" xml:"success,omitempty"`
-	// 基本圈客结果信息
-	QueryResult []*CustomerUmktInfoModel `json:"query_result,omitempty" xml:"query_result,omitempty" type:"Repeated"`
 }
 
 func (s SyncUmktRtEventresultResponse) String() string {
@@ -13161,16 +13157,6 @@ func (s *SyncUmktRtEventresultResponse) SetResultCode(v string) *SyncUmktRtEvent
 
 func (s *SyncUmktRtEventresultResponse) SetResultMsg(v string) *SyncUmktRtEventresultResponse {
 	s.ResultMsg = &v
-	return s
-}
-
-func (s *SyncUmktRtEventresultResponse) SetSuccess(v bool) *SyncUmktRtEventresultResponse {
-	s.Success = &v
-	return s
-}
-
-func (s *SyncUmktRtEventresultResponse) SetQueryResult(v []*CustomerUmktInfoModel) *SyncUmktRtEventresultResponse {
-	s.QueryResult = v
 	return s
 }
 
@@ -13241,6 +13227,100 @@ func (s *ImportUmktSceneUploadResponse) SetResultCode(v string) *ImportUmktScene
 
 func (s *ImportUmktSceneUploadResponse) SetResultMsg(v string) *ImportUmktSceneUploadResponse {
 	s.ResultMsg = &v
+	return s
+}
+
+type BatchqueryUmktRtTailmarketingRequest struct {
+	// OAuth模式下的授权token
+	AuthToken         *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
+	ProductInstanceId *string `json:"product_instance_id,omitempty" xml:"product_instance_id,omitempty"`
+	// 营销计划id
+	PlanId *int64 `json:"plan_id,omitempty" xml:"plan_id,omitempty" require:"true"`
+	//
+	// 用户列表传输模版
+	QueryTemplate *string `json:"query_template,omitempty" xml:"query_template,omitempty" require:"true"`
+	//
+	// 用户查询凭证列表
+	CustomerKeys []*string `json:"customer_keys,omitempty" xml:"customer_keys,omitempty" require:"true" type:"Repeated"`
+	// 业务方流水号
+	BizSerialNo *string `json:"biz_serial_no,omitempty" xml:"biz_serial_no,omitempty" require:"true"`
+}
+
+func (s BatchqueryUmktRtTailmarketingRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s BatchqueryUmktRtTailmarketingRequest) GoString() string {
+	return s.String()
+}
+
+func (s *BatchqueryUmktRtTailmarketingRequest) SetAuthToken(v string) *BatchqueryUmktRtTailmarketingRequest {
+	s.AuthToken = &v
+	return s
+}
+
+func (s *BatchqueryUmktRtTailmarketingRequest) SetProductInstanceId(v string) *BatchqueryUmktRtTailmarketingRequest {
+	s.ProductInstanceId = &v
+	return s
+}
+
+func (s *BatchqueryUmktRtTailmarketingRequest) SetPlanId(v int64) *BatchqueryUmktRtTailmarketingRequest {
+	s.PlanId = &v
+	return s
+}
+
+func (s *BatchqueryUmktRtTailmarketingRequest) SetQueryTemplate(v string) *BatchqueryUmktRtTailmarketingRequest {
+	s.QueryTemplate = &v
+	return s
+}
+
+func (s *BatchqueryUmktRtTailmarketingRequest) SetCustomerKeys(v []*string) *BatchqueryUmktRtTailmarketingRequest {
+	s.CustomerKeys = v
+	return s
+}
+
+func (s *BatchqueryUmktRtTailmarketingRequest) SetBizSerialNo(v string) *BatchqueryUmktRtTailmarketingRequest {
+	s.BizSerialNo = &v
+	return s
+}
+
+type BatchqueryUmktRtTailmarketingResponse struct {
+	// 请求唯一ID，用于链路跟踪和问题排查
+	ReqMsgId *string `json:"req_msg_id,omitempty" xml:"req_msg_id,omitempty"`
+	// 结果码，一般OK表示调用成功
+	ResultCode *string `json:"result_code,omitempty" xml:"result_code,omitempty"`
+	// 异常信息的文本描述
+	ResultMsg *string `json:"result_msg,omitempty" xml:"result_msg,omitempty"`
+	//
+	// 实时营销单条结果
+	QueryResult []*CustomerUmktInfoModel `json:"query_result,omitempty" xml:"query_result,omitempty" type:"Repeated"`
+}
+
+func (s BatchqueryUmktRtTailmarketingResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s BatchqueryUmktRtTailmarketingResponse) GoString() string {
+	return s.String()
+}
+
+func (s *BatchqueryUmktRtTailmarketingResponse) SetReqMsgId(v string) *BatchqueryUmktRtTailmarketingResponse {
+	s.ReqMsgId = &v
+	return s
+}
+
+func (s *BatchqueryUmktRtTailmarketingResponse) SetResultCode(v string) *BatchqueryUmktRtTailmarketingResponse {
+	s.ResultCode = &v
+	return s
+}
+
+func (s *BatchqueryUmktRtTailmarketingResponse) SetResultMsg(v string) *BatchqueryUmktRtTailmarketingResponse {
+	s.ResultMsg = &v
+	return s
+}
+
+func (s *BatchqueryUmktRtTailmarketingResponse) SetQueryResult(v []*CustomerUmktInfoModel) *BatchqueryUmktRtTailmarketingResponse {
+	s.QueryResult = v
 	return s
 }
 
@@ -13486,7 +13566,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.10.4"),
+				"sdk_version":      tea.String("1.10.5"),
 			}
 			if !tea.BoolValue(util.Empty(client.SecurityToken)) {
 				request_.Query["security_token"] = client.SecurityToken
@@ -16647,6 +16727,40 @@ func (client *Client) ImportUmktSceneUploadEx(request *ImportUmktSceneUploadRequ
 	}
 	_result = &ImportUmktSceneUploadResponse{}
 	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("riskplus.umkt.scene.upload.import"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+/**
+ * Description: 富信贴尾实时圈客
+ * Summary: 富信贴尾实时圈客
+ */
+func (client *Client) BatchqueryUmktRtTailmarketing(request *BatchqueryUmktRtTailmarketingRequest) (_result *BatchqueryUmktRtTailmarketingResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &BatchqueryUmktRtTailmarketingResponse{}
+	_body, _err := client.BatchqueryUmktRtTailmarketingEx(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+/**
+ * Description: 富信贴尾实时圈客
+ * Summary: 富信贴尾实时圈客
+ */
+func (client *Client) BatchqueryUmktRtTailmarketingEx(request *BatchqueryUmktRtTailmarketingRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *BatchqueryUmktRtTailmarketingResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = &BatchqueryUmktRtTailmarketingResponse{}
+	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("riskplus.umkt.rt.tailmarketing.batchquery"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
 	if _err != nil {
 		return _result, _err
 	}
