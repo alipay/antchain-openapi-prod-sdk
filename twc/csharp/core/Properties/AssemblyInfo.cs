d445869b7ba322ff049040295682981eab7e22fd
sdk-team <sdk-team@alibabacloud.com>
2022-04-26 15:31:15 +0000

    Generated SDK for TWC.

--- twc/csharp/core/Properties/AssemblyInfo.cs
+++ twc/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("829dc800-c540-11ec-93fb-2713a4ded726")]
+[assembly: Guid("d88cbbb0-c575-11ec-857d-0d3410081873")]
 
 // Version information for an assembly consists of the following four values:
 //
