17c8e5d20782e708f42f23e3f0d61a6e0adb0201
sdk-team <sdk-team@alibabacloud.com>
2022-05-18 11:18:56 +0000

    Generated SDK for MPAASFACEVERIFY.

--- mpaasfaceverify/go/client/client.go
+++ mpaasfaceverify/go/client/client.go
@@ -829,15 +829,15 @@ type InitCertifyrecordRealpersonRequest struct {
 	ChargeCode *string `json:"charge_code,omitempty" xml:"charge_code,omitempty" require:"true"`
 	//
 	// 预留扩展业务参数
-	ExternParam *string `json:"extern_param,omitempty" xml:"extern_param,omitempty" require:"true"`
+	ExternParam *string `json:"extern_param,omitempty" xml:"extern_param,omitempty"`
 	// 用户身份信息
 	IdentityParam *string `json:"identity_param,omitempty" xml:"identity_param,omitempty" require:"true"`
 	// metainfo环境参数
-	Metainfo *string `json:"metainfo,omitempty" xml:"metainfo,omitempty" require:"true"`
+	Metainfo *string `json:"metainfo,omitempty" xml:"metainfo,omitempty"`
 	// 操作类型
-	OperationType *string `json:"operation_type,omitempty" xml:"operation_type,omitempty" require:"true"`
+	OperationType *string `json:"operation_type,omitempty" xml:"operation_type,omitempty"`
 	// 比对源图片
-	RefImg *string `json:"ref_img,omitempty" xml:"ref_img,omitempty" require:"true"`
+	RefImg *string `json:"ref_img,omitempty" xml:"ref_img,omitempty"`
 }
 
 func (s InitCertifyrecordRealpersonRequest) String() string {
@@ -969,13 +969,13 @@ type InitCertifyrecordRequest struct {
 	// 计费规则码
 	ChargeCode *string `json:"charge_code,omitempty" xml:"charge_code,omitempty" require:"true"`
 	// 预留扩展业务参数
-	ExternParam *string `json:"extern_param,omitempty" xml:"extern_param,omitempty" require:"true"`
+	ExternParam *string `json:"extern_param,omitempty" xml:"extern_param,omitempty"`
 	// metainfo环境参数
-	Metainfo *string `json:"metainfo,omitempty" xml:"metainfo,omitempty" require:"true"`
+	Metainfo *string `json:"metainfo,omitempty" xml:"metainfo,omitempty"`
 	// 操作类型
-	OperationType *string `json:"operation_type,omitempty" xml:"operation_type,omitempty" require:"true"`
+	OperationType *string `json:"operation_type,omitempty" xml:"operation_type,omitempty"`
 	// 比对源图片
-	RefImg *string `json:"ref_img,omitempty" xml:"ref_img,omitempty" require:"true"`
+	RefImg *string `json:"ref_img,omitempty" xml:"ref_img,omitempty"`
 }
 
 func (s InitCertifyrecordRequest) String() string {
@@ -1090,6 +1090,104 @@ func (s *InitCertifyrecordResponse) SetCertifyId(v string) *InitCertifyrecordRes
 
 func (s *InitCertifyrecordResponse) SetCertifyUrl(v string) *InitCertifyrecordResponse {
 	s.CertifyUrl = &v
+	return s
+}
+
+type QueryCertifyrecordRequest struct {
+	// OAuth模式下的授权token
+	AuthToken         *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
+	ProductInstanceId *string `json:"product_instance_id,omitempty" xml:"product_instance_id,omitempty"`
+	// 租户请求的唯一标志，该标识作为对账的关键信息，商户要保证其唯一性
+	BizId *string `json:"biz_id,omitempty" xml:"biz_id,omitempty" require:"true"`
+	// 预留扩展业务参数
+	ExternParam *string `json:"extern_param,omitempty" xml:"extern_param,omitempty"`
+	// certifyId，用于查询认证结果
+	CertifyId *string `json:"certify_id,omitempty" xml:"certify_id,omitempty" require:"true"`
+}
+
+func (s QueryCertifyrecordRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s QueryCertifyrecordRequest) GoString() string {
+	return s.String()
+}
+
+func (s *QueryCertifyrecordRequest) SetAuthToken(v string) *QueryCertifyrecordRequest {
+	s.AuthToken = &v
+	return s
+}
+
+func (s *QueryCertifyrecordRequest) SetProductInstanceId(v string) *QueryCertifyrecordRequest {
+	s.ProductInstanceId = &v
+	return s
+}
+
+func (s *QueryCertifyrecordRequest) SetBizId(v string) *QueryCertifyrecordRequest {
+	s.BizId = &v
+	return s
+}
+
+func (s *QueryCertifyrecordRequest) SetExternParam(v string) *QueryCertifyrecordRequest {
+	s.ExternParam = &v
+	return s
+}
+
+func (s *QueryCertifyrecordRequest) SetCertifyId(v string) *QueryCertifyrecordRequest {
+	s.CertifyId = &v
+	return s
+}
+
+type QueryCertifyrecordResponse struct {
+	// 请求唯一ID，用于链路跟踪和问题排查
+	ReqMsgId *string `json:"req_msg_id,omitempty" xml:"req_msg_id,omitempty"`
+	// 结果码，一般OK表示调用成功
+	ResultCode *string `json:"result_code,omitempty" xml:"result_code,omitempty"`
+	// 异常信息的文本描述
+	ResultMsg *string `json:"result_msg,omitempty" xml:"result_msg,omitempty"`
+	// 预留扩展结果
+	ExternInfo *string `json:"extern_info,omitempty" xml:"extern_info,omitempty"`
+	// 产品结果明细，不影响决策
+	ResultCodeSub *string `json:"result_code_sub,omitempty" xml:"result_code_sub,omitempty"`
+	// result_code_sub对应的文案
+	ResultMsgSub *string `json:"result_msg_sub,omitempty" xml:"result_msg_sub,omitempty"`
+}
+
+func (s QueryCertifyrecordResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s QueryCertifyrecordResponse) GoString() string {
+	return s.String()
+}
+
+func (s *QueryCertifyrecordResponse) SetReqMsgId(v string) *QueryCertifyrecordResponse {
+	s.ReqMsgId = &v
+	return s
+}
+
+func (s *QueryCertifyrecordResponse) SetResultCode(v string) *QueryCertifyrecordResponse {
+	s.ResultCode = &v
+	return s
+}
+
+func (s *QueryCertifyrecordResponse) SetResultMsg(v string) *QueryCertifyrecordResponse {
+	s.ResultMsg = &v
+	return s
+}
+
+func (s *QueryCertifyrecordResponse) SetExternInfo(v string) *QueryCertifyrecordResponse {
+	s.ExternInfo = &v
+	return s
+}
+
+func (s *QueryCertifyrecordResponse) SetResultCodeSub(v string) *QueryCertifyrecordResponse {
+	s.ResultCodeSub = &v
+	return s
+}
+
+func (s *QueryCertifyrecordResponse) SetResultMsgSub(v string) *QueryCertifyrecordResponse {
+	s.ResultMsgSub = &v
 	return s
 }
 
@@ -1215,7 +1313,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.1.9"),
+				"sdk_version":      tea.String("1.1.10"),
 			}
 			if !tea.BoolValue(util.Empty(client.SecurityToken)) {
 				request_.Query["security_token"] = client.SecurityToken
@@ -1528,6 +1626,40 @@ func (client *Client) InitCertifyrecordEx(request *InitCertifyrecordRequest, hea
 	}
 	_result = &InitCertifyrecordResponse{}
 	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("antfin.mpaasfaceverify.certifyrecord.init"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+/**
+ * Description: 调用“实人认证结果查询(certifyId)”接口可以通过certifyId查询当次认证的结果
+ * Summary: 实人认证查询(certifyId)
+ */
+func (client *Client) QueryCertifyrecord(request *QueryCertifyrecordRequest) (_result *QueryCertifyrecordResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &QueryCertifyrecordResponse{}
+	_body, _err := client.QueryCertifyrecordEx(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+/**
+ * Description: 调用“实人认证结果查询(certifyId)”接口可以通过certifyId查询当次认证的结果
+ * Summary: 实人认证查询(certifyId)
+ */
+func (client *Client) QueryCertifyrecordEx(request *QueryCertifyrecordRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *QueryCertifyrecordResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = &QueryCertifyrecordResponse{}
+	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("antfin.mpaasfaceverify.certifyrecord.query"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
 	if _err != nil {
 		return _result, _err
 	}
