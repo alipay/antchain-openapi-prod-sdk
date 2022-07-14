7b39942ab5b3ed044fef0cc3c18f49f16f172350
sdk-team <sdk-team@alibabacloud.com>
2022-07-14 08:41:48 +0000

    Generated SDK for INTEGRATION_MACHINE.

--- integration_machine/csharp/core/Properties/AssemblyInfo.cs
+++ integration_machine/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fa884480-0343-11ed-805a-cf2c1fb1ae09")]
+[assembly: Guid("c71ff220-0350-11ed-afa1-f1c93f49acae")]
 
 // Version information for an assembly consists of the following four values:
 //
