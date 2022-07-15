f0442f11e28562b1a8220b129d4f3a6b5754785f
sdk-team <sdk-team@alibabacloud.com>
2022-07-15 03:33:26 +0000

    Generated SDK for INTEGRATION_MACHINE.

--- integration_machine/nodejs/src/client.ts
+++ integration_machine/nodejs/src/client.ts
@@ -1195,7 +1195,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.11",
+          sdk_version: "1.0.12",
         };
         if (!Util.empty(this._securityToken)) {
           request_.query["security_token"] = this._securityToken;
