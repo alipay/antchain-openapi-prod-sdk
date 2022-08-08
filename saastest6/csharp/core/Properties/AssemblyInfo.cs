f81c834ccd296293c05cc3d2eb9bf90d49ddb87f
sdk-team <sdk-team@alibabacloud.com>
2022-08-08 09:00:40 +0000

    Generated SDK for SaasTest6.

--- saastest6/csharp/core/Properties/AssemblyInfo.cs
+++ saastest6/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f29a8720-16f7-11ed-8429-d7c9bc405a82")]
+[assembly: Guid("8db54100-16f8-11ed-909f-ed34642a482a")]
 
 // Version information for an assembly consists of the following four values:
 //
