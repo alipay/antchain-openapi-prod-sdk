66330568b6f639ff2af00ea81bee4a685b1298ce
sdk-team <sdk-team@alibabacloud.com>
2022-08-09 06:18:54 +0000

    Generated SDK for eb193ab60ccc42ddb781f570a741c3fa.

---
+++ eb193ab60ccc42ddb781f570a741c3fa/java/src/main/java/com/antgroup/antchain/openapi/eb193ab60ccc42ddb781f570a741c3fa/models/QueryDemoAdAsdAsdResponse.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.eb193ab60ccc42ddb781f570a741c3fa.models;
+
+import com.aliyun.tea.*;
+
+public class QueryDemoAdAsdAsdResponse extends TeaModel {
+    // 请求唯一ID，用于链路跟踪和问题排查
+    @NameInMap("req_msg_id")
+    public String reqMsgId;
+
+    // 结果码，一般OK表示调用成功
+    @NameInMap("result_code")
+    public String resultCode;
+
+    // 异常信息的文本描述
+    @NameInMap("result_msg")
+    public String resultMsg;
+
+    public static QueryDemoAdAsdAsdResponse build(java.util.Map<String, ?> map) throws Exception {
+        QueryDemoAdAsdAsdResponse self = new QueryDemoAdAsdAsdResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public QueryDemoAdAsdAsdResponse setReqMsgId(String reqMsgId) {
+        this.reqMsgId = reqMsgId;
+        return this;
+    }
+    public String getReqMsgId() {
+        return this.reqMsgId;
+    }
+
+    public QueryDemoAdAsdAsdResponse setResultCode(String resultCode) {
+        this.resultCode = resultCode;
+        return this;
+    }
+    public String getResultCode() {
+        return this.resultCode;
+    }
+
+    public QueryDemoAdAsdAsdResponse setResultMsg(String resultMsg) {
+        this.resultMsg = resultMsg;
+        return this;
+    }
+    public String getResultMsg() {
+        return this.resultMsg;
+    }
+
+}
---
+++ eb193ab60ccc42ddb781f570a741c3fa/java/src/main/java/com/antgroup/antchain/openapi/eb193ab60ccc42ddb781f570a741c3fa/models/QueryMap.java
@@ -0,0 +1,37 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.eb193ab60ccc42ddb781f570a741c3fa.models;
+
+import com.aliyun.tea.*;
+
+public class QueryMap extends TeaModel {
+    // 键值
+    @NameInMap("name")
+    @Validation(required = true)
+    public String name;
+
+    // 额外用户信息
+    @NameInMap("value")
+    public java.util.List<NameValuePair> value;
+
+    public static QueryMap build(java.util.Map<String, ?> map) throws Exception {
+        QueryMap self = new QueryMap();
+        return TeaModel.build(map, self);
+    }
+
+    public QueryMap setName(String name) {
+        this.name = name;
+        return this;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public QueryMap setValue(java.util.List<NameValuePair> value) {
+        this.value = value;
+        return this;
+    }
+    public java.util.List<NameValuePair> getValue() {
+        return this.value;
+    }
+
+}
