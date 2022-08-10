1a0aa2a8b4711cd814f7e274b13d719326adf597
sdk-team <sdk-team@alibabacloud.com>
2022-08-10 04:40:28 +0000

    Generated SDK for SaasTest6.

--- saastest6/go/client/client.go
+++ saastest6/go/client/client.go
@@ -196,6 +196,32 @@ func (s *DemoClass) SetSomeList(v []*string) *DemoClass {
 	return s
 }
 
+// 键值对，兼容map用
+type NameValuePair struct {
+	// 键名
+	Name *string `json:"name,omitempty" xml:"name,omitempty" require:"true"`
+	// 键值
+	Value *string `json:"value,omitempty" xml:"value,omitempty" require:"true"`
+}
+
+func (s NameValuePair) String() string {
+	return tea.Prettify(s)
+}
+
+func (s NameValuePair) GoString() string {
+	return s.String()
+}
+
+func (s *NameValuePair) SetName(v string) *NameValuePair {
+	s.Name = &v
+	return s
+}
+
+func (s *NameValuePair) SetValue(v string) *NameValuePair {
+	s.Value = &v
+	return s
+}
+
 // TestStruct
 type TestStruct struct {
 	// x
@@ -226,6 +252,32 @@ func (s *TestStruct) SetY(v *DemoClass) *TestStruct {
 
 func (s *TestStruct) SetZ(v []*DemoClass) *TestStruct {
 	s.Z = v
+	return s
+}
+
+// Map<String,Object> 集合
+type QueryMap struct {
+	// 键值
+	Name *string `json:"name,omitempty" xml:"name,omitempty" require:"true"`
+	// 额外用户信息
+	Value []*NameValuePair `json:"value,omitempty" xml:"value,omitempty" type:"Repeated"`
+}
+
+func (s QueryMap) String() string {
+	return tea.Prettify(s)
+}
+
+func (s QueryMap) GoString() string {
+	return s.String()
+}
+
+func (s *QueryMap) SetName(v string) *QueryMap {
+	s.Name = &v
+	return s
+}
+
+func (s *QueryMap) SetValue(v []*NameValuePair) *QueryMap {
+	s.Value = v
 	return s
 }
 
@@ -473,6 +525,90 @@ func (s *CreateAntcloudOpenapiApiserviceGroupResponse) SetResultMsg(v string) *C
 	return s
 }
 
+type InitDemoBbpInsuranceUserRequest struct {
+	// OAuth模式下的授权token
+	AuthToken         *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
+	ProductInstanceId *string `json:"product_instance_id,omitempty" xml:"product_instance_id,omitempty"`
+	// 保司编码
+	BusinessCode *string `json:"business_code,omitempty" xml:"business_code,omitempty" require:"true"`
+	// 第三方id，此处为天猫uid
+	ThirdPartId *string `json:"third_part_id,omitempty" xml:"third_part_id,omitempty" require:"true"`
+	// 来源渠道
+	Channel *string `json:"channel,omitempty" xml:"channel,omitempty" require:"true"`
+	// 埋点信息
+	Burieds *QueryMap `json:"burieds,omitempty" xml:"burieds,omitempty"`
+}
+
+func (s InitDemoBbpInsuranceUserRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s InitDemoBbpInsuranceUserRequest) GoString() string {
+	return s.String()
+}
+
+func (s *InitDemoBbpInsuranceUserRequest) SetAuthToken(v string) *InitDemoBbpInsuranceUserRequest {
+	s.AuthToken = &v
+	return s
+}
+
+func (s *InitDemoBbpInsuranceUserRequest) SetProductInstanceId(v string) *InitDemoBbpInsuranceUserRequest {
+	s.ProductInstanceId = &v
+	return s
+}
+
+func (s *InitDemoBbpInsuranceUserRequest) SetBusinessCode(v string) *InitDemoBbpInsuranceUserRequest {
+	s.BusinessCode = &v
+	return s
+}
+
+func (s *InitDemoBbpInsuranceUserRequest) SetThirdPartId(v string) *InitDemoBbpInsuranceUserRequest {
+	s.ThirdPartId = &v
+	return s
+}
+
+func (s *InitDemoBbpInsuranceUserRequest) SetChannel(v string) *InitDemoBbpInsuranceUserRequest {
+	s.Channel = &v
+	return s
+}
+
+func (s *InitDemoBbpInsuranceUserRequest) SetBurieds(v *QueryMap) *InitDemoBbpInsuranceUserRequest {
+	s.Burieds = v
+	return s
+}
+
+type InitDemoBbpInsuranceUserResponse struct {
+	// 请求唯一ID，用于链路跟踪和问题排查
+	ReqMsgId *string `json:"req_msg_id,omitempty" xml:"req_msg_id,omitempty"`
+	// 结果码，一般OK表示调用成功
+	ResultCode *string `json:"result_code,omitempty" xml:"result_code,omitempty"`
+	// 异常信息的文本描述
+	ResultMsg *string `json:"result_msg,omitempty" xml:"result_msg,omitempty"`
+}
+
+func (s InitDemoBbpInsuranceUserResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s InitDemoBbpInsuranceUserResponse) GoString() string {
+	return s.String()
+}
+
+func (s *InitDemoBbpInsuranceUserResponse) SetReqMsgId(v string) *InitDemoBbpInsuranceUserResponse {
+	s.ReqMsgId = &v
+	return s
+}
+
+func (s *InitDemoBbpInsuranceUserResponse) SetResultCode(v string) *InitDemoBbpInsuranceUserResponse {
+	s.ResultCode = &v
+	return s
+}
+
+func (s *InitDemoBbpInsuranceUserResponse) SetResultMsg(v string) *InitDemoBbpInsuranceUserResponse {
+	s.ResultMsg = &v
+	return s
+}
+
 type CreateAntcloudGatewayxFileUploadRequest struct {
 	// OAuth模式下的授权token
 	AuthToken *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
@@ -715,7 +851,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.0.10"),
+				"sdk_version":      tea.String("1.0.11"),
 			}
 			if !tea.BoolValue(util.Empty(client.SecurityToken)) {
 				request_.Query["security_token"] = client.SecurityToken
@@ -855,6 +991,40 @@ func (client *Client) CreateAntcloudOpenapiApiserviceGroupEx(request *CreateAntc
 	}
 	_result = &CreateAntcloudOpenapiApiserviceGroupResponse{}
 	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("antcloud.openapi.apiservice.group.create"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+/**
+ * Description: 保司用户埋点信息
+ * Summary: 用户登陆页面埋点
+ */
+func (client *Client) InitDemoBbpInsuranceUser(request *InitDemoBbpInsuranceUserRequest) (_result *InitDemoBbpInsuranceUserResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &InitDemoBbpInsuranceUserResponse{}
+	_body, _err := client.InitDemoBbpInsuranceUserEx(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+/**
+ * Description: 保司用户埋点信息
+ * Summary: 用户登陆页面埋点
+ */
+func (client *Client) InitDemoBbpInsuranceUserEx(request *InitDemoBbpInsuranceUserRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *InitDemoBbpInsuranceUserResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = &InitDemoBbpInsuranceUserResponse{}
+	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("demo.bbp.insurance.user.init"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
 	if _err != nil {
 		return _result, _err
 	}
