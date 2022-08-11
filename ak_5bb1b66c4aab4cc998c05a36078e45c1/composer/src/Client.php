0b6e1bf208d627d262bf7524342c213864e76cce
sdk-team <sdk-team@alibabacloud.com>
2022-08-11 10:58:14 +0000

    Generated SDK for ak_5bb1b66c4aab4cc998c05a36078e45c1.

--- ak_5bb1b66c4aab4cc998c05a36078e45c1/composer/src/Client.php
+++ ak_5bb1b66c4aab4cc998c05a36078e45c1/composer/src/Client.php
@@ -186,7 +186,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.0.1',
+                    'sdk_version'      => '1.0.2',
                 ];
                 if (!Utils::empty_($this->_securityToken)) {
                     $_request->query['security_token'] = $this->_securityToken;
