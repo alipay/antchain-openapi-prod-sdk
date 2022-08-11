3a4c120cbec2b8b26eb96634515e6e25cba6f693
sdk-team <sdk-team@alibabacloud.com>
2022-08-11 13:01:04 +0000

    Generated SDK for ak_96c72ef879f04758909898e2412a9789.

---
+++ ak_96c72ef879f04758909898e2412a9789/csharp/core/Models/XNameValuePair.cs
@@ -0,0 +1,25 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_96c72ef879f04758909898e2412a9789.Models
+{
+    // 键值对
+    public class XNameValuePair : TeaModel {
+        // 键名
+        [NameInMap("name")]
+        [Validation(Required=true)]
+        public string Name { get; set; }
+
+        // 键值
+        [NameInMap("value")]
+        [Validation(Required=true)]
+        public string Value { get; set; }
+
+    }
+
+}
