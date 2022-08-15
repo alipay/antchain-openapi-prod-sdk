79ac07f31c842501b1ee6a8f13bdc6f50978a680
sdk-team <sdk-team@alibabacloud.com>
2022-08-15 06:07:59 +0000

    Generated SDK for ak_708d6673ed844a78b9e93fb80465e6d8.

---
+++ ak_708d6673ed844a78b9e93fb80465e6d8/csharp/core/Models/UpdateDemoCjtestCjRequest.cs
@@ -0,0 +1,38 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_708d6673ed844a78b9e93fb80465e6d8.Models
+{
+    public class UpdateDemoCjtestCjRequest : TeaModel {
+        // OAuth模式下的授权token
+        [NameInMap("auth_token")]
+        [Validation(Required=false)]
+        public string AuthToken { get; set; }
+
+        [NameInMap("product_instance_id")]
+        [Validation(Required=false)]
+        public string ProductInstanceId { get; set; }
+
+        // 测试参数
+        [NameInMap("var1")]
+        [Validation(Required=true)]
+        public string Var1 { get; set; }
+
+        // 测试参数2
+        [NameInMap("var2")]
+        [Validation(Required=true)]
+        public string Var2 { get; set; }
+
+        // 被授权机构did
+        [NameInMap("subject")]
+        [Validation(Required=false)]
+        public string Subject { get; set; }
+
+    }
+
+}
