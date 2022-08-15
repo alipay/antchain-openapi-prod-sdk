347b72c148f7b3c0bf9c2964b0e37a8af7d1d1af
sdk-team <sdk-team@alibabacloud.com>
2022-08-15 08:14:21 +0000

    Generated SDK for ak_1da5dd6b1f8c448e8a643a570edd63b8.

---
+++ ak_1da5dd6b1f8c448e8a643a570edd63b8/csharp/core/Models/BindDemoAaaBbbCccRequest.cs
@@ -0,0 +1,28 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_1da5dd6b1f8c448e8a643a570edd63b8.Models
+{
+    public class BindDemoAaaBbbCccRequest : TeaModel {
+        // OAuth模式下的授权token
+        [NameInMap("auth_token")]
+        [Validation(Required=false)]
+        public string AuthToken { get; set; }
+
+        [NameInMap("product_instance_id")]
+        [Validation(Required=false)]
+        public string ProductInstanceId { get; set; }
+
+        // 123
+        [NameInMap("data")]
+        [Validation(Required=true)]
+        public string Data { get; set; }
+
+    }
+
+}
---
+++ ak_1da5dd6b1f8c448e8a643a570edd63b8/csharp/core/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("antchain-ak_1da5dd6b1f8c448e8a643a570edd63b8")]
+[assembly: AssemblyDescription("Ant Chain ak_1da5dd6b1f8c448e8a643a570edd63b8 SDK Library for .NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ant Chain, Inc")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3e5a7b00-1c72-11ed-a2dd-158676a87eae")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
