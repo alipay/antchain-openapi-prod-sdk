d5ba6abc572ead786e9dae20c211611baffda52a
sdk-team <sdk-team@alibabacloud.com>
2022-08-15 08:00:37 +0000

    Generated SDK for ak_5bb1b66c4aab4cc998c05a36078e45c1.

--- ak_5bb1b66c4aab4cc998c05a36078e45c1/java/src/main/java/com/antgroup/antchain/openapi/ak_5bb1b66c4aab4cc998c05a36078e45c1/Client.java
+++ ak_5bb1b66c4aab4cc998c05a36078e45c1/java/src/main/java/com/antgroup/antchain/openapi/ak_5bb1b66c4aab4cc998c05a36078e45c1/Client.java
@@ -110,7 +110,9 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.0.6")
+                    new TeaPair("sdk_version", "1.0.7"),
+                    new TeaPair("_prod_code", "ak_5bb1b66c4aab4cc998c05a36078e45c1"),
+                    new TeaPair("_prod_channel", "saas")
                 );
                 if (!com.aliyun.teautil.Common.empty(_securityToken)) {
                     request_.query.put("security_token", _securityToken);
@@ -271,25 +273,6 @@ public class Client {
     public VerifyAntchainBbpMetaResponse verifyAntchainBbpMetaEx(VerifyAntchainBbpMetaRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "antchain.bbp.meta.verify", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new VerifyAntchainBbpMetaResponse());
-    }
-
-    /**
-     * Description: s
-     * Summary: ss
-     */
-    public BindDemoDemoCheckEeeResponse bindDemoDemoCheckEee(BindDemoDemoCheckEeeRequest request) throws Exception {
-        RuntimeOptions runtime = new RuntimeOptions();
-        java.util.Map<String, String> headers = new java.util.HashMap<>();
-        return this.bindDemoDemoCheckEeeEx(request, headers, runtime);
-    }
-
-    /**
-     * Description: s
-     * Summary: ss
-     */
-    public BindDemoDemoCheckEeeResponse bindDemoDemoCheckEeeEx(BindDemoDemoCheckEeeRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
-        com.aliyun.teautil.Common.validateModel(request);
-        return TeaModel.toModel(this.doRequest("1.0", "demo.demo.check.eee.bind", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new BindDemoDemoCheckEeeResponse());
     }
 
     /**
