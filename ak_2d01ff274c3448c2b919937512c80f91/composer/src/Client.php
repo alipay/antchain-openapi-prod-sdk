66acedad0384502d0a27dbe8c9a981bb91cda471
sdk-team <sdk-team@alibabacloud.com>
2022-08-19 07:32:07 +0000

    Generated SDK for ak_2d01ff274c3448c2b919937512c80f91.

--- ak_2d01ff274c3448c2b919937512c80f91/composer/src/Client.php
+++ ak_2d01ff274c3448c2b919937512c80f91/composer/src/Client.php
@@ -13,6 +13,8 @@ use AlibabaCloud\Tea\Utils\Utils;
 use AlibabaCloud\Tea\Utils\Utils\RuntimeOptions;
 use AntChain\Ak_2d01ff274c3448c2b919937512c80f91\Models\BindDemoAaaBbbCccRequest;
 use AntChain\Ak_2d01ff274c3448c2b919937512c80f91\Models\BindDemoAaaBbbCccResponse;
+use AntChain\Ak_2d01ff274c3448c2b919937512c80f91\Models\QueryDemoAaaBbbCccRequest;
+use AntChain\Ak_2d01ff274c3448c2b919937512c80f91\Models\QueryDemoAaaBbbCccResponse;
 use AntChain\Ak_2d01ff274c3448c2b919937512c80f91\Models\QueryDemoAdAsdAsdRequest;
 use AntChain\Ak_2d01ff274c3448c2b919937512c80f91\Models\QueryDemoAdAsdAsdResponse;
 use AntChain\Util\UtilClient;
@@ -161,7 +163,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.0.3',
+                    'sdk_version'      => '1.0.4',
                     '_prod_code'       => 'ak_2d01ff274c3448c2b919937512c80f91',
                     '_prod_channel'    => 'saas',
                 ];
@@ -273,5 +275,38 @@ class Client
         Utils::validateModel($request);
 
         return QueryDemoAdAsdAsdResponse::fromMap($this->doRequest('1.0', 'demo.ad.asd.asd.query', 'HTTPS', 'POST', '/gateway.do', Tea::merge($request), $headers, $runtime));
+    }
+
+    /**
+     * Description: 自动化测试创建1
+     * Summary: 自动化测试创建（勿动）.
+     *
+     * @param QueryDemoAaaBbbCccRequest $request
+     *
+     * @return QueryDemoAaaBbbCccResponse
+     */
+    public function queryDemoAaaBbbCcc($request)
+    {
+        $runtime = new RuntimeOptions([]);
+        $headers = [];
+
+        return $this->queryDemoAaaBbbCccEx($request, $headers, $runtime);
+    }
+
+    /**
+     * Description: 自动化测试创建1
+     * Summary: 自动化测试创建（勿动）.
+     *
+     * @param QueryDemoAaaBbbCccRequest $request
+     * @param string[]                  $headers
+     * @param RuntimeOptions            $runtime
+     *
+     * @return QueryDemoAaaBbbCccResponse
+     */
+    public function queryDemoAaaBbbCccEx($request, $headers, $runtime)
+    {
+        Utils::validateModel($request);
+
+        return QueryDemoAaaBbbCccResponse::fromMap($this->doRequest('1.0', 'demo.aaa.bbb.ccc.query', 'HTTPS', 'POST', '/gateway.do', Tea::merge($request), $headers, $runtime));
     }
 }
