024cf8a8f17768293c1e166b5bd62e9f6bc2676e
sdk-team <sdk-team@alibabacloud.com>
2022-08-19 03:37:34 +0000

    Generated SDK for ak_2d01ff274c3448c2b919937512c80f91.

--- ak_2d01ff274c3448c2b919937512c80f91/python/antchain_sdk_ak_2d01ff274c3448c2b919937512c80f91/client.py
+++ ak_2d01ff274c3448c2b919937512c80f91/python/antchain_sdk_ak_2d01ff274c3448c2b919937512c80f91/client.py
@@ -135,7 +135,7 @@ class Client:
                     'req_msg_id': AntchainUtils.get_nonce(),
                     'access_key': self._access_key_id,
                     'base_sdk_version': 'TeaSDK-2.0',
-                    'sdk_version': '1.0.0',
+                    'sdk_version': '1.0.2',
                     '_prod_code': 'ak_2d01ff274c3448c2b919937512c80f91',
                     '_prod_channel': 'saas'
                 }
@@ -239,7 +239,7 @@ class Client:
                     'req_msg_id': AntchainUtils.get_nonce(),
                     'access_key': self._access_key_id,
                     'base_sdk_version': 'TeaSDK-2.0',
-                    'sdk_version': '1.0.0',
+                    'sdk_version': '1.0.2',
                     '_prod_code': 'ak_2d01ff274c3448c2b919937512c80f91',
                     '_prod_channel': 'saas'
                 }
