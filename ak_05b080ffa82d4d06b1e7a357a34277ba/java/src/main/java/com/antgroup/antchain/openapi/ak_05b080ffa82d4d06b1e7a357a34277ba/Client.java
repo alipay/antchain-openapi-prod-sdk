8d22cd986f0e8cc71f7d2f7a4757ee12d99b9d32
sdk-team <sdk-team@alibabacloud.com>
2022-08-19 03:48:00 +0000

    Generated SDK for ak_05b080ffa82d4d06b1e7a357a34277ba.

--- ak_05b080ffa82d4d06b1e7a357a34277ba/java/src/main/java/com/antgroup/antchain/openapi/ak_05b080ffa82d4d06b1e7a357a34277ba/Client.java
+++ ak_05b080ffa82d4d06b1e7a357a34277ba/java/src/main/java/com/antgroup/antchain/openapi/ak_05b080ffa82d4d06b1e7a357a34277ba/Client.java
@@ -110,7 +110,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.0.0"),
+                    new TeaPair("sdk_version", "1.0.1"),
                     new TeaPair("_prod_code", "ak_05b080ffa82d4d06b1e7a357a34277ba"),
                     new TeaPair("_prod_channel", "saas")
                 );
@@ -178,6 +178,25 @@ public class Client {
     public BindDemoAaaBbbCccResponse bindDemoAaaBbbCccEx(BindDemoAaaBbbCccRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "demo.aaa.bbb.ccc.bind", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new BindDemoAaaBbbCccResponse());
+    }
+
+    /**
+     * Description: 1·2
+     * Summary: 测试
+     */
+    public BindDemoAsdAsdResponse bindDemoAsdAsd(BindDemoAsdAsdRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.bindDemoAsdAsdEx(request, headers, runtime);
+    }
+
+    /**
+     * Description: 1·2
+     * Summary: 测试
+     */
+    public BindDemoAsdAsdResponse bindDemoAsdAsdEx(BindDemoAsdAsdRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "demo.asd.asd.bind", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new BindDemoAsdAsdResponse());
     }
 
     /**
---
+++ ak_05b080ffa82d4d06b1e7a357a34277ba/java/src/main/java/com/antgroup/antchain/openapi/ak_05b080ffa82d4d06b1e7a357a34277ba/models/BindDemoAsdAsdRequest.java
@@ -0,0 +1,35 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.ak_05b080ffa82d4d06b1e7a357a34277ba.models;
+
+import com.aliyun.tea.*;
+
+public class BindDemoAsdAsdRequest extends TeaModel {
+    // OAuth模式下的授权token
+    @NameInMap("auth_token")
+    public String authToken;
+
+    @NameInMap("product_instance_id")
+    public String productInstanceId;
+
+    public static BindDemoAsdAsdRequest build(java.util.Map<String, ?> map) throws Exception {
+        BindDemoAsdAsdRequest self = new BindDemoAsdAsdRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public BindDemoAsdAsdRequest setAuthToken(String authToken) {
+        this.authToken = authToken;
+        return this;
+    }
+    public String getAuthToken() {
+        return this.authToken;
+    }
+
+    public BindDemoAsdAsdRequest setProductInstanceId(String productInstanceId) {
+        this.productInstanceId = productInstanceId;
+        return this;
+    }
+    public String getProductInstanceId() {
+        return this.productInstanceId;
+    }
+
+}
