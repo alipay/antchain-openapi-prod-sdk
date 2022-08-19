95532d84de65c4c06e42f1655b258d09f25afdae
sdk-team <sdk-team@alibabacloud.com>
2022-08-19 02:45:30 +0000

    Generated SDK for ak_2d01ff274c3448c2b919937512c80f91.

---
+++ ak_2d01ff274c3448c2b919937512c80f91/java/src/main/java/com/antgroup/antchain/openapi/ak_2d01ff274c3448c2b919937512c80f91/models/Dog.java
@@ -0,0 +1,76 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.ak_2d01ff274c3448c2b919937512c80f91.models;
+
+import com.aliyun.tea.*;
+
+public class Dog extends TeaModel {
+    // 名字
+    @NameInMap("name")
+    @Validation(required = true, maxLength = 32, minLength = 2)
+    public String name;
+
+    // 年龄
+    @NameInMap("age")
+    @Validation(required = true, maximum = 100)
+    public Long age;
+
+    // 是否已领养
+    @NameInMap("is_adopted")
+    @Validation(required = true)
+    public Boolean isAdopted;
+
+    // 领养时间
+    @NameInMap("adopted_date")
+    @Validation(pattern = "\\d{4}[-]\\d{1,2}[-]\\d{1,2}[T]\\d{2}:\\d{2}:\\d{2}([Z]|([\\.]\\d{1,9})?[\\+]\\d{2}[\\:]?\\d{2})")
+    public String adoptedDate;
+
+    // 标记
+    @NameInMap("marks")
+    public java.util.List<String> marks;
+
+    public static Dog build(java.util.Map<String, ?> map) throws Exception {
+        Dog self = new Dog();
+        return TeaModel.build(map, self);
+    }
+
+    public Dog setName(String name) {
+        this.name = name;
+        return this;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public Dog setAge(Long age) {
+        this.age = age;
+        return this;
+    }
+    public Long getAge() {
+        return this.age;
+    }
+
+    public Dog setIsAdopted(Boolean isAdopted) {
+        this.isAdopted = isAdopted;
+        return this;
+    }
+    public Boolean getIsAdopted() {
+        return this.isAdopted;
+    }
+
+    public Dog setAdoptedDate(String adoptedDate) {
+        this.adoptedDate = adoptedDate;
+        return this;
+    }
+    public String getAdoptedDate() {
+        return this.adoptedDate;
+    }
+
+    public Dog setMarks(java.util.List<String> marks) {
+        this.marks = marks;
+        return this;
+    }
+    public java.util.List<String> getMarks() {
+        return this.marks;
+    }
+
+}
---
+++ ak_2d01ff274c3448c2b919937512c80f91/java/src/main/java/com/antgroup/antchain/openapi/ak_2d01ff274c3448c2b919937512c80f91/models/DogHome.java
@@ -0,0 +1,38 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.ak_2d01ff274c3448c2b919937512c80f91.models;
+
+import com.aliyun.tea.*;
+
+public class DogHome extends TeaModel {
+    // 狗狗成员
+    @NameInMap("dog_members")
+    @Validation(required = true)
+    public java.util.List<Dog> dogMembers;
+
+    // 狗狗头目
+    @NameInMap("leader")
+    @Validation(required = true)
+    public Dog leader;
+
+    public static DogHome build(java.util.Map<String, ?> map) throws Exception {
+        DogHome self = new DogHome();
+        return TeaModel.build(map, self);
+    }
+
+    public DogHome setDogMembers(java.util.List<Dog> dogMembers) {
+        this.dogMembers = dogMembers;
+        return this;
+    }
+    public java.util.List<Dog> getDogMembers() {
+        return this.dogMembers;
+    }
+
+    public DogHome setLeader(Dog leader) {
+        this.leader = leader;
+        return this;
+    }
+    public Dog getLeader() {
+        return this.leader;
+    }
+
+}
