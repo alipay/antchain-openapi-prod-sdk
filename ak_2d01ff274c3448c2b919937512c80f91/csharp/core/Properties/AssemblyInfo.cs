c14afc111ab1cda793cfb28b425dbb87acc56291
sdk-team <sdk-team@alibabacloud.com>
2022-08-19 07:32:17 +0000

    Generated SDK for ak_2d01ff274c3448c2b919937512c80f91.

--- ak_2d01ff274c3448c2b919937512c80f91/csharp/core/Properties/AssemblyInfo.cs
+++ ak_2d01ff274c3448c2b919937512c80f91/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("dc8fbe40-1f86-11ed-9f61-67495ba382fe")]
+[assembly: Guid("05c72690-1f91-11ed-91c4-dfa9c54e2436")]
 
 // Version information for an assembly consists of the following four values:
 //
