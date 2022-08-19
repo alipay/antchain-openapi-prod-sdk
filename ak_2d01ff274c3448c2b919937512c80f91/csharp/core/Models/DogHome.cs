75ff0ad491625ba3ba70596e4c9481a4530b6db2
sdk-team <sdk-team@alibabacloud.com>
2022-08-19 02:45:31 +0000

    Generated SDK for ak_2d01ff274c3448c2b919937512c80f91.

---
+++ ak_2d01ff274c3448c2b919937512c80f91/csharp/core/Models/DogHome.cs
@@ -0,0 +1,25 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_2d01ff274c3448c2b919937512c80f91.Models
+{
+    // 狗狗之家
+    public class DogHome : TeaModel {
+        // 狗狗成员
+        [NameInMap("dog_members")]
+        [Validation(Required=true)]
+        public List<Dog> DogMembers { get; set; }
+
+        // 狗狗头目
+        [NameInMap("leader")]
+        [Validation(Required=true)]
+        public Dog Leader { get; set; }
+
+    }
+
+}
---
+++ ak_2d01ff274c3448c2b919937512c80f91/csharp/core/Models/GetAntcloudCatAgeRequest.cs
@@ -0,0 +1,29 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_2d01ff274c3448c2b919937512c80f91.Models
+{
+    public class GetAntcloudCatAgeRequest : TeaModel {
+        // OAuth模式下的授权token
+        [NameInMap("auth_token")]
+        [Validation(Required=false)]
+        public string AuthToken { get; set; }
+
+        // 猫猫的id
+        [NameInMap("id")]
+        [Validation(Required=true)]
+        public string Id { get; set; }
+
+        // a a a
+        [NameInMap("time")]
+        [Validation(Required=false, Pattern="\\d{4}[-]\\d{1,2}[-]\\d{1,2}[T]\\d{2}:\\d{2}:\\d{2}([Z]|([\\.]\\d{1,9})?[\\+]\\d{2}[\\:]?\\d{2})")]
+        public string Time { get; set; }
+
+    }
+
+}
