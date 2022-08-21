af149e24a913e764570b47f51d034225e50b379e
sdk-team <sdk-team@alibabacloud.com>
2022-08-21 09:21:36 +0000

    Generated SDK for NFTX.

--- nftx/csharp/core/Properties/AssemblyInfo.cs
+++ nftx/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9ebf5b30-2129-11ed-9776-3f153a1b6bcc")]
+[assembly: Guid("9ff88860-2132-11ed-bc91-eb3f5167c0ed")]
 
 // Version information for an assembly consists of the following four values:
 //
