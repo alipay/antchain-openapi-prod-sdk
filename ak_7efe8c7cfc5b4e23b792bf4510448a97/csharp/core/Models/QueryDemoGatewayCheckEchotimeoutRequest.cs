9157bf2133da2ef3371531934a3a1c63ebbe346f
sdk-team <sdk-team@alibabacloud.com>
2022-08-22 01:54:16 +0000

    Generated SDK for ak_7efe8c7cfc5b4e23b792bf4510448a97.

---
+++ ak_7efe8c7cfc5b4e23b792bf4510448a97/csharp/core/Models/QueryDemoGatewayCheckEchotimeoutRequest.cs
@@ -0,0 +1,28 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_7efe8c7cfc5b4e23b792bf4510448a97.Models
+{
+    public class QueryDemoGatewayCheckEchotimeoutRequest : TeaModel {
+        // OAuth模式下的授权token
+        [NameInMap("auth_token")]
+        [Validation(Required=false)]
+        public string AuthToken { get; set; }
+
+        [NameInMap("product_instance_id")]
+        [Validation(Required=false)]
+        public string ProductInstanceId { get; set; }
+
+        // 10
+        [NameInMap("timeout")]
+        [Validation(Required=false)]
+        public string Timeout { get; set; }
+
+    }
+
+}
---
+++ ak_7efe8c7cfc5b4e23b792bf4510448a97/csharp/core/Models/TestStruct.cs
@@ -0,0 +1,30 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_7efe8c7cfc5b4e23b792bf4510448a97.Models
+{
+    // TestStruct
+    public class TestStruct : TeaModel {
+        // x
+        [NameInMap("x")]
+        [Validation(Required=true)]
+        public string X { get; set; }
+
+        // y
+        [NameInMap("y")]
+        [Validation(Required=true)]
+        public DemoClass Y { get; set; }
+
+        // z
+        [NameInMap("z")]
+        [Validation(Required=true)]
+        public List<DemoClass> Z { get; set; }
+
+    }
+
+}
