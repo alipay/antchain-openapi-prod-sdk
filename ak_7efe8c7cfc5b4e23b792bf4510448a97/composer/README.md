5a77e2cdd3eb90abd27c49f9dbaf0c196c5b95ed
sdk-team <sdk-team@alibabacloud.com>
2022-08-22 01:54:05 +0000

    Generated SDK for ak_7efe8c7cfc5b4e23b792bf4510448a97.

---
+++ ak_7efe8c7cfc5b4e23b792bf4510448a97/composer/README.md
@@ -0,0 +1,31 @@
+English | [简体中文](README-CN.md)
+
+# Ant Chain ak_7efe8c7cfc5b4e23b792bf4510448a97 SDK for PHP
+
+## Installation
+
+### Composer
+
+```bash
+composer require antchain/ak_7efe8c7cfc5b4e23b792bf4510448a97
+```
+
+## Issues
+
+[Opening an Issue](https://github.com/alipay/antchain-openapi-prod-sdk/issues/new), Issues not conforming to the guidelines may be closed immediately.
+
+## Usage
+
+[Quick Examples](https://github.com/alipay/antchain-openapi-prod-sdk/blob/master/docs/0-Examples-EN.md#quick-examples)
+
+## Changelog
+
+Detailed changes for each release are documented in the [release notes](./ChangeLog.txt).
+
+## References
+
+* [Latest Release](https://github.com/antchain-openapi-sdk-php)
+
+## License
+
+[Apache-2.0](http://www.apache.org/licenses/LICENSE-2.0)
---
+++ ak_7efe8c7cfc5b4e23b792bf4510448a97/composer/src/Models/TestStruct.php
@@ -0,0 +1,95 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AntChain\Ak_7efe8c7cfc5b4e23b792bf4510448a97\Models;
+
+use AlibabaCloud\Tea\Model;
+
+class TestStruct extends Model
+{
+    // x
+    /**
+     * @example x
+     *
+     * @var string
+     */
+    public $x;
+
+    // y
+    /**
+     * @example y
+     *
+     * @var DemoClass
+     */
+    public $y;
+
+    // z
+    /**
+     * @example
+     *
+     * @var DemoClass[]
+     */
+    public $z;
+    protected $_name = [
+        'x' => 'x',
+        'y' => 'y',
+        'z' => 'z',
+    ];
+
+    public function validate()
+    {
+        Model::validateRequired('x', $this->x, true);
+        Model::validateRequired('y', $this->y, true);
+        Model::validateRequired('z', $this->z, true);
+    }
+
+    public function toMap()
+    {
+        $res = [];
+        if (null !== $this->x) {
+            $res['x'] = $this->x;
+        }
+        if (null !== $this->y) {
+            $res['y'] = null !== $this->y ? $this->y->toMap() : null;
+        }
+        if (null !== $this->z) {
+            $res['z'] = [];
+            if (null !== $this->z && \is_array($this->z)) {
+                $n = 0;
+                foreach ($this->z as $item) {
+                    $res['z'][$n++] = null !== $item ? $item->toMap() : $item;
+                }
+            }
+        }
+
+        return $res;
+    }
+
+    /**
+     * @param array $map
+     *
+     * @return TestStruct
+     */
+    public static function fromMap($map = [])
+    {
+        $model = new self();
+        if (isset($map['x'])) {
+            $model->x = $map['x'];
+        }
+        if (isset($map['y'])) {
+            $model->y = DemoClass::fromMap($map['y']);
+        }
+        if (isset($map['z'])) {
+            if (!empty($map['z'])) {
+                $model->z = [];
+                $n        = 0;
+                foreach ($map['z'] as $item) {
+                    $model->z[$n++] = null !== $item ? DemoClass::fromMap($item) : $item;
+                }
+            }
+        }
+
+        return $model;
+    }
+}
