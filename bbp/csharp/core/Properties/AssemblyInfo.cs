85ff343633e69c75911aae7a0de615ea735429df
sdk-team <sdk-team@alibabacloud.com>
2022-08-23 09:47:38 +0000

    Generated SDK for BBP.

--- bbp/csharp/core/Properties/AssemblyInfo.cs
+++ bbp/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("84aa0f80-22c6-11ed-b36c-210efc914541")]
+[assembly: Guid("97d73360-22c8-11ed-b9d0-4b3a1df74dd3")]
 
 // Version information for an assembly consists of the following four values:
 //
