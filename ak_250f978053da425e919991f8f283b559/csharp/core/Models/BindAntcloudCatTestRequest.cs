dba423d9cdaad864cae844dc5809a47d8234f688
sdk-team <sdk-team@alibabacloud.com>
2022-08-23 02:22:04 +0000

    Generated SDK for ak_250f978053da425e919991f8f283b559.

---
+++ ak_250f978053da425e919991f8f283b559/csharp/core/Models/BindAntcloudCatTestRequest.cs
@@ -0,0 +1,19 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_250f978053da425e919991f8f283b559.Models
+{
+    public class BindAntcloudCatTestRequest : TeaModel {
+        // OAuth模式下的授权token
+        [NameInMap("auth_token")]
+        [Validation(Required=false)]
+        public string AuthToken { get; set; }
+
+    }
+
+}
