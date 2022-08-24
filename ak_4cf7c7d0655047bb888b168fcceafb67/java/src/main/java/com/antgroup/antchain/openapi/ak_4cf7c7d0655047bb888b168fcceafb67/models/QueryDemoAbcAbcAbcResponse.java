aed37ccf366f271b4f45936c204c4a57511b75a7
sdk-team <sdk-team@alibabacloud.com>
2022-08-24 11:14:52 +0000

    Generated SDK for ak_4cf7c7d0655047bb888b168fcceafb67.

---
+++ ak_4cf7c7d0655047bb888b168fcceafb67/java/src/main/java/com/antgroup/antchain/openapi/ak_4cf7c7d0655047bb888b168fcceafb67/models/QueryDemoAbcAbcAbcResponse.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.ak_4cf7c7d0655047bb888b168fcceafb67.models;
+
+import com.aliyun.tea.*;
+
+public class QueryDemoAbcAbcAbcResponse extends TeaModel {
+    // 请求唯一ID，用于链路跟踪和问题排查
+    @NameInMap("req_msg_id")
+    public String reqMsgId;
+
+    // 结果码，一般OK表示调用成功
+    @NameInMap("result_code")
+    public String resultCode;
+
+    // 异常信息的文本描述
+    @NameInMap("result_msg")
+    public String resultMsg;
+
+    public static QueryDemoAbcAbcAbcResponse build(java.util.Map<String, ?> map) throws Exception {
+        QueryDemoAbcAbcAbcResponse self = new QueryDemoAbcAbcAbcResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public QueryDemoAbcAbcAbcResponse setReqMsgId(String reqMsgId) {
+        this.reqMsgId = reqMsgId;
+        return this;
+    }
+    public String getReqMsgId() {
+        return this.reqMsgId;
+    }
+
+    public QueryDemoAbcAbcAbcResponse setResultCode(String resultCode) {
+        this.resultCode = resultCode;
+        return this;
+    }
+    public String getResultCode() {
+        return this.resultCode;
+    }
+
+    public QueryDemoAbcAbcAbcResponse setResultMsg(String resultMsg) {
+        this.resultMsg = resultMsg;
+        return this;
+    }
+    public String getResultMsg() {
+        return this.resultMsg;
+    }
+
+}
