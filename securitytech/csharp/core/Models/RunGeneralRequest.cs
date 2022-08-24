c2f797b434ce87cd1148990b95e6ac315966b444
sdk-team <sdk-team@alibabacloud.com>
2022-08-24 12:48:08 +0000

    Generated SDK for SECURITYTECH.

---
+++ securitytech/csharp/core/Models/RunGeneralRequest.cs
@@ -0,0 +1,33 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.SECURITYTECH.Models
+{
+    public class RunGeneralRequest : TeaModel {
+        // OAuth模式下的授权token
+        [NameInMap("auth_token")]
+        [Validation(Required=false)]
+        public string AuthToken { get; set; }
+
+        [NameInMap("product_instance_id")]
+        [Validation(Required=false)]
+        public string ProductInstanceId { get; set; }
+
+        // 业务请求数据，json格式
+        [NameInMap("request")]
+        [Validation(Required=true)]
+        public string Request { get; set; }
+
+        // 要调用的具体的服务名称
+        [NameInMap("service_name")]
+        [Validation(Required=true)]
+        public string ServiceName { get; set; }
+
+    }
+
+}
