a6305a48c4294277ce3ef9efdec3303db4d50e6c
sdk-team <sdk-team@alibabacloud.com>
2022-08-25 07:18:00 +0000

    Generated SDK for ak_6efc6591476e4813ac8f7c9b6359c0b7.

---
+++ ak_6efc6591476e4813ac8f7c9b6359c0b7/composer/autoload.php
@@ -0,0 +1,17 @@
+<?php
+
+if (file_exists(__DIR__ . \DIRECTORY_SEPARATOR . 'vendor' . \DIRECTORY_SEPARATOR . 'autoload.php')) {
+    require_once __DIR__ . \DIRECTORY_SEPARATOR . 'vendor' . \DIRECTORY_SEPARATOR . 'autoload.php';
+}
+
+spl_autoload_register(function ($class) {
+    $name = str_replace('AntChain\\Ak_6efc6591476e4813ac8f7c9b6359c0b7\\', '', $class);
+    $file = __DIR__ . \DIRECTORY_SEPARATOR . 'src' . \DIRECTORY_SEPARATOR . str_replace('\\', \DIRECTORY_SEPARATOR, $name) . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+
+        return true;
+    }
+
+    return false;
+});
---
+++ ak_6efc6591476e4813ac8f7c9b6359c0b7/composer/src/Models/KeyValuePair.php
@@ -0,0 +1,66 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AntChain\Ak_6efc6591476e4813ac8f7c9b6359c0b7\Models;
+
+use AlibabaCloud\Tea\Model;
+
+class KeyValuePair extends Model
+{
+    // key
+    /**
+     * @example key
+     *
+     * @var string
+     */
+    public $key;
+
+    // value
+    /**
+     * @example value
+     *
+     * @var string
+     */
+    public $value;
+    protected $_name = [
+        'key'   => 'key',
+        'value' => 'value',
+    ];
+
+    public function validate()
+    {
+        Model::validateRequired('key', $this->key, true);
+    }
+
+    public function toMap()
+    {
+        $res = [];
+        if (null !== $this->key) {
+            $res['key'] = $this->key;
+        }
+        if (null !== $this->value) {
+            $res['value'] = $this->value;
+        }
+
+        return $res;
+    }
+
+    /**
+     * @param array $map
+     *
+     * @return KeyValuePair
+     */
+    public static function fromMap($map = [])
+    {
+        $model = new self();
+        if (isset($map['key'])) {
+            $model->key = $map['key'];
+        }
+        if (isset($map['value'])) {
+            $model->value = $map['value'];
+        }
+
+        return $model;
+    }
+}
