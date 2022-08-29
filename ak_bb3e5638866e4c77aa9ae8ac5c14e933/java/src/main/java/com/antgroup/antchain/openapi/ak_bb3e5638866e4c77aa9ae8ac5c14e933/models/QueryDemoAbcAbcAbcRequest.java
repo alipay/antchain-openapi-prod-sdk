700bbc7330166a2f3919ff83e98338c6c6e6e077
sdk-team <sdk-team@alibabacloud.com>
2022-08-29 03:24:49 +0000

    Generated SDK for ak_bb3e5638866e4c77aa9ae8ac5c14e933.

---
+++ ak_bb3e5638866e4c77aa9ae8ac5c14e933/java/src/main/java/com/antgroup/antchain/openapi/ak_bb3e5638866e4c77aa9ae8ac5c14e933/models/QueryDemoAbcAbcAbcRequest.java
@@ -0,0 +1,35 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.ak_bb3e5638866e4c77aa9ae8ac5c14e933.models;
+
+import com.aliyun.tea.*;
+
+public class QueryDemoAbcAbcAbcRequest extends TeaModel {
+    // OAuth模式下的授权token
+    @NameInMap("auth_token")
+    public String authToken;
+
+    @NameInMap("product_instance_id")
+    public String productInstanceId;
+
+    public static QueryDemoAbcAbcAbcRequest build(java.util.Map<String, ?> map) throws Exception {
+        QueryDemoAbcAbcAbcRequest self = new QueryDemoAbcAbcAbcRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public QueryDemoAbcAbcAbcRequest setAuthToken(String authToken) {
+        this.authToken = authToken;
+        return this;
+    }
+    public String getAuthToken() {
+        return this.authToken;
+    }
+
+    public QueryDemoAbcAbcAbcRequest setProductInstanceId(String productInstanceId) {
+        this.productInstanceId = productInstanceId;
+        return this;
+    }
+    public String getProductInstanceId() {
+        return this.productInstanceId;
+    }
+
+}
