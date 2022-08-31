c036e50bdaa64838e7a1b50d2bb0ba596a992468
sdk-team <sdk-team@alibabacloud.com>
2022-08-31 03:36:14 +0000

    Generated SDK for ak_f1fed1793fe14f559e3f176b1fc85850.

---
+++ ak_f1fed1793fe14f559e3f176b1fc85850/java/src/main/java/com/antgroup/antchain/openapi/ak_f1fed1793fe14f559e3f176b1fc85850/models/ExecAntchainBbpContractReconciliationResponse.java
@@ -0,0 +1,60 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.ak_f1fed1793fe14f559e3f176b1fc85850.models;
+
+import com.aliyun.tea.*;
+
+public class ExecAntchainBbpContractReconciliationResponse extends TeaModel {
+    // 请求唯一ID，用于链路跟踪和问题排查
+    @NameInMap("req_msg_id")
+    public String reqMsgId;
+
+    // 结果码，一般OK表示调用成功
+    @NameInMap("result_code")
+    public String resultCode;
+
+    // 异常信息的文本描述
+    @NameInMap("result_msg")
+    public String resultMsg;
+
+    // 结算单
+    @NameInMap("reconciliation")
+    public Reconciliation reconciliation;
+
+    public static ExecAntchainBbpContractReconciliationResponse build(java.util.Map<String, ?> map) throws Exception {
+        ExecAntchainBbpContractReconciliationResponse self = new ExecAntchainBbpContractReconciliationResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public ExecAntchainBbpContractReconciliationResponse setReqMsgId(String reqMsgId) {
+        this.reqMsgId = reqMsgId;
+        return this;
+    }
+    public String getReqMsgId() {
+        return this.reqMsgId;
+    }
+
+    public ExecAntchainBbpContractReconciliationResponse setResultCode(String resultCode) {
+        this.resultCode = resultCode;
+        return this;
+    }
+    public String getResultCode() {
+        return this.resultCode;
+    }
+
+    public ExecAntchainBbpContractReconciliationResponse setResultMsg(String resultMsg) {
+        this.resultMsg = resultMsg;
+        return this;
+    }
+    public String getResultMsg() {
+        return this.resultMsg;
+    }
+
+    public ExecAntchainBbpContractReconciliationResponse setReconciliation(Reconciliation reconciliation) {
+        this.reconciliation = reconciliation;
+        return this;
+    }
+    public Reconciliation getReconciliation() {
+        return this.reconciliation;
+    }
+
+}
