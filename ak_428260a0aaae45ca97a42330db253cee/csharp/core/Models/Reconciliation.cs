2eb921963202c5eb7ea0818991e20f086398766e
sdk-team <sdk-team@alibabacloud.com>
2022-08-31 10:00:06 +0000

    Generated SDK for ak_428260a0aaae45ca97a42330db253cee.

---
+++ ak_428260a0aaae45ca97a42330db253cee/csharp/core/Models/Reconciliation.cs
@@ -0,0 +1,65 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_428260a0aaae45ca97a42330db253cee.Models
+{
+    // 对账单
+    public class Reconciliation : TeaModel {
+        // 供应商
+        [NameInMap("sup_code")]
+        [Validation(Required=true)]
+        public string SupCode { get; set; }
+
+        // 供应商名称
+        [NameInMap("sup_name")]
+        [Validation(Required=true)]
+        public string SupName { get; set; }
+
+        // 结算金额(单位分)
+        [NameInMap("settlement_amount")]
+        [Validation(Required=true)]
+        public long? SettlementAmount { get; set; }
+
+        // 长城确认时间
+        [NameInMap("confirm_date_gw")]
+        [Validation(Required=true)]
+        public string ConfirmDateGw { get; set; }
+
+        // 长城方确认人
+        [NameInMap("confirmer_gw")]
+        [Validation(Required=true)]
+        public string ConfirmerGw { get; set; }
+
+        // 长城确认状态
+        [NameInMap("confirm_state_gw")]
+        [Validation(Required=true)]
+        public string ConfirmStateGw { get; set; }
+
+        // 服务方确认时间
+        [NameInMap("confirm_date_sup")]
+        [Validation(Required=true)]
+        public string ConfirmDateSup { get; set; }
+
+        // 服务方确认人
+        [NameInMap("confirmer_sup")]
+        [Validation(Required=true)]
+        public string ConfirmerSup { get; set; }
+
+        // 服务方确认状态
+        [NameInMap("confirm_state_sup")]
+        [Validation(Required=true)]
+        public string ConfirmStateSup { get; set; }
+
+        // 计算时间
+        [NameInMap("socre_date")]
+        [Validation(Required=true)]
+        public string SocreDate { get; set; }
+
+    }
+
+}
---
+++ ak_428260a0aaae45ca97a42330db253cee/csharp/core/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("antchain-ak_428260a0aaae45ca97a42330db253cee")]
+[assembly: AssemblyDescription("Ant Chain ak_428260a0aaae45ca97a42330db253cee SDK Library for .NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ant Chain, Inc")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ad133cf0-2913-11ed-9912-7167b2a90b98")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
