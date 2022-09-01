95d8336447a0e077513bcf88a0b5bf16277d1431
sdk-team <sdk-team@alibabacloud.com>
2022-09-01 16:16:19 +0000

    Generated SDK for TWC.

--- twc/composer/src/Client.php
+++ twc/composer/src/Client.php
@@ -698,7 +698,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.7.73',
+                    'sdk_version'      => '1.7.74',
                     '_prod_code'       => 'TWC',
                     '_prod_channel'    => 'undefined',
                 ];
