f6032e1262be54fb302233775cce9228ca1f370b
sdk-team <sdk-team@alibabacloud.com>
2022-09-16 09:03:28 +0000

    Generated SDK for ak_9620cd2b3b2f441d90280aee41564281.

--- ak_9620cd2b3b2f441d90280aee41564281/java/src/main/java/com/antgroup/antchain/openapi/ak_9620cd2b3b2f441d90280aee41564281/Client.java
+++ ak_9620cd2b3b2f441d90280aee41564281/java/src/main/java/com/antgroup/antchain/openapi/ak_9620cd2b3b2f441d90280aee41564281/Client.java
@@ -110,7 +110,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.0.1"),
+                    new TeaPair("sdk_version", "1.0.2"),
                     new TeaPair("_prod_code", "ak_9620cd2b3b2f441d90280aee41564281"),
                     new TeaPair("_prod_channel", "saas")
                 );
@@ -197,24 +197,5 @@ public class Client {
     public VerifyAntchainBbpMetaResponse verifyAntchainBbpMetaEx(VerifyAntchainBbpMetaRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "antchain.bbp.meta.verify", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new VerifyAntchainBbpMetaResponse());
-    }
-
-    /**
-     * Description: 记录用户登录保司小程序信息
-     * Summary: 用户登陆页面埋点
-     */
-    public InitAntchainBbpInsuranceUserResponse initAntchainBbpInsuranceUser(InitAntchainBbpInsuranceUserRequest request) throws Exception {
-        RuntimeOptions runtime = new RuntimeOptions();
-        java.util.Map<String, String> headers = new java.util.HashMap<>();
-        return this.initAntchainBbpInsuranceUserEx(request, headers, runtime);
-    }
-
-    /**
-     * Description: 记录用户登录保司小程序信息
-     * Summary: 用户登陆页面埋点
-     */
-    public InitAntchainBbpInsuranceUserResponse initAntchainBbpInsuranceUserEx(InitAntchainBbpInsuranceUserRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
-        com.aliyun.teautil.Common.validateModel(request);
-        return TeaModel.toModel(this.doRequest("1.0", "antchain.bbp.insurance.user.init", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new InitAntchainBbpInsuranceUserResponse());
     }
 }
