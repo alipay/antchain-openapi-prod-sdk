d642a7a512ca620a6bfc51349bc2a2aede641b75
sdk-team <sdk-team@alibabacloud.com>
2022-09-16 08:45:29 +0000

    Generated SDK for ak_9620cd2b3b2f441d90280aee41564281.

--- ak_9620cd2b3b2f441d90280aee41564281/java/src/main/java/com/antgroup/antchain/openapi/ak_9620cd2b3b2f441d90280aee41564281/Client.java
+++ ak_9620cd2b3b2f441d90280aee41564281/java/src/main/java/com/antgroup/antchain/openapi/ak_9620cd2b3b2f441d90280aee41564281/Client.java
@@ -110,7 +110,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.0.0"),
+                    new TeaPair("sdk_version", "1.0.1"),
                     new TeaPair("_prod_code", "ak_9620cd2b3b2f441d90280aee41564281"),
                     new TeaPair("_prod_channel", "saas")
                 );
@@ -178,5 +178,43 @@ public class Client {
     public AuthAntchainBbpCustomerResponse authAntchainBbpCustomerEx(AuthAntchainBbpCustomerRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "antchain.bbp.customer.auth", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new AuthAntchainBbpCustomerResponse());
+    }
+
+    /**
+     * Description: 对企业身份进行认证
+     * Summary: 企业四要素认证
+     */
+    public VerifyAntchainBbpMetaResponse verifyAntchainBbpMeta(VerifyAntchainBbpMetaRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.verifyAntchainBbpMetaEx(request, headers, runtime);
+    }
+
+    /**
+     * Description: 对企业身份进行认证
+     * Summary: 企业四要素认证
+     */
+    public VerifyAntchainBbpMetaResponse verifyAntchainBbpMetaEx(VerifyAntchainBbpMetaRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "antchain.bbp.meta.verify", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new VerifyAntchainBbpMetaResponse());
+    }
+
+    /**
+     * Description: 记录用户登录保司小程序信息
+     * Summary: 用户登陆页面埋点
+     */
+    public InitAntchainBbpInsuranceUserResponse initAntchainBbpInsuranceUser(InitAntchainBbpInsuranceUserRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.initAntchainBbpInsuranceUserEx(request, headers, runtime);
+    }
+
+    /**
+     * Description: 记录用户登录保司小程序信息
+     * Summary: 用户登陆页面埋点
+     */
+    public InitAntchainBbpInsuranceUserResponse initAntchainBbpInsuranceUserEx(InitAntchainBbpInsuranceUserRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "antchain.bbp.insurance.user.init", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new InitAntchainBbpInsuranceUserResponse());
     }
 }
