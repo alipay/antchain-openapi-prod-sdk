6b8659b3002a655a00bfea7e361e519e3a13d837
sdk-team <sdk-team@alibabacloud.com>
2022-09-21 01:57:06 +0000

    Generated SDK for ak_50eaf604cdf649e99d6b6d4d6525a52f.

---
+++ ak_50eaf604cdf649e99d6b6d4d6525a52f/csharp/core/Models/QueryMap.cs
@@ -0,0 +1,25 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_50eaf604cdf649e99d6b6d4d6525a52f.Models
+{
+    // Map<String,Object> 集合
+    public class QueryMap : TeaModel {
+        // 键值
+        [NameInMap("name")]
+        [Validation(Required=true)]
+        public string Name { get; set; }
+
+        // 额外用户信息
+        [NameInMap("value")]
+        [Validation(Required=false)]
+        public List<NameValuePair> Value { get; set; }
+
+    }
+
+}
---
+++ ak_50eaf604cdf649e99d6b6d4d6525a52f/csharp/core/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("antchain-ak_50eaf604cdf649e99d6b6d4d6525a52f")]
+[assembly: AssemblyDescription("Ant Chain ak_50eaf604cdf649e99d6b6d4d6525a52f SDK Library for .NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ant Chain, Inc")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ace3a380-3950-11ed-838a-0172d90ced80")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
