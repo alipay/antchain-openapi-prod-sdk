c19b5943e2573e0a8bd43879d1b76dc34eeb5350
sdk-team <sdk-team@alibabacloud.com>
2022-09-21 10:01:52 +0000

    Generated SDK for ak_f2ba648e36a44708ab93a511f45f8a19.

---
+++ ak_f2ba648e36a44708ab93a511f45f8a19/composer/src/Models/QueryDemoTestGatewayTestRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AntChain\Ak_f2ba648e36a44708ab93a511f45f8a19\Models;
+
+use AlibabaCloud\Tea\Model;
+
+class QueryDemoTestGatewayTestRequest extends Model
+{
+    // OAuth模式下的授权token
+    /**
+     * @var string
+     */
+    public $authToken;
+
+    /**
+     * @var string
+     */
+    public $productInstanceId;
+    protected $_name = [
+        'authToken'         => 'auth_token',
+        'productInstanceId' => 'product_instance_id',
+    ];
+
+    public function validate()
+    {
+    }
+
+    public function toMap()
+    {
+        $res = [];
+        if (null !== $this->authToken) {
+            $res['auth_token'] = $this->authToken;
+        }
+        if (null !== $this->productInstanceId) {
+            $res['product_instance_id'] = $this->productInstanceId;
+        }
+
+        return $res;
+    }
+
+    /**
+     * @param array $map
+     *
+     * @return QueryDemoTestGatewayTestRequest
+     */
+    public static function fromMap($map = [])
+    {
+        $model = new self();
+        if (isset($map['auth_token'])) {
+            $model->authToken = $map['auth_token'];
+        }
+        if (isset($map['product_instance_id'])) {
+            $model->productInstanceId = $map['product_instance_id'];
+        }
+
+        return $model;
+    }
+}
---
+++ ak_f2ba648e36a44708ab93a511f45f8a19/composer/src/Models/QueryDemoTestTestRequest.php
@@ -0,0 +1,73 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AntChain\Ak_f2ba648e36a44708ab93a511f45f8a19\Models;
+
+use AlibabaCloud\Tea\Model;
+
+class QueryDemoTestTestRequest extends Model
+{
+    // OAuth模式下的授权token
+    /**
+     * @var string
+     */
+    public $authToken;
+
+    /**
+     * @var string
+     */
+    public $productInstanceId;
+
+    /**
+     * @var string
+     */
+    public $acpmpcore;
+    protected $_name = [
+        'authToken'         => 'auth_token',
+        'productInstanceId' => 'product_instance_id',
+        'acpmpcore'         => 'acpmpcore',
+    ];
+
+    public function validate()
+    {
+        Model::validateRequired('acpmpcore', $this->acpmpcore, true);
+    }
+
+    public function toMap()
+    {
+        $res = [];
+        if (null !== $this->authToken) {
+            $res['auth_token'] = $this->authToken;
+        }
+        if (null !== $this->productInstanceId) {
+            $res['product_instance_id'] = $this->productInstanceId;
+        }
+        if (null !== $this->acpmpcore) {
+            $res['acpmpcore'] = $this->acpmpcore;
+        }
+
+        return $res;
+    }
+
+    /**
+     * @param array $map
+     *
+     * @return QueryDemoTestTestRequest
+     */
+    public static function fromMap($map = [])
+    {
+        $model = new self();
+        if (isset($map['auth_token'])) {
+            $model->authToken = $map['auth_token'];
+        }
+        if (isset($map['product_instance_id'])) {
+            $model->productInstanceId = $map['product_instance_id'];
+        }
+        if (isset($map['acpmpcore'])) {
+            $model->acpmpcore = $map['acpmpcore'];
+        }
+
+        return $model;
+    }
+}
