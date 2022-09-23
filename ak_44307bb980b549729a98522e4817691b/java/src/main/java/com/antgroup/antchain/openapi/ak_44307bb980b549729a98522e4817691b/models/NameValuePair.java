637f2148d3054bd0bfa217fc9edc0a3530853ffd
sdk-team <sdk-team@alibabacloud.com>
2022-09-23 07:05:10 +0000

    Generated SDK for ak_44307bb980b549729a98522e4817691b.

---
+++ ak_44307bb980b549729a98522e4817691b/java/src/main/java/com/antgroup/antchain/openapi/ak_44307bb980b549729a98522e4817691b/models/NameValuePair.java
@@ -0,0 +1,38 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.ak_44307bb980b549729a98522e4817691b.models;
+
+import com.aliyun.tea.*;
+
+public class NameValuePair extends TeaModel {
+    // 键名
+    @NameInMap("name")
+    @Validation(required = true)
+    public String name;
+
+    // 键值
+    @NameInMap("value")
+    @Validation(required = true)
+    public String value;
+
+    public static NameValuePair build(java.util.Map<String, ?> map) throws Exception {
+        NameValuePair self = new NameValuePair();
+        return TeaModel.build(map, self);
+    }
+
+    public NameValuePair setName(String name) {
+        this.name = name;
+        return this;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public NameValuePair setValue(String value) {
+        this.value = value;
+        return this;
+    }
+    public String getValue() {
+        return this.value;
+    }
+
+}
