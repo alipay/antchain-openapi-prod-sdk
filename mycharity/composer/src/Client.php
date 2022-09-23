201b064f8d49c845b7f56d62e00770af875969f2
sdk-team <sdk-team@alibabacloud.com>
2022-09-23 06:19:22 +0000

    Generated SDK for MYCHARITY.

--- mycharity/composer/src/Client.php
+++ mycharity/composer/src/Client.php
@@ -208,7 +208,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.0.13',
+                    'sdk_version'      => '1.0.14',
                     '_prod_code'       => 'MYCHARITY',
                     '_prod_channel'    => 'undefined',
                 ];
