f549c58f257fbec07311d9637c59cc50bf342676
sdk-team <sdk-team@alibabacloud.com>
2022-10-14 05:40:15 +0000

    Generated SDK for INTEGRATION_MACHINE.

--- integration_machine/go/client/client.go
+++ integration_machine/go/client/client.go
@@ -2173,6 +2173,8 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
 				"sdk_version":      tea.String("1.0.18"),
+				"_prod_code":       tea.String("INTEGRATION_MACHINE"),
+				"_prod_channel":    tea.String("undefined"),
 			}
 			if !tea.BoolValue(util.Empty(client.SecurityToken)) {
 				request_.Query["security_token"] = client.SecurityToken
