510012628f579f65ed37a007c7243dfab46cd39e
sdk-team <sdk-team@alibabacloud.com>
2022-10-21 07:54:49 +0000

    Generated SDK for RISKPLUS.

---
+++ riskplus/composer/src/Models/Contract.php
@@ -0,0 +1,147 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AntChain\RISKPLUS\Models;
+
+use AlibabaCloud\Tea\Model;
+
+class Contract extends Model
+{
+    // 关联编号
+    /**
+     * @example 123123
+     *
+     * @var string
+     */
+    public $relationNo;
+
+    // 合同编号
+    /**
+     * @example 123123
+     *
+     * @var string
+     */
+    public $contractNo;
+
+    // 合同名称
+    /**
+     * @example 用信合同
+     *
+     * @var string
+     */
+    public $contractName;
+
+    // 合同类型
+    /**
+     * @example 0
+     *
+     * @var string
+     */
+    public $contractType;
+
+    // 客户编号
+    /**
+     * @example PCM123xxxx
+     *
+     * @var string
+     */
+    public $customNo;
+
+    // 合同存放目录
+    /**
+     * @example https://11111
+     *
+     * @var string
+     */
+    public $savePath;
+
+    // 合同金额
+    /**
+     * @example
+     *
+     * @var int
+     */
+    public $contractAmount;
+    protected $_name = [
+        'relationNo'     => 'relation_no',
+        'contractNo'     => 'contract_no',
+        'contractName'   => 'contract_name',
+        'contractType'   => 'contract_type',
+        'customNo'       => 'custom_no',
+        'savePath'       => 'save_path',
+        'contractAmount' => 'contract_amount',
+    ];
+
+    public function validate()
+    {
+        Model::validateRequired('relationNo', $this->relationNo, true);
+        Model::validateRequired('contractNo', $this->contractNo, true);
+        Model::validateRequired('contractName', $this->contractName, true);
+        Model::validateRequired('contractType', $this->contractType, true);
+        Model::validateRequired('customNo', $this->customNo, true);
+        Model::validateRequired('savePath', $this->savePath, true);
+        Model::validateRequired('contractAmount', $this->contractAmount, true);
+    }
+
+    public function toMap()
+    {
+        $res = [];
+        if (null !== $this->relationNo) {
+            $res['relation_no'] = $this->relationNo;
+        }
+        if (null !== $this->contractNo) {
+            $res['contract_no'] = $this->contractNo;
+        }
+        if (null !== $this->contractName) {
+            $res['contract_name'] = $this->contractName;
+        }
+        if (null !== $this->contractType) {
+            $res['contract_type'] = $this->contractType;
+        }
+        if (null !== $this->customNo) {
+            $res['custom_no'] = $this->customNo;
+        }
+        if (null !== $this->savePath) {
+            $res['save_path'] = $this->savePath;
+        }
+        if (null !== $this->contractAmount) {
+            $res['contract_amount'] = $this->contractAmount;
+        }
+
+        return $res;
+    }
+
+    /**
+     * @param array $map
+     *
+     * @return Contract
+     */
+    public static function fromMap($map = [])
+    {
+        $model = new self();
+        if (isset($map['relation_no'])) {
+            $model->relationNo = $map['relation_no'];
+        }
+        if (isset($map['contract_no'])) {
+            $model->contractNo = $map['contract_no'];
+        }
+        if (isset($map['contract_name'])) {
+            $model->contractName = $map['contract_name'];
+        }
+        if (isset($map['contract_type'])) {
+            $model->contractType = $map['contract_type'];
+        }
+        if (isset($map['custom_no'])) {
+            $model->customNo = $map['custom_no'];
+        }
+        if (isset($map['save_path'])) {
+            $model->savePath = $map['save_path'];
+        }
+        if (isset($map['contract_amount'])) {
+            $model->contractAmount = $map['contract_amount'];
+        }
+
+        return $model;
+    }
+}
--- riskplus/composer/src/Models/QueryDubbridgeSearchContractResponse.php
+++ riskplus/composer/src/Models/QueryDubbridgeSearchContractResponse.php
@@ -26,58 +26,16 @@ class QueryDubbridgeSearchContractResponse extends Model
      */
     public $resultMsg;
 
-    // 关联编号（授信/用信）
+    // 合同信息列表
     /**
-     * @var string
+     * @var Contract[]
      */
-    public $relationNo;
-
-    // 合同编号
-    /**
-     * @var string
-     */
-    public $contractNo;
-
-    // 合同名称
-    /**
-     * @var string
-     */
-    public $contractName;
-
-    // 合同类型：0:电子合同签署授权协议 1:借款合同 2:抵押合同 3:担保合同 4:用信合同 5:通用合同 6:征信授权书
-    /**
-     * @var string
-     */
-    public $contractType;
-
-    // 客户编号
-    /**
-     * @var string
-     */
-    public $customNo;
-
-    // 下载地址
-    /**
-     * @var string
-     */
-    public $savePath;
-
-    // 合同金额
-    /**
-     * @var int
-     */
-    public $contractAmount;
+    public $contractResponseList;
     protected $_name = [
-        'reqMsgId'       => 'req_msg_id',
-        'resultCode'     => 'result_code',
-        'resultMsg'      => 'result_msg',
-        'relationNo'     => 'relation_no',
-        'contractNo'     => 'contract_no',
-        'contractName'   => 'contract_name',
-        'contractType'   => 'contract_type',
-        'customNo'       => 'custom_no',
-        'savePath'       => 'save_path',
-        'contractAmount' => 'contract_amount',
+        'reqMsgId'             => 'req_msg_id',
+        'resultCode'           => 'result_code',
+        'resultMsg'            => 'result_msg',
+        'contractResponseList' => 'contract_response_list',
     ];
 
     public function validate()
@@ -96,26 +54,14 @@ class QueryDubbridgeSearchContractResponse extends Model
         if (null !== $this->resultMsg) {
             $res['result_msg'] = $this->resultMsg;
         }
-        if (null !== $this->relationNo) {
-            $res['relation_no'] = $this->relationNo;
-        }
-        if (null !== $this->contractNo) {
-            $res['contract_no'] = $this->contractNo;
-        }
-        if (null !== $this->contractName) {
-            $res['contract_name'] = $this->contractName;
-        }
-        if (null !== $this->contractType) {
-            $res['contract_type'] = $this->contractType;
-        }
-        if (null !== $this->customNo) {
-            $res['custom_no'] = $this->customNo;
-        }
-        if (null !== $this->savePath) {
-            $res['save_path'] = $this->savePath;
-        }
-        if (null !== $this->contractAmount) {
-            $res['contract_amount'] = $this->contractAmount;
+        if (null !== $this->contractResponseList) {
+            $res['contract_response_list'] = [];
+            if (null !== $this->contractResponseList && \is_array($this->contractResponseList)) {
+                $n = 0;
+                foreach ($this->contractResponseList as $item) {
+                    $res['contract_response_list'][$n++] = null !== $item ? $item->toMap() : $item;
+                }
+            }
         }
 
         return $res;
@@ -138,26 +84,14 @@ class QueryDubbridgeSearchContractResponse extends Model
         if (isset($map['result_msg'])) {
             $model->resultMsg = $map['result_msg'];
         }
-        if (isset($map['relation_no'])) {
-            $model->relationNo = $map['relation_no'];
-        }
-        if (isset($map['contract_no'])) {
-            $model->contractNo = $map['contract_no'];
-        }
-        if (isset($map['contract_name'])) {
-            $model->contractName = $map['contract_name'];
-        }
-        if (isset($map['contract_type'])) {
-            $model->contractType = $map['contract_type'];
-        }
-        if (isset($map['custom_no'])) {
-            $model->customNo = $map['custom_no'];
-        }
-        if (isset($map['save_path'])) {
-            $model->savePath = $map['save_path'];
-        }
-        if (isset($map['contract_amount'])) {
-            $model->contractAmount = $map['contract_amount'];
+        if (isset($map['contract_response_list'])) {
+            if (!empty($map['contract_response_list'])) {
+                $model->contractResponseList = [];
+                $n                           = 0;
+                foreach ($map['contract_response_list'] as $item) {
+                    $model->contractResponseList[$n++] = null !== $item ? Contract::fromMap($item) : $item;
+                }
+            }
         }
 
         return $model;
