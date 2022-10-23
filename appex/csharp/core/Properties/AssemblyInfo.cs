111c8d962dbdee573f7032af6f99020c0d2745a8
sdk-team <sdk-team@alibabacloud.com>
2022-10-23 09:31:49 +0000

    Generated SDK for APPEX.

--- appex/csharp/core/Properties/AssemblyInfo.cs
+++ appex/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c11bcba0-3afb-11ed-bd5f-2da805ad692f")]
+[assembly: Guid("7f5330b0-52b5-11ed-9a55-093d3da83661")]
 
 // Version information for an assembly consists of the following four values:
 //
