6ac09da3d3cfa00df87286b992da04552f59bd00
sdk-team <sdk-team@alibabacloud.com>
2022-11-16 12:51:43 +0000

    Generated SDK for ak_16850fb7b8894abc88a6401ef5c9387e.

---
+++ ak_16850fb7b8894abc88a6401ef5c9387e/composer/src/Models/ImportDemoSaasTestTestbRequest.php
@@ -0,0 +1,88 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AntChain\Ak_16850fb7b8894abc88a6401ef5c9387e\Models;
+
+use AlibabaCloud\Tea\Model;
+
+class ImportDemoSaasTestTestbRequest extends Model
+{
+    // OAuth模式下的授权token
+    /**
+     * @var string
+     */
+    public $authToken;
+
+    /**
+     * @var string
+     */
+    public $productInstanceId;
+
+    // 李四
+    /**
+     * @var string
+     */
+    public $name;
+
+    // 18
+    /**
+     * @var int
+     */
+    public $age;
+    protected $_name = [
+        'authToken'         => 'auth_token',
+        'productInstanceId' => 'product_instance_id',
+        'name'              => 'name',
+        'age'               => 'age',
+    ];
+
+    public function validate()
+    {
+        Model::validateRequired('name', $this->name, true);
+        Model::validateRequired('age', $this->age, true);
+    }
+
+    public function toMap()
+    {
+        $res = [];
+        if (null !== $this->authToken) {
+            $res['auth_token'] = $this->authToken;
+        }
+        if (null !== $this->productInstanceId) {
+            $res['product_instance_id'] = $this->productInstanceId;
+        }
+        if (null !== $this->name) {
+            $res['name'] = $this->name;
+        }
+        if (null !== $this->age) {
+            $res['age'] = $this->age;
+        }
+
+        return $res;
+    }
+
+    /**
+     * @param array $map
+     *
+     * @return ImportDemoSaasTestTestbRequest
+     */
+    public static function fromMap($map = [])
+    {
+        $model = new self();
+        if (isset($map['auth_token'])) {
+            $model->authToken = $map['auth_token'];
+        }
+        if (isset($map['product_instance_id'])) {
+            $model->productInstanceId = $map['product_instance_id'];
+        }
+        if (isset($map['name'])) {
+            $model->name = $map['name'];
+        }
+        if (isset($map['age'])) {
+            $model->age = $map['age'];
+        }
+
+        return $model;
+    }
+}
