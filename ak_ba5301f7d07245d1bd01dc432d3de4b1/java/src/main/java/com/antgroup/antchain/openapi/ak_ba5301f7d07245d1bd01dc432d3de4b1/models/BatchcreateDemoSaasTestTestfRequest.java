7d2a23a4f94cfc6ec62edd71552009ef4980f982
sdk-team <sdk-team@alibabacloud.com>
2022-11-17 07:20:24 +0000

    Generated SDK for ak_ba5301f7d07245d1bd01dc432d3de4b1.

---
+++ ak_ba5301f7d07245d1bd01dc432d3de4b1/java/src/main/java/com/antgroup/antchain/openapi/ak_ba5301f7d07245d1bd01dc432d3de4b1/models/BatchcreateDemoSaasTestTestfRequest.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.ak_ba5301f7d07245d1bd01dc432d3de4b1.models;
+
+import com.aliyun.tea.*;
+
+public class BatchcreateDemoSaasTestTestfRequest extends TeaModel {
+    // OAuth模式下的授权token
+    @NameInMap("auth_token")
+    public String authToken;
+
+    @NameInMap("product_instance_id")
+    public String productInstanceId;
+
+    // test
+    @NameInMap("name")
+    @Validation(required = true)
+    public String name;
+
+    public static BatchcreateDemoSaasTestTestfRequest build(java.util.Map<String, ?> map) throws Exception {
+        BatchcreateDemoSaasTestTestfRequest self = new BatchcreateDemoSaasTestTestfRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public BatchcreateDemoSaasTestTestfRequest setAuthToken(String authToken) {
+        this.authToken = authToken;
+        return this;
+    }
+    public String getAuthToken() {
+        return this.authToken;
+    }
+
+    public BatchcreateDemoSaasTestTestfRequest setProductInstanceId(String productInstanceId) {
+        this.productInstanceId = productInstanceId;
+        return this;
+    }
+    public String getProductInstanceId() {
+        return this.productInstanceId;
+    }
+
+    public BatchcreateDemoSaasTestTestfRequest setName(String name) {
+        this.name = name;
+        return this;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+}
