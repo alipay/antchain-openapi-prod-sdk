9f615be816ce615e1f9152c9956f937ec024039c
sdk-team <sdk-team@alibabacloud.com>
2022-11-17 11:12:54 +0000

    Generated SDK for BCCR.

--- bccr/composer/src/Client.php
+++ bccr/composer/src/Client.php
@@ -270,7 +270,9 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.17.13',
+                    'sdk_version'      => '1.17.14',
+                    '_prod_code'       => 'BCCR',
+                    '_prod_channel'    => 'undefined',
                 ];
                 if (!Utils::empty_($this->_securityToken)) {
                     $_request->query['security_token'] = $this->_securityToken;
