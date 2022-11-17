86bf6c77e447b3fca34cdef15cae5c3d1f978574
sdk-team <sdk-team@alibabacloud.com>
2022-11-17 12:39:09 +0000

    Generated SDK for ak_af85f9fd5e88491296f8c6ff584ff090.

---
+++ ak_af85f9fd5e88491296f8c6ff584ff090/csharp/core/Models/QueryAntchainBbpContractReconciliationRequest.cs
@@ -0,0 +1,33 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_af85f9fd5e88491296f8c6ff584ff090.Models
+{
+    public class QueryAntchainBbpContractReconciliationRequest : TeaModel {
+        // OAuth模式下的授权token
+        [NameInMap("auth_token")]
+        [Validation(Required=false)]
+        public string AuthToken { get; set; }
+
+        [NameInMap("product_instance_id")]
+        [Validation(Required=false)]
+        public string ProductInstanceId { get; set; }
+
+        // 供应商code
+        [NameInMap("sup_code")]
+        [Validation(Required=true)]
+        public string SupCode { get; set; }
+
+        // 月份
+        [NameInMap("score_dates")]
+        [Validation(Required=true)]
+        public List<string> ScoreDates { get; set; }
+
+    }
+
+}
