8f6332cff3984763b9f3695fb0312119b0fc840f
sdk-team <sdk-team@alibabacloud.com>
2022-11-17 04:09:02 +0000

    Generated SDK for ak_167ce39da2ad4da286116805d1fe87d9.

--- ak_167ce39da2ad4da286116805d1fe87d9/go/client/client.go
+++ ak_167ce39da2ad4da286116805d1fe87d9/go/client/client.go
@@ -502,6 +502,8 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
 				"sdk_version":      tea.String("1.0.2"),
+				"_prod_code":       tea.String("ak_167ce39da2ad4da286116805d1fe87d9"),
+				"_prod_channel":    tea.String("saas"),
 			}
 			if !tea.BoolValue(util.Empty(client.SecurityToken)) {
 				request_.Query["security_token"] = client.SecurityToken
