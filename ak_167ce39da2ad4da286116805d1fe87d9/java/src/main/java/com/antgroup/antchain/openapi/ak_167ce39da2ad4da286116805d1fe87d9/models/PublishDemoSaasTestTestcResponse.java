e8af46737ba238d99752381340f1d188bbbe18c6
sdk-team <sdk-team@alibabacloud.com>
2022-11-17 04:05:30 +0000

    Generated SDK for ak_167ce39da2ad4da286116805d1fe87d9.

---
+++ ak_167ce39da2ad4da286116805d1fe87d9/java/src/main/java/com/antgroup/antchain/openapi/ak_167ce39da2ad4da286116805d1fe87d9/models/PublishDemoSaasTestTestcResponse.java
@@ -0,0 +1,60 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.ak_167ce39da2ad4da286116805d1fe87d9.models;
+
+import com.aliyun.tea.*;
+
+public class PublishDemoSaasTestTestcResponse extends TeaModel {
+    // 请求唯一ID，用于链路跟踪和问题排查
+    @NameInMap("req_msg_id")
+    public String reqMsgId;
+
+    // 结果码，一般OK表示调用成功
+    @NameInMap("result_code")
+    public String resultCode;
+
+    // 异常信息的文本描述
+    @NameInMap("result_msg")
+    public String resultMsg;
+
+    // 男
+    @NameInMap("sex")
+    public String sex;
+
+    public static PublishDemoSaasTestTestcResponse build(java.util.Map<String, ?> map) throws Exception {
+        PublishDemoSaasTestTestcResponse self = new PublishDemoSaasTestTestcResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public PublishDemoSaasTestTestcResponse setReqMsgId(String reqMsgId) {
+        this.reqMsgId = reqMsgId;
+        return this;
+    }
+    public String getReqMsgId() {
+        return this.reqMsgId;
+    }
+
+    public PublishDemoSaasTestTestcResponse setResultCode(String resultCode) {
+        this.resultCode = resultCode;
+        return this;
+    }
+    public String getResultCode() {
+        return this.resultCode;
+    }
+
+    public PublishDemoSaasTestTestcResponse setResultMsg(String resultMsg) {
+        this.resultMsg = resultMsg;
+        return this;
+    }
+    public String getResultMsg() {
+        return this.resultMsg;
+    }
+
+    public PublishDemoSaasTestTestcResponse setSex(String sex) {
+        this.sex = sex;
+        return this;
+    }
+    public String getSex() {
+        return this.sex;
+    }
+
+}
