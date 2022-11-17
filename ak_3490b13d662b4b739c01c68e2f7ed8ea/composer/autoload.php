2afe316927322bee6aa669ea8bcaaffd54cc421f
sdk-team <sdk-team@alibabacloud.com>
2022-11-17 08:39:38 +0000

    Generated SDK for ak_3490b13d662b4b739c01c68e2f7ed8ea.

---
+++ ak_3490b13d662b4b739c01c68e2f7ed8ea/composer/autoload.php
@@ -0,0 +1,17 @@
+<?php
+
+if (file_exists(__DIR__ . \DIRECTORY_SEPARATOR . 'vendor' . \DIRECTORY_SEPARATOR . 'autoload.php')) {
+    require_once __DIR__ . \DIRECTORY_SEPARATOR . 'vendor' . \DIRECTORY_SEPARATOR . 'autoload.php';
+}
+
+spl_autoload_register(function ($class) {
+    $name = str_replace('AntChain\\Ak_3490b13d662b4b739c01c68e2f7ed8ea\\', '', $class);
+    $file = __DIR__ . \DIRECTORY_SEPARATOR . 'src' . \DIRECTORY_SEPARATOR . str_replace('\\', \DIRECTORY_SEPARATOR, $name) . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+
+        return true;
+    }
+
+    return false;
+});
