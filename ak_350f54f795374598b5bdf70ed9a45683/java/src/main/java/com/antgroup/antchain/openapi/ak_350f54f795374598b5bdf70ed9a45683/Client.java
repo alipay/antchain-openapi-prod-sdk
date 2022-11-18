4c2b7cdf59cd5007e21c850ae6665e98e24e7e60
sdk-team <sdk-team@alibabacloud.com>
2022-11-18 03:16:27 +0000

    Generated SDK for ak_350f54f795374598b5bdf70ed9a45683.

--- ak_350f54f795374598b5bdf70ed9a45683/java/src/main/java/com/antgroup/antchain/openapi/ak_350f54f795374598b5bdf70ed9a45683/Client.java
+++ ak_350f54f795374598b5bdf70ed9a45683/java/src/main/java/com/antgroup/antchain/openapi/ak_350f54f795374598b5bdf70ed9a45683/Client.java
@@ -110,9 +110,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.0.0"),
-                    new TeaPair("_prod_code", "ak_350f54f795374598b5bdf70ed9a45683"),
-                    new TeaPair("_prod_channel", "saas")
+                    new TeaPair("sdk_version", "1.0.2")
                 );
                 if (!com.aliyun.teautil.Common.empty(_securityToken)) {
                     request_.query.put("security_token", _securityToken);
@@ -162,6 +160,25 @@ public class Client {
     }
 
     /**
+     * Description: 自动化测试创建
+     * Summary: 自动化测试创建
+     */
+    public BindDemoAaaBbbCccResponse bindDemoAaaBbbCcc(BindDemoAaaBbbCccRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.bindDemoAaaBbbCccEx(request, headers, runtime);
+    }
+
+    /**
+     * Description: 自动化测试创建
+     * Summary: 自动化测试创建
+     */
+    public BindDemoAaaBbbCccResponse bindDemoAaaBbbCccEx(BindDemoAaaBbbCccRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "demo.aaa.bbb.ccc.bind", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new BindDemoAaaBbbCccResponse());
+    }
+
+    /**
      * Description: testa
      * Summary: 测试用api
      */
@@ -178,5 +195,24 @@ public class Client {
     public QueryDemoSaasTestTestaResponse queryDemoSaasTestTestaEx(QueryDemoSaasTestTestaRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "demo.saas.test.testa.query", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new QueryDemoSaasTestTestaResponse());
+    }
+
+    /**
+     * Description: testB
+     * Summary: 测试用api
+     */
+    public ImportDemoSaasTestTestbResponse importDemoSaasTestTestb(ImportDemoSaasTestTestbRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.importDemoSaasTestTestbEx(request, headers, runtime);
+    }
+
+    /**
+     * Description: testB
+     * Summary: 测试用api
+     */
+    public ImportDemoSaasTestTestbResponse importDemoSaasTestTestbEx(ImportDemoSaasTestTestbRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "demo.saas.test.testb.import", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new ImportDemoSaasTestTestbResponse());
     }
 }
