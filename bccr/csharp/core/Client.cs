0ddec9ac7b6775e7458edf61e72e59c47d40a3fd
sdk-team <sdk-team@alibabacloud.com>
2022-11-18 06:22:16 +0000

    Generated SDK for BCCR.

--- bccr/csharp/core/Client.cs
+++ bccr/csharp/core/Client.cs
@@ -137,9 +137,7 @@ namespace AntChain.SDK.BCCR
                         {"req_msg_id", AntChain.AlipayUtil.AntchainUtils.GetNonce()},
                         {"access_key", _accessKeyId},
                         {"base_sdk_version", "TeaSDK-2.0"},
-                        {"sdk_version", "1.17.14"},
-                        {"_prod_code", "BCCR"},
-                        {"_prod_channel", "undefined"},
+                        {"sdk_version", "1.17.16"},
                     };
                     if (!AlibabaCloud.TeaUtil.Common.Empty(_securityToken))
                     {
@@ -263,9 +261,7 @@ namespace AntChain.SDK.BCCR
                         {"req_msg_id", AntChain.AlipayUtil.AntchainUtils.GetNonce()},
                         {"access_key", _accessKeyId},
                         {"base_sdk_version", "TeaSDK-2.0"},
-                        {"sdk_version", "1.17.14"},
-                        {"_prod_code", "BCCR"},
-                        {"_prod_channel", "undefined"},
+                        {"sdk_version", "1.17.16"},
                     };
                     if (!AlibabaCloud.TeaUtil.Common.Empty(_securityToken))
                     {
--- bccr/csharp/core/Properties/AssemblyInfo.cs
+++ bccr/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c00ddf80-6668-11ed-a098-f58e7efaddd0")]
+[assembly: Guid("5143ae30-6709-11ed-98e4-87d5f909fee3")]
 
 // Version information for an assembly consists of the following four values:
 //
