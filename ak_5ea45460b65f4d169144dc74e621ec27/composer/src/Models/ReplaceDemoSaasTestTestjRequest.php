23c63f5c20a8e7a6657d52ccc087124071328593
sdk-team <sdk-team@alibabacloud.com>
2022-11-18 03:59:04 +0000

    Generated SDK for ak_5ea45460b65f4d169144dc74e621ec27.

---
+++ ak_5ea45460b65f4d169144dc74e621ec27/composer/src/Models/ReplaceDemoSaasTestTestjRequest.php
@@ -0,0 +1,74 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AntChain\Ak_5ea45460b65f4d169144dc74e621ec27\Models;
+
+use AlibabaCloud\Tea\Model;
+
+class ReplaceDemoSaasTestTestjRequest extends Model
+{
+    // OAuth模式下的授权token
+    /**
+     * @var string
+     */
+    public $authToken;
+
+    /**
+     * @var string
+     */
+    public $productInstanceId;
+
+    // test
+    /**
+     * @var string
+     */
+    public $name;
+    protected $_name = [
+        'authToken'         => 'auth_token',
+        'productInstanceId' => 'product_instance_id',
+        'name'              => 'name',
+    ];
+
+    public function validate()
+    {
+        Model::validateRequired('name', $this->name, true);
+    }
+
+    public function toMap()
+    {
+        $res = [];
+        if (null !== $this->authToken) {
+            $res['auth_token'] = $this->authToken;
+        }
+        if (null !== $this->productInstanceId) {
+            $res['product_instance_id'] = $this->productInstanceId;
+        }
+        if (null !== $this->name) {
+            $res['name'] = $this->name;
+        }
+
+        return $res;
+    }
+
+    /**
+     * @param array $map
+     *
+     * @return ReplaceDemoSaasTestTestjRequest
+     */
+    public static function fromMap($map = [])
+    {
+        $model = new self();
+        if (isset($map['auth_token'])) {
+            $model->authToken = $map['auth_token'];
+        }
+        if (isset($map['product_instance_id'])) {
+            $model->productInstanceId = $map['product_instance_id'];
+        }
+        if (isset($map['name'])) {
+            $model->name = $map['name'];
+        }
+
+        return $model;
+    }
+}
