6a5f3c086c0eab21322cc1d83bc876da76cb5228
sdk-team <sdk-team@alibabacloud.com>
2022-11-18 03:14:09 +0000

    Generated SDK for ak_2a208836e8ae421bb0fbcdbd0dab8d14.

--- ak_2a208836e8ae421bb0fbcdbd0dab8d14/python/antchain_sdk_ak_2a208836e8ae421bb0fbcdbd0dab8d14/client.py
+++ ak_2a208836e8ae421bb0fbcdbd0dab8d14/python/antchain_sdk_ak_2a208836e8ae421bb0fbcdbd0dab8d14/client.py
@@ -134,7 +134,7 @@ class Client:
                     'req_msg_id': AntchainUtils.get_nonce(),
                     'access_key': self._access_key_id,
                     'base_sdk_version': 'TeaSDK-2.0',
-                    'sdk_version': '1.0.0'
+                    'sdk_version': '1.0.1'
                 }
                 if not UtilClient.empty(self._security_token):
                     _request.query['security_token'] = self._security_token
@@ -235,7 +235,7 @@ class Client:
                     'req_msg_id': AntchainUtils.get_nonce(),
                     'access_key': self._access_key_id,
                     'base_sdk_version': 'TeaSDK-2.0',
-                    'sdk_version': '1.0.0'
+                    'sdk_version': '1.0.1'
                 }
                 if not UtilClient.empty(self._security_token):
                     _request.query['security_token'] = self._security_token
@@ -323,4 +323,60 @@ class Client:
         return TeaCore.from_map(
             ak__2a_208836e_8ae_421bb_0fbcdbd_0dab_8d_14_models.QueryDemoSaasTestTestaResponse(),
             await self.do_request_async('1.0', 'demo.saas.test.testa.query', 'HTTPS', 'POST', f'/gateway.do', TeaCore.to_map(request), headers, runtime)
+        )
+
+    def import_demo_saas_test_testb(
+        self,
+        request: ak__2a_208836e_8ae_421bb_0fbcdbd_0dab_8d_14_models.ImportDemoSaasTestTestbRequest,
+    ) -> ak__2a_208836e_8ae_421bb_0fbcdbd_0dab_8d_14_models.ImportDemoSaasTestTestbResponse:
+        """
+        Description: testB
+        Summary: 测试用api
+        """
+        runtime = util_models.RuntimeOptions()
+        headers = {}
+        return self.import_demo_saas_test_testb_ex(request, headers, runtime)
+
+    async def import_demo_saas_test_testb_async(
+        self,
+        request: ak__2a_208836e_8ae_421bb_0fbcdbd_0dab_8d_14_models.ImportDemoSaasTestTestbRequest,
+    ) -> ak__2a_208836e_8ae_421bb_0fbcdbd_0dab_8d_14_models.ImportDemoSaasTestTestbResponse:
+        """
+        Description: testB
+        Summary: 测试用api
+        """
+        runtime = util_models.RuntimeOptions()
+        headers = {}
+        return await self.import_demo_saas_test_testb_ex_async(request, headers, runtime)
+
+    def import_demo_saas_test_testb_ex(
+        self,
+        request: ak__2a_208836e_8ae_421bb_0fbcdbd_0dab_8d_14_models.ImportDemoSaasTestTestbRequest,
+        headers: Dict[str, str],
+        runtime: util_models.RuntimeOptions,
+    ) -> ak__2a_208836e_8ae_421bb_0fbcdbd_0dab_8d_14_models.ImportDemoSaasTestTestbResponse:
+        """
+        Description: testB
+        Summary: 测试用api
+        """
+        UtilClient.validate_model(request)
+        return TeaCore.from_map(
+            ak__2a_208836e_8ae_421bb_0fbcdbd_0dab_8d_14_models.ImportDemoSaasTestTestbResponse(),
+            self.do_request('1.0', 'demo.saas.test.testb.import', 'HTTPS', 'POST', f'/gateway.do', TeaCore.to_map(request), headers, runtime)
+        )
+
+    async def import_demo_saas_test_testb_ex_async(
+        self,
+        request: ak__2a_208836e_8ae_421bb_0fbcdbd_0dab_8d_14_models.ImportDemoSaasTestTestbRequest,
+        headers: Dict[str, str],
+        runtime: util_models.RuntimeOptions,
+    ) -> ak__2a_208836e_8ae_421bb_0fbcdbd_0dab_8d_14_models.ImportDemoSaasTestTestbResponse:
+        """
+        Description: testB
+        Summary: 测试用api
+        """
+        UtilClient.validate_model(request)
+        return TeaCore.from_map(
+            ak__2a_208836e_8ae_421bb_0fbcdbd_0dab_8d_14_models.ImportDemoSaasTestTestbResponse(),
+            await self.do_request_async('1.0', 'demo.saas.test.testb.import', 'HTTPS', 'POST', f'/gateway.do', TeaCore.to_map(request), headers, runtime)
         )
