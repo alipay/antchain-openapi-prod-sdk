5b3500653cf5786a16168a93b9f7b3fb4b037823
sdk-team <sdk-team@alibabacloud.com>
2022-11-18 03:36:13 +0000

    Generated SDK for ak_7e01ccbdecc14c008ab7d5a9af95a61f.

---
+++ ak_7e01ccbdecc14c008ab7d5a9af95a61f/csharp/README.md
@@ -0,0 +1,27 @@
+English | [简体中文](README-CN.md)
+
+# Ant Chain ak_7e01ccbdecc14c008ab7d5a9af95a61f SDK for C#
+
+## Installation
+
+### .NET
+
+```bash
+donet add package AntChain.SDK.Ak_7e01ccbdecc14c008ab7d5a9af95a61f
+```
+
+## Issues
+
+[Opening an Issue](https://github.com/alipay/antchain-openapi-prod-sdk/issues/new), Issues not conforming to the guidelines may be closed immediately.
+
+## Changelog
+
+Detailed changes for each release are documented in the [release notes](./ChangeLog.md).
+
+## References
+
+* [Latest Release](https://github.com/alipay/antchain-openapi-prod-sdk/)
+
+## License
+
+[Apache-2.0](http://www.apache.org/licenses/LICENSE-2.0)
---
+++ ak_7e01ccbdecc14c008ab7d5a9af95a61f/csharp/core/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("antchain-ak_7e01ccbdecc14c008ab7d5a9af95a61f")]
+[assembly: AssemblyDescription("Ant Chain ak_7e01ccbdecc14c008ab7d5a9af95a61f SDK Library for .NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ant Chain, Inc")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2226f290-66f2-11ed-a936-a72d6c97e5c2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
