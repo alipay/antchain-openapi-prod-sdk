f8706825cb4a828129ec7a837edeab6b7730092e
sdk-team <sdk-team@alibabacloud.com>
2022-11-18 12:28:27 +0000

    Generated SDK for ak_1fef8815252948ebb01da07898dd0fb2.

--- ak_1fef8815252948ebb01da07898dd0fb2/go/client/client.go
+++ ak_1fef8815252948ebb01da07898dd0fb2/go/client/client.go
@@ -1435,6 +1435,76 @@ func (s *QueryDemoSaasTestTestaResponse) SetSex(v string) *QueryDemoSaasTestTest
 	return s
 }
 
+type ResetDemoSaasTestTestdRequest struct {
+	// OAuth模式下的授权token
+	AuthToken         *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
+	ProductInstanceId *string `json:"product_instance_id,omitempty" xml:"product_instance_id,omitempty"`
+	// test
+	Name *string `json:"name,omitempty" xml:"name,omitempty" require:"true"`
+}
+
+func (s ResetDemoSaasTestTestdRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ResetDemoSaasTestTestdRequest) GoString() string {
+	return s.String()
+}
+
+func (s *ResetDemoSaasTestTestdRequest) SetAuthToken(v string) *ResetDemoSaasTestTestdRequest {
+	s.AuthToken = &v
+	return s
+}
+
+func (s *ResetDemoSaasTestTestdRequest) SetProductInstanceId(v string) *ResetDemoSaasTestTestdRequest {
+	s.ProductInstanceId = &v
+	return s
+}
+
+func (s *ResetDemoSaasTestTestdRequest) SetName(v string) *ResetDemoSaasTestTestdRequest {
+	s.Name = &v
+	return s
+}
+
+type ResetDemoSaasTestTestdResponse struct {
+	// 请求唯一ID，用于链路跟踪和问题排查
+	ReqMsgId *string `json:"req_msg_id,omitempty" xml:"req_msg_id,omitempty"`
+	// 结果码，一般OK表示调用成功
+	ResultCode *string `json:"result_code,omitempty" xml:"result_code,omitempty"`
+	// 异常信息的文本描述
+	ResultMsg *string `json:"result_msg,omitempty" xml:"result_msg,omitempty"`
+	// test
+	Age *string `json:"age,omitempty" xml:"age,omitempty"`
+}
+
+func (s ResetDemoSaasTestTestdResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ResetDemoSaasTestTestdResponse) GoString() string {
+	return s.String()
+}
+
+func (s *ResetDemoSaasTestTestdResponse) SetReqMsgId(v string) *ResetDemoSaasTestTestdResponse {
+	s.ReqMsgId = &v
+	return s
+}
+
+func (s *ResetDemoSaasTestTestdResponse) SetResultCode(v string) *ResetDemoSaasTestTestdResponse {
+	s.ResultCode = &v
+	return s
+}
+
+func (s *ResetDemoSaasTestTestdResponse) SetResultMsg(v string) *ResetDemoSaasTestTestdResponse {
+	s.ResultMsg = &v
+	return s
+}
+
+func (s *ResetDemoSaasTestTestdResponse) SetAge(v string) *ResetDemoSaasTestTestdResponse {
+	s.Age = &v
+	return s
+}
+
 type Client struct {
 	Endpoint                *string
 	RegionId                *string
@@ -1557,7 +1627,9 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.0.1"),
+				"sdk_version":      tea.String("1.0.2"),
+				"_prod_code":       tea.String("ak_1fef8815252948ebb01da07898dd0fb2"),
+				"_prod_channel":    tea.String("saas"),
 			}
 			if !tea.BoolValue(util.Empty(client.SecurityToken)) {
 				request_.Query["security_token"] = client.SecurityToken
@@ -1914,6 +1986,40 @@ func (client *Client) QueryDemoSaasTestTestaEx(request *QueryDemoSaasTestTestaRe
 	}
 	_result = &QueryDemoSaasTestTestaResponse{}
 	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("demo.saas.test.testa.query"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+/**
+ * Description: 共享能力租户流量测试
+ * Summary: 共享能力租户流量测试
+ */
+func (client *Client) ResetDemoSaasTestTestd(request *ResetDemoSaasTestTestdRequest) (_result *ResetDemoSaasTestTestdResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &ResetDemoSaasTestTestdResponse{}
+	_body, _err := client.ResetDemoSaasTestTestdEx(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+/**
+ * Description: 共享能力租户流量测试
+ * Summary: 共享能力租户流量测试
+ */
+func (client *Client) ResetDemoSaasTestTestdEx(request *ResetDemoSaasTestTestdRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *ResetDemoSaasTestTestdResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = &ResetDemoSaasTestTestdResponse{}
+	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("demo.saas.test.testd.reset"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
 	if _err != nil {
 		return _result, _err
 	}
