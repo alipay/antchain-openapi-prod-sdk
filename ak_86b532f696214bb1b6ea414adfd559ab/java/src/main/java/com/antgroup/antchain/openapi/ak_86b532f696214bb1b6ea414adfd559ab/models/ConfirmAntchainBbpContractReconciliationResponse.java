9ffc16b7e3dfffab965bdfe9d65623f2aac35d28
sdk-team <sdk-team@alibabacloud.com>
2022-11-19 02:16:05 +0000

    Generated SDK for ak_86b532f696214bb1b6ea414adfd559ab.

---
+++ ak_86b532f696214bb1b6ea414adfd559ab/java/src/main/java/com/antgroup/antchain/openapi/ak_86b532f696214bb1b6ea414adfd559ab/models/ConfirmAntchainBbpContractReconciliationResponse.java
@@ -0,0 +1,60 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.ak_86b532f696214bb1b6ea414adfd559ab.models;
+
+import com.aliyun.tea.*;
+
+public class ConfirmAntchainBbpContractReconciliationResponse extends TeaModel {
+    // 请求唯一ID，用于链路跟踪和问题排查
+    @NameInMap("req_msg_id")
+    public String reqMsgId;
+
+    // 结果码，一般OK表示调用成功
+    @NameInMap("result_code")
+    public String resultCode;
+
+    // 异常信息的文本描述
+    @NameInMap("result_msg")
+    public String resultMsg;
+
+    // 结算单
+    @NameInMap("reconciliation")
+    public Reconciliation reconciliation;
+
+    public static ConfirmAntchainBbpContractReconciliationResponse build(java.util.Map<String, ?> map) throws Exception {
+        ConfirmAntchainBbpContractReconciliationResponse self = new ConfirmAntchainBbpContractReconciliationResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public ConfirmAntchainBbpContractReconciliationResponse setReqMsgId(String reqMsgId) {
+        this.reqMsgId = reqMsgId;
+        return this;
+    }
+    public String getReqMsgId() {
+        return this.reqMsgId;
+    }
+
+    public ConfirmAntchainBbpContractReconciliationResponse setResultCode(String resultCode) {
+        this.resultCode = resultCode;
+        return this;
+    }
+    public String getResultCode() {
+        return this.resultCode;
+    }
+
+    public ConfirmAntchainBbpContractReconciliationResponse setResultMsg(String resultMsg) {
+        this.resultMsg = resultMsg;
+        return this;
+    }
+    public String getResultMsg() {
+        return this.resultMsg;
+    }
+
+    public ConfirmAntchainBbpContractReconciliationResponse setReconciliation(Reconciliation reconciliation) {
+        this.reconciliation = reconciliation;
+        return this;
+    }
+    public Reconciliation getReconciliation() {
+        return this.reconciliation;
+    }
+
+}
---
+++ ak_86b532f696214bb1b6ea414adfd559ab/java/src/main/java/com/antgroup/antchain/openapi/ak_86b532f696214bb1b6ea414adfd559ab/models/Reconciliation.java
@@ -0,0 +1,142 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.ak_86b532f696214bb1b6ea414adfd559ab.models;
+
+import com.aliyun.tea.*;
+
+public class Reconciliation extends TeaModel {
+    // 供应商
+    @NameInMap("sup_code")
+    @Validation(required = true)
+    public String supCode;
+
+    // 供应商名称
+    @NameInMap("sup_name")
+    @Validation(required = true)
+    public String supName;
+
+    // 结算金额(单位分)
+    @NameInMap("settlement_amount")
+    @Validation(required = true)
+    public Long settlementAmount;
+
+    // 长城确认时间
+    @NameInMap("confirm_date_gw")
+    @Validation(required = true)
+    public String confirmDateGw;
+
+    // 长城方确认人
+    @NameInMap("confirmer_gw")
+    @Validation(required = true)
+    public String confirmerGw;
+
+    // 长城确认状态
+    @NameInMap("confirm_state_gw")
+    @Validation(required = true)
+    public String confirmStateGw;
+
+    // 服务方确认时间
+    @NameInMap("confirm_date_sup")
+    @Validation(required = true)
+    public String confirmDateSup;
+
+    // 服务方确认人
+    @NameInMap("confirmer_sup")
+    @Validation(required = true)
+    public String confirmerSup;
+
+    // 服务方确认状态
+    @NameInMap("confirm_state_sup")
+    @Validation(required = true)
+    public String confirmStateSup;
+
+    // 计算时间
+    @NameInMap("socre_date")
+    @Validation(required = true)
+    public String socreDate;
+
+    public static Reconciliation build(java.util.Map<String, ?> map) throws Exception {
+        Reconciliation self = new Reconciliation();
+        return TeaModel.build(map, self);
+    }
+
+    public Reconciliation setSupCode(String supCode) {
+        this.supCode = supCode;
+        return this;
+    }
+    public String getSupCode() {
+        return this.supCode;
+    }
+
+    public Reconciliation setSupName(String supName) {
+        this.supName = supName;
+        return this;
+    }
+    public String getSupName() {
+        return this.supName;
+    }
+
+    public Reconciliation setSettlementAmount(Long settlementAmount) {
+        this.settlementAmount = settlementAmount;
+        return this;
+    }
+    public Long getSettlementAmount() {
+        return this.settlementAmount;
+    }
+
+    public Reconciliation setConfirmDateGw(String confirmDateGw) {
+        this.confirmDateGw = confirmDateGw;
+        return this;
+    }
+    public String getConfirmDateGw() {
+        return this.confirmDateGw;
+    }
+
+    public Reconciliation setConfirmerGw(String confirmerGw) {
+        this.confirmerGw = confirmerGw;
+        return this;
+    }
+    public String getConfirmerGw() {
+        return this.confirmerGw;
+    }
+
+    public Reconciliation setConfirmStateGw(String confirmStateGw) {
+        this.confirmStateGw = confirmStateGw;
+        return this;
+    }
+    public String getConfirmStateGw() {
+        return this.confirmStateGw;
+    }
+
+    public Reconciliation setConfirmDateSup(String confirmDateSup) {
+        this.confirmDateSup = confirmDateSup;
+        return this;
+    }
+    public String getConfirmDateSup() {
+        return this.confirmDateSup;
+    }
+
+    public Reconciliation setConfirmerSup(String confirmerSup) {
+        this.confirmerSup = confirmerSup;
+        return this;
+    }
+    public String getConfirmerSup() {
+        return this.confirmerSup;
+    }
+
+    public Reconciliation setConfirmStateSup(String confirmStateSup) {
+        this.confirmStateSup = confirmStateSup;
+        return this;
+    }
+    public String getConfirmStateSup() {
+        return this.confirmStateSup;
+    }
+
+    public Reconciliation setSocreDate(String socreDate) {
+        this.socreDate = socreDate;
+        return this;
+    }
+    public String getSocreDate() {
+        return this.socreDate;
+    }
+
+}
