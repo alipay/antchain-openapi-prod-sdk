37d4517509ee34a87103ac8ded5bed35e3450007
sdk-team <sdk-team@alibabacloud.com>
2022-11-21 08:00:06 +0000

    Generated SDK for ak_ff19a88a999c4b3587aa011598273c77.

---
+++ ak_ff19a88a999c4b3587aa011598273c77/java/src/main/java/com/antgroup/antchain/openapi/ak_ff19a88a999c4b3587aa011598273c77/models/QueryMap.java
@@ -0,0 +1,37 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.ak_ff19a88a999c4b3587aa011598273c77.models;
+
+import com.aliyun.tea.*;
+
+public class QueryMap extends TeaModel {
+    // 键值
+    @NameInMap("name")
+    @Validation(required = true)
+    public String name;
+
+    // 额外用户信息
+    @NameInMap("value")
+    public java.util.List<NameValuePair> value;
+
+    public static QueryMap build(java.util.Map<String, ?> map) throws Exception {
+        QueryMap self = new QueryMap();
+        return TeaModel.build(map, self);
+    }
+
+    public QueryMap setName(String name) {
+        this.name = name;
+        return this;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public QueryMap setValue(java.util.List<NameValuePair> value) {
+        this.value = value;
+        return this;
+    }
+    public java.util.List<NameValuePair> getValue() {
+        return this.value;
+    }
+
+}
