8aa7dca68dc7f5f057aa53aa07c80f93f5d96f67
sdk-team <sdk-team@alibabacloud.com>
2022-11-21 08:00:03 +0000

    Generated SDK for ak_ff19a88a999c4b3587aa011598273c77.

---
+++ ak_ff19a88a999c4b3587aa011598273c77/csharp/core/Models/NameValuePair.cs
@@ -0,0 +1,25 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_ff19a88a999c4b3587aa011598273c77.Models
+{
+    // 键值对，兼容map用
+    public class NameValuePair : TeaModel {
+        // 键名
+        [NameInMap("name")]
+        [Validation(Required=true)]
+        public string Name { get; set; }
+
+        // 键值
+        [NameInMap("value")]
+        [Validation(Required=true)]
+        public string Value { get; set; }
+
+    }
+
+}
