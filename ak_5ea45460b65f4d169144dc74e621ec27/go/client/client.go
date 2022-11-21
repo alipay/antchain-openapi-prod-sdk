d3ff517451e42a58223034e6500c3f89fef83541
sdk-team <sdk-team@alibabacloud.com>
2022-11-21 06:45:09 +0000

    Generated SDK for ak_5ea45460b65f4d169144dc74e621ec27.

--- ak_5ea45460b65f4d169144dc74e621ec27/go/client/client.go
+++ ak_5ea45460b65f4d169144dc74e621ec27/go/client/client.go
@@ -218,76 +218,6 @@ func (s *ReplaceDemoSaasTestTestjResponse) SetAge(v string) *ReplaceDemoSaasTest
 	return s
 }
 
-type SendDemoSaasTestTestkRequest struct {
-	// OAuth模式下的授权token
-	AuthToken         *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
-	ProductInstanceId *string `json:"product_instance_id,omitempty" xml:"product_instance_id,omitempty"`
-	// test
-	Age *string `json:"age,omitempty" xml:"age,omitempty" require:"true"`
-}
-
-func (s SendDemoSaasTestTestkRequest) String() string {
-	return tea.Prettify(s)
-}
-
-func (s SendDemoSaasTestTestkRequest) GoString() string {
-	return s.String()
-}
-
-func (s *SendDemoSaasTestTestkRequest) SetAuthToken(v string) *SendDemoSaasTestTestkRequest {
-	s.AuthToken = &v
-	return s
-}
-
-func (s *SendDemoSaasTestTestkRequest) SetProductInstanceId(v string) *SendDemoSaasTestTestkRequest {
-	s.ProductInstanceId = &v
-	return s
-}
-
-func (s *SendDemoSaasTestTestkRequest) SetAge(v string) *SendDemoSaasTestTestkRequest {
-	s.Age = &v
-	return s
-}
-
-type SendDemoSaasTestTestkResponse struct {
-	// 请求唯一ID，用于链路跟踪和问题排查
-	ReqMsgId *string `json:"req_msg_id,omitempty" xml:"req_msg_id,omitempty"`
-	// 结果码，一般OK表示调用成功
-	ResultCode *string `json:"result_code,omitempty" xml:"result_code,omitempty"`
-	// 异常信息的文本描述
-	ResultMsg *string `json:"result_msg,omitempty" xml:"result_msg,omitempty"`
-	// test
-	Sex *string `json:"sex,omitempty" xml:"sex,omitempty"`
-}
-
-func (s SendDemoSaasTestTestkResponse) String() string {
-	return tea.Prettify(s)
-}
-
-func (s SendDemoSaasTestTestkResponse) GoString() string {
-	return s.String()
-}
-
-func (s *SendDemoSaasTestTestkResponse) SetReqMsgId(v string) *SendDemoSaasTestTestkResponse {
-	s.ReqMsgId = &v
-	return s
-}
-
-func (s *SendDemoSaasTestTestkResponse) SetResultCode(v string) *SendDemoSaasTestTestkResponse {
-	s.ResultCode = &v
-	return s
-}
-
-func (s *SendDemoSaasTestTestkResponse) SetResultMsg(v string) *SendDemoSaasTestTestkResponse {
-	s.ResultMsg = &v
-	return s
-}
-
-func (s *SendDemoSaasTestTestkResponse) SetSex(v string) *SendDemoSaasTestTestkResponse {
-	s.Sex = &v
-	return s
-}
-
 type Client struct {
 	Endpoint                *string
 	RegionId                *string
@@ -410,7 +340,9 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.0.0"),
+				"sdk_version":      tea.String("1.0.1"),
+				"_prod_code":       tea.String("ak_5ea45460b65f4d169144dc74e621ec27"),
+				"_prod_channel":    tea.String("saas"),
 			}
 			if !tea.BoolValue(util.Empty(client.SecurityToken)) {
 				request_.Query["security_token"] = client.SecurityToken
@@ -493,40 +425,6 @@ func (client *Client) ReplaceDemoSaasTestTestjEx(request *ReplaceDemoSaasTestTes
 	}
 	_result = &ReplaceDemoSaasTestTestjResponse{}
 	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("demo.saas.test.testj.replace"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
-	if _err != nil {
-		return _result, _err
-	}
-	_err = tea.Convert(_body, &_result)
-	return _result, _err
-}
-
-/**
- * Description: 共享能力租户流量测试
- * Summary: 共享能力租户流量测试
- */
-func (client *Client) SendDemoSaasTestTestk(request *SendDemoSaasTestTestkRequest) (_result *SendDemoSaasTestTestkResponse, _err error) {
-	runtime := &util.RuntimeOptions{}
-	headers := make(map[string]*string)
-	_result = &SendDemoSaasTestTestkResponse{}
-	_body, _err := client.SendDemoSaasTestTestkEx(request, headers, runtime)
-	if _err != nil {
-		return _result, _err
-	}
-	_result = _body
-	return _result, _err
-}
-
-/**
- * Description: 共享能力租户流量测试
- * Summary: 共享能力租户流量测试
- */
-func (client *Client) SendDemoSaasTestTestkEx(request *SendDemoSaasTestTestkRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *SendDemoSaasTestTestkResponse, _err error) {
-	_err = util.ValidateModel(request)
-	if _err != nil {
-		return _result, _err
-	}
-	_result = &SendDemoSaasTestTestkResponse{}
-	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("demo.saas.test.testk.send"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
 	if _err != nil {
 		return _result, _err
 	}
