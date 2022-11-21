b0e0ae03a8b3f5e70f3cdf8c77c1260fbba97a67
sdk-team <sdk-team@alibabacloud.com>
2022-11-21 06:57:16 +0000

    Generated SDK for ak_10abee70bc4f4d4d90334ebfb4895c12.

---
+++ ak_10abee70bc4f4d4d90334ebfb4895c12/csharp/core/Models/QueryDemoSaasTestTestaResponse.cs
@@ -0,0 +1,34 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_10abee70bc4f4d4d90334ebfb4895c12.Models
+{
+    public class QueryDemoSaasTestTestaResponse : TeaModel {
+        // 请求唯一ID，用于链路跟踪和问题排查
+        [NameInMap("req_msg_id")]
+        [Validation(Required=false)]
+        public string ReqMsgId { get; set; }
+
+        // 结果码，一般OK表示调用成功
+        [NameInMap("result_code")]
+        [Validation(Required=false)]
+        public string ResultCode { get; set; }
+
+        // 异常信息的文本描述
+        [NameInMap("result_msg")]
+        [Validation(Required=false)]
+        public string ResultMsg { get; set; }
+
+        // 男
+        [NameInMap("sex")]
+        [Validation(Required=false)]
+        public string Sex { get; set; }
+
+    }
+
+}
---
+++ ak_10abee70bc4f4d4d90334ebfb4895c12/csharp/core/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("antchain-ak_10abee70bc4f4d4d90334ebfb4895c12")]
+[assembly: AssemblyDescription("Ant Chain ak_10abee70bc4f4d4d90334ebfb4895c12 SDK Library for .NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ant Chain, Inc")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b75559a0-6969-11ed-bc87-b36078d1d2fa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
