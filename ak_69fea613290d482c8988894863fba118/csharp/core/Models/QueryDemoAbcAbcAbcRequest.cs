d6e4b5e4ba10a555fc0f1ceb2cc668f5b64c8d97
sdk-team <sdk-team@alibabacloud.com>
2022-11-22 09:52:05 +0000

    Generated SDK for ak_69fea613290d482c8988894863fba118.

---
+++ ak_69fea613290d482c8988894863fba118/csharp/core/Models/QueryDemoAbcAbcAbcRequest.cs
@@ -0,0 +1,23 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_69fea613290d482c8988894863fba118.Models
+{
+    public class QueryDemoAbcAbcAbcRequest : TeaModel {
+        // OAuth模式下的授权token
+        [NameInMap("auth_token")]
+        [Validation(Required=false)]
+        public string AuthToken { get; set; }
+
+        [NameInMap("product_instance_id")]
+        [Validation(Required=false)]
+        public string ProductInstanceId { get; set; }
+
+    }
+
+}
