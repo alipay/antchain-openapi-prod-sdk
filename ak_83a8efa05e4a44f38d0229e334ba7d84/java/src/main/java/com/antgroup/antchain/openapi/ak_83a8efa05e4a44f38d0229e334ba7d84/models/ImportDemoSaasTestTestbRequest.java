b663d7ce38c3020f1844bb5a2bd839005ff0f319
sdk-team <sdk-team@alibabacloud.com>
2022-11-22 07:34:18 +0000

    Generated SDK for ak_83a8efa05e4a44f38d0229e334ba7d84.

---
+++ ak_83a8efa05e4a44f38d0229e334ba7d84/java/src/main/java/com/antgroup/antchain/openapi/ak_83a8efa05e4a44f38d0229e334ba7d84/models/ImportDemoSaasTestTestbRequest.java
@@ -0,0 +1,61 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.ak_83a8efa05e4a44f38d0229e334ba7d84.models;
+
+import com.aliyun.tea.*;
+
+public class ImportDemoSaasTestTestbRequest extends TeaModel {
+    // OAuth模式下的授权token
+    @NameInMap("auth_token")
+    public String authToken;
+
+    @NameInMap("product_instance_id")
+    public String productInstanceId;
+
+    // 李四
+    @NameInMap("name")
+    @Validation(required = true)
+    public String name;
+
+    // 18
+    @NameInMap("age")
+    @Validation(required = true)
+    public Long age;
+
+    public static ImportDemoSaasTestTestbRequest build(java.util.Map<String, ?> map) throws Exception {
+        ImportDemoSaasTestTestbRequest self = new ImportDemoSaasTestTestbRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public ImportDemoSaasTestTestbRequest setAuthToken(String authToken) {
+        this.authToken = authToken;
+        return this;
+    }
+    public String getAuthToken() {
+        return this.authToken;
+    }
+
+    public ImportDemoSaasTestTestbRequest setProductInstanceId(String productInstanceId) {
+        this.productInstanceId = productInstanceId;
+        return this;
+    }
+    public String getProductInstanceId() {
+        return this.productInstanceId;
+    }
+
+    public ImportDemoSaasTestTestbRequest setName(String name) {
+        this.name = name;
+        return this;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public ImportDemoSaasTestTestbRequest setAge(Long age) {
+        this.age = age;
+        return this;
+    }
+    public Long getAge() {
+        return this.age;
+    }
+
+}
