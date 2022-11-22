9df4ab1de3923e8cdaeb9b5992c83026232a1453
sdk-team <sdk-team@alibabacloud.com>
2022-11-22 02:51:26 +0000

    Generated SDK for REALPERSON.

--- realperson/nodejs/src/client.ts
+++ realperson/nodejs/src/client.ts
@@ -1568,6 +1568,315 @@ export class QueryThreemetaOnlinetimeResponse extends $tea.Model {
   }
 }
 
+export class InitFacevrfZimRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  // 业务参数
+  bizData?: string;
+  // 渠道
+  channel?: string;
+  // 商户
+  merchant?: string;
+  // 环境参数
+  metaInfo: string;
+  // 产品节点
+  produceNode?: string;
+  // 产品名称
+  productName?: string;
+  // 实人认证id
+  // 
+  zimId: string;
+  // 身份信息参数
+  // 
+  zimPrincipal?: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+      bizData: 'biz_data',
+      channel: 'channel',
+      merchant: 'merchant',
+      metaInfo: 'meta_info',
+      produceNode: 'produce_node',
+      productName: 'product_name',
+      zimId: 'zim_id',
+      zimPrincipal: 'zim_principal',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+      bizData: 'string',
+      channel: 'string',
+      merchant: 'string',
+      metaInfo: 'string',
+      produceNode: 'string',
+      productName: 'string',
+      zimId: 'string',
+      zimPrincipal: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class InitFacevrfZimResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  // 预留扩展结果
+  // 
+  extParams?: string;
+  // 描述
+  message?: string;
+  // 协议
+  protocol?: string;
+  // 返回码
+  retCode?: string;
+  // 明细返回码
+  retCodeSub?: string;
+  // 明细返回码对应的文案
+  retMessageSub?: string;
+  // 实人认证id
+  zimId?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+      extParams: 'ext_params',
+      message: 'message',
+      protocol: 'protocol',
+      retCode: 'ret_code',
+      retCodeSub: 'ret_code_sub',
+      retMessageSub: 'ret_message_sub',
+      zimId: 'zim_id',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+      extParams: 'string',
+      message: 'string',
+      protocol: 'string',
+      retCode: 'string',
+      retCodeSub: 'string',
+      retMessageSub: 'string',
+      zimId: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class VerifyFacevrfZimRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  // 扩展信息,Map的json格式
+  externParam?: string;
+  // 人脸业务参数
+  zimData: string;
+  // 7501f6c2dd57e06fe0d4202d3aaab58e
+  zimId: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+      externParam: 'extern_param',
+      zimData: 'zim_data',
+      zimId: 'zim_id',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+      externParam: 'string',
+      zimData: 'string',
+      zimId: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class VerifyFacevrfZimResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  // 预留扩展结果
+  extParams?: string;
+  // ""
+  hasNext?: string;
+  // ""
+  nextProtocol?: string;
+  // 产品返回明细码
+  productRetCode?: string;
+  // 明细返回码
+  retCodeSub?: string;
+  // 明细返回码对应的文案
+  retMessageSub?: string;
+  // 验证返回明细码
+  validationRetCode?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+      extParams: 'ext_params',
+      hasNext: 'has_next',
+      nextProtocol: 'next_protocol',
+      productRetCode: 'product_ret_code',
+      retCodeSub: 'ret_code_sub',
+      retMessageSub: 'ret_message_sub',
+      validationRetCode: 'validation_ret_code',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+      extParams: 'string',
+      hasNext: 'string',
+      nextProtocol: 'string',
+      productRetCode: 'string',
+      retCodeSub: 'string',
+      retMessageSub: 'string',
+      validationRetCode: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class RecognizeDocIndividualcardRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  // 长度不超过32位的0-9A-Za-z字符串。
+  // 外部请求ID，由调用方自行生成并自行保证唯一，以便问题定位。
+  outOrderNo: string;
+  // 待识别的卡类型。取值约束：ID_CARD（身份证）;EEP_TO_ML_CARD（港澳来往大陆通行证）;BANK_CARD（银行卡）
+  cardType: string;
+  // 传入的图片是base64编码的图片还是图片的URL。取值约束：BASE64（类型为base64）；URL（类型为URL）
+  dataType: string;
+  // 传入的图片的具体内容，需要与data_type的选择保持一致。
+  dataContent: string;
+  // 入参data_content是否经公钥RSA加密。不填默认不加密。取值约束：0（不加密）；1（加密）
+  reqEncType?: string;
+  // 出参ocr_info是否经AES加密。不填默认不加密。取值约束：0（不加密）；1（加密）
+  respEncType?: string;
+  // 经过公钥RSA加密的AES密钥，用于对出参ocr_info加密。当resp_enc_type =1时必填。
+  respEncToken?: string;
+  // 扩展信息JSON串。
+  externParam?: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+      outOrderNo: 'out_order_no',
+      cardType: 'card_type',
+      dataType: 'data_type',
+      dataContent: 'data_content',
+      reqEncType: 'req_enc_type',
+      respEncType: 'resp_enc_type',
+      respEncToken: 'resp_enc_token',
+      externParam: 'extern_param',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+      outOrderNo: 'string',
+      cardType: 'string',
+      dataType: 'string',
+      dataContent: 'string',
+      reqEncType: 'string',
+      respEncType: 'string',
+      respEncToken: 'string',
+      externParam: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class RecognizeDocIndividualcardResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  // 返回结果码
+  retCode?: string;
+  // 错误码
+  retCodeSub?: string;
+  // 错误信息
+  retMessageSub?: string;
+  // 识别结果，为JSON串。如果入参resp_enc_type=1则是经过AES加密后的JSON串。
+  ocrInfo?: string;
+  // 扩展信息JSON串。
+  extInfo?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+      retCode: 'ret_code',
+      retCodeSub: 'ret_code_sub',
+      retMessageSub: 'ret_message_sub',
+      ocrInfo: 'ocr_info',
+      extInfo: 'ext_info',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+      retCode: 'string',
+      retCodeSub: 'string',
+      retMessageSub: 'string',
+      ocrInfo: 'string',
+      extInfo: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class CreateAntcloudGatewayxFileUploadRequest extends $tea.Model {
   // OAuth模式下的授权token
   authToken?: string;
@@ -1769,7 +2078,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.8.0",
+          sdk_version: "1.9.0",
           _prod_code: "REALPERSON",
           _prod_channel: "undefined",
         };
@@ -2140,6 +2449,63 @@ export default class Client {
   async queryThreemetaOnlinetimeEx(request: QueryThreemetaOnlinetimeRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<QueryThreemetaOnlinetimeResponse> {
     Util.validateModel(request);
     return $tea.cast<QueryThreemetaOnlinetimeResponse>(await this.doRequest("1.0", "di.realperson.threemeta.onlinetime.query", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new QueryThreemetaOnlinetimeResponse({}));
+  }
+
+  /**
+   * Description: 客户端初始化认证(OEM专用)
+   * Summary: 客户端初始化认证(OEM专用)
+   */
+  async initFacevrfZim(request: InitFacevrfZimRequest): Promise<InitFacevrfZimResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.initFacevrfZimEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 客户端初始化认证(OEM专用)
+   * Summary: 客户端初始化认证(OEM专用)
+   */
+  async initFacevrfZimEx(request: InitFacevrfZimRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<InitFacevrfZimResponse> {
+    Util.validateModel(request);
+    return $tea.cast<InitFacevrfZimResponse>(await this.doRequest("1.0", "di.realperson.facevrf.zim.init", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new InitFacevrfZimResponse({}));
+  }
+
+  /**
+   * Description: 客户端人脸验证(OEM专用)
+   * Summary: 客户端人脸验证(OEM专用)
+   */
+  async verifyFacevrfZim(request: VerifyFacevrfZimRequest): Promise<VerifyFacevrfZimResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.verifyFacevrfZimEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 客户端人脸验证(OEM专用)
+   * Summary: 客户端人脸验证(OEM专用)
+   */
+  async verifyFacevrfZimEx(request: VerifyFacevrfZimRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<VerifyFacevrfZimResponse> {
+    Util.validateModel(request);
+    return $tea.cast<VerifyFacevrfZimResponse>(await this.doRequest("1.0", "di.realperson.facevrf.zim.verify", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new VerifyFacevrfZimResponse({}));
+  }
+
+  /**
+   * Description: 卡证OCR
+   * Summary: 卡证OCR
+   */
+  async recognizeDocIndividualcard(request: RecognizeDocIndividualcardRequest): Promise<RecognizeDocIndividualcardResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.recognizeDocIndividualcardEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 卡证OCR
+   * Summary: 卡证OCR
+   */
+  async recognizeDocIndividualcardEx(request: RecognizeDocIndividualcardRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<RecognizeDocIndividualcardResponse> {
+    Util.validateModel(request);
+    return $tea.cast<RecognizeDocIndividualcardResponse>(await this.doRequest("1.0", "di.realperson.doc.individualcard.recognize", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new RecognizeDocIndividualcardResponse({}));
   }
 
   /**
