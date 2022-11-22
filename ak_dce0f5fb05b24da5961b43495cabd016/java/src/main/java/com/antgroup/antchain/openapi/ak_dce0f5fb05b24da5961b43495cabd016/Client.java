c90a8a5c0632fdc7c7a7d06843724bd6808af819
sdk-team <sdk-team@alibabacloud.com>
2022-11-22 08:06:26 +0000

    Generated SDK for ak_dce0f5fb05b24da5961b43495cabd016.

--- ak_dce0f5fb05b24da5961b43495cabd016/java/src/main/java/com/antgroup/antchain/openapi/ak_dce0f5fb05b24da5961b43495cabd016/Client.java
+++ ak_dce0f5fb05b24da5961b43495cabd016/java/src/main/java/com/antgroup/antchain/openapi/ak_dce0f5fb05b24da5961b43495cabd016/Client.java
@@ -110,7 +110,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.0.0"),
+                    new TeaPair("sdk_version", "1.0.1"),
                     new TeaPair("_prod_code", "ak_dce0f5fb05b24da5961b43495cabd016"),
                     new TeaPair("_prod_channel", "saas")
                 );
@@ -159,6 +159,25 @@ public class Client {
         }
 
         throw new TeaUnretryableException(_lastRequest, _lastException);
+    }
+
+    /**
+     * Description: 自动化测试创建
+     * Summary: 自动化测试创建
+     */
+    public BindDemoAaaBbbCccResponse bindDemoAaaBbbCcc(BindDemoAaaBbbCccRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.bindDemoAaaBbbCccEx(request, headers, runtime);
+    }
+
+    /**
+     * Description: 自动化测试创建
+     * Summary: 自动化测试创建
+     */
+    public BindDemoAaaBbbCccResponse bindDemoAaaBbbCccEx(BindDemoAaaBbbCccRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "demo.aaa.bbb.ccc.bind", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new BindDemoAaaBbbCccResponse());
     }
 
     /**
