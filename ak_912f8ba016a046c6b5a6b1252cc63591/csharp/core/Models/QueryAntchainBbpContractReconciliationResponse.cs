235aa1618337289f24859cb89f775b8470546222
sdk-team <sdk-team@alibabacloud.com>
2022-11-22 13:57:09 +0000

    Generated SDK for ak_912f8ba016a046c6b5a6b1252cc63591.

---
+++ ak_912f8ba016a046c6b5a6b1252cc63591/csharp/core/Models/QueryAntchainBbpContractReconciliationResponse.cs
@@ -0,0 +1,34 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_912f8ba016a046c6b5a6b1252cc63591.Models
+{
+    public class QueryAntchainBbpContractReconciliationResponse : TeaModel {
+        // 请求唯一ID，用于链路跟踪和问题排查
+        [NameInMap("req_msg_id")]
+        [Validation(Required=false)]
+        public string ReqMsgId { get; set; }
+
+        // 结果码，一般OK表示调用成功
+        [NameInMap("result_code")]
+        [Validation(Required=false)]
+        public string ResultCode { get; set; }
+
+        // 异常信息的文本描述
+        [NameInMap("result_msg")]
+        [Validation(Required=false)]
+        public string ResultMsg { get; set; }
+
+        // 结算单
+        [NameInMap("reconciliations")]
+        [Validation(Required=false)]
+        public List<Reconciliation> Reconciliations { get; set; }
+
+    }
+
+}
---
+++ ak_912f8ba016a046c6b5a6b1252cc63591/csharp/core/Models/SpProvider.cs
@@ -0,0 +1,65 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_912f8ba016a046c6b5a6b1252cc63591.Models
+{
+    // 供应商
+    public class SpProvider : TeaModel {
+        // 供应商编号
+        [NameInMap("sup_code")]
+        [Validation(Required=true)]
+        public string SupCode { get; set; }
+
+        // 供应商名称
+        [NameInMap("sup_name")]
+        [Validation(Required=true)]
+        public string SupName { get; set; }
+
+        // 文件名称
+        [NameInMap("file_name")]
+        [Validation(Required=true)]
+        public string FileName { get; set; }
+
+        // 文件code
+        [NameInMap("file_code")]
+        [Validation(Required=true)]
+        public string FileCode { get; set; }
+
+        // hash的值
+        [NameInMap("hash_code")]
+        [Validation(Required=true)]
+        public string HashCode { get; set; }
+
+        // 开始时间
+        [NameInMap("start_date")]
+        [Validation(Required=true)]
+        public string StartDate { get; set; }
+
+        // 结束日期
+        [NameInMap("closing_date")]
+        [Validation(Required=true)]
+        public string ClosingDate { get; set; }
+
+        // 文件路径
+        [NameInMap("file_path")]
+        [Validation(Required=true)]
+        public string FilePath { get; set; }
+
+        // 2021-03-04 11:16:23
+        [NameInMap("create_date")]
+        [Validation(Required=true)]
+        public string CreateDate { get; set; }
+
+        // 创建者
+        [NameInMap("creator")]
+        [Validation(Required=true)]
+        public string Creator { get; set; }
+
+    }
+
+}
