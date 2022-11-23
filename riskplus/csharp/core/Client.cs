761a13b61277e119955ea2425962e4d08537aa5f
sdk-team <sdk-team@alibabacloud.com>
2022-11-23 02:38:20 +0000

    Generated SDK for RISKPLUS.

--- riskplus/csharp/core/Client.cs
+++ riskplus/csharp/core/Client.cs
@@ -138,6 +138,8 @@ namespace AntChain.SDK.RISKPLUS
                         {"access_key", _accessKeyId},
                         {"base_sdk_version", "TeaSDK-2.0"},
                         {"sdk_version", "1.15.4"},
+                        {"_prod_code", "RISKPLUS"},
+                        {"_prod_channel", "undefined"},
                     };
                     if (!AlibabaCloud.TeaUtil.Common.Empty(_securityToken))
                     {
@@ -262,6 +264,8 @@ namespace AntChain.SDK.RISKPLUS
                         {"access_key", _accessKeyId},
                         {"base_sdk_version", "TeaSDK-2.0"},
                         {"sdk_version", "1.15.4"},
+                        {"_prod_code", "RISKPLUS"},
+                        {"_prod_channel", "undefined"},
                     };
                     if (!AlibabaCloud.TeaUtil.Common.Empty(_securityToken))
                     {
--- riskplus/csharp/core/Properties/AssemblyInfo.cs
+++ riskplus/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b700a910-6a5d-11ed-9125-2129dec4e501")]
+[assembly: Guid("d751d250-6ad7-11ed-b2a6-396fd3727d42")]
 
 // Version information for an assembly consists of the following four values:
 //
