28a4990964631c7e36a8e46db755bf665ca076da
sdk-team <sdk-team@alibabacloud.com>
2022-11-27 02:16:46 +0000

    Generated SDK for ak_ad0d19a6b6a04a689ab86c58c9e7c71e.

---
+++ ak_ad0d19a6b6a04a689ab86c58c9e7c71e/composer/autoload.php
@@ -0,0 +1,17 @@
+<?php
+
+if (file_exists(__DIR__ . \DIRECTORY_SEPARATOR . 'vendor' . \DIRECTORY_SEPARATOR . 'autoload.php')) {
+    require_once __DIR__ . \DIRECTORY_SEPARATOR . 'vendor' . \DIRECTORY_SEPARATOR . 'autoload.php';
+}
+
+spl_autoload_register(function ($class) {
+    $name = str_replace('AntChain\\Ak_ad0d19a6b6a04a689ab86c58c9e7c71e\\', '', $class);
+    $file = __DIR__ . \DIRECTORY_SEPARATOR . 'src' . \DIRECTORY_SEPARATOR . str_replace('\\', \DIRECTORY_SEPARATOR, $name) . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+
+        return true;
+    }
+
+    return false;
+});
