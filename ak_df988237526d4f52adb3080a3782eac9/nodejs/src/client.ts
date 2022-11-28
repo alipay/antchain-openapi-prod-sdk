cdbc9bb61b13cc81268f6f061ede17a53d487797
sdk-team <sdk-team@alibabacloud.com>
2022-11-28 14:45:59 +0000

    Generated SDK for ak_df988237526d4f52adb3080a3782eac9.

--- ak_df988237526d4f52adb3080a3782eac9/nodejs/src/client.ts
+++ ak_df988237526d4f52adb3080a3782eac9/nodejs/src/client.ts
@@ -246,6 +246,71 @@ export class InitDemoBbpInsuranceUserResponse extends $tea.Model {
   }
 }
 
+export class UploadDemoCjtestSourceFileRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  // file
+  fileObject?: Readable;
+  fileObjectName?: string;
+  fileId: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+      fileObject: 'fileObject',
+      fileObjectName: 'fileObjectName',
+      fileId: 'file_id',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+      fileObject: 'Readable',
+      fileObjectName: 'string',
+      fileId: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class UploadDemoCjtestSourceFileResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  // test
+  res?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+      res: 'res',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+      res: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class ImportDemoComCnTestRequest extends $tea.Model {
   // OAuth模式下的授权token
   authToken?: string;
@@ -358,6 +423,57 @@ export class QueryDemoGongxiangTestDemoResponse extends $tea.Model {
       resultCode: 'string',
       resultMsg: 'string',
       character1: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class RegisterDemoTestBizeventMessageRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class RegisterDemoTestBizeventMessageResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
     };
   }
 
@@ -567,7 +683,9 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.6",
+          sdk_version: "1.0.7",
+          _prod_code: "ak_df988237526d4f52adb3080a3782eac9",
+          _prod_channel: "saas",
         };
         if (!Util.empty(this._securityToken)) {
           request_.query["security_token"] = this._securityToken;
@@ -652,6 +770,46 @@ export default class Client {
   }
 
   /**
+   * Description: 文件测试
+   * Summary: 文件测试
+   */
+  async uploadDemoCjtestSourceFile(request: UploadDemoCjtestSourceFileRequest): Promise<UploadDemoCjtestSourceFileResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.uploadDemoCjtestSourceFileEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 文件测试
+   * Summary: 文件测试
+   */
+  async uploadDemoCjtestSourceFileEx(request: UploadDemoCjtestSourceFileRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<UploadDemoCjtestSourceFileResponse> {
+    if (!Util.isUnset(request.fileObject)) {
+      let uploadReq = new CreateAntcloudGatewayxFileUploadRequest({
+        authToken: request.authToken,
+        apiCode: "demo.cjtest.source.file.upload",
+        fileName: request.fileObjectName,
+      });
+      let uploadResp = await this.createAntcloudGatewayxFileUploadEx(uploadReq, headers, runtime);
+      if (!AntchainUtil.isSuccess(uploadResp.resultCode, "ok")) {
+        let uploadDemoCjtestSourceFileResponse = new UploadDemoCjtestSourceFileResponse({
+          reqMsgId: uploadResp.reqMsgId,
+          resultCode: uploadResp.resultCode,
+          resultMsg: uploadResp.resultMsg,
+        });
+        return uploadDemoCjtestSourceFileResponse;
+      }
+
+      let uploadHeaders = AntchainUtil.parseUploadHeaders(uploadResp.uploadHeaders);
+      await AntchainUtil.putObject(request.fileObject, uploadHeaders, uploadResp.uploadUrl);
+      request.fileId = uploadResp.fileId;
+    }
+
+    Util.validateModel(request);
+    return $tea.cast<UploadDemoCjtestSourceFileResponse>(await this.doRequest("1.0", "demo.cjtest.source.file.upload", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new UploadDemoCjtestSourceFileResponse({}));
+  }
+
+  /**
    * Description: 长捷,qiujianglong.qjl
    * Summary: api简介
    */
@@ -708,6 +866,25 @@ export default class Client {
   async queryDemoGongxiangTestDemoEx(request: QueryDemoGongxiangTestDemoRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<QueryDemoGongxiangTestDemoResponse> {
     Util.validateModel(request);
     return $tea.cast<QueryDemoGongxiangTestDemoResponse>(await this.doRequest("1.0", "demo.gongxiang.test.demo.query", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new QueryDemoGongxiangTestDemoResponse({}));
+  }
+
+  /**
+   * Description: 随机测试
+   * Summary: 消息发送及消费
+   */
+  async registerDemoTestBizeventMessage(request: RegisterDemoTestBizeventMessageRequest): Promise<RegisterDemoTestBizeventMessageResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.registerDemoTestBizeventMessageEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 随机测试
+   * Summary: 消息发送及消费
+   */
+  async registerDemoTestBizeventMessageEx(request: RegisterDemoTestBizeventMessageRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<RegisterDemoTestBizeventMessageResponse> {
+    Util.validateModel(request);
+    return $tea.cast<RegisterDemoTestBizeventMessageResponse>(await this.doRequest("1.0", "demo.test.bizevent.message.register", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new RegisterDemoTestBizeventMessageResponse({}));
   }
 
   /**
