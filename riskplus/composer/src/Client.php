b3198610f885374d704b3136d90172a5b8f82542
sdk-team <sdk-team@alibabacloud.com>
2022-11-29 06:04:50 +0000

    Generated SDK for RISKPLUS.

--- riskplus/composer/src/Client.php
+++ riskplus/composer/src/Client.php
@@ -418,9 +418,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.15.5',
-                    '_prod_code'       => 'RISKPLUS',
-                    '_prod_channel'    => 'undefined',
+                    'sdk_version'      => '1.15.6',
                 ];
                 if (!Utils::empty_($this->_securityToken)) {
                     $_request->query['security_token'] = $this->_securityToken;
