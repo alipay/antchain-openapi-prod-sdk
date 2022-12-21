775c447ffe70aa45fdd23c51b36d470b2255d1d8
sdk-team <sdk-team@alibabacloud.com>
2022-12-21 02:26:53 +0000

    Generated SDK for BOT.

--- bot/csharp/core/Properties/AssemblyInfo.cs
+++ bot/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("825840f0-8034-11ed-a8ae-37a7651b64e6")]
+[assembly: Guid("e5f99850-80d6-11ed-989b-a74f0d6c9173")]
 
 // Version information for an assembly consists of the following four values:
 //
