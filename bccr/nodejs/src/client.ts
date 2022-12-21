27b20ea2f4c66224cd48e1cf9f03e42df5c0b685
sdk-team <sdk-team@alibabacloud.com>
2022-12-21 02:25:13 +0000

    Generated SDK for BCCR.

--- bccr/nodejs/src/client.ts
+++ bccr/nodejs/src/client.ts
@@ -7621,8 +7621,6 @@ export default class Client {
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
           sdk_version: "1.17.33",
-          _prod_code: "BCCR",
-          _prod_channel: "undefined",
         };
         if (!Util.empty(this._securityToken)) {
           request_.query["security_token"] = this._securityToken;
