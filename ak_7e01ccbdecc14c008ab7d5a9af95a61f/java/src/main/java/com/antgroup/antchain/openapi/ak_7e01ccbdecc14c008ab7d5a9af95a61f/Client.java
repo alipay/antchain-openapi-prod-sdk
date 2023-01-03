9f9f4acaebae0269d04f9f76db8b2a244d5700a0
sdk-team <sdk-team@alibabacloud.com>
2023-01-03 09:12:22 +0000

    Generated SDK for ak_7e01ccbdecc14c008ab7d5a9af95a61f.

--- ak_7e01ccbdecc14c008ab7d5a9af95a61f/java/src/main/java/com/antgroup/antchain/openapi/ak_7e01ccbdecc14c008ab7d5a9af95a61f/Client.java
+++ ak_7e01ccbdecc14c008ab7d5a9af95a61f/java/src/main/java/com/antgroup/antchain/openapi/ak_7e01ccbdecc14c008ab7d5a9af95a61f/Client.java
@@ -110,7 +110,9 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.0.0")
+                    new TeaPair("sdk_version", "1.0.1"),
+                    new TeaPair("_prod_code", "ak_7e01ccbdecc14c008ab7d5a9af95a61f"),
+                    new TeaPair("_prod_channel", "saas")
                 );
                 if (!com.aliyun.teautil.Common.empty(_securityToken)) {
                     request_.query.put("security_token", _securityToken);
@@ -160,6 +162,25 @@ public class Client {
     }
 
     /**
+     * Description: 结算单确认
+     * Summary: 结算单确认
+     */
+    public ConfirmAntchainBbpContractReconciliationResponse confirmAntchainBbpContractReconciliation(ConfirmAntchainBbpContractReconciliationRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.confirmAntchainBbpContractReconciliationEx(request, headers, runtime);
+    }
+
+    /**
+     * Description: 结算单确认
+     * Summary: 结算单确认
+     */
+    public ConfirmAntchainBbpContractReconciliationResponse confirmAntchainBbpContractReconciliationEx(ConfirmAntchainBbpContractReconciliationRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "antchain.bbp.contract.reconciliation.confirm", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new ConfirmAntchainBbpContractReconciliationResponse());
+    }
+
+    /**
      * Description: testa
      * Summary: 测试用api
      */
@@ -176,5 +197,24 @@ public class Client {
     public QueryDemoSaasTestTestaResponse queryDemoSaasTestTestaEx(QueryDemoSaasTestTestaRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "demo.saas.test.testa.query", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new QueryDemoSaasTestTestaResponse());
+    }
+
+    /**
+     * Description: testB
+     * Summary: 测试用api
+     */
+    public ImportDemoSaasTestTestbResponse importDemoSaasTestTestb(ImportDemoSaasTestTestbRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.importDemoSaasTestTestbEx(request, headers, runtime);
+    }
+
+    /**
+     * Description: testB
+     * Summary: 测试用api
+     */
+    public ImportDemoSaasTestTestbResponse importDemoSaasTestTestbEx(ImportDemoSaasTestTestbRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "demo.saas.test.testb.import", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new ImportDemoSaasTestTestbResponse());
     }
 }
