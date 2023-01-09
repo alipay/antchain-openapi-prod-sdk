7af81c75982dfa5658decbf4f2c9e48601ed77a3
sdk-team <sdk-team@alibabacloud.com>
2023-01-09 07:11:33 +0000

    Generated SDK for RISKPLUS.

--- riskplus/go/client/client.go
+++ riskplus/go/client/client.go
@@ -18519,6 +18519,111 @@ func (s *ApplyUmktPhonenumberstatusforsmsResponse) SetCarrier(v string) *ApplyUm
 	return s
 }
 
+type BatchqueryUmktRtTopnRequest struct {
+	// OAuth模式下的授权token
+	AuthToken         *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
+	ProductInstanceId *string `json:"product_instance_id,omitempty" xml:"product_instance_id,omitempty"`
+	// 场景策略id
+	SceneStrategyId *int64 `json:"scene_strategy_id,omitempty" xml:"scene_strategy_id,omitempty" require:"true"`
+	// 用户列表传输模版
+	QueryTemplate *string `json:"query_template,omitempty" xml:"query_template,omitempty" require:"true"`
+	// 用户凭证信息
+	CustomerKeys []*string `json:"customer_keys,omitempty" xml:"customer_keys,omitempty" require:"true" type:"Repeated"`
+	// 业务流水号
+	BizSerialNo *string `json:"biz_serial_no,omitempty" xml:"biz_serial_no,omitempty" require:"true"`
+	// 访问类型，PROD/TEST，正式流量/测试流量
+	VisitType *string `json:"visit_type,omitempty" xml:"visit_type,omitempty" require:"true"`
+}
+
+func (s BatchqueryUmktRtTopnRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s BatchqueryUmktRtTopnRequest) GoString() string {
+	return s.String()
+}
+
+func (s *BatchqueryUmktRtTopnRequest) SetAuthToken(v string) *BatchqueryUmktRtTopnRequest {
+	s.AuthToken = &v
+	return s
+}
+
+func (s *BatchqueryUmktRtTopnRequest) SetProductInstanceId(v string) *BatchqueryUmktRtTopnRequest {
+	s.ProductInstanceId = &v
+	return s
+}
+
+func (s *BatchqueryUmktRtTopnRequest) SetSceneStrategyId(v int64) *BatchqueryUmktRtTopnRequest {
+	s.SceneStrategyId = &v
+	return s
+}
+
+func (s *BatchqueryUmktRtTopnRequest) SetQueryTemplate(v string) *BatchqueryUmktRtTopnRequest {
+	s.QueryTemplate = &v
+	return s
+}
+
+func (s *BatchqueryUmktRtTopnRequest) SetCustomerKeys(v []*string) *BatchqueryUmktRtTopnRequest {
+	s.CustomerKeys = v
+	return s
+}
+
+func (s *BatchqueryUmktRtTopnRequest) SetBizSerialNo(v string) *BatchqueryUmktRtTopnRequest {
+	s.BizSerialNo = &v
+	return s
+}
+
+func (s *BatchqueryUmktRtTopnRequest) SetVisitType(v string) *BatchqueryUmktRtTopnRequest {
+	s.VisitType = &v
+	return s
+}
+
+type BatchqueryUmktRtTopnResponse struct {
+	// 请求唯一ID，用于链路跟踪和问题排查
+	ReqMsgId *string `json:"req_msg_id,omitempty" xml:"req_msg_id,omitempty"`
+	// 结果码，一般OK表示调用成功
+	ResultCode *string `json:"result_code,omitempty" xml:"result_code,omitempty"`
+	// 异常信息的文本描述
+	ResultMsg *string `json:"result_msg,omitempty" xml:"result_msg,omitempty"`
+	// 处理是否成功
+	Success *bool `json:"success,omitempty" xml:"success,omitempty"`
+	// 实时营销单条结果
+	QueryResult []*CustomerUmktInfoModel `json:"query_result,omitempty" xml:"query_result,omitempty" type:"Repeated"`
+}
+
+func (s BatchqueryUmktRtTopnResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s BatchqueryUmktRtTopnResponse) GoString() string {
+	return s.String()
+}
+
+func (s *BatchqueryUmktRtTopnResponse) SetReqMsgId(v string) *BatchqueryUmktRtTopnResponse {
+	s.ReqMsgId = &v
+	return s
+}
+
+func (s *BatchqueryUmktRtTopnResponse) SetResultCode(v string) *BatchqueryUmktRtTopnResponse {
+	s.ResultCode = &v
+	return s
+}
+
+func (s *BatchqueryUmktRtTopnResponse) SetResultMsg(v string) *BatchqueryUmktRtTopnResponse {
+	s.ResultMsg = &v
+	return s
+}
+
+func (s *BatchqueryUmktRtTopnResponse) SetSuccess(v bool) *BatchqueryUmktRtTopnResponse {
+	s.Success = &v
+	return s
+}
+
+func (s *BatchqueryUmktRtTopnResponse) SetQueryResult(v []*CustomerUmktInfoModel) *BatchqueryUmktRtTopnResponse {
+	s.QueryResult = v
+	return s
+}
+
 type CreateAntcloudGatewayxFileUploadRequest struct {
 	// OAuth模式下的授权token
 	AuthToken *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
@@ -18761,7 +18866,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.16.1"),
+				"sdk_version":      tea.String("1.16.2"),
 			}
 			if !tea.BoolValue(util.Empty(client.SecurityToken)) {
 				request_.Query["security_token"] = client.SecurityToken
@@ -23319,6 +23424,40 @@ func (client *Client) ApplyUmktPhonenumberstatusforsmsEx(request *ApplyUmktPhone
 	}
 	_result = &ApplyUmktPhonenumberstatusforsmsResponse{}
 	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("riskplus.umkt.phonenumberstatusforsms.apply"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+/**
+ * Description: 营销实时topN圈客
+ * Summary: 营销实时topN圈客
+ */
+func (client *Client) BatchqueryUmktRtTopn(request *BatchqueryUmktRtTopnRequest) (_result *BatchqueryUmktRtTopnResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &BatchqueryUmktRtTopnResponse{}
+	_body, _err := client.BatchqueryUmktRtTopnEx(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+/**
+ * Description: 营销实时topN圈客
+ * Summary: 营销实时topN圈客
+ */
+func (client *Client) BatchqueryUmktRtTopnEx(request *BatchqueryUmktRtTopnRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *BatchqueryUmktRtTopnResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = &BatchqueryUmktRtTopnResponse{}
+	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("riskplus.umkt.rt.topn.batchquery"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
 	if _err != nil {
 		return _result, _err
 	}
