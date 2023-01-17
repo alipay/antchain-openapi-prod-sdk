13ca4e1115387321ca31a09633a4e575d1e81797
sdk-team <sdk-team@alibabacloud.com>
2023-01-17 10:50:27 +0000

    Generated SDK for RISKPLUS.

--- riskplus/nodejs/src/client.ts
+++ riskplus/nodejs/src/client.ts
@@ -6806,11 +6806,11 @@ export class ApplyDubbridgeCustomerAgreementsignRequest extends $tea.Model {
   // 资金方编号
   fundCode?: string;
   // 渠道号
-  channelCode: string;
+  channelCode?: string;
   // 项目编号
-  projectCode: string;
+  projectCode?: string;
   // 渠道号
-  orgCode: string;
+  orgCode?: string;
   static names(): { [key: string]: string } {
     return {
       authToken: 'auth_token',
@@ -14331,7 +14331,9 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.16.3",
+          sdk_version: "1.16.4",
+          _prod_code: "RISKPLUS",
+          _prod_channel: "undefined",
         };
         if (!Util.empty(this._securityToken)) {
           request_.query["security_token"] = this._securityToken;
