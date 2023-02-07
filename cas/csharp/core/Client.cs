74d4765944ccce45610eaf188127bd31743369a8
sdk-team <sdk-team@alibabacloud.com>
2023-02-07 09:55:43 +0000

    Generated SDK for CAS.

--- cas/csharp/core/Client.cs
+++ cas/csharp/core/Client.cs
@@ -138,8 +138,6 @@ namespace AntChain.SDK.CAS
                         {"access_key", _accessKeyId},
                         {"base_sdk_version", "TeaSDK-2.0"},
                         {"sdk_version", "1.4.58"},
-                        {"_prod_code", "CAS"},
-                        {"_prod_channel", "undefined"},
                     };
                     if (!AlibabaCloud.TeaUtil.Common.Empty(_securityToken))
                     {
@@ -264,8 +262,6 @@ namespace AntChain.SDK.CAS
                         {"access_key", _accessKeyId},
                         {"base_sdk_version", "TeaSDK-2.0"},
                         {"sdk_version", "1.4.58"},
-                        {"_prod_code", "CAS"},
-                        {"_prod_channel", "undefined"},
                     };
                     if (!AlibabaCloud.TeaUtil.Common.Empty(_securityToken))
                     {
--- cas/csharp/core/Properties/AssemblyInfo.cs
+++ cas/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e4036090-a6c8-11ed-86dd-67aa61a9d380")]
+[assembly: Guid("860fead0-a6cd-11ed-ad43-eb7832b2ce8c")]
 
 // Version information for an assembly consists of the following four values:
 //
