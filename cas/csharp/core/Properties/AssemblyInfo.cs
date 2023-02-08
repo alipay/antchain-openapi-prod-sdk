de2055d9e9ad95c6cd611c5701e28df662ebb4b9
sdk-team <sdk-team@alibabacloud.com>
2023-02-08 02:17:54 +0000

    Generated SDK for CAS.

--- cas/csharp/core/Properties/AssemblyInfo.cs
+++ cas/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f9657d20-a6db-11ed-820d-f55256ffbb05")]
+[assembly: Guid("ba540d10-a756-11ed-99bb-83a414806be5")]
 
 // Version information for an assembly consists of the following four values:
 //
