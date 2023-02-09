59a025f6b37e8d9da3781a851e73c56e633cdd41
sdk-team <sdk-team@alibabacloud.com>
2023-02-09 10:35:07 +0000

    Generated SDK for ak_24b462c17fab4e6998dcceeb37c41789.

---
+++ ak_24b462c17fab4e6998dcceeb37c41789/java/src/main/java/com/antgroup/antchain/openapi/ak_24b462c17fab4e6998dcceeb37c41789/models/BindDemoCenterAbilityRequest.java
@@ -0,0 +1,35 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.ak_24b462c17fab4e6998dcceeb37c41789.models;
+
+import com.aliyun.tea.*;
+
+public class BindDemoCenterAbilityRequest extends TeaModel {
+    // OAuth模式下的授权token
+    @NameInMap("auth_token")
+    public String authToken;
+
+    @NameInMap("product_instance_id")
+    public String productInstanceId;
+
+    public static BindDemoCenterAbilityRequest build(java.util.Map<String, ?> map) throws Exception {
+        BindDemoCenterAbilityRequest self = new BindDemoCenterAbilityRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public BindDemoCenterAbilityRequest setAuthToken(String authToken) {
+        this.authToken = authToken;
+        return this;
+    }
+    public String getAuthToken() {
+        return this.authToken;
+    }
+
+    public BindDemoCenterAbilityRequest setProductInstanceId(String productInstanceId) {
+        this.productInstanceId = productInstanceId;
+        return this;
+    }
+    public String getProductInstanceId() {
+        return this.productInstanceId;
+    }
+
+}
