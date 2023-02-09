35d9dada118fa50613150179482ec7ec12d651f3
sdk-team <sdk-team@alibabacloud.com>
2023-02-09 11:57:39 +0000

    Generated SDK for ak_0865f558338744bd8da4fcade32586f9.

---
+++ ak_0865f558338744bd8da4fcade32586f9/csharp/core/Models/BindDemoCenterAbilityRequest.cs
@@ -0,0 +1,23 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_0865f558338744bd8da4fcade32586f9.Models
+{
+    public class BindDemoCenterAbilityRequest : TeaModel {
+        // OAuth模式下的授权token
+        [NameInMap("auth_token")]
+        [Validation(Required=false)]
+        public string AuthToken { get; set; }
+
+        [NameInMap("product_instance_id")]
+        [Validation(Required=false)]
+        public string ProductInstanceId { get; set; }
+
+    }
+
+}
