e0cf1ccf2f7ff5c5a7fd3e3edacede377f1e401c
sdk-team <sdk-team@alibabacloud.com>
2023-03-27 06:00:32 +0000

    Generated SDK for SMARTACCOUNT.

---
+++ smartaccount/csharp/core/Models/BizResult.cs
@@ -0,0 +1,40 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.SMARTACCOUNT.Models
+{
+    // 通用业务返回
+    public class BizResult : TeaModel {
+        // 是否成功
+        [NameInMap("success")]
+        [Validation(Required=true)]
+        public bool? Success { get; set; }
+
+        // 错误码
+        [NameInMap("error_code")]
+        [Validation(Required=false)]
+        public string ErrorCode { get; set; }
+
+        // 是否重试
+        [NameInMap("need_retry")]
+        [Validation(Required=true)]
+        public bool? NeedRetry { get; set; }
+
+        // 失败错误信息
+        [NameInMap("error_view_msg")]
+        [Validation(Required=false)]
+        public string ErrorViewMsg { get; set; }
+
+        // 成功返回信息，与function相匹配
+        [NameInMap("encoded_result_obj")]
+        [Validation(Required=false)]
+        public string EncodedResultObj { get; set; }
+
+    }
+
+}
---
+++ smartaccount/csharp/core/Models/SystemParam.cs
@@ -0,0 +1,30 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.SMARTACCOUNT.Models
+{
+    // 标准系统参数
+    public class SystemParam : TeaModel {
+        // 合作伙伴
+        [NameInMap("partner")]
+        [Validation(Required=true)]
+        public string Partner { get; set; }
+
+        // 业务场景单元
+        [NameInMap("function")]
+        [Validation(Required=true)]
+        public string Function { get; set; }
+
+        // 业务幂等号
+        [NameInMap("request_id")]
+        [Validation(Required=false)]
+        public string RequestId { get; set; }
+
+    }
+
+}
