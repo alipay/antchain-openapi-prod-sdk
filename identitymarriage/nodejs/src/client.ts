f572831a82872c6df243b099b6a92e46c0041090
sdk-team <sdk-team@alibabacloud.com>
2023-04-03 08:40:06 +0000

    Generated SDK for IDENTITYMARRIAGE.

--- identitymarriage/nodejs/src/client.ts
+++ identitymarriage/nodejs/src/client.ts
@@ -136,6 +136,65 @@ export class CheckMarriageInfoResponse extends $tea.Model {
   }
 }
 
+export class NotifyMarriageInfoRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  // bizContent
+  bizContent: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+      bizContent: 'biz_content',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+      bizContent: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class NotifyMarriageInfoResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  // data
+  data?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+      data: 'data',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+      data: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 
 export default class Client {
   _endpoint: string;
@@ -249,7 +308,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.2",
+          sdk_version: "1.0.3",
           _prod_code: "IDENTITYMARRIAGE",
           _prod_channel: "undefined",
         };
@@ -314,6 +373,25 @@ export default class Client {
   async checkMarriageInfoEx(request: CheckMarriageInfoRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<CheckMarriageInfoResponse> {
     Util.validateModel(request);
     return $tea.cast<CheckMarriageInfoResponse>(await this.doRequest("1.0", "identity.marriage.marriage.info.check", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new CheckMarriageInfoResponse({}));
+  }
+
+  /**
+   * Description: 婚姻状况通知
+   * Summary: 婚姻状况通知
+   */
+  async notifyMarriageInfo(request: NotifyMarriageInfoRequest): Promise<NotifyMarriageInfoResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.notifyMarriageInfoEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 婚姻状况通知
+   * Summary: 婚姻状况通知
+   */
+  async notifyMarriageInfoEx(request: NotifyMarriageInfoRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<NotifyMarriageInfoResponse> {
+    Util.validateModel(request);
+    return $tea.cast<NotifyMarriageInfoResponse>(await this.doRequest("1.0", "identity.marriage.marriage.info.notify", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new NotifyMarriageInfoResponse({}));
   }
 
 }
