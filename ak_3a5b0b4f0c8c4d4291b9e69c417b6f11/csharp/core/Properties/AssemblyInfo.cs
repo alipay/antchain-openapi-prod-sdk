078b88ee4a240b6b439efab0e9daf975eea60e4c
sdk-team <sdk-team@alibabacloud.com>
2023-04-17 11:21:22 +0000

    Generated SDK for ak_3a5b0b4f0c8c4d4291b9e69c417b6f11.

---
+++ ak_3a5b0b4f0c8c4d4291b9e69c417b6f11/csharp/core/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("antchain-ak_3a5b0b4f0c8c4d4291b9e69c417b6f11")]
+[assembly: AssemblyDescription("Ant Chain ak_3a5b0b4f0c8c4d4291b9e69c417b6f11 SDK Library for .NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ant Chain, Inc")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f5b955e0-dd11-11ed-9c7f-93bcba8e3a02")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
