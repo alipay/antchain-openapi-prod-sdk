aa72f2dd972fede90bf377298eebda61e5a2999d
sdk-team <sdk-team@alibabacloud.com>
2023-04-17 11:21:44 +0000

    Generated SDK for ak_d54d5654167e4859a1f3a1f3ff237b7c.

---
+++ ak_d54d5654167e4859a1f3a1f3ff237b7c/csharp/README-CN.md
@@ -0,0 +1,29 @@
+[English](README.md) | 简体中文
+
+# Ant Chain ak_d54d5654167e4859a1f3a1f3ff237b7c SDK for C#
+
+## 安装
+
+### .NET
+
+```bash
+donet add package AntChain.SDK.Ak_d54d5654167e4859a1f3a1f3ff237b7c
+```
+
+## 问题
+
+[提交 Issue](https://github.com/alipay/antchain-openapi-prod-sdk/issues/new)，不符合指南的问题可能会立即关闭。
+
+## 发行说明
+
+每个版本的详细更改记录在[发行说明](./ChangeLog.txt)中。
+
+## 相关
+
+* [最新源码](https://github.com/antchain-openapi-prod-sdk)
+
+## 许可证
+
+[Apache-2.0](http://www.apache.org/licenses/LICENSE-2.0)
+
+Copyright (c) 2009-present, Alibaba Cloud All rights reserved.
---
+++ ak_d54d5654167e4859a1f3a1f3ff237b7c/csharp/core/Models/RunAntsecuritytechGatewayGeneralRequest.cs
@@ -0,0 +1,38 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_d54d5654167e4859a1f3a1f3ff237b7c.Models
+{
+    public class RunAntsecuritytechGatewayGeneralRequest : TeaModel {
+        // OAuth模式下的授权token
+        [NameInMap("auth_token")]
+        [Validation(Required=false)]
+        public string AuthToken { get; set; }
+
+        [NameInMap("product_instance_id")]
+        [Validation(Required=false)]
+        public string ProductInstanceId { get; set; }
+
+        // 业务请求数据，json格式
+        [NameInMap("request")]
+        [Validation(Required=true)]
+        public string Request { get; set; }
+
+        // 要调用的具体的服务名称
+        [NameInMap("service_name")]
+        [Validation(Required=true)]
+        public string ServiceName { get; set; }
+
+        // 扩展信息
+        [NameInMap("ext_info")]
+        [Validation(Required=false)]
+        public string ExtInfo { get; set; }
+
+    }
+
+}
