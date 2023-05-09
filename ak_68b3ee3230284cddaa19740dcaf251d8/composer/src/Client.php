4d2ead90c035498482963a13bcdfe1ac7cf05972
sdk-team <sdk-team@alibabacloud.com>
2023-05-09 10:06:36 +0000

    Generated SDK for ak_68b3ee3230284cddaa19740dcaf251d8.

--- ak_68b3ee3230284cddaa19740dcaf251d8/composer/src/Client.php
+++ ak_68b3ee3230284cddaa19740dcaf251d8/composer/src/Client.php
@@ -15,8 +15,6 @@ use AntChain\Ak_68b3ee3230284cddaa19740dcaf251d8\Models\BindDemoAaaBbbCccRequest
 use AntChain\Ak_68b3ee3230284cddaa19740dcaf251d8\Models\BindDemoAaaBbbCccResponse;
 use AntChain\Ak_68b3ee3230284cddaa19740dcaf251d8\Models\BindDemoCenterAbilityRequest;
 use AntChain\Ak_68b3ee3230284cddaa19740dcaf251d8\Models\BindDemoCenterAbilityResponse;
-use AntChain\Ak_68b3ee3230284cddaa19740dcaf251d8\Models\BindDemoMoreAbilityTestabcRequest;
-use AntChain\Ak_68b3ee3230284cddaa19740dcaf251d8\Models\BindDemoMoreAbilityTestabcResponse;
 use AntChain\Ak_68b3ee3230284cddaa19740dcaf251d8\Models\PublishDemoSaasTestTestcRequest;
 use AntChain\Ak_68b3ee3230284cddaa19740dcaf251d8\Models\PublishDemoSaasTestTestcResponse;
 use AntChain\Util\UtilClient;
@@ -118,18 +116,18 @@ class Client
     {
         $runtime->validate();
         $_runtime = [
-            'timeouted'               => 'retry',
-            'readTimeout'             => Utils::defaultNumber($runtime->readTimeout, $this->_readTimeout),
-            'connectTimeout'          => Utils::defaultNumber($runtime->connectTimeout, $this->_connectTimeout),
-            'httpProxy'               => Utils::defaultString($runtime->httpProxy, $this->_httpProxy),
-            'httpsProxy'              => Utils::defaultString($runtime->httpsProxy, $this->_httpsProxy),
-            'noProxy'                 => Utils::defaultString($runtime->noProxy, $this->_noProxy),
-            'maxIdleConns'            => Utils::defaultNumber($runtime->maxIdleConns, $this->_maxIdleConns),
-            'maxIdleTimeMillis'       => $this->_maxIdleTimeMillis,
-            'keepAliveDurationMillis' => $this->_keepAliveDurationMillis,
-            'maxRequests'             => $this->_maxRequests,
-            'maxRequestsPerHost'      => $this->_maxRequestsPerHost,
-            'retry'                   => [
+            'timeouted'          => 'retry',
+            'readTimeout'        => Utils::defaultNumber($runtime->readTimeout, $this->_readTimeout),
+            'connectTimeout'     => Utils::defaultNumber($runtime->connectTimeout, $this->_connectTimeout),
+            'httpProxy'          => Utils::defaultString($runtime->httpProxy, $this->_httpProxy),
+            'httpsProxy'         => Utils::defaultString($runtime->httpsProxy, $this->_httpsProxy),
+            'noProxy'            => Utils::defaultString($runtime->noProxy, $this->_noProxy),
+            'maxIdleConns'       => Utils::defaultNumber($runtime->maxIdleConns, $this->_maxIdleConns),
+            'maxIdleTimeMillis'  => $this->_maxIdleTimeMillis,
+            'keepAliveDuration'  => $this->_keepAliveDurationMillis,
+            'maxRequests'        => $this->_maxRequests,
+            'maxRequestsPerHost' => $this->_maxRequestsPerHost,
+            'retry'              => [
                 'retryable'   => $runtime->autoretry,
                 'maxAttempts' => Utils::defaultNumber($runtime->maxAttempts, 3),
             ],
@@ -165,7 +163,9 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.0.1',
+                    'sdk_version'      => '1.0.0',
+                    '_prod_code'       => 'ak_68b3ee3230284cddaa19740dcaf251d8',
+                    '_prod_channel'    => 'saas',
                 ];
                 if (!Utils::empty_($this->_securityToken)) {
                     $_request->query['security_token'] = $this->_securityToken;
@@ -213,7 +213,7 @@ class Client
 
     /**
      * Description: 自动化测试创建test
-     * Summary: 自动化测试创建test.
+     * Summary: 自动化测试创建test1.
      *
      * @param BindDemoAaaBbbCccRequest $request
      *
@@ -229,7 +229,7 @@ class Client
 
     /**
      * Description: 自动化测试创建test
-     * Summary: 自动化测试创建test.
+     * Summary: 自动化测试创建test1.
      *
      * @param BindDemoAaaBbbCccRequest $request
      * @param string[]                 $headers
@@ -308,38 +308,5 @@ class Client
         Utils::validateModel($request);
 
         return BindDemoCenterAbilityResponse::fromMap($this->doRequest('1.0', 'demo.center.ability.bind', 'HTTPS', 'POST', '/gateway.do', Tea::merge($request), $headers, $runtime));
-    }
-
-    /**
-     * Description: 测试API绑定多个标签时的情况
-     * Summary: API绑定多个标签.
-     *
-     * @param BindDemoMoreAbilityTestabcRequest $request
-     *
-     * @return BindDemoMoreAbilityTestabcResponse
-     */
-    public function bindDemoMoreAbilityTestabc($request)
-    {
-        $runtime = new RuntimeOptions([]);
-        $headers = [];
-
-        return $this->bindDemoMoreAbilityTestabcEx($request, $headers, $runtime);
-    }
-
-    /**
-     * Description: 测试API绑定多个标签时的情况
-     * Summary: API绑定多个标签.
-     *
-     * @param BindDemoMoreAbilityTestabcRequest $request
-     * @param string[]                          $headers
-     * @param RuntimeOptions                    $runtime
-     *
-     * @return BindDemoMoreAbilityTestabcResponse
-     */
-    public function bindDemoMoreAbilityTestabcEx($request, $headers, $runtime)
-    {
-        Utils::validateModel($request);
-
-        return BindDemoMoreAbilityTestabcResponse::fromMap($this->doRequest('1.0', 'demo.more.ability.testabc.bind', 'HTTPS', 'POST', '/gateway.do', Tea::merge($request), $headers, $runtime));
     }
 }
