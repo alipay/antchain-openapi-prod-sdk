d7cea2049ee7ef9dd0524071f64f6ab72aa26571
sdk-team <sdk-team@alibabacloud.com>
2023-05-09 10:06:39 +0000

    Generated SDK for ak_68b3ee3230284cddaa19740dcaf251d8.

--- ak_68b3ee3230284cddaa19740dcaf251d8/nodejs/src/client.ts
+++ ak_68b3ee3230284cddaa19740dcaf251d8/nodejs/src/client.ts
@@ -246,57 +246,6 @@ export class BindDemoCenterAbilityResponse extends $tea.Model {
   }
 }
 
-export class BindDemoMoreAbilityTestabcRequest extends $tea.Model {
-  // OAuth模式下的授权token
-  authToken?: string;
-  productInstanceId?: string;
-  static names(): { [key: string]: string } {
-    return {
-      authToken: 'auth_token',
-      productInstanceId: 'product_instance_id',
-    };
-  }
-
-  static types(): { [key: string]: any } {
-    return {
-      authToken: 'string',
-      productInstanceId: 'string',
-    };
-  }
-
-  constructor(map?: { [key: string]: any }) {
-    super(map);
-  }
-}
-
-export class BindDemoMoreAbilityTestabcResponse extends $tea.Model {
-  // 请求唯一ID，用于链路跟踪和问题排查
-  reqMsgId?: string;
-  // 结果码，一般OK表示调用成功
-  resultCode?: string;
-  // 异常信息的文本描述
-  resultMsg?: string;
-  static names(): { [key: string]: string } {
-    return {
-      reqMsgId: 'req_msg_id',
-      resultCode: 'result_code',
-      resultMsg: 'result_msg',
-    };
-  }
-
-  static types(): { [key: string]: any } {
-    return {
-      reqMsgId: 'string',
-      resultCode: 'string',
-      resultMsg: 'string',
-    };
-  }
-
-  constructor(map?: { [key: string]: any }) {
-    super(map);
-  }
-}
-
 
 export default class Client {
   _endpoint: string;
@@ -371,7 +320,7 @@ export default class Client {
       noProxy: Util.defaultString(runtime.noProxy, this._noProxy),
       maxIdleConns: Util.defaultNumber(runtime.maxIdleConns, this._maxIdleConns),
       maxIdleTimeMillis: this._maxIdleTimeMillis,
-      keepAliveDurationMillis: this._keepAliveDurationMillis,
+      keepAliveDuration: this._keepAliveDurationMillis,
       maxRequests: this._maxRequests,
       maxRequestsPerHost: this._maxRequestsPerHost,
       retry: {
@@ -410,7 +359,9 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.1",
+          sdk_version: "1.0.0",
+          _prod_code: "ak_68b3ee3230284cddaa19740dcaf251d8",
+          _prod_channel: "saas",
         };
         if (!Util.empty(this._securityToken)) {
           request_.query["security_token"] = this._securityToken;
@@ -458,7 +409,7 @@ export default class Client {
 
   /**
    * Description: 自动化测试创建test
-   * Summary: 自动化测试创建test
+   * Summary: 自动化测试创建test1
    */
   async bindDemoAaaBbbCcc(request: BindDemoAaaBbbCccRequest): Promise<BindDemoAaaBbbCccResponse> {
     let runtime = new $Util.RuntimeOptions({ });
@@ -468,7 +419,7 @@ export default class Client {
 
   /**
    * Description: 自动化测试创建test
-   * Summary: 自动化测试创建test
+   * Summary: 自动化测试创建test1
    */
   async bindDemoAaaBbbCccEx(request: BindDemoAaaBbbCccRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<BindDemoAaaBbbCccResponse> {
     Util.validateModel(request);
@@ -511,25 +462,6 @@ export default class Client {
   async bindDemoCenterAbilityEx(request: BindDemoCenterAbilityRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<BindDemoCenterAbilityResponse> {
     Util.validateModel(request);
     return $tea.cast<BindDemoCenterAbilityResponse>(await this.doRequest("1.0", "demo.center.ability.bind", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new BindDemoCenterAbilityResponse({}));
-  }
-
-  /**
-   * Description: 测试API绑定多个标签时的情况
-   * Summary: API绑定多个标签
-   */
-  async bindDemoMoreAbilityTestabc(request: BindDemoMoreAbilityTestabcRequest): Promise<BindDemoMoreAbilityTestabcResponse> {
-    let runtime = new $Util.RuntimeOptions({ });
-    let headers : {[key: string ]: string} = { };
-    return await this.bindDemoMoreAbilityTestabcEx(request, headers, runtime);
-  }
-
-  /**
-   * Description: 测试API绑定多个标签时的情况
-   * Summary: API绑定多个标签
-   */
-  async bindDemoMoreAbilityTestabcEx(request: BindDemoMoreAbilityTestabcRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<BindDemoMoreAbilityTestabcResponse> {
-    Util.validateModel(request);
-    return $tea.cast<BindDemoMoreAbilityTestabcResponse>(await this.doRequest("1.0", "demo.more.ability.testabc.bind", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new BindDemoMoreAbilityTestabcResponse({}));
   }
 
 }
