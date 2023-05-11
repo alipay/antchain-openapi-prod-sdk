91d4a9d0f9de1729e0d7332ba65a55ca11cc5460
sdk-team <sdk-team@alibabacloud.com>
2023-05-11 06:01:06 +0000

    Generated SDK for ak_68b3ee3230284cddaa19740dcaf251d8.

--- ak_68b3ee3230284cddaa19740dcaf251d8/csharp/core/Client.cs
+++ ak_68b3ee3230284cddaa19740dcaf251d8/csharp/core/Client.cs
@@ -137,7 +137,7 @@ namespace AntChain.SDK.Ak_68b3ee3230284cddaa19740dcaf251d8
                         {"req_msg_id", AntChain.AlipayUtil.AntchainUtils.GetNonce()},
                         {"access_key", _accessKeyId},
                         {"base_sdk_version", "TeaSDK-2.0"},
-                        {"sdk_version", "1.0.3"},
+                        {"sdk_version", "1.0.4"},
                         {"_prod_code", "ak_68b3ee3230284cddaa19740dcaf251d8"},
                         {"_prod_channel", "saas"},
                     };
@@ -263,7 +263,7 @@ namespace AntChain.SDK.Ak_68b3ee3230284cddaa19740dcaf251d8
                         {"req_msg_id", AntChain.AlipayUtil.AntchainUtils.GetNonce()},
                         {"access_key", _accessKeyId},
                         {"base_sdk_version", "TeaSDK-2.0"},
-                        {"sdk_version", "1.0.3"},
+                        {"sdk_version", "1.0.4"},
                         {"_prod_code", "ak_68b3ee3230284cddaa19740dcaf251d8"},
                         {"_prod_channel", "saas"},
                     };
--- ak_68b3ee3230284cddaa19740dcaf251d8/csharp/core/Properties/AssemblyInfo.cs
+++ ak_68b3ee3230284cddaa19740dcaf251d8/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3a140820-efaf-11ed-98e9-fb3aaed3aaaa")]
+[assembly: Guid("32f75530-efc1-11ed-a641-bfcd908eecde")]
 
 // Version information for an assembly consists of the following four values:
 //
