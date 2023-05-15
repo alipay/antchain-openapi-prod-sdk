6bced81a3c71a6c1cd2e8921550d847020a59d06
sdk-team <sdk-team@alibabacloud.com>
2023-05-15 02:24:54 +0000

    Generated SDK for ak_50c620ebd72240219637191db5c3441d.

--- ak_50c620ebd72240219637191db5c3441d/java/src/main/java/com/antgroup/antchain/openapi/ak_50c620ebd72240219637191db5c3441d/Client.java
+++ ak_50c620ebd72240219637191db5c3441d/java/src/main/java/com/antgroup/antchain/openapi/ak_50c620ebd72240219637191db5c3441d/Client.java
@@ -110,7 +110,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.0.2"),
+                    new TeaPair("sdk_version", "1.0.3"),
                     new TeaPair("_prod_code", "ak_50c620ebd72240219637191db5c3441d"),
                     new TeaPair("_prod_channel", "saas")
                 );
