c016f768dbc128fc59ddcd40f1723fbca9cf9dda
sdk-team <sdk-team@alibabacloud.com>
2023-05-18 08:47:31 +0000

    Generated SDK for ak_de45e14cba2c4defbab7659a63578373.

---
+++ ak_de45e14cba2c4defbab7659a63578373/csharp/core/Models/CreateDemoSaasTestTestiRequest.cs
@@ -0,0 +1,23 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_de45e14cba2c4defbab7659a63578373.Models
+{
+    public class CreateDemoSaasTestTestiRequest : TeaModel {
+        // OAuth模式下的授权token
+        [NameInMap("auth_token")]
+        [Validation(Required=false)]
+        public string AuthToken { get; set; }
+
+        [NameInMap("product_instance_id")]
+        [Validation(Required=false)]
+        public string ProductInstanceId { get; set; }
+
+    }
+
+}
