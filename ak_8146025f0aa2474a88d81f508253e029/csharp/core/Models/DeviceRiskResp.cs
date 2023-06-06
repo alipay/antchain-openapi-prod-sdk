4c1f2af283cf6deed11f9ea19a61da914483330e
sdk-team <sdk-team@alibabacloud.com>
2023-06-06 03:48:04 +0000

    Generated SDK for ak_8146025f0aa2474a88d81f508253e029.

---
+++ ak_8146025f0aa2474a88d81f508253e029/csharp/core/Models/DeviceRiskResp.cs
@@ -0,0 +1,45 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_8146025f0aa2474a88d81f508253e029.Models
+{
+    // 设备风险查询data
+    public class DeviceRiskResp : TeaModel {
+        // apdid
+        [NameInMap("apdid")]
+        [Validation(Required=true)]
+        public string Apdid { get; set; }
+
+        // apdid
+        [NameInMap("apdid_token")]
+        [Validation(Required=true)]
+        public string ApdidToken { get; set; }
+
+        // riskLevel
+        [NameInMap("risk_level")]
+        [Validation(Required=true)]
+        public long? RiskLevel { get; set; }
+
+        // risk_desc
+        [NameInMap("risk_desc")]
+        [Validation(Required=true)]
+        public string RiskDesc { get; set; }
+
+        // sug_action
+        [NameInMap("sug_action")]
+        [Validation(Required=true)]
+        public string SugAction { get; set; }
+
+        // risk_labels
+        [NameInMap("risk_labels")]
+        [Validation(Required=true)]
+        public List<string> RiskLabels { get; set; }
+
+    }
+
+}
