37176129f695d14a8901d3817d69f226835f6d0b
sdk-team <sdk-team@alibabacloud.com>
2023-06-12 05:49:41 +0000

    Generated SDK for STLR.

--- stlr/csharp/core/Properties/AssemblyInfo.cs
+++ stlr/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cc4e0e50-08e3-11ee-8985-3957e0f8727d")]
+[assembly: Guid("e860ae80-08e4-11ee-9b09-d568d6e92307")]
 
 // Version information for an assembly consists of the following four values:
 //
