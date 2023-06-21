17cf22624a0b696bc157a32477125e13a6970d74
sdk-team <sdk-team@alibabacloud.com>
2023-06-21 07:26:32 +0000

    Generated SDK for MPAASFACEVERIFY.

--- mpaasfaceverify/composer/src/Client.php
+++ mpaasfaceverify/composer/src/Client.php
@@ -184,7 +184,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.1.19',
+                    'sdk_version'      => '1.2.0',
                     '_prod_code'       => 'MPAASFACEVERIFY',
                     '_prod_channel'    => 'undefined',
                 ];
