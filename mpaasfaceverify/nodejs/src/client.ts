0096f80cab4ee7fb530cf801ba2372fdf4a51bc4
sdk-team <sdk-team@alibabacloud.com>
2023-06-21 07:26:50 +0000

    Generated SDK for MPAASFACEVERIFY.

--- mpaasfaceverify/nodejs/src/client.ts
+++ mpaasfaceverify/nodejs/src/client.ts
@@ -1287,7 +1287,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.1.19",
+          sdk_version: "1.2.0",
           _prod_code: "MPAASFACEVERIFY",
           _prod_channel: "undefined",
         };
