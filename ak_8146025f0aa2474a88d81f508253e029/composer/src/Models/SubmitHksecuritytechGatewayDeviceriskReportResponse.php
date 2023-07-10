ab9bac3e7fef034d505c912083e0770d13e43474
sdk-team <sdk-team@alibabacloud.com>
2023-07-10 07:46:23 +0000

    Generated SDK for ak_8146025f0aa2474a88d81f508253e029.

--- ak_8146025f0aa2474a88d81f508253e029/composer/src/Models/SubmitHksecuritytechGatewayDeviceriskReportResponse.php
+++ ak_8146025f0aa2474a88d81f508253e029/composer/src/Models/SubmitHksecuritytechGatewayDeviceriskReportResponse.php
@@ -26,23 +26,79 @@ class SubmitHksecuritytechGatewayDeviceriskReportResponse extends Model
      */
     public $resultMsg;
 
-    // 1000
+    // success
     /**
-     * @var int
+     * @var bool
      */
-    public $resultStatus;
+    public $success;
 
-    // result
+    // apdid
     /**
-     * @var DeviceRiskReportResult
+     * @var string
      */
-    public $result;
+    public $apdid;
+
+    // token
+    /**
+     * @var string
+     */
+    public $token;
+
+    // current_time
+    /**
+     * @var string
+     */
+    public $currentTime;
+
+    // version
+    /**
+     * @var string
+     */
+    public $version;
+
+    // vkey_switch
+    /**
+     * @var string
+     */
+    public $vkeySwitch;
+
+    // bug_track_switch
+    /**
+     * @var string
+     */
+    public $bugTrackSwitch;
+
+    // app_list_ver
+    /**
+     * @var string
+     */
+    public $appListVer;
+
+    // dynamic_key
+    /**
+     * @var string
+     */
+    public $dynamicKey;
+
+    // result_data
+    /**
+     * @var DeviceRiskReportResultData
+     */
+    public $resultData;
     protected $_name = [
-        'reqMsgId'     => 'req_msg_id',
-        'resultCode'   => 'result_code',
-        'resultMsg'    => 'result_msg',
-        'resultStatus' => 'result_status',
-        'result'       => 'result',
+        'reqMsgId'       => 'req_msg_id',
+        'resultCode'     => 'result_code',
+        'resultMsg'      => 'result_msg',
+        'success'        => 'success',
+        'apdid'          => 'apdid',
+        'token'          => 'token',
+        'currentTime'    => 'current_time',
+        'version'        => 'version',
+        'vkeySwitch'     => 'vkey_switch',
+        'bugTrackSwitch' => 'bug_track_switch',
+        'appListVer'     => 'app_list_ver',
+        'dynamicKey'     => 'dynamic_key',
+        'resultData'     => 'result_data',
     ];
 
     public function validate()
@@ -61,11 +117,35 @@ class SubmitHksecuritytechGatewayDeviceriskReportResponse extends Model
         if (null !== $this->resultMsg) {
             $res['result_msg'] = $this->resultMsg;
         }
-        if (null !== $this->resultStatus) {
-            $res['result_status'] = $this->resultStatus;
+        if (null !== $this->success) {
+            $res['success'] = $this->success;
         }
-        if (null !== $this->result) {
-            $res['result'] = null !== $this->result ? $this->result->toMap() : null;
+        if (null !== $this->apdid) {
+            $res['apdid'] = $this->apdid;
+        }
+        if (null !== $this->token) {
+            $res['token'] = $this->token;
+        }
+        if (null !== $this->currentTime) {
+            $res['current_time'] = $this->currentTime;
+        }
+        if (null !== $this->version) {
+            $res['version'] = $this->version;
+        }
+        if (null !== $this->vkeySwitch) {
+            $res['vkey_switch'] = $this->vkeySwitch;
+        }
+        if (null !== $this->bugTrackSwitch) {
+            $res['bug_track_switch'] = $this->bugTrackSwitch;
+        }
+        if (null !== $this->appListVer) {
+            $res['app_list_ver'] = $this->appListVer;
+        }
+        if (null !== $this->dynamicKey) {
+            $res['dynamic_key'] = $this->dynamicKey;
+        }
+        if (null !== $this->resultData) {
+            $res['result_data'] = null !== $this->resultData ? $this->resultData->toMap() : null;
         }
 
         return $res;
@@ -88,11 +168,35 @@ class SubmitHksecuritytechGatewayDeviceriskReportResponse extends Model
         if (isset($map['result_msg'])) {
             $model->resultMsg = $map['result_msg'];
         }
-        if (isset($map['result_status'])) {
-            $model->resultStatus = $map['result_status'];
+        if (isset($map['success'])) {
+            $model->success = $map['success'];
         }
-        if (isset($map['result'])) {
-            $model->result = DeviceRiskReportResult::fromMap($map['result']);
+        if (isset($map['apdid'])) {
+            $model->apdid = $map['apdid'];
+        }
+        if (isset($map['token'])) {
+            $model->token = $map['token'];
+        }
+        if (isset($map['current_time'])) {
+            $model->currentTime = $map['current_time'];
+        }
+        if (isset($map['version'])) {
+            $model->version = $map['version'];
+        }
+        if (isset($map['vkey_switch'])) {
+            $model->vkeySwitch = $map['vkey_switch'];
+        }
+        if (isset($map['bug_track_switch'])) {
+            $model->bugTrackSwitch = $map['bug_track_switch'];
+        }
+        if (isset($map['app_list_ver'])) {
+            $model->appListVer = $map['app_list_ver'];
+        }
+        if (isset($map['dynamic_key'])) {
+            $model->dynamicKey = $map['dynamic_key'];
+        }
+        if (isset($map['result_data'])) {
+            $model->resultData = DeviceRiskReportResultData::fromMap($map['result_data']);
         }
 
         return $model;
