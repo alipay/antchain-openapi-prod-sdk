ce2a2643e07f040934d8f4895f45946000e0480e
sdk-team <sdk-team@alibabacloud.com>
2023-07-18 07:39:48 +0000

    Generated SDK for ak_743ccb6d20f546bcb851af6ce09cd478.

---
+++ ak_743ccb6d20f546bcb851af6ce09cd478/csharp/core/Models/QueryIdentityTagScoreRequest.cs
@@ -0,0 +1,73 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_743ccb6d20f546bcb851af6ce09cd478.Models
+{
+    public class QueryIdentityTagScoreRequest : TeaModel {
+        // OAuth模式下的授权token
+        [NameInMap("auth_token")]
+        [Validation(Required=false)]
+        public string AuthToken { get; set; }
+
+        [NameInMap("product_instance_id")]
+        [Validation(Required=false)]
+        public string ProductInstanceId { get; set; }
+
+        // 请求流水id
+        [NameInMap("request_id")]
+        [Validation(Required=true)]
+        public string RequestId { get; set; }
+
+        // 订单号
+        [NameInMap("order_id")]
+        [Validation(Required=true)]
+        public string OrderId { get; set; }
+
+        // 模型类型列表
+        [NameInMap("model_id_list")]
+        [Validation(Required=true)]
+        public List<string> ModelIdList { get; set; }
+
+        // 用户id
+        [NameInMap("user_id")]
+        [Validation(Required=true)]
+        public string UserId { get; set; }
+
+        // 用户id类型：
+        // "ID_NO"： 身份证号,
+        // "MOBILE_NO"：手机号
+        [NameInMap("user_id_type")]
+        [Validation(Required=true)]
+        public string UserIdType { get; set; }
+
+        // 加密类型:
+        // "MD5"：MD5（32位小写）
+        // "SHA256" ： SHA256（密文小写）
+        // "SM3"： SM3（密文大写
+        [NameInMap("encrypt_type")]
+        [Validation(Required=true)]
+        public string EncryptType { get; set; }
+
+        // 用户授权模版编号
+        [NameInMap("auth_template_no")]
+        [Validation(Required=true)]
+        public string AuthTemplateNo { get; set; }
+
+        // 用户授权编码
+        [NameInMap("auth_no")]
+        [Validation(Required=true)]
+        public string AuthNo { get; set; }
+
+        // 客户发起请求时间, 格式："yyyy-MM-dd HH:mm:ss"
+        [NameInMap("request_time")]
+        [Validation(Required=false)]
+        public string RequestTime { get; set; }
+
+    }
+
+}
---
+++ ak_743ccb6d20f546bcb851af6ce09cd478/csharp/core/Models/QueryIdentityTagScoreResponse.cs
@@ -0,0 +1,37 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_743ccb6d20f546bcb851af6ce09cd478.Models
+{
+    public class QueryIdentityTagScoreResponse : TeaModel {
+        // 请求唯一ID，用于链路跟踪和问题排查
+        [NameInMap("req_msg_id")]
+        [Validation(Required=false)]
+        public string ReqMsgId { get; set; }
+
+        // 结果码，一般OK表示调用成功
+        [NameInMap("result_code")]
+        [Validation(Required=false)]
+        public string ResultCode { get; set; }
+
+        // 异常信息的文本描述
+        [NameInMap("result_msg")]
+        [Validation(Required=false)]
+        public string ResultMsg { get; set; }
+
+        // 业务响应结果，
+        // score: 模型分数，
+        // modelId：模型id
+        // }
+        [NameInMap("data")]
+        [Validation(Required=false)]
+        public string Data { get; set; }
+
+    }
+
+}
