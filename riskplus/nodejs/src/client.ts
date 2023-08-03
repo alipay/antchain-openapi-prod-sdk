351dc269df40acae2f0c78626fd283b7f42a4957
sdk-team <sdk-team@alibabacloud.com>
2023-08-03 13:15:36 +0000

    Generated SDK for RISKPLUS.

--- riskplus/nodejs/src/client.ts
+++ riskplus/nodejs/src/client.ts
@@ -15200,6 +15200,231 @@ export class ApplyUmktRtBatchmarketingResponse extends $tea.Model {
   }
 }
 
+export class CallbackUmktRobotcallRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  // 外呼号码，支持密文
+  customerKey: string;
+  // 当前呼叫次数	     Integer
+  currentCallTimes: number;
+  // 号码的模版类型
+  keyTemplate: string;
+  // 导入号码时返回的批次号
+  batchId: string;
+  // 外呼类型
+  // 建议按照如下约定给到
+  // 2001:批量-预测外呼
+  // 2002:批量-AI外呼-不转人工
+  // 2003:批量-AI外呼-接通转人工
+  // 2004: 批量-AI外呼-智能转人工
+  // 2005:批量-语音通知
+  callType: number;
+  // 用户自定义标签
+  tag?: string;
+  // 外呼呼叫实例id
+  callId: string;
+  // 外呼任务编号
+  taskId: number;
+  // 外呼的话术模板ID，可以为空
+  templateId?: number;
+  // 外呼状态编码
+  statusCode: number;
+  // 外呼状态编码对应描述
+  statusDescription: string;
+  // 转人工状态编码
+  transferStatusCode: string;
+  // 转人工状态编码对应描述
+  transferStatus: string;
+  // 分配坐席ID,可以为空
+  agentId?: number;
+  // 建议填写坐席在贵司业务系统唯一标识，用于查询对应agentId；可以为空。
+  agentTag?: string;
+  // 坐席分机号，可以为空
+  agentExtension?: string;
+  // 导入时间，格式:2019-01-09 14:14:19
+  importTime: string;
+  // 开始通话时间，格式：2019-01-09 14:14:19
+  callBeginTime: string;
+  // 振铃时长,单位毫秒
+  ringTime: number;
+  // 接通时间
+  answerTime?: string;
+  // 通话时长，单位：大于1分钟，显示分钟秒，小于1分钟，显示秒
+  speakingTime: string;
+  // 通话时长，单位：秒
+  speakingDuration: number;
+  // 通话挂断时间
+  hangupTime: string;
+  // 对话轮次
+  speakingTurns: number;
+  // 坐席通话时长，单位：大于1分钟，显示分钟秒，小于1分钟，显示秒
+  agentSpeakingTime: string;
+  // 坐席通话时长，单位：秒
+  agentSpeakingDuration: number;
+  // 意向标签
+  intentTag: string;
+  // 意向说明
+  intentDescription: string;
+  // 个性标签
+  individualTag?: string;
+  // 回复关键词
+  keywords?: string;
+  // 挂机方式
+  hungupType: number;
+  // 挂机短信，1:发送，2:不发送
+  sms: number;
+  // 对话录音,url
+  chatRecord?: string;
+  // 对话记录
+  chats?: string;
+  // 0:不添加，1:添加
+  addWx?: number;
+  // 加微进度可选值：已申请、加微成功
+  addWxStatus?: string;
+  // 是否接通重呼 0正常外呼，1接通重呼
+  answerRecall: number;
+  // 导入号码时的参数值
+  properties?: string;
+  // 导入号码时的业务参数值
+  bizProperties?: string;
+  // 拦截原因 可选值：黑名单拦截，灰名单拦截，异常号码拦截
+  interceptReason?: string;
+  // 回调冗余字段
+  extInfo?: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+      customerKey: 'customer_key',
+      currentCallTimes: 'current_call_times',
+      keyTemplate: 'key_template',
+      batchId: 'batch_id',
+      callType: 'call_type',
+      tag: 'tag',
+      callId: 'call_id',
+      taskId: 'task_id',
+      templateId: 'template_id',
+      statusCode: 'status_code',
+      statusDescription: 'status_description',
+      transferStatusCode: 'transfer_status_code',
+      transferStatus: 'transfer_status',
+      agentId: 'agent_id',
+      agentTag: 'agent_tag',
+      agentExtension: 'agent_extension',
+      importTime: 'import_time',
+      callBeginTime: 'call_begin_time',
+      ringTime: 'ring_time',
+      answerTime: 'answer_time',
+      speakingTime: 'speaking_time',
+      speakingDuration: 'speaking_duration',
+      hangupTime: 'hangup_time',
+      speakingTurns: 'speaking_turns',
+      agentSpeakingTime: 'agent_speaking_time',
+      agentSpeakingDuration: 'agent_speaking_duration',
+      intentTag: 'intent_tag',
+      intentDescription: 'intent_description',
+      individualTag: 'individual_tag',
+      keywords: 'keywords',
+      hungupType: 'hungup_type',
+      sms: 'sms',
+      chatRecord: 'chat_record',
+      chats: 'chats',
+      addWx: 'add_wx',
+      addWxStatus: 'add_wx_status',
+      answerRecall: 'answer_recall',
+      properties: 'properties',
+      bizProperties: 'biz_properties',
+      interceptReason: 'intercept_reason',
+      extInfo: 'ext_info',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+      customerKey: 'string',
+      currentCallTimes: 'number',
+      keyTemplate: 'string',
+      batchId: 'string',
+      callType: 'number',
+      tag: 'string',
+      callId: 'string',
+      taskId: 'number',
+      templateId: 'number',
+      statusCode: 'number',
+      statusDescription: 'string',
+      transferStatusCode: 'string',
+      transferStatus: 'string',
+      agentId: 'number',
+      agentTag: 'string',
+      agentExtension: 'string',
+      importTime: 'string',
+      callBeginTime: 'string',
+      ringTime: 'number',
+      answerTime: 'string',
+      speakingTime: 'string',
+      speakingDuration: 'number',
+      hangupTime: 'string',
+      speakingTurns: 'number',
+      agentSpeakingTime: 'string',
+      agentSpeakingDuration: 'number',
+      intentTag: 'string',
+      intentDescription: 'string',
+      individualTag: 'string',
+      keywords: 'string',
+      hungupType: 'number',
+      sms: 'number',
+      chatRecord: 'string',
+      chats: 'string',
+      addWx: 'number',
+      addWxStatus: 'string',
+      answerRecall: 'number',
+      properties: 'string',
+      bizProperties: 'string',
+      interceptReason: 'string',
+      extInfo: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class CallbackUmktRobotcallResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  // 是否成功
+  success?: boolean;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+      success: 'success',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+      success: 'boolean',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class CreateAntcloudGatewayxFileUploadRequest extends $tea.Model {
   // OAuth模式下的授权token
   authToken?: string;
@@ -15401,7 +15626,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.16.36",
+          sdk_version: "1.16.38",
           _prod_code: "RISKPLUS",
           _prod_channel: "undefined",
         };
@@ -18252,6 +18477,25 @@ export default class Client {
   async applyUmktRtBatchmarketingEx(request: ApplyUmktRtBatchmarketingRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<ApplyUmktRtBatchmarketingResponse> {
     Util.validateModel(request);
     return $tea.cast<ApplyUmktRtBatchmarketingResponse>(await this.doRequest("1.0", "riskplus.umkt.rt.batchmarketing.apply", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new ApplyUmktRtBatchmarketingResponse({}));
+  }
+
+  /**
+   * Description: 新接入ai外呼服务商的回调接口
+   * Summary: ai外呼回调接口
+   */
+  async callbackUmktRobotcall(request: CallbackUmktRobotcallRequest): Promise<CallbackUmktRobotcallResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.callbackUmktRobotcallEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 新接入ai外呼服务商的回调接口
+   * Summary: ai外呼回调接口
+   */
+  async callbackUmktRobotcallEx(request: CallbackUmktRobotcallRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<CallbackUmktRobotcallResponse> {
+    Util.validateModel(request);
+    return $tea.cast<CallbackUmktRobotcallResponse>(await this.doRequest("1.0", "riskplus.umkt.robotcall.callback", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new CallbackUmktRobotcallResponse({}));
   }
 
   /**
