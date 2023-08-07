7ffbf4f529fa3812618f0bb0103e449451abba38
sdk-team <sdk-team@alibabacloud.com>
2023-08-07 08:05:23 +0000

    Generated SDK for NFTC.

--- nftc/csharp/core/Properties/AssemblyInfo.cs
+++ nftc/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("dc887180-34f7-11ee-a4c3-2973a498c524")]
+[assembly: Guid("22155af0-34f9-11ee-a6d5-0f18c4d20994")]
 
 // Version information for an assembly consists of the following four values:
 //
