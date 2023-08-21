54f69ec94cc49e6b27db71838a8da38af0ec1e91
sdk-team <sdk-team@alibabacloud.com>
2023-08-21 07:28:05 +0000

    Generated SDK for IDENTITYMARRIAGE.

--- identitymarriage/go/client/client.go
+++ identitymarriage/go/client/client.go
@@ -365,6 +365,76 @@ func (s *UploadFileDataResponse) SetData(v string) *UploadFileDataResponse {
 	return s
 }
 
+type CheckMarriageCoupleinfoRequest struct {
+	// OAuth模式下的授权token
+	AuthToken         *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
+	ProductInstanceId *string `json:"product_instance_id,omitempty" xml:"product_instance_id,omitempty"`
+	// 加密字段
+	BizContent *string `json:"biz_content,omitempty" xml:"biz_content,omitempty" require:"true"`
+}
+
+func (s CheckMarriageCoupleinfoRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s CheckMarriageCoupleinfoRequest) GoString() string {
+	return s.String()
+}
+
+func (s *CheckMarriageCoupleinfoRequest) SetAuthToken(v string) *CheckMarriageCoupleinfoRequest {
+	s.AuthToken = &v
+	return s
+}
+
+func (s *CheckMarriageCoupleinfoRequest) SetProductInstanceId(v string) *CheckMarriageCoupleinfoRequest {
+	s.ProductInstanceId = &v
+	return s
+}
+
+func (s *CheckMarriageCoupleinfoRequest) SetBizContent(v string) *CheckMarriageCoupleinfoRequest {
+	s.BizContent = &v
+	return s
+}
+
+type CheckMarriageCoupleinfoResponse struct {
+	// 请求唯一ID，用于链路跟踪和问题排查
+	ReqMsgId *string `json:"req_msg_id,omitempty" xml:"req_msg_id,omitempty"`
+	// 结果码，一般OK表示调用成功
+	ResultCode *string `json:"result_code,omitempty" xml:"result_code,omitempty"`
+	// 异常信息的文本描述
+	ResultMsg *string `json:"result_msg,omitempty" xml:"result_msg,omitempty"`
+	// 返回数据
+	Data *string `json:"data,omitempty" xml:"data,omitempty"`
+}
+
+func (s CheckMarriageCoupleinfoResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s CheckMarriageCoupleinfoResponse) GoString() string {
+	return s.String()
+}
+
+func (s *CheckMarriageCoupleinfoResponse) SetReqMsgId(v string) *CheckMarriageCoupleinfoResponse {
+	s.ReqMsgId = &v
+	return s
+}
+
+func (s *CheckMarriageCoupleinfoResponse) SetResultCode(v string) *CheckMarriageCoupleinfoResponse {
+	s.ResultCode = &v
+	return s
+}
+
+func (s *CheckMarriageCoupleinfoResponse) SetResultMsg(v string) *CheckMarriageCoupleinfoResponse {
+	s.ResultMsg = &v
+	return s
+}
+
+func (s *CheckMarriageCoupleinfoResponse) SetData(v string) *CheckMarriageCoupleinfoResponse {
+	s.Data = &v
+	return s
+}
+
 type Client struct {
 	Endpoint                *string
 	RegionId                *string
@@ -487,7 +557,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.0.6"),
+				"sdk_version":      tea.String("1.0.8"),
 				"_prod_code":       tea.String("IDENTITYMARRIAGE"),
 				"_prod_channel":    tea.String("undefined"),
 			}
@@ -640,6 +710,40 @@ func (client *Client) UploadFileDataEx(request *UploadFileDataRequest, headers m
 	}
 	_result = &UploadFileDataResponse{}
 	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("identity.marriage.file.data.upload"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+/**
+ * Description: 双人婚姻状况核查
+ * Summary: 双人婚姻状况核查
+ */
+func (client *Client) CheckMarriageCoupleinfo(request *CheckMarriageCoupleinfoRequest) (_result *CheckMarriageCoupleinfoResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &CheckMarriageCoupleinfoResponse{}
+	_body, _err := client.CheckMarriageCoupleinfoEx(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+/**
+ * Description: 双人婚姻状况核查
+ * Summary: 双人婚姻状况核查
+ */
+func (client *Client) CheckMarriageCoupleinfoEx(request *CheckMarriageCoupleinfoRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *CheckMarriageCoupleinfoResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = &CheckMarriageCoupleinfoResponse{}
+	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("identity.marriage.marriage.coupleinfo.check"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
 	if _err != nil {
 		return _result, _err
 	}
