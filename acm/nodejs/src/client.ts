66aab0c5afa5740bf82d386ac14b9691a98394bf
sdk-team <sdk-team@alibabacloud.com>
2023-09-13 06:18:25 +0000

    Generated SDK for ACM.

--- acm/nodejs/src/client.ts
+++ acm/nodejs/src/client.ts
@@ -3454,7 +3454,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.4.8",
+          sdk_version: "1.4.9",
           _prod_code: "acm",
           _prod_channel: "undefined",
         };
