df9ac1faa8bef03b7c33303db0b255b09e41802c
sdk-team <sdk-team@alibabacloud.com>
2023-09-13 06:18:08 +0000

    Generated SDK for ACM.

--- acm/go/client/client.go
+++ acm/go/client/client.go
@@ -4488,7 +4488,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.4.8"),
+				"sdk_version":      tea.String("1.4.9"),
 				"_prod_code":       tea.String("acm"),
 				"_prod_channel":    tea.String("undefined"),
 			}
