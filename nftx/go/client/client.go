969bb898286107294d77d8b138899d330b0ea71d
sdk-team <sdk-team@alibabacloud.com>
2023-09-18 12:11:11 +0000

    Generated SDK for NFTX.

--- nftx/go/client/client.go
+++ nftx/go/client/client.go
@@ -2343,6 +2343,97 @@ func (s *ImportResourceConsigneeinfoResponse) SetResultMsg(v string) *ImportReso
 	return s
 }
 
+type UpdateNftPropertyRequest struct {
+	// OAuth模式下的授权token
+	AuthToken         *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
+	ProductInstanceId *string `json:"product_instance_id,omitempty" xml:"product_instance_id,omitempty"`
+	// 用户id标识
+	IdNo *string `json:"id_no,omitempty" xml:"id_no,omitempty" require:"true"`
+	// 用户id类型
+	IdType *string `json:"id_type,omitempty" xml:"id_type,omitempty" require:"true"`
+	// 资产标识
+	NftId *string `json:"nft_id,omitempty" xml:"nft_id,omitempty" require:"true"`
+	// 更新的来源
+	Source *string `json:"source,omitempty" xml:"source,omitempty" require:"true"`
+	// Json字符串：通过解析该字段，根据提前约定的key进行取值操作
+	UpdateInfo *string `json:"update_info,omitempty" xml:"update_info,omitempty" require:"true"`
+}
+
+func (s UpdateNftPropertyRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s UpdateNftPropertyRequest) GoString() string {
+	return s.String()
+}
+
+func (s *UpdateNftPropertyRequest) SetAuthToken(v string) *UpdateNftPropertyRequest {
+	s.AuthToken = &v
+	return s
+}
+
+func (s *UpdateNftPropertyRequest) SetProductInstanceId(v string) *UpdateNftPropertyRequest {
+	s.ProductInstanceId = &v
+	return s
+}
+
+func (s *UpdateNftPropertyRequest) SetIdNo(v string) *UpdateNftPropertyRequest {
+	s.IdNo = &v
+	return s
+}
+
+func (s *UpdateNftPropertyRequest) SetIdType(v string) *UpdateNftPropertyRequest {
+	s.IdType = &v
+	return s
+}
+
+func (s *UpdateNftPropertyRequest) SetNftId(v string) *UpdateNftPropertyRequest {
+	s.NftId = &v
+	return s
+}
+
+func (s *UpdateNftPropertyRequest) SetSource(v string) *UpdateNftPropertyRequest {
+	s.Source = &v
+	return s
+}
+
+func (s *UpdateNftPropertyRequest) SetUpdateInfo(v string) *UpdateNftPropertyRequest {
+	s.UpdateInfo = &v
+	return s
+}
+
+type UpdateNftPropertyResponse struct {
+	// 请求唯一ID，用于链路跟踪和问题排查
+	ReqMsgId *string `json:"req_msg_id,omitempty" xml:"req_msg_id,omitempty"`
+	// 结果码，一般OK表示调用成功
+	ResultCode *string `json:"result_code,omitempty" xml:"result_code,omitempty"`
+	// 异常信息的文本描述
+	ResultMsg *string `json:"result_msg,omitempty" xml:"result_msg,omitempty"`
+}
+
+func (s UpdateNftPropertyResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s UpdateNftPropertyResponse) GoString() string {
+	return s.String()
+}
+
+func (s *UpdateNftPropertyResponse) SetReqMsgId(v string) *UpdateNftPropertyResponse {
+	s.ReqMsgId = &v
+	return s
+}
+
+func (s *UpdateNftPropertyResponse) SetResultCode(v string) *UpdateNftPropertyResponse {
+	s.ResultCode = &v
+	return s
+}
+
+func (s *UpdateNftPropertyResponse) SetResultMsg(v string) *UpdateNftPropertyResponse {
+	s.ResultMsg = &v
+	return s
+}
+
 type ApplyOauthTokenRequest struct {
 	// OAuth模式下的授权token
 	AuthToken         *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
@@ -2664,7 +2755,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.8.10"),
+				"sdk_version":      tea.String("1.9.0"),
 				"_prod_code":       tea.String("NFTX"),
 				"_prod_channel":    tea.String("undefined"),
 			}
@@ -3293,6 +3384,40 @@ func (client *Client) ImportResourceConsigneeinfoEx(request *ImportResourceConsi
 	}
 	_result = &ImportResourceConsigneeinfoResponse{}
 	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("antchain.nftx.resource.consigneeinfo.import"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+/**
+ * Description: 资产属性更新开放接口
+ * Summary: 资产属性更新
+ */
+func (client *Client) UpdateNftProperty(request *UpdateNftPropertyRequest) (_result *UpdateNftPropertyResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &UpdateNftPropertyResponse{}
+	_body, _err := client.UpdateNftPropertyEx(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+/**
+ * Description: 资产属性更新开放接口
+ * Summary: 资产属性更新
+ */
+func (client *Client) UpdateNftPropertyEx(request *UpdateNftPropertyRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *UpdateNftPropertyResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = &UpdateNftPropertyResponse{}
+	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("antchain.nftx.nft.property.update"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
 	if _err != nil {
 		return _result, _err
 	}
