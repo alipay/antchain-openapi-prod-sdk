8e70700993d01c2c8e5320b2cb0ddde9ca50a9b9
sdk-team <sdk-team@alibabacloud.com>
2023-11-01 06:54:08 +0000

    Generated SDK for ANTVERSE.

--- antverse/nodejs/src/client.ts
+++ antverse/nodejs/src/client.ts
@@ -461,7 +461,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.5",
+          sdk_version: "1.0.6",
           _prod_code: "ANTVERSE",
           _prod_channel: "undefined",
         };
