0dc17e5e1498490d226ee3569ea026dfac707fa7
sdk-team <sdk-team@alibabacloud.com>
2023-11-09 09:20:42 +0000

    Generated SDK for ak_195dff03d395462ea294bafdba69df3f.

---
+++ ak_195dff03d395462ea294bafdba69df3f/nodejs/src/client.ts
@@ -0,0 +1,502 @@
+// This file is auto-generated, don't edit it
+import AntchainUtil from '@antchain/alipay-util';
+import Util, * as $Util from '@alicloud/tea-util';
+import RPCUtil from '@alicloud/rpc-util';
+import * as $tea from '@alicloud/tea-typescript';
+
+/**
+ * Model for initing client
+ */
+export class Config extends $tea.Model {
+  accessKeyId?: string;
+  accessKeySecret?: string;
+  securityToken?: string;
+  protocol?: string;
+  readTimeout?: number;
+  connectTimeout?: number;
+  httpProxy?: string;
+  httpsProxy?: string;
+  endpoint?: string;
+  noProxy?: string;
+  maxIdleConns?: number;
+  userAgent?: string;
+  socks5Proxy?: string;
+  socks5NetWork?: string;
+  maxIdleTimeMillis?: number;
+  keepAliveDurationMillis?: number;
+  maxRequests?: number;
+  maxRequestsPerHost?: number;
+  static names(): { [key: string]: string } {
+    return {
+      accessKeyId: 'accessKeyId',
+      accessKeySecret: 'accessKeySecret',
+      securityToken: 'securityToken',
+      protocol: 'protocol',
+      readTimeout: 'readTimeout',
+      connectTimeout: 'connectTimeout',
+      httpProxy: 'httpProxy',
+      httpsProxy: 'httpsProxy',
+      endpoint: 'endpoint',
+      noProxy: 'noProxy',
+      maxIdleConns: 'maxIdleConns',
+      userAgent: 'userAgent',
+      socks5Proxy: 'socks5Proxy',
+      socks5NetWork: 'socks5NetWork',
+      maxIdleTimeMillis: 'maxIdleTimeMillis',
+      keepAliveDurationMillis: 'keepAliveDurationMillis',
+      maxRequests: 'maxRequests',
+      maxRequestsPerHost: 'maxRequestsPerHost',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      accessKeyId: 'string',
+      accessKeySecret: 'string',
+      securityToken: 'string',
+      protocol: 'string',
+      readTimeout: 'number',
+      connectTimeout: 'number',
+      httpProxy: 'string',
+      httpsProxy: 'string',
+      endpoint: 'string',
+      noProxy: 'string',
+      maxIdleConns: 'number',
+      userAgent: 'string',
+      socks5Proxy: 'string',
+      socks5NetWork: 'string',
+      maxIdleTimeMillis: 'number',
+      keepAliveDurationMillis: 'number',
+      maxRequests: 'number',
+      maxRequestsPerHost: 'number',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class SubmitAntchainAtoFrontSignRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  // 商户的订单号
+  orderId: string;
+  // CRED_PSN_CH_IDCARD： 大陆身份证 
+  // CRED_PSN_CH_TWCARD：台湾来往大陆通行证 
+  // CRED_PSN_CH_MACAO"：澳门来往大陆通行证 
+  // CRED_PSN_CH_HONGKONG：香港来往大陆通行证 
+  // CRED_PSN_PASSPORT：护照
+  userIdType: string;
+  // 用户证件号，需要采用RSA加密传输
+  userIdNumber: string;
+  // 姓名，需要采用RSA加密传输
+  // 
+  userName: string;
+  // 用户手机号，可不传；传的话需要采用RSA加密传输
+  // 
+  userMobile?: string;
+  // 用户的电子邮箱，可不传；传的话需要采用RSA加密传输
+  // 
+  userEmail?: string;
+  // 签署有效期，时间戳，例如：new Date().getTime()
+  signValidity?: string;
+  // 1-短信；2-邮件
+  flowNotifyType?: string;
+  // 业务场景，电子合同签署协议的时候的标题
+  // 
+  businessScene: string;
+  // 签署完成跳转链接
+  // 
+  signedRedirectUrl?: string;
+  // 签署的电子合同模板信息，List<Object>的JSON格式，Object如下： { templateId:__, // String格式 templateArgs: { "模板参数key":"模板参数值", // 必须为String } }
+  templateList: string;
+  // 用户的支付宝uid
+  alipayUserId: string;
+  // 商户签署区域标识。对应在合同模板的机构签署区域中的tag值(如果合同模板的签署区域的tag值为空，则可以不传这个参数)。必须完全对应，否则在多方签署的情况下根据tag找到不到对应的签署机构，会出错。
+  merchantName: string;
+  // 商户签署区域标识。对应在合同模板的机构签署区域中的tag值(如果合同模板的签署区域的tag值为空，则可以不传这个参数)。必须完全对应，否则在多方签署的情况下根据tag找到不到对应的签署机构，会出错。
+  merchantTag?: string;
+  // 电子合同签署顺序，如果只有1方企业签署，传入1即可。如果是多方，并且需要设置签署顺序，则需要将这个值以及thirdSigner中的signOrder做一个签署顺序。
+  merchantSignOrder?: number;
+  // CRED_ORG_USCC：统一社会信用代码，
+  // CRED_ORG_REGCODE：工商注册号，
+  // 只支持这两个值
+  merchantIdType: string;
+  // 商户证件号，需要采用RSA加密传输
+  merchantIdNumber: string;
+  // 法人姓名，需要RSA加密传输
+  // 
+  merchantLegalName?: string;
+  // 法人证件号，需要采用RSA加密传输
+  // 
+  merchantLegalIdNumber?: string;
+  // 多方签署的其他参与方的签署信息，json的array格式，参考：[{"tag":"zf_a","orgName":"上海网络科技有限公司","orgIdType":"CRED_ORG_REGCODE","orgIdNumber":"12098760923","orgLegalName":"王大浪","orgLegalIdNumber":"107120196708289012"}]，其中：orgIdNumber、orgLegalName、orgLegalIdNumber需要加密传输。
+  thirdSigner?: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+      orderId: 'order_id',
+      userIdType: 'user_id_type',
+      userIdNumber: 'user_id_number',
+      userName: 'user_name',
+      userMobile: 'user_mobile',
+      userEmail: 'user_email',
+      signValidity: 'sign_validity',
+      flowNotifyType: 'flow_notify_type',
+      businessScene: 'business_scene',
+      signedRedirectUrl: 'signed_redirect_url',
+      templateList: 'template_list',
+      alipayUserId: 'alipay_user_id',
+      merchantName: 'merchant_name',
+      merchantTag: 'merchant_tag',
+      merchantSignOrder: 'merchant_sign_order',
+      merchantIdType: 'merchant_id_type',
+      merchantIdNumber: 'merchant_id_number',
+      merchantLegalName: 'merchant_legal_name',
+      merchantLegalIdNumber: 'merchant_legal_id_number',
+      thirdSigner: 'third_signer',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+      orderId: 'string',
+      userIdType: 'string',
+      userIdNumber: 'string',
+      userName: 'string',
+      userMobile: 'string',
+      userEmail: 'string',
+      signValidity: 'string',
+      flowNotifyType: 'string',
+      businessScene: 'string',
+      signedRedirectUrl: 'string',
+      templateList: 'string',
+      alipayUserId: 'string',
+      merchantName: 'string',
+      merchantTag: 'string',
+      merchantSignOrder: 'number',
+      merchantIdType: 'string',
+      merchantIdNumber: 'string',
+      merchantLegalName: 'string',
+      merchantLegalIdNumber: 'string',
+      thirdSigner: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class SubmitAntchainAtoFrontSignResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  // 签署合同单号
+  // 
+  signNo?: string;
+  // 电子签署流程ID
+  // 
+  flowId?: string;
+  // 签署用户ID
+  // 
+  accountId?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+      signNo: 'sign_no',
+      flowId: 'flow_id',
+      accountId: 'account_id',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+      signNo: 'string',
+      flowId: 'string',
+      accountId: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class SyncAntchainAtoFrontTradeRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  // 类型
+  // 1. orderFullInfo ， 订单所有信息同步
+  type: string;
+  // json.toString
+  bizContent: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+      type: 'type',
+      bizContent: 'biz_content',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+      type: 'string',
+      bizContent: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class SyncAntchainAtoFrontTradeResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  // 返回信息
+  responseData?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+      responseData: 'response_data',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+      responseData: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+
+export default class Client {
+  _endpoint: string;
+  _regionId: string;
+  _accessKeyId: string;
+  _accessKeySecret: string;
+  _protocol: string;
+  _userAgent: string;
+  _readTimeout: number;
+  _connectTimeout: number;
+  _httpProxy: string;
+  _httpsProxy: string;
+  _socks5Proxy: string;
+  _socks5NetWork: string;
+  _noProxy: string;
+  _maxIdleConns: number;
+  _securityToken: string;
+  _maxIdleTimeMillis: number;
+  _keepAliveDurationMillis: number;
+  _maxRequests: number;
+  _maxRequestsPerHost: number;
+
+  /**
+   * Init client with Config
+   * @param config config contains the necessary information to create a client
+   */
+  constructor(config: Config) {
+    if (Util.isUnset($tea.toMap(config))) {
+      throw $tea.newError({
+        code: "ParameterMissing",
+        message: "'config' can not be unset",
+      });
+    }
+
+    this._accessKeyId = config.accessKeyId;
+    this._accessKeySecret = config.accessKeySecret;
+    this._securityToken = config.securityToken;
+    this._endpoint = config.endpoint;
+    this._protocol = config.protocol;
+    this._userAgent = config.userAgent;
+    this._readTimeout = Util.defaultNumber(config.readTimeout, 20000);
+    this._connectTimeout = Util.defaultNumber(config.connectTimeout, 20000);
+    this._httpProxy = config.httpProxy;
+    this._httpsProxy = config.httpsProxy;
+    this._noProxy = config.noProxy;
+    this._socks5Proxy = config.socks5Proxy;
+    this._socks5NetWork = config.socks5NetWork;
+    this._maxIdleConns = Util.defaultNumber(config.maxIdleConns, 60000);
+    this._maxIdleTimeMillis = Util.defaultNumber(config.maxIdleTimeMillis, 5);
+    this._keepAliveDurationMillis = Util.defaultNumber(config.keepAliveDurationMillis, 5000);
+    this._maxRequests = Util.defaultNumber(config.maxRequests, 100);
+    this._maxRequestsPerHost = Util.defaultNumber(config.maxRequestsPerHost, 100);
+  }
+
+  /**
+   * Encapsulate the request and invoke the network
+   * @param action api name
+   * @param protocol http or https
+   * @param method e.g. GET
+   * @param pathname pathname of every api
+   * @param request which contains request params
+   * @param runtime which controls some details of call api, such as retry times
+   * @return the response
+   */
+  async doRequest(version: string, action: string, protocol: string, method: string, pathname: string, request: {[key: string]: any}, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<{[key: string]: any}> {
+    let _runtime: { [key: string]: any } = {
+      timeouted: "retry",
+      readTimeout: Util.defaultNumber(runtime.readTimeout, this._readTimeout),
+      connectTimeout: Util.defaultNumber(runtime.connectTimeout, this._connectTimeout),
+      httpProxy: Util.defaultString(runtime.httpProxy, this._httpProxy),
+      httpsProxy: Util.defaultString(runtime.httpsProxy, this._httpsProxy),
+      noProxy: Util.defaultString(runtime.noProxy, this._noProxy),
+      maxIdleConns: Util.defaultNumber(runtime.maxIdleConns, this._maxIdleConns),
+      maxIdleTimeMillis: this._maxIdleTimeMillis,
+      keepAliveDuration: this._keepAliveDurationMillis,
+      maxRequests: this._maxRequests,
+      maxRequestsPerHost: this._maxRequestsPerHost,
+      retry: {
+        retryable: runtime.autoretry,
+        maxAttempts: Util.defaultNumber(runtime.maxAttempts, 3),
+      },
+      backoff: {
+        policy: Util.defaultString(runtime.backoffPolicy, "no"),
+        period: Util.defaultNumber(runtime.backoffPeriod, 1),
+      },
+      ignoreSSL: runtime.ignoreSSL,
+    }
+
+    let _lastRequest = null;
+    let _now = Date.now();
+    let _retryTimes = 0;
+    while ($tea.allowRetry(_runtime['retry'], _retryTimes, _now)) {
+      if (_retryTimes > 0) {
+        let _backoffTime = $tea.getBackoffTime(_runtime['backoff'], _retryTimes);
+        if (_backoffTime > 0) {
+          await $tea.sleep(_backoffTime);
+        }
+      }
+
+      _retryTimes = _retryTimes + 1;
+      try {
+        let request_ = new $tea.Request();
+        request_.protocol = Util.defaultString(this._protocol, protocol);
+        request_.method = method;
+        request_.pathname = pathname;
+        request_.query = {
+          method: action,
+          version: version,
+          sign_type: "HmacSHA1",
+          req_time: AntchainUtil.getTimestamp(),
+          req_msg_id: AntchainUtil.getNonce(),
+          access_key: this._accessKeyId,
+          base_sdk_version: "TeaSDK-2.0",
+          sdk_version: "1.0.0",
+          _prod_code: "ak_195dff03d395462ea294bafdba69df3f",
+          _prod_channel: "saas",
+        };
+        if (!Util.empty(this._securityToken)) {
+          request_.query["security_token"] = this._securityToken;
+        }
+
+        request_.headers = {
+          host: Util.defaultString(this._endpoint, "openapi.antchain.antgroup.com"),
+          'user-agent': Util.getUserAgent(this._userAgent),
+          ...headers,
+        };
+        let tmp = Util.anyifyMapValue(RPCUtil.query(request));
+        request_.body = new $tea.BytesReadable(Util.toFormString(tmp));
+        request_.headers["content-type"] = "application/x-www-form-urlencoded";
+        let signedParam = {
+          ...request_.query,
+          ...RPCUtil.query(request),
+        };
+        request_.query["sign"] = AntchainUtil.getSignature(signedParam, this._accessKeySecret);
+        _lastRequest = request_;
+        let response_ = await $tea.doAction(request_, _runtime);
+
+        let raw = await Util.readAsString(response_.body);
+        let obj = Util.parseJSON(raw);
+        let res = Util.assertAsMap(obj);
+        let resp = Util.assertAsMap(res["response"]);
+        if (AntchainUtil.hasError(raw, this._accessKeySecret)) {
+          throw $tea.newError({
+            message: resp["result_msg"],
+            data: resp,
+            code: resp["result_code"],
+          });
+        }
+
+        return resp;
+      } catch (ex) {
+        if ($tea.isRetryable(ex)) {
+          continue;
+        }
+        throw ex;
+      }
+    }
+
+    throw $tea.newUnretryableError(_lastRequest);
+  }
+
+  /**
+   * Description: 提交前置签署的电子合同签署流程
+   * Summary: 提交前置签署的电子合同签署流程
+   */
+  async submitAntchainAtoFrontSign(request: SubmitAntchainAtoFrontSignRequest): Promise<SubmitAntchainAtoFrontSignResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.submitAntchainAtoFrontSignEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 提交前置签署的电子合同签署流程
+   * Summary: 提交前置签署的电子合同签署流程
+   */
+  async submitAntchainAtoFrontSignEx(request: SubmitAntchainAtoFrontSignRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<SubmitAntchainAtoFrontSignResponse> {
+    Util.validateModel(request);
+    return $tea.cast<SubmitAntchainAtoFrontSignResponse>(await this.doRequest("1.0", "antchain.ato.front.sign.submit", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new SubmitAntchainAtoFrontSignResponse({}));
+  }
+
+  /**
+   * Description: 订单创建，前置签署
+   * Summary: 前置签署订单创建
+   */
+  async syncAntchainAtoFrontTrade(request: SyncAntchainAtoFrontTradeRequest): Promise<SyncAntchainAtoFrontTradeResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.syncAntchainAtoFrontTradeEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 订单创建，前置签署
+   * Summary: 前置签署订单创建
+   */
+  async syncAntchainAtoFrontTradeEx(request: SyncAntchainAtoFrontTradeRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<SyncAntchainAtoFrontTradeResponse> {
+    Util.validateModel(request);
+    return $tea.cast<SyncAntchainAtoFrontTradeResponse>(await this.doRequest("1.0", "antchain.ato.front.trade.sync", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new SyncAntchainAtoFrontTradeResponse({}));
+  }
+
+}
