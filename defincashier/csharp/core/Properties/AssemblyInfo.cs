becd2391bd9144f6c8f395a843c7b6bdb6e73310
sdk-team <sdk-team@alibabacloud.com>
2023-11-23 03:07:17 +0000

    Generated SDK for DEFINCASHIER.

--- defincashier/csharp/core/Properties/AssemblyInfo.cs
+++ defincashier/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4b0131d0-89a6-11ee-92c0-e55043f32383")]
+[assembly: Guid("637edcb0-89ad-11ee-88eb-6104acbe7861")]
 
 // Version information for an assembly consists of the following four values:
 //
