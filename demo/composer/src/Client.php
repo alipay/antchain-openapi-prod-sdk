c65be3d34790dd4997e5e5be7b15f43a654c94f9
sdk-team <sdk-team@alibabacloud.com>
2023-11-29 07:33:10 +0000

    Generated SDK for DEMO.

--- demo/composer/src/Client.php
+++ demo/composer/src/Client.php
@@ -272,7 +272,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.0.229',
+                    'sdk_version'      => '1.0.230',
                     '_prod_code'       => 'DEMO',
                     '_prod_channel'    => 'undefined',
                 ];
