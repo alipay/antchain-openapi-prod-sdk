5bd1153df465263ac22ba4e901dc632b0e996417
sdk-team <sdk-team@alibabacloud.com>
2023-11-29 03:39:54 +0000

    Generated SDK for DEMO.

--- demo/csharp/core/Properties/AssemblyInfo.cs
+++ demo/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("48370000-8e61-11ee-ba18-bbfc27ba09f1")]
+[assembly: Guid("eeb0a100-8e68-11ee-9e4a-03cf2f289f87")]
 
 // Version information for an assembly consists of the following four values:
 //
