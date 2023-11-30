5839005e8308078aff44fde5f06980bbcd1dd974
sdk-team <sdk-team@alibabacloud.com>
2023-11-30 03:24:26 +0000

    Generated SDK for DEMO.

--- demo/nodejs/src/client.ts
+++ demo/nodejs/src/client.ts
@@ -4387,7 +4387,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.230",
+          sdk_version: "1.0.231",
           _prod_code: "DEMO",
           _prod_channel: "undefined",
         };
