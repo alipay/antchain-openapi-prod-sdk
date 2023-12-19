fb5daeafc057d9651735a799049d8218f4256f2e
sdk-team <sdk-team@alibabacloud.com>
2023-12-19 10:45:17 +0000

    Generated SDK for REALPERSON.

--- realperson/composer/src/Client.php
+++ realperson/composer/src/Client.php
@@ -220,7 +220,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.15.1',
+                    'sdk_version'      => '1.15.2',
                     '_prod_code'       => 'REALPERSON',
                     '_prod_channel'    => 'undefined',
                 ];
