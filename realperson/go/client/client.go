aa2d27b1258c686b2062231834d09f13f3bf1364
sdk-team <sdk-team@alibabacloud.com>
2023-12-19 10:45:21 +0000

    Generated SDK for REALPERSON.

--- realperson/go/client/client.go
+++ realperson/go/client/client.go
@@ -4114,7 +4114,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.15.1"),
+				"sdk_version":      tea.String("1.15.2"),
 				"_prod_code":       tea.String("REALPERSON"),
 				"_prod_channel":    tea.String("undefined"),
 			}
