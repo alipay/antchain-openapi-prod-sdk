68b963b789cf33333f8252ac011cd86cff08bf8f
sdk-team <sdk-team@alibabacloud.com>
2023-12-28 13:37:30 +0000

    Generated SDK for BOT.

--- bot/composer/src/Client.php
+++ bot/composer/src/Client.php
@@ -566,7 +566,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.10.2',
+                    'sdk_version'      => '1.10.3',
                     '_prod_code'       => 'BOT',
                     '_prod_channel'    => 'undefined',
                 ];
