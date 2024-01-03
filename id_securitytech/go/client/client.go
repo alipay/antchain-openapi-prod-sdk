1174d67c5639d0c4e45740a1871417ae7ec9d23d
sdk-team <sdk-team@alibabacloud.com>
2024-01-03 01:32:42 +0000

    Generated SDK for ID_SECURITYTECH.

--- id_securitytech/go/client/client.go
+++ id_securitytech/go/client/client.go
@@ -840,7 +840,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.0.0"),
+				"sdk_version":      tea.String("1.0.1"),
 				"_prod_code":       tea.String("ID_SECURITYTECH"),
 				"_prod_channel":    tea.String("undefined"),
 			}
