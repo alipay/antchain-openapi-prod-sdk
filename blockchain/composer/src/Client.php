6813c123101d3962b49e828eb8abe9f7ce23f3a6
sdk-team <sdk-team@alibabacloud.com>
2024-01-08 06:21:12 +0000

    Generated SDK for BLOCKCHAIN.

--- blockchain/composer/src/Client.php
+++ blockchain/composer/src/Client.php
@@ -1366,7 +1366,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.27.11',
+                    'sdk_version'      => '1.27.12',
                     '_prod_code'       => 'BLOCKCHAIN',
                     '_prod_channel'    => 'undefined',
                 ];
