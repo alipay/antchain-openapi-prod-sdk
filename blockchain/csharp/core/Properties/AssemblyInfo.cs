c570f44714a7d55b0aafd0f4016a48ef8eee2c8b
sdk-team <sdk-team@alibabacloud.com>
2024-01-08 05:48:07 +0000

    Generated SDK for BLOCKCHAIN.

--- blockchain/csharp/core/Properties/AssemblyInfo.cs
+++ blockchain/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8c398a70-add3-11ee-b3c5-574dc4140d90")]
+[assembly: Guid("73aa9b50-ade9-11ee-abad-236c9fee1060")]
 
 // Version information for an assembly consists of the following four values:
 //
