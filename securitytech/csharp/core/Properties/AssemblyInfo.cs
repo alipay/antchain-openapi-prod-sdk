ac4885c036ca65e9f6f80d133688a11ef3cec442
sdk-team <sdk-team@alibabacloud.com>
2024-01-25 08:31:10 +0000

    Generated SDK for SECURITYTECH.

--- securitytech/csharp/core/Properties/AssemblyInfo.cs
+++ securitytech/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("42be54b0-bb58-11ee-90ab-fb7a803af606")]
+[assembly: Guid("109b69b0-bb5c-11ee-a0a0-777d97962844")]
 
 // Version information for an assembly consists of the following four values:
 //
