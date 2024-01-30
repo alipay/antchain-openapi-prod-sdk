7c508a92f378ccc5148a860f9ebe5b02bc0e8a97
sdk-team <sdk-team@alibabacloud.com>
2024-01-30 04:05:35 +0000

    Generated SDK for ak_c175eb6e902d4839bce77e1b7a390162.

---
+++ ak_c175eb6e902d4839bce77e1b7a390162/csharp/core/Models/WANJIE.cs
@@ -0,0 +1,35 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Ak_c175eb6e902d4839bce77e1b7a390162.Models
+{
+    // data
+    public class WANJIE : TeaModel {
+        // code
+        [NameInMap("code")]
+        [Validation(Required=true)]
+        public string Code { get; set; }
+
+        // abilityAttribute
+        [NameInMap("ability_attribute")]
+        [Validation(Required=true)]
+        public string AbilityAttribute { get; set; }
+
+        // pastAttribute
+        [NameInMap("past_attribute")]
+        [Validation(Required=false)]
+        public string PastAttribute { get; set; }
+
+        // stabilityAttribute
+        [NameInMap("stability_attribute")]
+        [Validation(Required=false)]
+        public string StabilityAttribute { get; set; }
+
+    }
+
+}
