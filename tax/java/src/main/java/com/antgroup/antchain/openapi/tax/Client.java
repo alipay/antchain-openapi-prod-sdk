46893a323a54b3794ade0080cc59a5e92878d1b3
sdk-team <sdk-team@alibabacloud.com>
2024-02-01 09:22:12 +0000

    Generated SDK for TAX.

--- tax/java/src/main/java/com/antgroup/antchain/openapi/tax/Client.java
+++ tax/java/src/main/java/com/antgroup/antchain/openapi/tax/Client.java
@@ -110,7 +110,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.8.12"),
+                    new TeaPair("sdk_version", "1.8.13"),
                     new TeaPair("_prod_code", "TAX"),
                     new TeaPair("_prod_channel", "undefined")
                 );
@@ -847,5 +847,24 @@ public class Client {
     public QueryPdataRiskResponse queryPdataRiskEx(QueryPdataRiskRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "blockchain.tax.pdata.risk.query", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new QueryPdataRiskResponse());
+    }
+
+    /**
+     * Description: 核保医疗查询
+     * Summary: 核保医疗查询
+     */
+    public QueryPdataUnderwriteResponse queryPdataUnderwrite(QueryPdataUnderwriteRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.queryPdataUnderwriteEx(request, headers, runtime);
+    }
+
+    /**
+     * Description: 核保医疗查询
+     * Summary: 核保医疗查询
+     */
+    public QueryPdataUnderwriteResponse queryPdataUnderwriteEx(QueryPdataUnderwriteRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "blockchain.tax.pdata.underwrite.query", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new QueryPdataUnderwriteResponse());
     }
 }
