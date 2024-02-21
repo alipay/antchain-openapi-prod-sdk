7ddffee41dafea4df95292de243c429897aef92a
sdk-team <sdk-team@alibabacloud.com>
2024-02-21 03:16:23 +0000

    Generated SDK for DEMO.

--- demo/go/client/client.go
+++ demo/go/client/client.go
@@ -4571,6 +4571,90 @@ func (s *QueryAcopmTestobjectTestsubResponse) SetProviderid(v string) *QueryAcop
 	return s
 }
 
+type QueryAcopmTestobTestsubRequest struct {
+	// OAuth模式下的授权token
+	AuthToken         *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
+	ProductInstanceId *string `json:"product_instance_id,omitempty" xml:"product_instance_id,omitempty"`
+	// 主键
+	Id *int64 `json:"id,omitempty" xml:"id,omitempty" require:"true" minimum:"1"`
+	// 名称
+	Name *string `json:"name,omitempty" xml:"name,omitempty" require:"true"`
+}
+
+func (s QueryAcopmTestobTestsubRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s QueryAcopmTestobTestsubRequest) GoString() string {
+	return s.String()
+}
+
+func (s *QueryAcopmTestobTestsubRequest) SetAuthToken(v string) *QueryAcopmTestobTestsubRequest {
+	s.AuthToken = &v
+	return s
+}
+
+func (s *QueryAcopmTestobTestsubRequest) SetProductInstanceId(v string) *QueryAcopmTestobTestsubRequest {
+	s.ProductInstanceId = &v
+	return s
+}
+
+func (s *QueryAcopmTestobTestsubRequest) SetId(v int64) *QueryAcopmTestobTestsubRequest {
+	s.Id = &v
+	return s
+}
+
+func (s *QueryAcopmTestobTestsubRequest) SetName(v string) *QueryAcopmTestobTestsubRequest {
+	s.Name = &v
+	return s
+}
+
+type QueryAcopmTestobTestsubResponse struct {
+	// 请求唯一ID，用于链路跟踪和问题排查
+	ReqMsgId *string `json:"req_msg_id,omitempty" xml:"req_msg_id,omitempty"`
+	// 结果码，一般OK表示调用成功
+	ResultCode *string `json:"result_code,omitempty" xml:"result_code,omitempty"`
+	// 异常信息的文本描述
+	ResultMsg *string `json:"result_msg,omitempty" xml:"result_msg,omitempty"`
+	// 主键
+	Id *int64 `json:"id,omitempty" xml:"id,omitempty"`
+	// 名称
+	Name *string `json:"name,omitempty" xml:"name,omitempty"`
+}
+
+func (s QueryAcopmTestobTestsubResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s QueryAcopmTestobTestsubResponse) GoString() string {
+	return s.String()
+}
+
+func (s *QueryAcopmTestobTestsubResponse) SetReqMsgId(v string) *QueryAcopmTestobTestsubResponse {
+	s.ReqMsgId = &v
+	return s
+}
+
+func (s *QueryAcopmTestobTestsubResponse) SetResultCode(v string) *QueryAcopmTestobTestsubResponse {
+	s.ResultCode = &v
+	return s
+}
+
+func (s *QueryAcopmTestobTestsubResponse) SetResultMsg(v string) *QueryAcopmTestobTestsubResponse {
+	s.ResultMsg = &v
+	return s
+}
+
+func (s *QueryAcopmTestobTestsubResponse) SetId(v int64) *QueryAcopmTestobTestsubResponse {
+	s.Id = &v
+	return s
+}
+
+func (s *QueryAcopmTestobTestsubResponse) SetName(v string) *QueryAcopmTestobTestsubResponse {
+	s.Name = &v
+	return s
+}
+
 type QueryGongxiangTestDemoRequest struct {
 	// OAuth模式下的授权token
 	AuthToken         *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
@@ -5975,7 +6059,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.0.259"),
+				"sdk_version":      tea.String("1.0.262"),
 				"_prod_code":       tea.String("DEMO"),
 				"_prod_channel":    tea.String("undefined"),
 			}
@@ -7747,6 +7831,40 @@ func (client *Client) QueryAcopmTestobjectTestsubEx(request *QueryAcopmTestobjec
 	}
 	_result = &QueryAcopmTestobjectTestsubResponse{}
 	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("demo.acopm.testobject.testsub.query"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+/**
+ * Description: 测试API创建流程
+ * Summary: API测试查询接口(只是测试使用)
+ */
+func (client *Client) QueryAcopmTestobTestsub(request *QueryAcopmTestobTestsubRequest) (_result *QueryAcopmTestobTestsubResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &QueryAcopmTestobTestsubResponse{}
+	_body, _err := client.QueryAcopmTestobTestsubEx(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+/**
+ * Description: 测试API创建流程
+ * Summary: API测试查询接口(只是测试使用)
+ */
+func (client *Client) QueryAcopmTestobTestsubEx(request *QueryAcopmTestobTestsubRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *QueryAcopmTestobTestsubResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = &QueryAcopmTestobTestsubResponse{}
+	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("demo.acopm.testob.testsub.query"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
 	if _err != nil {
 		return _result, _err
 	}
