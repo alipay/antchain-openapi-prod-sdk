7b46de14c5a9b7d3f9c63020846de2801289cd91
sdk-team <sdk-team@alibabacloud.com>
2024-02-22 02:23:15 +0000

    Generated SDK for REALPERSON.

---
+++ realperson/csharp/core/Models/QueryCarrierNetstatusRequest.cs
@@ -0,0 +1,43 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.REALPERSON.Models
+{
+    public class QueryCarrierNetstatusRequest : TeaModel {
+        // OAuth模式下的授权token
+        [NameInMap("auth_token")]
+        [Validation(Required=false)]
+        public string AuthToken { get; set; }
+
+        [NameInMap("product_instance_id")]
+        [Validation(Required=false)]
+        public string ProductInstanceId { get; set; }
+
+        // 外部请求ID，为32位以内的字母数字组合，由调用方自行生成、保证唯一并留存，以便问题定位。
+        [NameInMap("outer_order_no")]
+        [Validation(Required=true)]
+        public string OuterOrderNo { get; set; }
+
+        // 手机号码
+        [NameInMap("mobile")]
+        [Validation(Required=true)]
+        public string Mobile { get; set; }
+
+        // 运营商类型： CHINA_TELECOM； CHINA_MOBILE； CHINA_UNICOM
+        [NameInMap("carrier")]
+        [Validation(Required=false)]
+        public string Carrier { get; set; }
+
+        // 扩展信息，预留字段
+        [NameInMap("extern_param")]
+        [Validation(Required=true)]
+        public string ExternParam { get; set; }
+
+    }
+
+}
--- realperson/csharp/core/Properties/AssemblyInfo.cs
+++ realperson/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("65b6eaf0-a54f-11ee-ae24-b1550d4edb7e")]
+[assembly: Guid("4de93ee0-d129-11ee-903d-11cccdc4bb7d")]
 
 // Version information for an assembly consists of the following four values:
 //
