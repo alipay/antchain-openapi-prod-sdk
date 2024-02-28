15d92bb7204f0da0288e053bb2b4a0962e323852
sdk-team <sdk-team@alibabacloud.com>
2024-02-28 11:16:32 +0000

    Generated SDK for TWC.

--- twc/go/client/client.go
+++ twc/go/client/client.go
@@ -47959,7 +47959,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.12.17"),
+				"sdk_version":      tea.String("1.12.18"),
 				"_prod_code":       tea.String("TWC"),
 				"_prod_channel":    tea.String("undefined"),
 			}
