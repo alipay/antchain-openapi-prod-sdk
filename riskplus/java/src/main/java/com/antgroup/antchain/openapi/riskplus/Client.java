4ec54d2efe8df0c8ec563b49925b29119787232d
sdk-team <sdk-team@alibabacloud.com>
2024-04-01 09:53:51 +0000

    Generated SDK for RISKPLUS.

--- riskplus/java/src/main/java/com/antgroup/antchain/openapi/riskplus/Client.java
+++ riskplus/java/src/main/java/com/antgroup/antchain/openapi/riskplus/Client.java
@@ -110,7 +110,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.17.7"),
+                    new TeaPair("sdk_version", "1.17.9"),
                     new TeaPair("_prod_code", "RISKPLUS"),
                     new TeaPair("_prod_channel", "undefined")
                 );
@@ -1609,6 +1609,44 @@ public class Client {
     public SendQmpCardsmsBatchResponse sendQmpCardsmsBatchEx(SendQmpCardsmsBatchRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "riskplus.qmp.cardsms.batch.send", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new SendQmpCardsmsBatchResponse());
+    }
+
+    /**
+     * Description: 上行短信回调
+     * Summary: 上行短信回调
+     */
+    public CallbackQmpSmsUpResponse callbackQmpSmsUp(CallbackQmpSmsUpRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.callbackQmpSmsUpEx(request, headers, runtime);
+    }
+
+    /**
+     * Description: 上行短信回调
+     * Summary: 上行短信回调
+     */
+    public CallbackQmpSmsUpResponse callbackQmpSmsUpEx(CallbackQmpSmsUpRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "riskplus.qmp.sms.up.callback", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new CallbackQmpSmsUpResponse());
+    }
+
+    /**
+     * Description: 短信状态回调接口
+     * Summary: 短信状态回调接口
+     */
+    public CallbackQmpSmsReportResponse callbackQmpSmsReport(CallbackQmpSmsReportRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.callbackQmpSmsReportEx(request, headers, runtime);
+    }
+
+    /**
+     * Description: 短信状态回调接口
+     * Summary: 短信状态回调接口
+     */
+    public CallbackQmpSmsReportResponse callbackQmpSmsReportEx(CallbackQmpSmsReportRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "riskplus.qmp.sms.report.callback", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new CallbackQmpSmsReportResponse());
     }
 
     /**
