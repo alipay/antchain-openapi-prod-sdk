adb380e3fe241f6e663fbe14c814f9d3f0e582a7
sdk-team <sdk-team@alibabacloud.com>
2024-04-07 07:34:27 +0000

    Generated SDK for SAAS.

--- saas/composer/src/Client.php
+++ saas/composer/src/Client.php
@@ -194,7 +194,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.1.29',
+                    'sdk_version'      => '1.1.30',
                     '_prod_code'       => 'SAAS',
                     '_prod_channel'    => 'undefined',
                 ];
