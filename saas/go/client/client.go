36182be3e4728395318ed380a144f0e6a60289df
sdk-team <sdk-team@alibabacloud.com>
2024-04-07 07:34:21 +0000

    Generated SDK for SAAS.

--- saas/go/client/client.go
+++ saas/go/client/client.go
@@ -2098,7 +2098,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.1.29"),
+				"sdk_version":      tea.String("1.1.30"),
 				"_prod_code":       tea.String("SAAS"),
 				"_prod_channel":    tea.String("undefined"),
 			}
