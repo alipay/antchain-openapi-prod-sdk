533c88c36632aa4aeff51fe47739b6f82f9c5a74
sdk-team <sdk-team@alibabacloud.com>
2024-05-14 11:29:07 +0000

    Generated SDK for NFTC.

--- nftc/csharp/core/Client.cs
+++ nftc/csharp/core/Client.cs
@@ -137,7 +137,7 @@ namespace AntChain.SDK.NFTC
                         {"req_msg_id", AntChain.AlipayUtil.AntchainUtils.GetNonce()},
                         {"access_key", _accessKeyId},
                         {"base_sdk_version", "TeaSDK-2.0"},
-                        {"sdk_version", "1.0.23"},
+                        {"sdk_version", "1.0.24"},
                         {"_prod_code", "NFTC"},
                         {"_prod_channel", "undefined"},
                     };
@@ -263,7 +263,7 @@ namespace AntChain.SDK.NFTC
                         {"req_msg_id", AntChain.AlipayUtil.AntchainUtils.GetNonce()},
                         {"access_key", _accessKeyId},
                         {"base_sdk_version", "TeaSDK-2.0"},
-                        {"sdk_version", "1.0.23"},
+                        {"sdk_version", "1.0.24"},
                         {"_prod_code", "NFTC"},
                         {"_prod_channel", "undefined"},
                     };
--- nftc/csharp/core/Properties/AssemblyInfo.cs
+++ nftc/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4ae32a50-0cff-11ef-a9b8-a5c60efb778a")]
+[assembly: Guid("244c0700-11e5-11ef-8aaf-657321e855af")]
 
 // Version information for an assembly consists of the following four values:
 //
