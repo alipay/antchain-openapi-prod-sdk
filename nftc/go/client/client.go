17ee6c2d9d43981fbcb39e2d1f9cc3ce4e6c543e
sdk-team <sdk-team@alibabacloud.com>
2024-05-14 11:29:17 +0000

    Generated SDK for NFTC.

--- nftc/go/client/client.go
+++ nftc/go/client/client.go
@@ -2771,7 +2771,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.0.23"),
+				"sdk_version":      tea.String("1.0.24"),
 				"_prod_code":       tea.String("NFTC"),
 				"_prod_channel":    tea.String("undefined"),
 			}
