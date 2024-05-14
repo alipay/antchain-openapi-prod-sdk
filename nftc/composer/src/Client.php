2aed14d3aa37255beeb5892eef43c5a01feeb908
sdk-team <sdk-team@alibabacloud.com>
2024-05-14 11:28:53 +0000

    Generated SDK for NFTC.

--- nftc/composer/src/Client.php
+++ nftc/composer/src/Client.php
@@ -200,7 +200,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.0.23',
+                    'sdk_version'      => '1.0.24',
                     '_prod_code'       => 'NFTC',
                     '_prod_channel'    => 'undefined',
                 ];
