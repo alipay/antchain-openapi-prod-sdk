77e95e82cca3a1fef09180a7741c6718113291b0
sdk-team <sdk-team@alibabacloud.com>
2024-06-17 11:19:59 +0000

    Generated SDK for RISKPLUS.

--- riskplus/nodejs/src/client.ts
+++ riskplus/nodejs/src/client.ts
@@ -19633,7 +19633,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.19.12",
+          sdk_version: "1.19.13",
           _prod_code: "RISKPLUS",
           _prod_channel: "undefined",
         };
