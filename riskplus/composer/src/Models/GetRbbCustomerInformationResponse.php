81bcd9df43a31dcf0ed0b1068638b762b46a0c7a
sdk-team <sdk-team@alibabacloud.com>
2024-06-18 12:42:32 +0000

    Generated SDK for RISKPLUS.

--- riskplus/composer/src/Models/GetRbbCustomerInformationResponse.php
+++ riskplus/composer/src/Models/GetRbbCustomerInformationResponse.php
@@ -25,10 +25,17 @@ class GetRbbCustomerInformationResponse extends Model
      * @var string
      */
     public $resultMsg;
+
+    // 客户信息结果数据
+    /**
+     * @var string
+     */
+    public $resultData;
     protected $_name = [
         'reqMsgId'   => 'req_msg_id',
         'resultCode' => 'result_code',
         'resultMsg'  => 'result_msg',
+        'resultData' => 'result_data',
     ];
 
     public function validate()
@@ -46,6 +53,9 @@ class GetRbbCustomerInformationResponse extends Model
         }
         if (null !== $this->resultMsg) {
             $res['result_msg'] = $this->resultMsg;
+        }
+        if (null !== $this->resultData) {
+            $res['result_data'] = $this->resultData;
         }
 
         return $res;
@@ -67,6 +77,9 @@ class GetRbbCustomerInformationResponse extends Model
         }
         if (isset($map['result_msg'])) {
             $model->resultMsg = $map['result_msg'];
+        }
+        if (isset($map['result_data'])) {
+            $model->resultData = $map['result_data'];
         }
 
         return $model;
