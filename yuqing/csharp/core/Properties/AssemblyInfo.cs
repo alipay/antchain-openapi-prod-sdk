03e94a920305b8fc90c1b596a2950241325796de
sdk-team <sdk-team@alibabacloud.com>
2024-06-19 05:39:13 +0000

    Generated SDK for YUQING.

--- yuqing/csharp/core/Properties/AssemblyInfo.cs
+++ yuqing/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e01425a0-2d13-11ef-bf17-67f52f691f6d")]
+[assembly: Guid("3d6dbdc0-2dfe-11ef-984f-fbea204db10e")]
 
 // Version information for an assembly consists of the following four values:
 //
