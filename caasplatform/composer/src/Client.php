c4b8f9bd10369e43e29219c86486b372b523ced5
sdk-team <sdk-team@alibabacloud.com>
2024-06-20 08:32:40 +0000

    Generated SDK for CAASPLATFORM.

--- caasplatform/composer/src/Client.php
+++ caasplatform/composer/src/Client.php
@@ -222,7 +222,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.3.1',
+                    'sdk_version'      => '1.3.2',
                     '_prod_code'       => 'CAASPLATFORM',
                     '_prod_channel'    => 'undefined',
                 ];
