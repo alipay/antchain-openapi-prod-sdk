d31d016362c25005f81e4c6110f9b6c35be25527
sdk-team <sdk-team@alibabacloud.com>
2024-06-20 08:32:45 +0000

    Generated SDK for CAASPLATFORM.

--- caasplatform/go/client/client.go
+++ caasplatform/go/client/client.go
@@ -3922,7 +3922,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.3.1"),
+				"sdk_version":      tea.String("1.3.2"),
 				"_prod_code":       tea.String("CAASPLATFORM"),
 				"_prod_channel":    tea.String("undefined"),
 			}
