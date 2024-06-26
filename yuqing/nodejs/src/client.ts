ae27ae821e5d61201dad51ce0ce4959b4461acbd
sdk-team <sdk-team@alibabacloud.com>
2024-06-26 01:22:54 +0000

    Generated SDK for YUQING.

--- yuqing/nodejs/src/client.ts
+++ yuqing/nodejs/src/client.ts
@@ -3062,7 +3062,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.2.20",
+          sdk_version: "1.2.21",
           _prod_code: "YUQING",
           _prod_channel: "undefined",
         };
