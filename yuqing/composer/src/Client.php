ebefb2247821c0fcc9a1b30404cfa235a6ae3e44
sdk-team <sdk-team@alibabacloud.com>
2024-06-26 01:20:59 +0000

    Generated SDK for YUQING.

--- yuqing/composer/src/Client.php
+++ yuqing/composer/src/Client.php
@@ -200,7 +200,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.2.20',
+                    'sdk_version'      => '1.2.21',
                     '_prod_code'       => 'YUQING',
                     '_prod_channel'    => 'undefined',
                 ];
