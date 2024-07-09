58bea17d399b4d046c1dc5ae9c5aee01dc60c601
sdk-team <sdk-team@alibabacloud.com>
2024-07-09 09:44:22 +0000

    Generated SDK for TAX.

--- tax/composer/src/Client.php
+++ tax/composer/src/Client.php
@@ -244,7 +244,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.8.40',
+                    'sdk_version'      => '1.8.41',
                     '_prod_code'       => 'TAX',
                     '_prod_channel'    => 'undefined',
                 ];
