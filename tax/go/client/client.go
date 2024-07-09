b4d703d7c50c2864f2f15e4f55c89fca622c3ddd
sdk-team <sdk-team@alibabacloud.com>
2024-07-09 09:44:38 +0000

    Generated SDK for TAX.

--- tax/go/client/client.go
+++ tax/go/client/client.go
@@ -6523,7 +6523,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.8.40"),
+				"sdk_version":      tea.String("1.8.41"),
 				"_prod_code":       tea.String("TAX"),
 				"_prod_channel":    tea.String("undefined"),
 			}
