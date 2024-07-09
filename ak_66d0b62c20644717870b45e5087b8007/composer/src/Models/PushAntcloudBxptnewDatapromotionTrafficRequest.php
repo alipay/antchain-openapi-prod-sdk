872eabd168dfdce9e1fa8fd1fbd9237e76f9be53
sdk-team <sdk-team@alibabacloud.com>
2024-07-09 03:09:28 +0000

    Generated SDK for ak_66d0b62c20644717870b45e5087b8007.

---
+++ ak_66d0b62c20644717870b45e5087b8007/composer/src/Models/PushAntcloudBxptnewDatapromotionTrafficRequest.php
@@ -0,0 +1,90 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AntChain\Ak_66d0b62c20644717870b45e5087b8007\Models;
+
+use AlibabaCloud\Tea\Model;
+
+class PushAntcloudBxptnewDatapromotionTrafficRequest extends Model
+{
+    // OAuth模式下的授权token
+    /**
+     * @var string
+     */
+    public $authToken;
+
+    // 保险险种
+    /**
+     * @var string
+     */
+    public $insurerCode;
+
+    // 保险险种
+    /**
+     * @var string
+     */
+    public $insuranceType;
+
+    // 业务参数内容JSON字符串
+    /**
+     * @var string
+     */
+    public $bizContent;
+    protected $_name = [
+        'authToken'     => 'auth_token',
+        'insurerCode'   => 'insurer_code',
+        'insuranceType' => 'insurance_type',
+        'bizContent'    => 'biz_content',
+    ];
+
+    public function validate()
+    {
+        Model::validateRequired('insurerCode', $this->insurerCode, true);
+        Model::validateRequired('insuranceType', $this->insuranceType, true);
+        Model::validateRequired('bizContent', $this->bizContent, true);
+    }
+
+    public function toMap()
+    {
+        $res = [];
+        if (null !== $this->authToken) {
+            $res['auth_token'] = $this->authToken;
+        }
+        if (null !== $this->insurerCode) {
+            $res['insurer_code'] = $this->insurerCode;
+        }
+        if (null !== $this->insuranceType) {
+            $res['insurance_type'] = $this->insuranceType;
+        }
+        if (null !== $this->bizContent) {
+            $res['biz_content'] = $this->bizContent;
+        }
+
+        return $res;
+    }
+
+    /**
+     * @param array $map
+     *
+     * @return PushAntcloudBxptnewDatapromotionTrafficRequest
+     */
+    public static function fromMap($map = [])
+    {
+        $model = new self();
+        if (isset($map['auth_token'])) {
+            $model->authToken = $map['auth_token'];
+        }
+        if (isset($map['insurer_code'])) {
+            $model->insurerCode = $map['insurer_code'];
+        }
+        if (isset($map['insurance_type'])) {
+            $model->insuranceType = $map['insurance_type'];
+        }
+        if (isset($map['biz_content'])) {
+            $model->bizContent = $map['biz_content'];
+        }
+
+        return $model;
+    }
+}
