9c023840eaa97bbd0bd66ac072170fd5f5ed0f6a
sdk-team <sdk-team@alibabacloud.com>
2024-07-09 09:44:19 +0000

    Generated SDK for TAX.

--- tax/nodejs/src/client.ts
+++ tax/nodejs/src/client.ts
@@ -4931,7 +4931,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.8.40",
+          sdk_version: "1.8.41",
           _prod_code: "TAX",
           _prod_channel: "undefined",
         };
