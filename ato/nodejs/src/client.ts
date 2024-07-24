1cda6f026e93e4ace207c23b6e2bc40f2f2ba4a4
sdk-team <sdk-team@alibabacloud.com>
2024-07-24 09:33:36 +0000

    Generated SDK for ATO.

--- ato/nodejs/src/client.ts
+++ ato/nodejs/src/client.ts
@@ -6720,6 +6720,69 @@ export class QuerySignCreditResponse extends $tea.Model {
   }
 }
 
+export class CancelSignFlowRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  // 签署合同单号
+  signNo: string;
+  // 默认:“撤销”
+  revokeReason?: string;
+  // 发起人账户id，即发起本次签署的操作人个人账号id；如不传，默认由对接平台发起
+  operatorId?: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+      signNo: 'sign_no',
+      revokeReason: 'revoke_reason',
+      operatorId: 'operator_id',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+      signNo: 'string',
+      revokeReason: 'string',
+      operatorId: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class CancelSignFlowResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class SyncTradeRequest extends $tea.Model {
   // OAuth模式下的授权token
   authToken?: string;
@@ -8481,7 +8544,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.8.92",
+          sdk_version: "1.8.95",
           _prod_code: "ATO",
           _prod_channel: "undefined",
         };
@@ -10034,6 +10097,25 @@ export default class Client {
   async querySignCreditEx(request: QuerySignCreditRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<QuerySignCreditResponse> {
     Util.validateModel(request);
     return $tea.cast<QuerySignCreditResponse>(await this.doRequest("1.0", "antchain.ato.sign.credit.query", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new QuerySignCreditResponse({}));
+  }
+
+  /**
+   * Description: 撤销签署流程
+   * Summary: 撤销签署流程
+   */
+  async cancelSignFlow(request: CancelSignFlowRequest): Promise<CancelSignFlowResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.cancelSignFlowEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 撤销签署流程
+   * Summary: 撤销签署流程
+   */
+  async cancelSignFlowEx(request: CancelSignFlowRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<CancelSignFlowResponse> {
+    Util.validateModel(request);
+    return $tea.cast<CancelSignFlowResponse>(await this.doRequest("1.0", "antchain.ato.sign.flow.cancel", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new CancelSignFlowResponse({}));
   }
 
   /**
