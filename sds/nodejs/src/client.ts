3718b6efd26f988da8169202680262037542d3fd
sdk-team <sdk-team@alibabacloud.com>
2024-08-02 08:44:47 +0000

    Generated SDK for SDS.

--- sds/nodejs/src/client.ts
+++ sds/nodejs/src/client.ts
@@ -2,6 +2,7 @@
 import AntchainUtil from '@antchain/alipay-util';
 import Util, * as $Util from '@alicloud/tea-util';
 import RPCUtil from '@alicloud/rpc-util';
+import { Readable } from 'stream';
 import * as $tea from '@alicloud/tea-typescript';
 
 /**
@@ -77,17 +78,127 @@ export class Config extends $tea.Model {
   }
 }
 
+// 批处理结果
+export class BatchResult extends $tea.Model {
+  // 业务号
+  bizNo: string;
+  // 业务号类型
+  bizNoType: string;
+  // 结果
+  result: string;
+  static names(): { [key: string]: string } {
+    return {
+      bizNo: 'biz_no',
+      bizNoType: 'biz_no_type',
+      result: 'result',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      bizNo: 'string',
+      bizNoType: 'string',
+      result: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+// 地址，包含省、市、区(县)
+export class Address extends $tea.Model {
+  // 市级
+  city: string;
+  // 区、县级
+  district?: string;
+  static names(): { [key: string]: string } {
+    return {
+      city: 'city',
+      district: 'district',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      city: 'string',
+      district: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+// 业务号条件
+export class BizNoCondition extends $tea.Model {
+  // 枚举
+  // CITY 城市
+  // BLOCK 区县
+  // AGE 年龄
+  dimension: string;
+  // 枚举范围，每个维度的值是或的关系,需要校验场景和取值范围是否匹配
+  // CITY:区划码
+  // BLOCK:区划码（底包暂不支持）
+  // AGE:30+、40+、50+（底包暂不支持
+  valueScope: string[];
+  static names(): { [key: string]: string } {
+    return {
+      dimension: 'dimension',
+      valueScope: 'value_scope',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      dimension: 'string',
+      valueScope: { 'type': 'array', 'itemType': 'string' },
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+// 键值对
+export class XNameValuePair extends $tea.Model {
+  // 键名
+  name: string;
+  // 键值
+  value: string;
+  static names(): { [key: string]: string } {
+    return {
+      name: 'name',
+      value: 'value',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      name: 'string',
+      value: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class JudgeCrowdPrefermentRequest extends $tea.Model {
   // OAuth模式下的授权token
   authToken?: string;
   productInstanceId?: string;
-  // 业务号：可以标识用户的编码，例如手机号，身份证号等，通过业务号类型来控制，与biz_no_type和encrypt_type连用来确定编码形式。
+  // 业务号：可以标识用户的编码，例如手机号，身份证号等，通过业务号类型来控制，与biz_no_type和encrypt_type共同确定编码形式。
   bizNo: string;
   // 业务号类型：1-手机号，2-支付宝用户id
   bizNoType: string;
   // 加密方式：0-不加密，1-SHA1，2-MD5
   encryptType: string;
-  // 拓展属性：自定义结构，里面可传地址等信息
+  // json结构，可以传递自定义参数
   properties?: string;
   static names(): { [key: string]: string } {
     return {
@@ -140,6 +251,314 @@ export class JudgeCrowdPrefermentResponse extends $tea.Model {
       resultCode: 'string',
       resultMsg: 'string',
       isPreferment: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class SubmitScenedataTaskRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  // 约定的场景枚举
+  scene: string;
+  // 枚举
+  // PHONE_SHA1
+  // PHONE_MD5
+  bizNoType: string;
+  // 适配客户的来源
+  // 可能是客户的任务/AK
+  sourceMark?: string;
+  // 业务号预期条件
+  expectCondition?: BizNoCondition[];
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+      scene: 'scene',
+      bizNoType: 'biz_no_type',
+      sourceMark: 'source_mark',
+      expectCondition: 'expect_condition',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+      scene: 'string',
+      bizNoType: 'string',
+      sourceMark: 'string',
+      expectCondition: { 'type': 'array', 'itemType': BizNoCondition },
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class SubmitScenedataTaskResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  // 创建任务成功后，返回批次号
+  batchNo?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+      batchNo: 'batch_no',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+      batchNo: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class UploadScenedataFileRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  // 批次号
+  batchNo: string;
+  // 文件参数
+  fileObject?: Readable;
+  fileObjectName?: string;
+  fileId: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+      batchNo: 'batch_no',
+      fileObject: 'fileObject',
+      fileObjectName: 'fileObjectName',
+      fileId: 'file_id',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+      batchNo: 'string',
+      fileObject: 'Readable',
+      fileObjectName: 'string',
+      fileId: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class UploadScenedataFileResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  // batchNo对应的任务状态，上传后，返回RECEIVED
+  status?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+      status: 'status',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+      status: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class BatchqueryScenedataTaskresultRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  // 批次号
+  batchNo: string;
+  // 游标，上一次的最后一条
+  cursor?: string;
+  // 本次同步数量
+  syncNum?: number;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+      batchNo: 'batch_no',
+      cursor: 'cursor',
+      syncNum: 'sync_num',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+      batchNo: 'string',
+      cursor: 'string',
+      syncNum: 'number',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class BatchqueryScenedataTaskresultResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  // 0-未开始
+  // 1-可继续
+  // 2-结束
+  syncStatus?: string;
+  // 本次的最后一个游标，保存起来下一次使用
+  lastCursor?: string;
+  // 结果列表
+  resultList?: BatchResult[];
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+      syncStatus: 'sync_status',
+      lastCursor: 'last_cursor',
+      resultList: 'result_list',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+      syncStatus: 'string',
+      lastCursor: 'string',
+      resultList: { 'type': 'array', 'itemType': BatchResult },
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class CreateAntcloudGatewayxFileUploadRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  // 上传文件作用的openapi method
+  apiCode: string;
+  // 文件标签，多个标签;分割
+  fileLabel?: string;
+  // 自定义的文件元数据
+  fileMetadata?: string;
+  // 文件名，不传则随机生成文件名
+  fileName?: string;
+  // 文件的多媒体类型
+  mimeType?: string;
+  // 产品方的api归属集群，即productInstanceId
+  apiCluster?: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      apiCode: 'api_code',
+      fileLabel: 'file_label',
+      fileMetadata: 'file_metadata',
+      fileName: 'file_name',
+      mimeType: 'mime_type',
+      apiCluster: 'api_cluster',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      apiCode: 'string',
+      fileLabel: 'string',
+      fileMetadata: 'string',
+      fileName: 'string',
+      mimeType: 'string',
+      apiCluster: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class CreateAntcloudGatewayxFileUploadResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  // 上传有效期
+  expiredTime?: string;
+  // 32位文件唯一id
+  fileId?: string;
+  // 放入http请求头里
+  uploadHeaders?: XNameValuePair[];
+  // 文件上传地址
+  uploadUrl?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+      expiredTime: 'expired_time',
+      fileId: 'file_id',
+      uploadHeaders: 'upload_headers',
+      uploadUrl: 'upload_url',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+      expiredTime: 'string',
+      fileId: 'string',
+      uploadHeaders: { 'type': 'array', 'itemType': XNameValuePair },
+      uploadUrl: 'string',
     };
   }
 
@@ -261,7 +680,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.8",
+          sdk_version: "1.2.8",
           _prod_code: "SDS",
           _prod_channel: "default",
         };
@@ -326,6 +745,103 @@ export default class Client {
   async judgeCrowdPrefermentEx(request: JudgeCrowdPrefermentRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<JudgeCrowdPrefermentResponse> {
     Util.validateModel(request);
     return $tea.cast<JudgeCrowdPrefermentResponse>(await this.doRequest("1.0", "antchain.sds.crowd.preferment.judge", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new JudgeCrowdPrefermentResponse({}));
+  }
+
+  /**
+   * Description: 客户上传文件以及参数，创建任务，获取批次号异步查询处理结果。
+   * Summary: 场景数据批处理任务提交
+   */
+  async submitScenedataTask(request: SubmitScenedataTaskRequest): Promise<SubmitScenedataTaskResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.submitScenedataTaskEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 客户上传文件以及参数，创建任务，获取批次号异步查询处理结果。
+   * Summary: 场景数据批处理任务提交
+   */
+  async submitScenedataTaskEx(request: SubmitScenedataTaskRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<SubmitScenedataTaskResponse> {
+    Util.validateModel(request);
+    return $tea.cast<SubmitScenedataTaskResponse>(await this.doRequest("1.0", "antchain.sds.scenedata.task.submit", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new SubmitScenedataTaskResponse({}));
+  }
+
+  /**
+   * Description: 批次数据文件上传
+   * Summary: 批次数据文件上传
+   */
+  async uploadScenedataFile(request: UploadScenedataFileRequest): Promise<UploadScenedataFileResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.uploadScenedataFileEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 批次数据文件上传
+   * Summary: 批次数据文件上传
+   */
+  async uploadScenedataFileEx(request: UploadScenedataFileRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<UploadScenedataFileResponse> {
+    if (!Util.isUnset(request.fileObject)) {
+      let uploadReq = new CreateAntcloudGatewayxFileUploadRequest({
+        authToken: request.authToken,
+        apiCode: "antchain.sds.scenedata.file.upload",
+        fileName: request.fileObjectName,
+      });
+      let uploadResp = await this.createAntcloudGatewayxFileUploadEx(uploadReq, headers, runtime);
+      if (!AntchainUtil.isSuccess(uploadResp.resultCode, "ok")) {
+        let uploadScenedataFileResponse = new UploadScenedataFileResponse({
+          reqMsgId: uploadResp.reqMsgId,
+          resultCode: uploadResp.resultCode,
+          resultMsg: uploadResp.resultMsg,
+        });
+        return uploadScenedataFileResponse;
+      }
+
+      let uploadHeaders = AntchainUtil.parseUploadHeaders(uploadResp.uploadHeaders);
+      await AntchainUtil.putObject(request.fileObject, uploadHeaders, uploadResp.uploadUrl);
+      request.fileId = uploadResp.fileId;
+    }
+
+    Util.validateModel(request);
+    return $tea.cast<UploadScenedataFileResponse>(await this.doRequest("1.0", "antchain.sds.scenedata.file.upload", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new UploadScenedataFileResponse({}));
+  }
+
+  /**
+   * Description: 场景数据SaaS第一天预处理客户提交的文件处理任务，第二天客户调该接口批量查询任务结果
+   * Summary: 场景数据任务结果批量查询
+   */
+  async batchqueryScenedataTaskresult(request: BatchqueryScenedataTaskresultRequest): Promise<BatchqueryScenedataTaskresultResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.batchqueryScenedataTaskresultEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 场景数据SaaS第一天预处理客户提交的文件处理任务，第二天客户调该接口批量查询任务结果
+   * Summary: 场景数据任务结果批量查询
+   */
+  async batchqueryScenedataTaskresultEx(request: BatchqueryScenedataTaskresultRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<BatchqueryScenedataTaskresultResponse> {
+    Util.validateModel(request);
+    return $tea.cast<BatchqueryScenedataTaskresultResponse>(await this.doRequest("1.0", "antchain.sds.scenedata.taskresult.batchquery", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new BatchqueryScenedataTaskresultResponse({}));
+  }
+
+  /**
+   * Description: 创建HTTP PUT提交的文件上传
+   * Summary: 文件上传创建
+   */
+  async createAntcloudGatewayxFileUpload(request: CreateAntcloudGatewayxFileUploadRequest): Promise<CreateAntcloudGatewayxFileUploadResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.createAntcloudGatewayxFileUploadEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 创建HTTP PUT提交的文件上传
+   * Summary: 文件上传创建
+   */
+  async createAntcloudGatewayxFileUploadEx(request: CreateAntcloudGatewayxFileUploadRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<CreateAntcloudGatewayxFileUploadResponse> {
+    Util.validateModel(request);
+    return $tea.cast<CreateAntcloudGatewayxFileUploadResponse>(await this.doRequest("1.0", "antcloud.gatewayx.file.upload.create", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new CreateAntcloudGatewayxFileUploadResponse({}));
   }
 
 }
