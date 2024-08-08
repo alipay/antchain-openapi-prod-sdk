d8a0324df6020f38906b138b43ae3c4a072ae850
sdk-team <sdk-team@alibabacloud.com>
2024-08-08 12:14:13 +0000

    Generated SDK for SECURITYTECH.

--- securitytech/composer/src/Client.php
+++ securitytech/composer/src/Client.php
@@ -232,7 +232,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.2.25',
+                    'sdk_version'      => '1.2.26',
                     '_prod_code'       => 'SECURITYTECH',
                     '_prod_channel'    => 'undefined',
                 ];
