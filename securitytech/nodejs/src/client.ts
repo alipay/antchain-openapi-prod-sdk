2e1d956e10312afcd710d523116273538ab4eff3
sdk-team <sdk-team@alibabacloud.com>
2024-08-08 12:14:47 +0000

    Generated SDK for SECURITYTECH.

--- securitytech/nodejs/src/client.ts
+++ securitytech/nodejs/src/client.ts
@@ -4143,7 +4143,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.2.25",
+          sdk_version: "1.2.26",
           _prod_code: "SECURITYTECH",
           _prod_channel: "undefined",
         };
