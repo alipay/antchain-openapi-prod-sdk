e83607322439af21c9107c8e903c5ccbd3ff43fc
sdk-team <sdk-team@alibabacloud.com>
2024-08-08 12:14:11 +0000

    Generated SDK for SECURITYTECH.

--- securitytech/csharp/core/Client.cs
+++ securitytech/csharp/core/Client.cs
@@ -137,7 +137,7 @@ namespace AntChain.SDK.SECURITYTECH
                         {"req_msg_id", AntChain.AlipayUtil.AntchainUtils.GetNonce()},
                         {"access_key", _accessKeyId},
                         {"base_sdk_version", "TeaSDK-2.0"},
-                        {"sdk_version", "1.2.25"},
+                        {"sdk_version", "1.2.26"},
                         {"_prod_code", "SECURITYTECH"},
                         {"_prod_channel", "undefined"},
                     };
@@ -263,7 +263,7 @@ namespace AntChain.SDK.SECURITYTECH
                         {"req_msg_id", AntChain.AlipayUtil.AntchainUtils.GetNonce()},
                         {"access_key", _accessKeyId},
                         {"base_sdk_version", "TeaSDK-2.0"},
-                        {"sdk_version", "1.2.25"},
+                        {"sdk_version", "1.2.26"},
                         {"_prod_code", "SECURITYTECH"},
                         {"_prod_channel", "undefined"},
                     };
--- securitytech/csharp/core/Properties/AssemblyInfo.cs
+++ securitytech/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ed020420-557d-11ef-9327-5958de9589b8")]
+[assembly: Guid("b2820e10-557f-11ef-b123-316fe27d2ad8")]
 
 // Version information for an assembly consists of the following four values:
 //
