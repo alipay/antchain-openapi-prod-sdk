7be24158a8b8a72b1cdc5cec9744c556242af1ed
sdk-team <sdk-team@alibabacloud.com>
2024-08-28 06:32:12 +0000

    Generated SDK for AITECHGUARD.

---
+++ aitechguard/composer/src/Models/Map.php
@@ -0,0 +1,29 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AntChain\AITECHGUARD\Models;
+
+use AlibabaCloud\Tea\Model;
+
+class Map extends Model
+{
+    public function validate()
+    {
+    }
+
+    public function toMap()
+    {
+        return [];
+    }
+
+    /**
+     * @param array $map
+     *
+     * @return Map
+     */
+    public static function fromMap($map = [])
+    {
+        return new self();
+    }
+}
