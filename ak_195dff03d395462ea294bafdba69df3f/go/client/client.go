8d0be82c20ead9815e62a61fc1558fa3d577404b
sdk-team <sdk-team@alibabacloud.com>
2024-09-04 03:10:40 +0000

    Generated SDK for ak_195dff03d395462ea294bafdba69df3f.

--- ak_195dff03d395462ea294bafdba69df3f/go/client/client.go
+++ ak_195dff03d395462ea294bafdba69df3f/go/client/client.go
@@ -6512,7 +6512,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.3.6"),
+				"sdk_version":      tea.String("1.3.7"),
 				"_prod_code":       tea.String("ak_195dff03d395462ea294bafdba69df3f"),
 				"_prod_channel":    tea.String("saas"),
 			}
