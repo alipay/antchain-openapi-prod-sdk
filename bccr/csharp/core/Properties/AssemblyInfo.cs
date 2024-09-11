740ff0a0709459ed1d36c6fcab1db21c5bf316ba
sdk-team <sdk-team@alibabacloud.com>
2024-09-11 10:34:44 +0000

    Generated SDK for BCCR.

--- bccr/csharp/core/Properties/AssemblyInfo.cs
+++ bccr/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c40a5a30-701d-11ef-9109-89be4f171760")]
+[assembly: Guid("705df2f0-7029-11ef-b82a-65bc0f2a7ff7")]
 
 // Version information for an assembly consists of the following four values:
 //
