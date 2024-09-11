e9fc221e5e23df41401d3318382ff875c7f6b8bd
sdk-team <sdk-team@alibabacloud.com>
2024-09-11 10:34:16 +0000

    Generated SDK for BCCR.

--- bccr/python/antchain_sdk_bccr/client.py
+++ bccr/python/antchain_sdk_bccr/client.py
@@ -135,7 +135,7 @@ class Client:
                     'req_msg_id': AntchainUtils.get_nonce(),
                     'access_key': self._access_key_id,
                     'base_sdk_version': 'TeaSDK-2.0',
-                    'sdk_version': '1.19.6',
+                    'sdk_version': '1.19.8',
                     '_prod_code': 'BCCR',
                     '_prod_channel': 'undefined'
                 }
@@ -239,7 +239,7 @@ class Client:
                     'req_msg_id': AntchainUtils.get_nonce(),
                     'access_key': self._access_key_id,
                     'base_sdk_version': 'TeaSDK-2.0',
-                    'sdk_version': '1.19.6',
+                    'sdk_version': '1.19.8',
                     '_prod_code': 'BCCR',
                     '_prod_channel': 'undefined'
                 }
