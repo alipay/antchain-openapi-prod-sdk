8df9cda23e59391eb3b7906f9255b3ee1c0b60ba
sdk-team <sdk-team@alibabacloud.com>
2024-10-29 08:29:44 +0000

    Generated SDK for ATO.

--- ato/nodejs/src/client.ts
+++ ato/nodejs/src/client.ts
@@ -11194,7 +11194,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.9.72",
+          sdk_version: "1.9.73",
           _prod_code: "ATO",
           _prod_channel: "undefined",
         };
