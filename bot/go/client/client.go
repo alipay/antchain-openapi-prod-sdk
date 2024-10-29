41d6693af06b72d171702dcd3620d5cf20e3a8c7
sdk-team <sdk-team@alibabacloud.com>
2024-10-29 05:55:51 +0000

    Generated SDK for BOT.

--- bot/go/client/client.go
+++ bot/go/client/client.go
@@ -34939,7 +34939,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.11.55"),
+				"sdk_version":      tea.String("1.11.56"),
 				"_prod_code":       tea.String("BOT"),
 				"_prod_channel":    tea.String("undefined"),
 			}
