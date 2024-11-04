6011cea42e91adf7ca10d1423d319dc56d64ced1
sdk-team <sdk-team@alibabacloud.com>
2024-11-04 09:41:38 +0000

    Generated SDK for ak_6f8b0e7502ec49d28cd8ad0e5807bbf9.

--- ak_6f8b0e7502ec49d28cd8ad0e5807bbf9/python/setup.py
+++ ak_6f8b0e7502ec49d28cd8ad0e5807bbf9/python/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for antchain_ak_6f8b0e7502ec49d28cd8ad0e5807bbf9.
 
-Created on 18/06/2024
+Created on 04/11/2024
 
 @author: Ant Chain SDK
 """
@@ -38,7 +38,7 @@ URL = "https://github.com/alipay/antchain-openapi-prod-sdk"
 VERSION = __import__(PACKAGE).__version__
 REQUIRES = [
     "antchain_alipay_util>=1.0.1, <2.0.0",
-    "alibabacloud_tea_util>=0.3.12, <1.0.0",
+    "alibabacloud_tea_util>=0.3.13, <1.0.0",
     "alibabacloud_rpc_util>=0.0.4, <1.0.0"
 ]
 
