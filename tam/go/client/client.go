b93fd4d42e0e726fe66669c829940713b38a1889
sdk-team <sdk-team@alibabacloud.com>
2024-11-05 03:00:15 +0000

    Generated SDK for TAM.

--- tam/go/client/client.go
+++ tam/go/client/client.go
@@ -2097,7 +2097,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.3.4"),
+				"sdk_version":      tea.String("1.3.5"),
 				"_prod_code":       tea.String("TAM"),
 				"_prod_channel":    tea.String("undefined"),
 			}
