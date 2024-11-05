5d69b9d46504f8de80291e4ad1ec8603dfb59167
sdk-team <sdk-team@alibabacloud.com>
2024-11-05 03:00:35 +0000

    Generated SDK for TAM.

--- tam/nodejs/src/client.ts
+++ tam/nodejs/src/client.ts
@@ -1599,7 +1599,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.3.4",
+          sdk_version: "1.3.5",
           _prod_code: "TAM",
           _prod_channel: "undefined",
         };
