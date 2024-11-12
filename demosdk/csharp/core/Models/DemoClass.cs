0d2e6a1d90e18a325e907e38e44e06e163e7f933
sdk-team <sdk-team@alibabacloud.com>
2024-11-12 06:10:18 +0000

    Generated SDK for DEMOSDK.

---
+++ demosdk/csharp/core/Models/DemoClass.cs
@@ -0,0 +1,40 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.Antdigital_DEMOSDK.Models
+{
+    // Demo类1
+    public class DemoClass : TeaModel {
+        // 字符串测试
+        [NameInMap("some_string")]
+        [Validation(Required=true)]
+        public string SomeString { get; set; }
+
+        // 日期测试
+        [NameInMap("some_date")]
+        [Validation(Required=true, Pattern="\\d{4}[-]\\d{1,2}[-]\\d{1,2}[T]\\d{2}:\\d{2}:\\d{2}([Z]|([\\.]\\d{1,9})?[\\+]\\d{2}[\\:]?\\d{2})")]
+        public string SomeDate { get; set; }
+
+        // Boolean测试
+        [NameInMap("some_boolean")]
+        [Validation(Required=true)]
+        public bool? SomeBoolean { get; set; }
+
+        // 整数测试
+        [NameInMap("some_int")]
+        [Validation(Required=true)]
+        public long? SomeInt { get; set; }
+
+        // 列表测试
+        [NameInMap("some_list")]
+        [Validation(Required=true)]
+        public List<string> SomeList { get; set; }
+
+    }
+
+}
---
+++ demosdk/csharp/core/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("antchain-antdigital_demosdk")]
+[assembly: AssemblyDescription("Ant Chain antdigital_DEMOSDK SDK Library for .NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ant Chain, Inc")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c40f0830-a0bc-11ef-a180-73a225dc2816")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
