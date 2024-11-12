86171806c7f96b7eeb687f16b6b94c6a4d7f516b
sdk-team <sdk-team@alibabacloud.com>
2024-11-12 06:10:23 +0000

    Generated SDK for DEMOSDK.

---
+++ demosdk/python/antchain_sdk_antdigital_demosdk/models.py
@@ -0,0 +1,339 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+from Tea.model import TeaModel
+from typing import List
+
+
+class Config(TeaModel):
+    """
+    Model for initing client
+    """
+    def __init__(
+        self,
+        access_key_id: str = None,
+        access_key_secret: str = None,
+        security_token: str = None,
+        protocol: str = None,
+        read_timeout: int = None,
+        connect_timeout: int = None,
+        http_proxy: str = None,
+        https_proxy: str = None,
+        endpoint: str = None,
+        no_proxy: str = None,
+        max_idle_conns: int = None,
+        user_agent: str = None,
+        socks_5proxy: str = None,
+        socks_5net_work: str = None,
+        max_idle_time_millis: int = None,
+        keep_alive_duration_millis: int = None,
+        max_requests: int = None,
+        max_requests_per_host: int = None,
+    ):
+        # accesskey id
+        self.access_key_id = access_key_id
+        # accesskey secret
+        self.access_key_secret = access_key_secret
+        # security token
+        self.security_token = security_token
+        # http protocol
+        self.protocol = protocol
+        # read timeout
+        self.read_timeout = read_timeout
+        # connect timeout
+        self.connect_timeout = connect_timeout
+        # http proxy
+        self.http_proxy = http_proxy
+        # https proxy
+        self.https_proxy = https_proxy
+        # endpoint
+        self.endpoint = endpoint
+        # proxy white list
+        self.no_proxy = no_proxy
+        # max idle conns
+        self.max_idle_conns = max_idle_conns
+        # user agent
+        self.user_agent = user_agent
+        # socks5 proxy
+        self.socks_5proxy = socks_5proxy
+        # socks5 network
+        self.socks_5net_work = socks_5net_work
+        # 长链接最大空闲时长
+        self.max_idle_time_millis = max_idle_time_millis
+        # 长链接最大连接时长
+        self.keep_alive_duration_millis = keep_alive_duration_millis
+        # 最大连接数（长链接最大总数）
+        self.max_requests = max_requests
+        # 每个目标主机的最大连接数（分主机域名的长链接最大总数
+        self.max_requests_per_host = max_requests_per_host
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super().to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.access_key_id is not None:
+            result['accessKeyId'] = self.access_key_id
+        if self.access_key_secret is not None:
+            result['accessKeySecret'] = self.access_key_secret
+        if self.security_token is not None:
+            result['securityToken'] = self.security_token
+        if self.protocol is not None:
+            result['protocol'] = self.protocol
+        if self.read_timeout is not None:
+            result['readTimeout'] = self.read_timeout
+        if self.connect_timeout is not None:
+            result['connectTimeout'] = self.connect_timeout
+        if self.http_proxy is not None:
+            result['httpProxy'] = self.http_proxy
+        if self.https_proxy is not None:
+            result['httpsProxy'] = self.https_proxy
+        if self.endpoint is not None:
+            result['endpoint'] = self.endpoint
+        if self.no_proxy is not None:
+            result['noProxy'] = self.no_proxy
+        if self.max_idle_conns is not None:
+            result['maxIdleConns'] = self.max_idle_conns
+        if self.user_agent is not None:
+            result['userAgent'] = self.user_agent
+        if self.socks_5proxy is not None:
+            result['socks5Proxy'] = self.socks_5proxy
+        if self.socks_5net_work is not None:
+            result['socks5NetWork'] = self.socks_5net_work
+        if self.max_idle_time_millis is not None:
+            result['maxIdleTimeMillis'] = self.max_idle_time_millis
+        if self.keep_alive_duration_millis is not None:
+            result['keepAliveDurationMillis'] = self.keep_alive_duration_millis
+        if self.max_requests is not None:
+            result['maxRequests'] = self.max_requests
+        if self.max_requests_per_host is not None:
+            result['maxRequestsPerHost'] = self.max_requests_per_host
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('accessKeyId') is not None:
+            self.access_key_id = m.get('accessKeyId')
+        if m.get('accessKeySecret') is not None:
+            self.access_key_secret = m.get('accessKeySecret')
+        if m.get('securityToken') is not None:
+            self.security_token = m.get('securityToken')
+        if m.get('protocol') is not None:
+            self.protocol = m.get('protocol')
+        if m.get('readTimeout') is not None:
+            self.read_timeout = m.get('readTimeout')
+        if m.get('connectTimeout') is not None:
+            self.connect_timeout = m.get('connectTimeout')
+        if m.get('httpProxy') is not None:
+            self.http_proxy = m.get('httpProxy')
+        if m.get('httpsProxy') is not None:
+            self.https_proxy = m.get('httpsProxy')
+        if m.get('endpoint') is not None:
+            self.endpoint = m.get('endpoint')
+        if m.get('noProxy') is not None:
+            self.no_proxy = m.get('noProxy')
+        if m.get('maxIdleConns') is not None:
+            self.max_idle_conns = m.get('maxIdleConns')
+        if m.get('userAgent') is not None:
+            self.user_agent = m.get('userAgent')
+        if m.get('socks5Proxy') is not None:
+            self.socks_5proxy = m.get('socks5Proxy')
+        if m.get('socks5NetWork') is not None:
+            self.socks_5net_work = m.get('socks5NetWork')
+        if m.get('maxIdleTimeMillis') is not None:
+            self.max_idle_time_millis = m.get('maxIdleTimeMillis')
+        if m.get('keepAliveDurationMillis') is not None:
+            self.keep_alive_duration_millis = m.get('keepAliveDurationMillis')
+        if m.get('maxRequests') is not None:
+            self.max_requests = m.get('maxRequests')
+        if m.get('maxRequestsPerHost') is not None:
+            self.max_requests_per_host = m.get('maxRequestsPerHost')
+        return self
+
+
+class DemoClass(TeaModel):
+    def __init__(
+        self,
+        some_string: str = None,
+        some_date: str = None,
+        some_boolean: bool = None,
+        some_int: int = None,
+        some_list: List[str] = None,
+    ):
+        # 字符串测试
+        self.some_string = some_string
+        # 日期测试
+        self.some_date = some_date
+        # Boolean测试
+        self.some_boolean = some_boolean
+        # 整数测试
+        self.some_int = some_int
+        # 列表测试
+        self.some_list = some_list
+
+    def validate(self):
+        self.validate_required(self.some_string, 'some_string')
+        self.validate_required(self.some_date, 'some_date')
+        if self.some_date is not None:
+            self.validate_pattern(self.some_date, 'some_date', '\\d{4}[-]\\d{1,2}[-]\\d{1,2}[T]\\d{2}:\\d{2}:\\d{2}([Z]|([\\.]\\d{1,9})?[\\+]\\d{2}[\\:]?\\d{2})')
+        self.validate_required(self.some_boolean, 'some_boolean')
+        self.validate_required(self.some_int, 'some_int')
+        if self.some_int is not None:
+            self.validate_maximum(self.some_int, 'some_int', 2000)
+            self.validate_minimum(self.some_int, 'some_int', 1)
+        self.validate_required(self.some_list, 'some_list')
+
+    def to_map(self):
+        _map = super().to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.some_string is not None:
+            result['some_string'] = self.some_string
+        if self.some_date is not None:
+            result['some_date'] = self.some_date
+        if self.some_boolean is not None:
+            result['some_boolean'] = self.some_boolean
+        if self.some_int is not None:
+            result['some_int'] = self.some_int
+        if self.some_list is not None:
+            result['some_list'] = self.some_list
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('some_string') is not None:
+            self.some_string = m.get('some_string')
+        if m.get('some_date') is not None:
+            self.some_date = m.get('some_date')
+        if m.get('some_boolean') is not None:
+            self.some_boolean = m.get('some_boolean')
+        if m.get('some_int') is not None:
+            self.some_int = m.get('some_int')
+        if m.get('some_list') is not None:
+            self.some_list = m.get('some_list')
+        return self
+
+
+class ResultTest(TeaModel):
+    def __init__(
+        self,
+        name: str = None,
+    ):
+        # 姓名
+        self.name = name
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super().to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.name is not None:
+            result['name'] = self.name
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('name') is not None:
+            self.name = m.get('name')
+        return self
+
+
+class QueryCcXxRequest(TeaModel):
+    def __init__(
+        self,
+        auth_token: str = None,
+        product_instance_id: str = None,
+        name: str = None,
+    ):
+        # OAuth模式下的授权token
+        self.auth_token = auth_token
+        self.product_instance_id = product_instance_id
+        # aa
+        self.name = name
+
+    def validate(self):
+        self.validate_required(self.name, 'name')
+
+    def to_map(self):
+        _map = super().to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.auth_token is not None:
+            result['auth_token'] = self.auth_token
+        if self.product_instance_id is not None:
+            result['product_instance_id'] = self.product_instance_id
+        if self.name is not None:
+            result['name'] = self.name
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('auth_token') is not None:
+            self.auth_token = m.get('auth_token')
+        if m.get('product_instance_id') is not None:
+            self.product_instance_id = m.get('product_instance_id')
+        if m.get('name') is not None:
+            self.name = m.get('name')
+        return self
+
+
+class QueryCcXxResponse(TeaModel):
+    def __init__(
+        self,
+        req_msg_id: str = None,
+        result_code: str = None,
+        result_msg: str = None,
+        test: str = None,
+    ):
+        # 请求唯一ID，用于链路跟踪和问题排查
+        self.req_msg_id = req_msg_id
+        # 结果码，一般OK表示调用成功
+        self.result_code = result_code
+        # 异常信息的文本描述
+        self.result_msg = result_msg
+        # 1213
+        self.test = test
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super().to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.req_msg_id is not None:
+            result['req_msg_id'] = self.req_msg_id
+        if self.result_code is not None:
+            result['result_code'] = self.result_code
+        if self.result_msg is not None:
+            result['result_msg'] = self.result_msg
+        if self.test is not None:
+            result['test'] = self.test
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('req_msg_id') is not None:
+            self.req_msg_id = m.get('req_msg_id')
+        if m.get('result_code') is not None:
+            self.result_code = m.get('result_code')
+        if m.get('result_msg') is not None:
+            self.result_msg = m.get('result_msg')
+        if m.get('test') is not None:
+            self.test = m.get('test')
+        return self
+
+
