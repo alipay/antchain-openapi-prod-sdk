bb5a5cb904afb448408cc4660f902ecfde7553c8
sdk-team <sdk-team@alibabacloud.com>
2024-12-13 09:32:50 +0000

    Generated SDK for RISKPLUS.

--- riskplus/csharp/core/Properties/AssemblyInfo.cs
+++ riskplus/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2362de00-b932-11ef-9557-fb00783dc2ef")]
+[assembly: Guid("2fd41c50-b935-11ef-84ae-bb740feff4f4")]
 
 // Version information for an assembly consists of the following four values:
 //
