fe28bbf91d21eab67e28a09b4bda483fea88d9c8
sdk-team <sdk-team@alibabacloud.com>
2024-12-13 11:40:19 +0000

    Generated SDK for RISKPLUS.

--- riskplus/go/client/client.go
+++ riskplus/go/client/client.go
@@ -29129,7 +29129,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.22.3"),
+				"sdk_version":      tea.String("1.22.4"),
 				"_prod_code":       tea.String("RISKPLUS"),
 				"_prod_channel":    tea.String("undefined"),
 			}
