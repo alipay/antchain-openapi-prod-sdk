f7c716f3d09befb70db106896dcce1f87b7a4308
sdk-team <sdk-team@alibabacloud.com>
2024-12-27 07:29:51 +0000

    Generated SDK for ak_245215eadadd4dc9bba177d6ba6d593d.

--- ak_245215eadadd4dc9bba177d6ba6d593d/composer/src/Client.php
+++ ak_245215eadadd4dc9bba177d6ba6d593d/composer/src/Client.php
@@ -182,7 +182,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.2.1',
+                    'sdk_version'      => '1.2.2',
                     '_prod_code'       => 'ak_245215eadadd4dc9bba177d6ba6d593d',
                     '_prod_channel'    => 'saas',
                 ];
