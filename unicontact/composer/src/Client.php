475decb3cc940980a504b5a5eef2c1a14a921345
sdk-team <sdk-team@alibabacloud.com>
2024-12-27 06:59:13 +0000

    Generated SDK for UNICONTACT.

--- unicontact/composer/src/Client.php
+++ unicontact/composer/src/Client.php
@@ -166,7 +166,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.0.1',
+                    'sdk_version'      => '1.0.2',
                     '_prod_code'       => 'UNICONTACT',
                     '_prod_channel'    => 'default',
                 ];
