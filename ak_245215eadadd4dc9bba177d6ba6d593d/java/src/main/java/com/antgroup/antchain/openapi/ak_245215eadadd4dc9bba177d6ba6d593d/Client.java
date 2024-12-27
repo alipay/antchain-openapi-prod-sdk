10dd575e7da5985ccc3b76e35890cdc4e5894e0d
sdk-team <sdk-team@alibabacloud.com>
2024-12-27 07:29:30 +0000

    Generated SDK for ak_245215eadadd4dc9bba177d6ba6d593d.

--- ak_245215eadadd4dc9bba177d6ba6d593d/java/src/main/java/com/antgroup/antchain/openapi/ak_245215eadadd4dc9bba177d6ba6d593d/Client.java
+++ ak_245215eadadd4dc9bba177d6ba6d593d/java/src/main/java/com/antgroup/antchain/openapi/ak_245215eadadd4dc9bba177d6ba6d593d/Client.java
@@ -122,7 +122,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.2.1"),
+                    new TeaPair("sdk_version", "1.2.2"),
                     new TeaPair("_prod_code", "ak_245215eadadd4dc9bba177d6ba6d593d"),
                     new TeaPair("_prod_channel", "saas")
                 );
