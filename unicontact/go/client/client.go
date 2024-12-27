5bac33be00fc7f59e6a5f79096cb8720a7ab5635
sdk-team <sdk-team@alibabacloud.com>
2024-12-27 06:59:56 +0000

    Generated SDK for UNICONTACT.

--- unicontact/go/client/client.go
+++ unicontact/go/client/client.go
@@ -1051,7 +1051,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.0.1"),
+				"sdk_version":      tea.String("1.0.2"),
 				"_prod_code":       tea.String("UNICONTACT"),
 				"_prod_channel":    tea.String("default"),
 			}
