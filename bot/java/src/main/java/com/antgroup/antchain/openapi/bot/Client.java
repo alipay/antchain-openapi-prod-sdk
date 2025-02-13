55e35527e4bce1ad790430f44ddd4fffa31fb542
sdk-team <sdk-team@alibabacloud.com>
2025-02-13 03:49:10 +0000

    Generated SDK for BOT.

--- bot/java/src/main/java/com/antgroup/antchain/openapi/bot/Client.java
+++ bot/java/src/main/java/com/antgroup/antchain/openapi/bot/Client.java
@@ -122,7 +122,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.12.7"),
+                    new TeaPair("sdk_version", "1.12.8"),
                     new TeaPair("_prod_code", "BOT"),
                     new TeaPair("_prod_channel", "undefined")
                 );
@@ -5014,6 +5014,25 @@ public class Client {
     public QueryOssDownloadjoburlResponse queryOssDownloadjoburlEx(QueryOssDownloadjoburlRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "blockchain.bot.oss.downloadjoburl.query", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new QueryOssDownloadjoburlResponse());
+    }
+
+    /**
+     * Description: 要素项目同步
+     * Summary: 要素项目同步
+     */
+    public SyncAssetelementProjectResponse syncAssetelementProject(SyncAssetelementProjectRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.syncAssetelementProjectEx(request, headers, runtime);
+    }
+
+    /**
+     * Description: 要素项目同步
+     * Summary: 要素项目同步
+     */
+    public SyncAssetelementProjectResponse syncAssetelementProjectEx(SyncAssetelementProjectRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "blockchain.bot.assetelement.project.sync", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new SyncAssetelementProjectResponse());
     }
 
     /**
