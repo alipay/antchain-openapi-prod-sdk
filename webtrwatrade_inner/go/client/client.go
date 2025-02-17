58def89c78d5ba66d7ec2bdfed53d3c054a1e159
sdk-team <sdk-team@alibabacloud.com>
2025-02-17 06:29:26 +0000

    Generated SDK for WEBTRWATRADE_INNER.

--- webtrwatrade_inner/go/client/client.go
+++ webtrwatrade_inner/go/client/client.go
@@ -410,7 +410,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.0.1"),
+				"sdk_version":      tea.String("1.0.2"),
 				"_prod_code":       tea.String("WEBTRWATRADE_INNER"),
 				"_prod_channel":    tea.String("default"),
 			}
