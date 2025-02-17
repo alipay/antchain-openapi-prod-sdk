429d1bb88f782224b8cc3387e4148ce1e1261f58
sdk-team <sdk-team@alibabacloud.com>
2025-02-17 06:28:50 +0000

    Generated SDK for WEBTRWATRADE_INNER.

--- webtrwatrade_inner/java/src/main/java/com/antgroup/antchain/openapi/webtrwatrade_inner/Client.java
+++ webtrwatrade_inner/java/src/main/java/com/antgroup/antchain/openapi/webtrwatrade_inner/Client.java
@@ -122,7 +122,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.0.1"),
+                    new TeaPair("sdk_version", "1.0.2"),
                     new TeaPair("_prod_code", "WEBTRWATRADE_INNER"),
                     new TeaPair("_prod_channel", "default")
                 );
