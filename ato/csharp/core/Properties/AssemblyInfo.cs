c058a602af35750699dcb1d3dee7ca0b6bc3ea19
sdk-team <sdk-team@alibabacloud.com>
2025-03-05 01:39:45 +0000

    Generated SDK for ATO.

--- ato/csharp/core/Properties/AssemblyInfo.cs
+++ ato/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ebd1b320-f960-11ef-bd12-df260c627fa6")]
+[assembly: Guid("aeb3b9a0-f962-11ef-8921-613d707c7876")]
 
 // Version information for an assembly consists of the following four values:
 //
