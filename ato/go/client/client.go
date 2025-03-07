ccefb84c70b9e214e0ca53375c75f0e3dcdd2b9a
sdk-team <sdk-team@alibabacloud.com>
2025-03-07 07:57:05 +0000

    Generated SDK for ATO.

--- ato/go/client/client.go
+++ ato/go/client/client.go
@@ -20001,7 +20001,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.14.9"),
+				"sdk_version":      tea.String("1.14.10"),
 				"_prod_code":       tea.String("ATO"),
 				"_prod_channel":    tea.String("undefined"),
 			}
