167bcad2cd907f0ad2edf63ff6c8d1df567f53cb
sdk-team <sdk-team@alibabacloud.com>
2025-03-11 09:14:40 +0000

    Generated SDK for MORSERTA.

--- morserta/nodejs/src/client.ts
+++ morserta/nodejs/src/client.ts
@@ -378,7 +378,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "2.0.1",
+          sdk_version: "2.0.2",
           _prod_code: "MORSERTA",
           _prod_channel: "default",
         };
