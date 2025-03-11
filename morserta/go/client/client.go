571d704bc1197618ed1cab8b4791cdae401b3fb2
sdk-team <sdk-team@alibabacloud.com>
2025-03-11 09:14:43 +0000

    Generated SDK for MORSERTA.

--- morserta/go/client/client.go
+++ morserta/go/client/client.go
@@ -531,7 +531,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("2.0.1"),
+				"sdk_version":      tea.String("2.0.2"),
 				"_prod_code":       tea.String("MORSERTA"),
 				"_prod_channel":    tea.String("default"),
 			}
