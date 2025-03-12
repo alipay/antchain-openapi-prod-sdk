2474f387a9bbbf64c84eb1ea6814ea356e5a110f
sdk-team <sdk-team@alibabacloud.com>
2025-03-12 01:45:54 +0000

    Generated SDK for MORSERTA.

--- morserta/go/client/client.go
+++ morserta/go/client/client.go
@@ -409,6 +409,105 @@ func (s *ConvertAdDataResponse) SetRequestId(v string) *ConvertAdDataResponse {
 	return s
 }
 
+type ClickAdDataRequest struct {
+	// OAuth模式下的授权token
+	AuthToken *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
+	// 广告主id
+	AccountId *int64 `json:"account_id,omitempty" xml:"account_id,omitempty" require:"true"`
+	// 渠道，支持TENCENT
+	Channel *string `json:"channel,omitempty" xml:"channel,omitempty" require:"true"`
+	// 点击明细json string
+	Data *string `json:"data,omitempty" xml:"data,omitempty" require:"true"`
+}
+
+func (s ClickAdDataRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ClickAdDataRequest) GoString() string {
+	return s.String()
+}
+
+func (s *ClickAdDataRequest) SetAuthToken(v string) *ClickAdDataRequest {
+	s.AuthToken = &v
+	return s
+}
+
+func (s *ClickAdDataRequest) SetAccountId(v int64) *ClickAdDataRequest {
+	s.AccountId = &v
+	return s
+}
+
+func (s *ClickAdDataRequest) SetChannel(v string) *ClickAdDataRequest {
+	s.Channel = &v
+	return s
+}
+
+func (s *ClickAdDataRequest) SetData(v string) *ClickAdDataRequest {
+	s.Data = &v
+	return s
+}
+
+type ClickAdDataResponse struct {
+	// 请求唯一ID，用于链路跟踪和问题排查
+	ReqMsgId *string `json:"req_msg_id,omitempty" xml:"req_msg_id,omitempty"`
+	// 结果码，一般OK表示调用成功
+	ResultCode *string `json:"result_code,omitempty" xml:"result_code,omitempty"`
+	// 异常信息的文本描述
+	ResultMsg *string `json:"result_msg,omitempty" xml:"result_msg,omitempty"`
+	// 调用是否成功
+	Success *bool `json:"success,omitempty" xml:"success,omitempty"`
+	// 返回码
+	Code *string `json:"code,omitempty" xml:"code,omitempty"`
+	// 异常描述
+	Message *string `json:"message,omitempty" xml:"message,omitempty"`
+	// 调用id
+	RequestId *string `json:"request_id,omitempty" xml:"request_id,omitempty"`
+}
+
+func (s ClickAdDataResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ClickAdDataResponse) GoString() string {
+	return s.String()
+}
+
+func (s *ClickAdDataResponse) SetReqMsgId(v string) *ClickAdDataResponse {
+	s.ReqMsgId = &v
+	return s
+}
+
+func (s *ClickAdDataResponse) SetResultCode(v string) *ClickAdDataResponse {
+	s.ResultCode = &v
+	return s
+}
+
+func (s *ClickAdDataResponse) SetResultMsg(v string) *ClickAdDataResponse {
+	s.ResultMsg = &v
+	return s
+}
+
+func (s *ClickAdDataResponse) SetSuccess(v bool) *ClickAdDataResponse {
+	s.Success = &v
+	return s
+}
+
+func (s *ClickAdDataResponse) SetCode(v string) *ClickAdDataResponse {
+	s.Code = &v
+	return s
+}
+
+func (s *ClickAdDataResponse) SetMessage(v string) *ClickAdDataResponse {
+	s.Message = &v
+	return s
+}
+
+func (s *ClickAdDataResponse) SetRequestId(v string) *ClickAdDataResponse {
+	s.RequestId = &v
+	return s
+}
+
 type Client struct {
 	Endpoint                *string
 	RegionId                *string
@@ -531,7 +630,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("2.0.2"),
+				"sdk_version":      tea.String("2.0.4"),
 				"_prod_code":       tea.String("MORSERTA"),
 				"_prod_channel":    tea.String("default"),
 			}
@@ -650,6 +749,40 @@ func (client *Client) ConvertAdDataEx(request *ConvertAdDataRequest, headers map
 	}
 	_result = &ConvertAdDataResponse{}
 	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("antcloud.morserta.ad.data.convert"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+/**
+ * Description: 广告主点击数据回传
+ * Summary: 广告主点击数据回传
+ */
+func (client *Client) ClickAdData(request *ClickAdDataRequest) (_result *ClickAdDataResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &ClickAdDataResponse{}
+	_body, _err := client.ClickAdDataEx(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+/**
+ * Description: 广告主点击数据回传
+ * Summary: 广告主点击数据回传
+ */
+func (client *Client) ClickAdDataEx(request *ClickAdDataRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *ClickAdDataResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = &ClickAdDataResponse{}
+	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("antcloud.morserta.ad.data.click"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
 	if _err != nil {
 		return _result, _err
 	}
