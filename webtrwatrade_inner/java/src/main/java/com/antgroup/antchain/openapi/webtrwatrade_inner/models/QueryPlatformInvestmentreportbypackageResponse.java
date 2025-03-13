4e7cc40557b8bb366b3bc736ac5947a5ed0db1d3
sdk-team <sdk-team@alibabacloud.com>
2025-03-13 07:02:48 +0000

    Generated SDK for WEBTRWATRADE_INNER.

--- webtrwatrade_inner/java/src/main/java/com/antgroup/antchain/openapi/webtrwatrade_inner/models/QueryPlatformInvestmentreportbypackageResponse.java
+++ webtrwatrade_inner/java/src/main/java/com/antgroup/antchain/openapi/webtrwatrade_inner/models/QueryPlatformInvestmentreportbypackageResponse.java
@@ -16,37 +16,9 @@ public class QueryPlatformInvestmentreportbypackageResponse extends TeaModel {
     @NameInMap("result_msg")
     public String resultMsg;
 
-    // 资产抵押率（Collateral Ratio）
-    @NameInMap("collateral_rate")
-    public String collateralRate;
-
-    // 实际收益率（Annual Yield）
-    @NameInMap("actual_annualized_yield")
-    public String actualAnnualizedYield;
-
-    // 预期收益率（Expect Yield）
-    @NameInMap("estimated_annualized_yield")
-    public String estimatedAnnualizedYield;
-
-    // 毛利吻合率（Gross Profit Conformity）
-    @NameInMap("match_rate")
-    public String matchRate;
-
-    // 投资金额（Investment Allocation）
-    @NameInMap("investment_amount")
-    public MultiCurrencyMoney investmentAmount;
-
-    // 实际收益
-    @NameInMap("actual_annualized")
-    public MultiCurrencyMoney actualAnnualized;
-
-    // 预期收益
-    @NameInMap("estimated_annualized")
-    public MultiCurrencyMoney estimatedAnnualized;
-
-    // 数据日期
-    @NameInMap("dt")
-    public String dt;
+    // 内部投资者报表数据(资产包维度)
+    @NameInMap("data")
+    public InvestmentReportInner data;
 
     public static QueryPlatformInvestmentreportbypackageResponse build(java.util.Map<String, ?> map) throws Exception {
         QueryPlatformInvestmentreportbypackageResponse self = new QueryPlatformInvestmentreportbypackageResponse();
@@ -77,68 +49,12 @@ public class QueryPlatformInvestmentreportbypackageResponse extends TeaModel {
         return this.resultMsg;
     }
 
-    public QueryPlatformInvestmentreportbypackageResponse setCollateralRate(String collateralRate) {
-        this.collateralRate = collateralRate;
+    public QueryPlatformInvestmentreportbypackageResponse setData(InvestmentReportInner data) {
+        this.data = data;
         return this;
     }
-    public String getCollateralRate() {
-        return this.collateralRate;
-    }
-
-    public QueryPlatformInvestmentreportbypackageResponse setActualAnnualizedYield(String actualAnnualizedYield) {
-        this.actualAnnualizedYield = actualAnnualizedYield;
-        return this;
-    }
-    public String getActualAnnualizedYield() {
-        return this.actualAnnualizedYield;
-    }
-
-    public QueryPlatformInvestmentreportbypackageResponse setEstimatedAnnualizedYield(String estimatedAnnualizedYield) {
-        this.estimatedAnnualizedYield = estimatedAnnualizedYield;
-        return this;
-    }
-    public String getEstimatedAnnualizedYield() {
-        return this.estimatedAnnualizedYield;
-    }
-
-    public QueryPlatformInvestmentreportbypackageResponse setMatchRate(String matchRate) {
-        this.matchRate = matchRate;
-        return this;
-    }
-    public String getMatchRate() {
-        return this.matchRate;
-    }
-
-    public QueryPlatformInvestmentreportbypackageResponse setInvestmentAmount(MultiCurrencyMoney investmentAmount) {
-        this.investmentAmount = investmentAmount;
-        return this;
-    }
-    public MultiCurrencyMoney getInvestmentAmount() {
-        return this.investmentAmount;
-    }
-
-    public QueryPlatformInvestmentreportbypackageResponse setActualAnnualized(MultiCurrencyMoney actualAnnualized) {
-        this.actualAnnualized = actualAnnualized;
-        return this;
-    }
-    public MultiCurrencyMoney getActualAnnualized() {
-        return this.actualAnnualized;
-    }
-
-    public QueryPlatformInvestmentreportbypackageResponse setEstimatedAnnualized(MultiCurrencyMoney estimatedAnnualized) {
-        this.estimatedAnnualized = estimatedAnnualized;
-        return this;
-    }
-    public MultiCurrencyMoney getEstimatedAnnualized() {
-        return this.estimatedAnnualized;
-    }
-
-    public QueryPlatformInvestmentreportbypackageResponse setDt(String dt) {
-        this.dt = dt;
-        return this;
-    }
-    public String getDt() {
-        return this.dt;
+    public InvestmentReportInner getData() {
+        return this.data;
     }
 
 }
