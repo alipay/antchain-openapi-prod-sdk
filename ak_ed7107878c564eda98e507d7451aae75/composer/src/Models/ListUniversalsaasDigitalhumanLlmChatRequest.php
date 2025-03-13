a5f2dc884a17c875514faf02a93652f040224389
sdk-team <sdk-team@alibabacloud.com>
2025-03-13 01:48:48 +0000

    Generated SDK for ak_ed7107878c564eda98e507d7451aae75.

--- ak_ed7107878c564eda98e507d7451aae75/composer/src/Models/ListUniversalsaasDigitalhumanLlmChatRequest.php
+++ ak_ed7107878c564eda98e507d7451aae75/composer/src/Models/ListUniversalsaasDigitalhumanLlmChatRequest.php
@@ -35,19 +35,12 @@ class ListUniversalsaasDigitalhumanLlmChatRequest extends Model
     /**
      * @var int
      */
-    public $pageSizr;
-
-    // 页面大小
-    /**
-     * @var int
-     */
     public $pageSize;
     protected $_name = [
         'authToken'         => 'auth_token',
         'productInstanceId' => 'product_instance_id',
         'tenantCode'        => 'tenant_code',
         'pageNum'           => 'page_num',
-        'pageSizr'          => 'page_sizr',
         'pageSize'          => 'page_size',
     ];
 
@@ -70,9 +63,6 @@ class ListUniversalsaasDigitalhumanLlmChatRequest extends Model
         }
         if (null !== $this->pageNum) {
             $res['page_num'] = $this->pageNum;
-        }
-        if (null !== $this->pageSizr) {
-            $res['page_sizr'] = $this->pageSizr;
         }
         if (null !== $this->pageSize) {
             $res['page_size'] = $this->pageSize;
@@ -100,9 +90,6 @@ class ListUniversalsaasDigitalhumanLlmChatRequest extends Model
         }
         if (isset($map['page_num'])) {
             $model->pageNum = $map['page_num'];
-        }
-        if (isset($map['page_sizr'])) {
-            $model->pageSizr = $map['page_sizr'];
         }
         if (isset($map['page_size'])) {
             $model->pageSize = $map['page_size'];
