f2f232bcef2030548855288429f299f8da2dba9f
sdk-team <sdk-team@alibabacloud.com>
2025-04-02 14:24:42 +0000

    Generated SDK for QMPBACKFLOW.

--- qmpbackflow/composer/src/Client.php
+++ qmpbackflow/composer/src/Client.php
@@ -160,7 +160,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.0.0',
+                    'sdk_version'      => '1.0.1',
                     '_prod_code'       => 'RISKPLUS',
                     '_prod_channel'    => 'undefined',
                 ];
