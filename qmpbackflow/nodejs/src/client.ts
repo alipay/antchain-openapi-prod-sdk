c53e34e5418f733caf540a5d61872e557c8c806a
sdk-team <sdk-team@alibabacloud.com>
2025-04-02 14:24:59 +0000

    Generated SDK for QMPBACKFLOW.

--- qmpbackflow/nodejs/src/client.ts
+++ qmpbackflow/nodejs/src/client.ts
@@ -5221,7 +5221,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.0",
+          sdk_version: "1.0.1",
           _prod_code: "RISKPLUS",
           _prod_channel: "undefined",
         };
