fbc498783c36b39afc1875dc216e15a1ba209b0b
sdk-team <sdk-team@alibabacloud.com>
2025-04-08 02:13:52 +0000

    Generated SDK for SECURITYTECH.

--- securitytech/composer/src/Client.php
+++ securitytech/composer/src/Client.php
@@ -17,6 +17,8 @@ use AntChain\SECURITYTECH\Models\CancelSimOrderRequest;
 use AntChain\SECURITYTECH\Models\CancelSimOrderResponse;
 use AntChain\SECURITYTECH\Models\CheckOpticalIdentifyRequest;
 use AntChain\SECURITYTECH\Models\CheckOpticalIdentifyResponse;
+use AntChain\SECURITYTECH\Models\ConfirmSimOrderRequest;
+use AntChain\SECURITYTECH\Models\ConfirmSimOrderResponse;
 use AntChain\SECURITYTECH\Models\CreateBlueshieldSecuritypictureRequest;
 use AntChain\SECURITYTECH\Models\CreateBlueshieldSecuritypictureResponse;
 use AntChain\SECURITYTECH\Models\CreateBssecpicRequest;
@@ -268,7 +270,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.4.9',
+                    'sdk_version'      => '1.4.10',
                     '_prod_code'       => 'SECURITYTECH',
                     '_prod_channel'    => 'undefined',
                 ];
@@ -877,6 +879,39 @@ class Client
         Utils::validateModel($request);
 
         return PullSimSkuResponse::fromMap($this->doRequest('1.0', 'antsecuritytech.gateway.sim.sku.pull', 'HTTPS', 'POST', '/gateway.do', Tea::merge($request), $headers, $runtime));
+    }
+
+    /**
+     * Description: 一体机购车订单支付确认请求
+     * Summary: 一体机购车订单支付确认请求
+     *
+     * @param ConfirmSimOrderRequest $request
+     *
+     * @return ConfirmSimOrderResponse
+     */
+    public function confirmSimOrder($request)
+    {
+        $runtime = new RuntimeOptions([]);
+        $headers = [];
+
+        return $this->confirmSimOrderEx($request, $headers, $runtime);
+    }
+
+    /**
+     * Description: 一体机购车订单支付确认请求
+     * Summary: 一体机购车订单支付确认请求
+     *
+     * @param ConfirmSimOrderRequest $request
+     * @param string[]               $headers
+     * @param RuntimeOptions         $runtime
+     *
+     * @return ConfirmSimOrderResponse
+     */
+    public function confirmSimOrderEx($request, $headers, $runtime)
+    {
+        Utils::validateModel($request);
+
+        return ConfirmSimOrderResponse::fromMap($this->doRequest('1.0', 'antsecuritytech.gateway.sim.order.confirm', 'HTTPS', 'POST', '/gateway.do', Tea::merge($request), $headers, $runtime));
     }
 
     /**
