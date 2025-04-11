acdea18a652514cfca83ee0a69322b82e7910cb9
sdk-team <sdk-team@alibabacloud.com>
2025-04-11 09:13:56 +0000

    Generated SDK for UNICONTACT.

--- unicontact/nodejs/src/client.ts
+++ unicontact/nodejs/src/client.ts
@@ -627,6 +627,73 @@ export class ImportInitUploadResponse extends $tea.Model {
   }
 }
 
+export class ReplaceRobotcallPhoneRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  // 请求ID
+  reqId: string;
+  // 当前时间戳毫秒值
+  timestamp: number;
+  // 待解密号码
+  aesphone: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+      reqId: 'req_id',
+      timestamp: 'timestamp',
+      aesphone: 'aesphone',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+      reqId: 'string',
+      timestamp: 'number',
+      aesphone: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class ReplaceRobotcallPhoneResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  // 解密后的手机号
+  data?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+      data: 'data',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+      data: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 
 export default class Client {
   _endpoint: string;
@@ -740,7 +807,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.2",
+          sdk_version: "1.0.3",
           _prod_code: "UNICONTACT",
           _prod_channel: "default",
         };
@@ -862,6 +929,25 @@ export default class Client {
   async importInitUploadEx(request: ImportInitUploadRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<ImportInitUploadResponse> {
     Util.validateModel(request);
     return $tea.cast<ImportInitUploadResponse>(await this.doRequest("1.0", "antdigital.unicontact.init.upload.import", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new ImportInitUploadResponse({}));
+  }
+
+  /**
+   * Description: 外呼手机号解密
+   * Summary: 外呼手机号解密
+   */
+  async replaceRobotcallPhone(request: ReplaceRobotcallPhoneRequest): Promise<ReplaceRobotcallPhoneResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.replaceRobotcallPhoneEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 外呼手机号解密
+   * Summary: 外呼手机号解密
+   */
+  async replaceRobotcallPhoneEx(request: ReplaceRobotcallPhoneRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<ReplaceRobotcallPhoneResponse> {
+    Util.validateModel(request);
+    return $tea.cast<ReplaceRobotcallPhoneResponse>(await this.doRequest("1.0", "antdigital.unicontact.robotcall.phone.replace", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new ReplaceRobotcallPhoneResponse({}));
   }
 
 }
