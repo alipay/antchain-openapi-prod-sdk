4e81bdcdee2e11a2a746e80774c30f5b31b75276
sdk-team <sdk-team@alibabacloud.com>
2025-04-11 08:59:16 +0000

    Generated SDK for SHUZIWULIU.

--- shuziwuliu/composer/src/Client.php
+++ shuziwuliu/composer/src/Client.php
@@ -492,7 +492,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.7.13',
+                    'sdk_version'      => '1.7.14',
                     '_prod_code'       => 'SHUZIWULIU',
                     '_prod_channel'    => 'undefined',
                 ];
