442812a2aeae4ed00851ccda0b7223048ac65434
sdk-team <sdk-team@alibabacloud.com>
2025-04-17 09:04:28 +0000

    Generated SDK for ak_6f8b0e7502ec49d28cd8ad0e5807bbf9.

--- ak_6f8b0e7502ec49d28cd8ad0e5807bbf9/nodejs/src/client.ts
+++ ak_6f8b0e7502ec49d28cd8ad0e5807bbf9/nodejs/src/client.ts
@@ -183,6 +183,108 @@ export class BindDemoAaaBbbCcdResponse extends $tea.Model {
   }
 }
 
+export class QueryDemoABCRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class QueryDemoABCResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class QueryDemoAasSaSaRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class QueryDemoAasSaSaResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 
 export default class Client {
   _endpoint: string;
@@ -296,7 +398,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.4",
+          sdk_version: "1.0.5",
           _prod_code: "ak_6f8b0e7502ec49d28cd8ad0e5807bbf9",
           _prod_channel: "saas",
         };
@@ -380,6 +482,44 @@ export default class Client {
   async bindDemoAaaBbbCcdEx(request: BindDemoAaaBbbCcdRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<BindDemoAaaBbbCcdResponse> {
     Util.validateModel(request);
     return $tea.cast<BindDemoAaaBbbCcdResponse>(await this.doRequest("1.0", "demo.aaa.bbb.ccd.bind", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new BindDemoAaaBbbCcdResponse({}));
+  }
+
+  /**
+   * Description: a
+   * Summary: abcde
+   */
+  async queryDemoABC(request: QueryDemoABCRequest): Promise<QueryDemoABCResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.queryDemoABCEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: a
+   * Summary: abcde
+   */
+  async queryDemoABCEx(request: QueryDemoABCRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<QueryDemoABCResponse> {
+    Util.validateModel(request);
+    return $tea.cast<QueryDemoABCResponse>(await this.doRequest("1.0", "demo.a.b.c.query", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new QueryDemoABCResponse({}));
+  }
+
+  /**
+   * Description: sss
+   * Summary: sss
+   */
+  async queryDemoAasSaSa(request: QueryDemoAasSaSaRequest): Promise<QueryDemoAasSaSaResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.queryDemoAasSaSaEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: sss
+   * Summary: sss
+   */
+  async queryDemoAasSaSaEx(request: QueryDemoAasSaSaRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<QueryDemoAasSaSaResponse> {
+    Util.validateModel(request);
+    return $tea.cast<QueryDemoAasSaSaResponse>(await this.doRequest("1.0", "demo.aas.sa.sa.query", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new QueryDemoAasSaSaResponse({}));
   }
 
 }
