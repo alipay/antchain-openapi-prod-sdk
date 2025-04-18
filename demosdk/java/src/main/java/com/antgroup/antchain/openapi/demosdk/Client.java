990cbab9eb43f72021bc032088257d4f05170a01
sdk-team <sdk-team@alibabacloud.com>
2025-04-18 02:31:00 +0000

    Generated SDK for DEMOSDK.

--- demosdk/java/src/main/java/com/antgroup/antchain/openapi/demosdk/Client.java
+++ demosdk/java/src/main/java/com/antgroup/antchain/openapi/demosdk/Client.java
@@ -122,7 +122,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.3.6"),
+                    new TeaPair("sdk_version", "1.3.7"),
                     new TeaPair("_prod_code", "DEMOSDK"),
                     new TeaPair("_prod_channel", "default")
                 );
@@ -318,25 +318,6 @@ public class Client {
     public BindXxxResponse bindXxxEx(BindXxxRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "antchain.demosdk.xxx.bind", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new BindXxxResponse());
-    }
-
-    /**
-     * Description: 自动化测试创建test，请勿修改、删除
-     * Summary: 自动化测试创建test1
-     */
-    public UpdateAaaAcdResponse updateAaaAcd(UpdateAaaAcdRequest request) throws Exception {
-        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
-        java.util.Map<String, String> headers = new java.util.HashMap<>();
-        return this.updateAaaAcdEx(request, headers, runtime);
-    }
-
-    /**
-     * Description: 自动化测试创建test，请勿修改、删除
-     * Summary: 自动化测试创建test1
-     */
-    public UpdateAaaAcdResponse updateAaaAcdEx(UpdateAaaAcdRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
-        com.aliyun.teautil.Common.validateModel(request);
-        return TeaModel.toModel(this.doRequest("1.0", "antchain.demosdk.aaa.acd.update", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new UpdateAaaAcdResponse());
     }
 
     /**
