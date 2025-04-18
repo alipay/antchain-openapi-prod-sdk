bbad3a32f05d3cc275a44c0783733024f82a431f
sdk-team <sdk-team@alibabacloud.com>
2025-04-18 02:31:35 +0000

    Generated SDK for DEMOSDK.

--- demosdk/go/client/client.go
+++ demosdk/go/client/client.go
@@ -640,76 +640,6 @@ func (s *BindXxxResponse) SetResultMsg(v string) *BindXxxResponse {
 	return s
 }
 
-type UpdateAaaAcdRequest struct {
-	// OAuth模式下的授权token
-	AuthToken         *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
-	ProductInstanceId *string `json:"product_instance_id,omitempty" xml:"product_instance_id,omitempty"`
-	// 123
-	Date *string `json:"date,omitempty" xml:"date,omitempty" require:"true" pattern:"\\d{4}[-]\\d{1,2}[-]\\d{1,2}[T]\\d{2}:\\d{2}:\\d{2}([Z]|([\\.]\\d{1,9})?[\\+]\\d{2}[\\:]?\\d{2})"`
-	// 123
-	Data *string `json:"data,omitempty" xml:"data,omitempty" require:"true"`
-}
-
-func (s UpdateAaaAcdRequest) String() string {
-	return tea.Prettify(s)
-}
-
-func (s UpdateAaaAcdRequest) GoString() string {
-	return s.String()
-}
-
-func (s *UpdateAaaAcdRequest) SetAuthToken(v string) *UpdateAaaAcdRequest {
-	s.AuthToken = &v
-	return s
-}
-
-func (s *UpdateAaaAcdRequest) SetProductInstanceId(v string) *UpdateAaaAcdRequest {
-	s.ProductInstanceId = &v
-	return s
-}
-
-func (s *UpdateAaaAcdRequest) SetDate(v string) *UpdateAaaAcdRequest {
-	s.Date = &v
-	return s
-}
-
-func (s *UpdateAaaAcdRequest) SetData(v string) *UpdateAaaAcdRequest {
-	s.Data = &v
-	return s
-}
-
-type UpdateAaaAcdResponse struct {
-	// 请求唯一ID，用于链路跟踪和问题排查
-	ReqMsgId *string `json:"req_msg_id,omitempty" xml:"req_msg_id,omitempty"`
-	// 结果码，一般OK表示调用成功
-	ResultCode *string `json:"result_code,omitempty" xml:"result_code,omitempty"`
-	// 异常信息的文本描述
-	ResultMsg *string `json:"result_msg,omitempty" xml:"result_msg,omitempty"`
-}
-
-func (s UpdateAaaAcdResponse) String() string {
-	return tea.Prettify(s)
-}
-
-func (s UpdateAaaAcdResponse) GoString() string {
-	return s.String()
-}
-
-func (s *UpdateAaaAcdResponse) SetReqMsgId(v string) *UpdateAaaAcdResponse {
-	s.ReqMsgId = &v
-	return s
-}
-
-func (s *UpdateAaaAcdResponse) SetResultCode(v string) *UpdateAaaAcdResponse {
-	s.ResultCode = &v
-	return s
-}
-
-func (s *UpdateAaaAcdResponse) SetResultMsg(v string) *UpdateAaaAcdResponse {
-	s.ResultMsg = &v
-	return s
-}
-
 type QueryCcXxRequest struct {
 	// OAuth模式下的授权token
 	AuthToken         *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
@@ -1022,7 +952,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.3.6"),
+				"sdk_version":      tea.String("1.3.7"),
 				"_prod_code":       tea.String("DEMOSDK"),
 				"_prod_channel":    tea.String("default"),
 			}
@@ -1307,40 +1237,6 @@ func (client *Client) BindXxxEx(request *BindXxxRequest, headers map[string]*str
 	}
 	_result = &BindXxxResponse{}
 	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("antchain.demosdk.xxx.bind"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
-	if _err != nil {
-		return _result, _err
-	}
-	_err = tea.Convert(_body, &_result)
-	return _result, _err
-}
-
-/**
- * Description: 自动化测试创建test，请勿修改、删除
- * Summary: 自动化测试创建test1
- */
-func (client *Client) UpdateAaaAcd(request *UpdateAaaAcdRequest) (_result *UpdateAaaAcdResponse, _err error) {
-	runtime := &util.RuntimeOptions{}
-	headers := make(map[string]*string)
-	_result = &UpdateAaaAcdResponse{}
-	_body, _err := client.UpdateAaaAcdEx(request, headers, runtime)
-	if _err != nil {
-		return _result, _err
-	}
-	_result = _body
-	return _result, _err
-}
-
-/**
- * Description: 自动化测试创建test，请勿修改、删除
- * Summary: 自动化测试创建test1
- */
-func (client *Client) UpdateAaaAcdEx(request *UpdateAaaAcdRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *UpdateAaaAcdResponse, _err error) {
-	_err = util.ValidateModel(request)
-	if _err != nil {
-		return _result, _err
-	}
-	_result = &UpdateAaaAcdResponse{}
-	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("antchain.demosdk.aaa.acd.update"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
 	if _err != nil {
 		return _result, _err
 	}
