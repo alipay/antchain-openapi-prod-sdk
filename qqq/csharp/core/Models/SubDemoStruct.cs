aea2e8c4dd21ff7812222764c2f66e893bfde2be
sdk-team <sdk-team@alibabacloud.com>
2025-04-23 08:47:26 +0000

    Generated SDK for QQQ.

---
+++ qqq/csharp/core/Models/SubDemoStruct.cs
@@ -0,0 +1,25 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.DEMOSDK.Models
+{
+    // 子结构体
+    public class SubDemoStruct : TeaModel {
+        // name
+        [NameInMap("name")]
+        [Validation(Required=true)]
+        public string Name { get; set; }
+
+        // value
+        [NameInMap("value")]
+        [Validation(Required=true)]
+        public string Value { get; set; }
+
+    }
+
+}
