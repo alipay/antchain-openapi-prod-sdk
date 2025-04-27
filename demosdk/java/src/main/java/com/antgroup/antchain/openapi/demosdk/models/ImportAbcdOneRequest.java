78e4dd67ef87e5d5b4d72c35893302a80241547a
sdk-team <sdk-team@alibabacloud.com>
2025-04-27 06:53:39 +0000

    Generated SDK for DEMOSDK.

--- demosdk/java/src/main/java/com/antgroup/antchain/openapi/demosdk/models/ImportAbcdOneRequest.java
+++ demosdk/java/src/main/java/com/antgroup/antchain/openapi/demosdk/models/ImportAbcdOneRequest.java
@@ -11,6 +11,26 @@ public class ImportAbcdOneRequest extends TeaModel {
     @NameInMap("product_instance_id")
     public String productInstanceId;
 
+    // 超时时间
+    @NameInMap("timeout")
+    public String timeout;
+
+    // 请求编号
+    @NameInMap("count")
+    public Long count;
+
+    // 请求时间
+    @NameInMap("time")
+    public String time;
+
+    // 请求描述
+    @NameInMap("desc")
+    public String desc;
+
+    // 操作者
+    @NameInMap("operator")
+    public String operator;
+
     public static ImportAbcdOneRequest build(java.util.Map<String, ?> map) throws Exception {
         ImportAbcdOneRequest self = new ImportAbcdOneRequest();
         return TeaModel.build(map, self);
@@ -30,6 +50,46 @@ public class ImportAbcdOneRequest extends TeaModel {
     }
     public String getProductInstanceId() {
         return this.productInstanceId;
+    }
+
+    public ImportAbcdOneRequest setTimeout(String timeout) {
+        this.timeout = timeout;
+        return this;
+    }
+    public String getTimeout() {
+        return this.timeout;
+    }
+
+    public ImportAbcdOneRequest setCount(Long count) {
+        this.count = count;
+        return this;
+    }
+    public Long getCount() {
+        return this.count;
+    }
+
+    public ImportAbcdOneRequest setTime(String time) {
+        this.time = time;
+        return this;
+    }
+    public String getTime() {
+        return this.time;
+    }
+
+    public ImportAbcdOneRequest setDesc(String desc) {
+        this.desc = desc;
+        return this;
+    }
+    public String getDesc() {
+        return this.desc;
+    }
+
+    public ImportAbcdOneRequest setOperator(String operator) {
+        this.operator = operator;
+        return this;
+    }
+    public String getOperator() {
+        return this.operator;
     }
 
 }
--- demosdk/java/src/main/java/com/antgroup/antchain/openapi/demosdk/models/QueryAbcdOneRequest.java
+++ demosdk/java/src/main/java/com/antgroup/antchain/openapi/demosdk/models/QueryAbcdOneRequest.java
@@ -11,6 +11,26 @@ public class QueryAbcdOneRequest extends TeaModel {
     @NameInMap("product_instance_id")
     public String productInstanceId;
 
+    // 超时时间
+    @NameInMap("timeout")
+    public String timeout;
+
+    // 请求编号
+    @NameInMap("count")
+    public Long count;
+
+    // 请求时间
+    @NameInMap("time")
+    public String time;
+
+    // 请求描述
+    @NameInMap("desc")
+    public String desc;
+
+    // 操作人
+    @NameInMap("operator")
+    public String operator;
+
     public static QueryAbcdOneRequest build(java.util.Map<String, ?> map) throws Exception {
         QueryAbcdOneRequest self = new QueryAbcdOneRequest();
         return TeaModel.build(map, self);
@@ -30,6 +50,46 @@ public class QueryAbcdOneRequest extends TeaModel {
     }
     public String getProductInstanceId() {
         return this.productInstanceId;
+    }
+
+    public QueryAbcdOneRequest setTimeout(String timeout) {
+        this.timeout = timeout;
+        return this;
+    }
+    public String getTimeout() {
+        return this.timeout;
+    }
+
+    public QueryAbcdOneRequest setCount(Long count) {
+        this.count = count;
+        return this;
+    }
+    public Long getCount() {
+        return this.count;
+    }
+
+    public QueryAbcdOneRequest setTime(String time) {
+        this.time = time;
+        return this;
+    }
+    public String getTime() {
+        return this.time;
+    }
+
+    public QueryAbcdOneRequest setDesc(String desc) {
+        this.desc = desc;
+        return this;
+    }
+    public String getDesc() {
+        return this.desc;
+    }
+
+    public QueryAbcdOneRequest setOperator(String operator) {
+        this.operator = operator;
+        return this;
+    }
+    public String getOperator() {
+        return this.operator;
     }
 
 }
