a211aebaad4928f31c06dd554266e9e9187053f2
sdk-team <sdk-team@alibabacloud.com>
2025-04-28 05:40:12 +0000

    Generated SDK for BLOCKCHAIN.

--- blockchain/go/client/client.go
+++ blockchain/go/client/client.go
@@ -70690,7 +70690,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.28.43"),
+				"sdk_version":      tea.String("1.28.44"),
 				"_prod_code":       tea.String("BLOCKCHAIN"),
 				"_prod_channel":    tea.String("undefined"),
 			}
