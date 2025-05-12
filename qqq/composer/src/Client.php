d61a851287dee2c84e876641d3bc5b7f4b11850e
sdk-team <sdk-team@alibabacloud.com>
2025-05-12 09:40:31 +0000

    Generated SDK for QQQ.

--- qqq/composer/src/Client.php
+++ qqq/composer/src/Client.php
@@ -164,7 +164,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.0.4',
+                    'sdk_version'      => '1.0.5',
                     '_prod_code'       => 'DEMOSDK',
                     '_prod_channel'    => 'default',
                 ];
