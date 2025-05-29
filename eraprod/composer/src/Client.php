5e4e4cb285975c78d4aa0a32e0ab4ee157ce8042
sdk-team <sdk-team@alibabacloud.com>
2025-05-29 07:00:02 +0000

    Generated SDK for ERAPROD.

--- eraprod/composer/src/Client.php
+++ eraprod/composer/src/Client.php
@@ -159,7 +159,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.0.0',
+                    'sdk_version'      => '1.0.1',
                     '_prod_code'       => 'ERAPROD',
                     '_prod_channel'    => 'default',
                 ];
--- eraprod/composer/src/Models/OperateAisProxyResponse.php
+++ eraprod/composer/src/Models/OperateAisProxyResponse.php
@@ -25,10 +25,17 @@ class OperateAisProxyResponse extends Model
      * @var string
      */
     public $resultMsg;
+
+    // 返回结果，JSON结构
+    /**
+     * @var string
+     */
+    public $resultJson;
     protected $_name = [
         'reqMsgId'   => 'req_msg_id',
         'resultCode' => 'result_code',
         'resultMsg'  => 'result_msg',
+        'resultJson' => 'result_json',
     ];
 
     public function validate()
@@ -46,6 +53,9 @@ class OperateAisProxyResponse extends Model
         }
         if (null !== $this->resultMsg) {
             $res['result_msg'] = $this->resultMsg;
+        }
+        if (null !== $this->resultJson) {
+            $res['result_json'] = $this->resultJson;
         }
 
         return $res;
@@ -67,6 +77,9 @@ class OperateAisProxyResponse extends Model
         }
         if (isset($map['result_msg'])) {
             $model->resultMsg = $map['result_msg'];
+        }
+        if (isset($map['result_json'])) {
+            $model->resultJson = $map['result_json'];
         }
 
         return $model;
