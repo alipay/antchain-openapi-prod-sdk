e1c268b82c94d9e668e352086bd973838b0a030d
sdk-team <sdk-team@alibabacloud.com>
2025-06-05 10:59:22 +0000

    Generated SDK for BLOCKCHAIN.

--- blockchain/java/src/main/java/com/antgroup/antchain/openapi/blockchain/Client.java
+++ blockchain/java/src/main/java/com/antgroup/antchain/openapi/blockchain/Client.java
@@ -122,7 +122,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.28.49"),
+                    new TeaPair("sdk_version", "1.28.50"),
                     new TeaPair("_prod_code", "BLOCKCHAIN"),
                     new TeaPair("_prod_channel", "undefined")
                 );
