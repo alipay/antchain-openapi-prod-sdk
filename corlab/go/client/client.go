e51e2262b05bee5f67625006641f3114caceee69
sdk-team <sdk-team@alibabacloud.com>
2025-06-13 01:49:03 +0000

    Generated SDK for CORLAB.

--- corlab/go/client/client.go
+++ corlab/go/client/client.go
@@ -1041,7 +1041,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.0.14"),
+				"sdk_version":      tea.String("1.0.15"),
 				"_prod_code":       tea.String("CORLAB"),
 				"_prod_channel":    tea.String("default"),
 			}
