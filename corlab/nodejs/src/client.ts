b88f19d4083214a1e6fb5fa2cea50d4fc7d8a5d4
sdk-team <sdk-team@alibabacloud.com>
2025-07-03 09:03:21 +0000

    Generated SDK for CORLAB.

--- corlab/nodejs/src/client.ts
+++ corlab/nodejs/src/client.ts
@@ -796,7 +796,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.18",
+          sdk_version: "1.0.19",
           _prod_code: "CORLAB",
           _prod_channel: "default",
         };
