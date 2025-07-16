37668f89c306f6197ac566a29b5674833bcd3f76
sdk-team <sdk-team@alibabacloud.com>
2025-07-16 02:50:42 +0000

    Generated SDK for INSURANCE_SAAS.

--- insurance_saas/go/client/client.go
+++ insurance_saas/go/client/client.go
@@ -4126,7 +4126,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.9.2"),
+				"sdk_version":      tea.String("1.9.3"),
 				"_prod_code":       tea.String("INSURANCE_SAAS"),
 				"_prod_channel":    tea.String("undefined"),
 			}
