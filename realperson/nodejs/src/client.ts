e36bf04a332a57a02a68ceac361418b8b6a94f4a
sdk-team <sdk-team@alibabacloud.com>
2025-07-16 08:07:06 +0000

    Generated SDK for REALPERSON.

--- realperson/nodejs/src/client.ts
+++ realperson/nodejs/src/client.ts
@@ -5072,7 +5072,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.21.3",
+          sdk_version: "1.21.4",
           _prod_code: "REALPERSON",
           _prod_channel: "undefined",
         };
