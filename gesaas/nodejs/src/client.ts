ce8a9b306140db609a94073aebb69eceee91807c
sdk-team <sdk-team@alibabacloud.com>
2025-07-29 11:26:44 +0000

    Generated SDK for GESAAS.

--- gesaas/nodejs/src/client.ts
+++ gesaas/nodejs/src/client.ts
@@ -433,7 +433,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.2",
+          sdk_version: "1.0.3",
           _prod_code: "GESAAS",
           _prod_channel: "default",
         };
