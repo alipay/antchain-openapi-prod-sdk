4e23406a518c421f6a8b785ab80253b0a081f6a1
sdk-team <sdk-team@alibabacloud.com>
2025-08-12 02:11:30 +0000

    Generated SDK for DEMO.

--- demo/composer/src/Client.php
+++ demo/composer/src/Client.php
@@ -43,6 +43,8 @@ use AntChain\DEMO\Models\QueryApiBlackListRequest;
 use AntChain\DEMO\Models\QueryApiBlackListResponse;
 use AntChain\DEMO\Models\QueryApiWhiteListRequest;
 use AntChain\DEMO\Models\QueryApiWhiteListResponse;
+use AntChain\DEMO\Models\QueryAutoTestRequest;
+use AntChain\DEMO\Models\QueryAutoTestResponse;
 use AntChain\DEMO\Models\QueryGatewayCheckEchotenRequest;
 use AntChain\DEMO\Models\QueryGatewayCheckEchotenResponse;
 use AntChain\DEMO\Models\QueryGatewayCheckEchotimeoutokRequest;
@@ -262,7 +264,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.1.51',
+                    'sdk_version'      => '1.1.56',
                     '_prod_code'       => 'DEMO',
                     '_prod_channel'    => 'undefined',
                 ];
@@ -2010,6 +2012,39 @@ class Client
         Utils::validateModel($request);
 
         return BindAaaBbbCcdResponse::fromMap($this->doRequest('1.0', 'demo.aaa.bbb.ccd.bind', 'HTTPS', 'POST', '/gateway.do', Tea::merge($request), $headers, $runtime));
+    }
+
+    /**
+     * Description: 测试
+     * Summary: test.
+     *
+     * @param QueryAutoTestRequest $request
+     *
+     * @return QueryAutoTestResponse
+     */
+    public function queryAutoTest($request)
+    {
+        $runtime = new RuntimeOptions([]);
+        $headers = [];
+
+        return $this->queryAutoTestEx($request, $headers, $runtime);
+    }
+
+    /**
+     * Description: 测试
+     * Summary: test.
+     *
+     * @param QueryAutoTestRequest $request
+     * @param string[]             $headers
+     * @param RuntimeOptions       $runtime
+     *
+     * @return QueryAutoTestResponse
+     */
+    public function queryAutoTestEx($request, $headers, $runtime)
+    {
+        Utils::validateModel($request);
+
+        return QueryAutoTestResponse::fromMap($this->doRequest('1.0', 'demo.auto.test.query', 'HTTPS', 'POST', '/gateway.do', Tea::merge($request), $headers, $runtime));
     }
 
     /**
