4144b665f7817512bece1fda73196ea9863dcc64
sdk-team <sdk-team@alibabacloud.com>
2025-08-13 12:23:54 +0000

    Generated SDK for BOT.

--- bot/nodejs/src/client.ts
+++ bot/nodejs/src/client.ts
@@ -26914,6 +26914,73 @@ export class UpdateDeviceBydeviceidResponse extends $tea.Model {
   }
 }
 
+export class QueryTaskscanRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class QueryTaskscanResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  // 是否成功
+  success?: boolean;
+  // 错误信息
+  errormsg?: string;
+  // 错误码
+  errorcode?: string;
+  // 返回数据
+  data?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+      success: 'success',
+      errormsg: 'errormsg',
+      errorcode: 'errorcode',
+      data: 'data',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+      success: 'boolean',
+      errormsg: 'string',
+      errorcode: 'string',
+      data: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class ExecThingsdidOneapiRequest extends $tea.Model {
   // OAuth模式下的授权token
   authToken?: string;
@@ -28296,7 +28363,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.12.28",
+          sdk_version: "1.12.31",
           _prod_code: "BOT",
           _prod_channel: "undefined",
         };
@@ -33307,6 +33374,25 @@ export default class Client {
   async updateDeviceBydeviceidEx(request: UpdateDeviceBydeviceidRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<UpdateDeviceBydeviceidResponse> {
     Util.validateModel(request);
     return $tea.cast<UpdateDeviceBydeviceidResponse>(await this.doRequest("1.0", "blockchain.bot.device.bydeviceid.update", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new UpdateDeviceBydeviceidResponse({}));
+  }
+
+  /**
+   * Description: 时序异常检测任务扫描
+   * Summary: 任务扫描
+   */
+  async queryTaskscan(request: QueryTaskscanRequest): Promise<QueryTaskscanResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.queryTaskscanEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 时序异常检测任务扫描
+   * Summary: 任务扫描
+   */
+  async queryTaskscanEx(request: QueryTaskscanRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<QueryTaskscanResponse> {
+    Util.validateModel(request);
+    return $tea.cast<QueryTaskscanResponse>(await this.doRequest("1.0", "blockchain.bot.taskscan.query", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new QueryTaskscanResponse({}));
   }
 
   /**
