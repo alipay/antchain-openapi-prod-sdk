5586f653bbd580006df2579a9fa36593cfbc35b9
sdk-team <sdk-team@alibabacloud.com>
2025-08-13 12:25:34 +0000

    Generated SDK for BOT.

--- bot/go/client/client.go
+++ bot/go/client/client.go
@@ -35124,6 +35124,90 @@ func (s *UpdateDeviceBydeviceidResponse) SetResultMsg(v string) *UpdateDeviceByd
 	return s
 }
 
+type QueryTaskscanRequest struct {
+	// OAuth模式下的授权token
+	AuthToken         *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
+	ProductInstanceId *string `json:"product_instance_id,omitempty" xml:"product_instance_id,omitempty"`
+}
+
+func (s QueryTaskscanRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s QueryTaskscanRequest) GoString() string {
+	return s.String()
+}
+
+func (s *QueryTaskscanRequest) SetAuthToken(v string) *QueryTaskscanRequest {
+	s.AuthToken = &v
+	return s
+}
+
+func (s *QueryTaskscanRequest) SetProductInstanceId(v string) *QueryTaskscanRequest {
+	s.ProductInstanceId = &v
+	return s
+}
+
+type QueryTaskscanResponse struct {
+	// 请求唯一ID，用于链路跟踪和问题排查
+	ReqMsgId *string `json:"req_msg_id,omitempty" xml:"req_msg_id,omitempty"`
+	// 结果码，一般OK表示调用成功
+	ResultCode *string `json:"result_code,omitempty" xml:"result_code,omitempty"`
+	// 异常信息的文本描述
+	ResultMsg *string `json:"result_msg,omitempty" xml:"result_msg,omitempty"`
+	// 是否成功
+	Success *bool `json:"success,omitempty" xml:"success,omitempty"`
+	// 错误信息
+	Errormsg *string `json:"errormsg,omitempty" xml:"errormsg,omitempty"`
+	// 错误码
+	Errorcode *string `json:"errorcode,omitempty" xml:"errorcode,omitempty"`
+	// 返回数据
+	Data *string `json:"data,omitempty" xml:"data,omitempty"`
+}
+
+func (s QueryTaskscanResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s QueryTaskscanResponse) GoString() string {
+	return s.String()
+}
+
+func (s *QueryTaskscanResponse) SetReqMsgId(v string) *QueryTaskscanResponse {
+	s.ReqMsgId = &v
+	return s
+}
+
+func (s *QueryTaskscanResponse) SetResultCode(v string) *QueryTaskscanResponse {
+	s.ResultCode = &v
+	return s
+}
+
+func (s *QueryTaskscanResponse) SetResultMsg(v string) *QueryTaskscanResponse {
+	s.ResultMsg = &v
+	return s
+}
+
+func (s *QueryTaskscanResponse) SetSuccess(v bool) *QueryTaskscanResponse {
+	s.Success = &v
+	return s
+}
+
+func (s *QueryTaskscanResponse) SetErrormsg(v string) *QueryTaskscanResponse {
+	s.Errormsg = &v
+	return s
+}
+
+func (s *QueryTaskscanResponse) SetErrorcode(v string) *QueryTaskscanResponse {
+	s.Errorcode = &v
+	return s
+}
+
+func (s *QueryTaskscanResponse) SetData(v string) *QueryTaskscanResponse {
+	s.Data = &v
+	return s
+}
+
 type ExecThingsdidOneapiRequest struct {
 	// OAuth模式下的授权token
 	AuthToken         *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
@@ -36894,7 +36978,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.12.28"),
+				"sdk_version":      tea.String("1.12.31"),
 				"_prod_code":       tea.String("BOT"),
 				"_prod_channel":    tea.String("undefined"),
 			}
@@ -45811,6 +45895,40 @@ func (client *Client) UpdateDeviceBydeviceidEx(request *UpdateDeviceBydeviceidRe
 	}
 	_result = &UpdateDeviceBydeviceidResponse{}
 	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("blockchain.bot.device.bydeviceid.update"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+/**
+ * Description: 时序异常检测任务扫描
+ * Summary: 任务扫描
+ */
+func (client *Client) QueryTaskscan(request *QueryTaskscanRequest) (_result *QueryTaskscanResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &QueryTaskscanResponse{}
+	_body, _err := client.QueryTaskscanEx(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+/**
+ * Description: 时序异常检测任务扫描
+ * Summary: 任务扫描
+ */
+func (client *Client) QueryTaskscanEx(request *QueryTaskscanRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *QueryTaskscanResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = &QueryTaskscanResponse{}
+	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("blockchain.bot.taskscan.query"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
 	if _err != nil {
 		return _result, _err
 	}
