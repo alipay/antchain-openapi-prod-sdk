162f70f695a115babcd59c49e76b3f1ccb8f4896
sdk-team <sdk-team@alibabacloud.com>
2025-08-14 03:34:26 +0000

    Generated SDK for DOG.

--- dog/composer/src/Client.php
+++ dog/composer/src/Client.php
@@ -170,7 +170,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.0.6',
+                    'sdk_version'      => '1.0.8',
                     '_prod_code'       => 'DOG',
                     '_prod_channel'    => 'undefined',
                 ];
