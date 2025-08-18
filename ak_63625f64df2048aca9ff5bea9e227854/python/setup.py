3e0147089fec7fcf613a66d9a912f72a5a8308b7
sdk-team <sdk-team@alibabacloud.com>
2025-08-18 09:28:07 +0000

    Generated SDK for ak_63625f64df2048aca9ff5bea9e227854.

--- ak_63625f64df2048aca9ff5bea9e227854/python/setup.py
+++ ak_63625f64df2048aca9ff5bea9e227854/python/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for antchain_ak_63625f64df2048aca9ff5bea9e227854.
 
-Created on 13/08/2025
+Created on 18/08/2025
 
 @author: Ant Chain SDK
 """
