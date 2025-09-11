a2dc34a44c8c2b7dbcea100e794c09577cdd82d7
sdk-team <sdk-team@alibabacloud.com>
2025-09-11 07:16:16 +0000

    Generated SDK for WEBTRWATRADE.

--- webtrwatrade/nodejs/src/client.ts
+++ webtrwatrade/nodejs/src/client.ts
@@ -5074,7 +5074,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.25",
+          sdk_version: "1.0.26",
           _prod_code: "WEBTRWATRADE",
           _prod_channel: "default",
         };
