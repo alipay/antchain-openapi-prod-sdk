136d575b76b16952e887dfaa483cea312c8f532e
sdk-team <sdk-team@alibabacloud.com>
2025-09-17 05:52:24 +0000

    Generated SDK for RISKPLUS.

--- riskplus/java/src/main/java/com/antgroup/antchain/openapi/riskplus/models/QueryBenefithubReportInformationRequest.java
+++ riskplus/java/src/main/java/com/antgroup/antchain/openapi/riskplus/models/QueryBenefithubReportInformationRequest.java
@@ -16,6 +16,21 @@ public class QueryBenefithubReportInformationRequest extends TeaModel {
     @Validation(required = true)
     public String orderNo;
 
+    // 业务分配的平台code，8位纯大写字母code
+    @NameInMap("platform_code")
+    @Validation(required = true)
+    public String platformCode;
+
+    // 产品码
+    @NameInMap("product_code")
+    @Validation(required = true)
+    public String productCode;
+
+    // 平台code、用户唯一ID 组合出全局唯一id
+    @NameInMap("user_unique_id")
+    @Validation(required = true)
+    public String userUniqueId;
+
     public static QueryBenefithubReportInformationRequest build(java.util.Map<String, ?> map) throws Exception {
         QueryBenefithubReportInformationRequest self = new QueryBenefithubReportInformationRequest();
         return TeaModel.build(map, self);
@@ -43,6 +58,30 @@ public class QueryBenefithubReportInformationRequest extends TeaModel {
     }
     public String getOrderNo() {
         return this.orderNo;
+    }
+
+    public QueryBenefithubReportInformationRequest setPlatformCode(String platformCode) {
+        this.platformCode = platformCode;
+        return this;
+    }
+    public String getPlatformCode() {
+        return this.platformCode;
+    }
+
+    public QueryBenefithubReportInformationRequest setProductCode(String productCode) {
+        this.productCode = productCode;
+        return this;
+    }
+    public String getProductCode() {
+        return this.productCode;
+    }
+
+    public QueryBenefithubReportInformationRequest setUserUniqueId(String userUniqueId) {
+        this.userUniqueId = userUniqueId;
+        return this;
+    }
+    public String getUserUniqueId() {
+        return this.userUniqueId;
     }
 
 }
