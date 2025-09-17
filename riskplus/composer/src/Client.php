0deadbe0130d863e94f7acd2b4e5f4236cbebc56
sdk-team <sdk-team@alibabacloud.com>
2025-09-17 05:53:31 +0000

    Generated SDK for RISKPLUS.

--- riskplus/composer/src/Client.php
+++ riskplus/composer/src/Client.php
@@ -632,7 +632,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.26.4',
+                    'sdk_version'      => '1.26.5',
                     '_prod_code'       => 'RISKPLUS',
                     '_prod_channel'    => 'undefined',
                 ];
--- riskplus/composer/src/Models/QueryBenefithubReportInformationRequest.php
+++ riskplus/composer/src/Models/QueryBenefithubReportInformationRequest.php
@@ -24,15 +24,39 @@ class QueryBenefithubReportInformationRequest extends Model
      * @var string
      */
     public $orderNo;
+
+    // 业务分配的平台code，8位纯大写字母code
+    /**
+     * @var string
+     */
+    public $platformCode;
+
+    // 产品码
+    /**
+     * @var string
+     */
+    public $productCode;
+
+    // 平台code、用户唯一ID 组合出全局唯一id
+    /**
+     * @var string
+     */
+    public $userUniqueId;
     protected $_name = [
         'authToken'         => 'auth_token',
         'productInstanceId' => 'product_instance_id',
         'orderNo'           => 'order_no',
+        'platformCode'      => 'platform_code',
+        'productCode'       => 'product_code',
+        'userUniqueId'      => 'user_unique_id',
     ];
 
     public function validate()
     {
         Model::validateRequired('orderNo', $this->orderNo, true);
+        Model::validateRequired('platformCode', $this->platformCode, true);
+        Model::validateRequired('productCode', $this->productCode, true);
+        Model::validateRequired('userUniqueId', $this->userUniqueId, true);
     }
 
     public function toMap()
@@ -46,6 +70,15 @@ class QueryBenefithubReportInformationRequest extends Model
         }
         if (null !== $this->orderNo) {
             $res['order_no'] = $this->orderNo;
+        }
+        if (null !== $this->platformCode) {
+            $res['platform_code'] = $this->platformCode;
+        }
+        if (null !== $this->productCode) {
+            $res['product_code'] = $this->productCode;
+        }
+        if (null !== $this->userUniqueId) {
+            $res['user_unique_id'] = $this->userUniqueId;
         }
 
         return $res;
@@ -67,6 +100,15 @@ class QueryBenefithubReportInformationRequest extends Model
         }
         if (isset($map['order_no'])) {
             $model->orderNo = $map['order_no'];
+        }
+        if (isset($map['platform_code'])) {
+            $model->platformCode = $map['platform_code'];
+        }
+        if (isset($map['product_code'])) {
+            $model->productCode = $map['product_code'];
+        }
+        if (isset($map['user_unique_id'])) {
+            $model->userUniqueId = $map['user_unique_id'];
         }
 
         return $model;
