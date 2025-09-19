46b5fdfc858e4a0a593ebe0e6d3db8ce22179b9c
sdk-team <sdk-team@alibabacloud.com>
2025-09-19 08:52:01 +0000

    Generated SDK for RISKPLUS.

--- riskplus/java/src/main/java/com/antgroup/antchain/openapi/riskplus/Client.java
+++ riskplus/java/src/main/java/com/antgroup/antchain/openapi/riskplus/Client.java
@@ -126,7 +126,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.26.6"),
+                    new TeaPair("sdk_version", "1.26.7"),
                     new TeaPair("_prod_code", "RISKPLUS"),
                     new TeaPair("_prod_channel", "undefined")
                 );
@@ -270,6 +270,27 @@ public class Client {
     public QueryBenefithubReportInformationResponse queryBenefithubReportInformationEx(QueryBenefithubReportInformationRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "riskplus.benefithub.report.information.query", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new QueryBenefithubReportInformationResponse());
+    }
+
+    /**
+     * <b>description</b> :
+     * <p>Description: 渠道查询报告有效期
+     * Summary: 渠道查询报告有效期</p>
+     */
+    public QueryBenefithubReportEffectiveResponse queryBenefithubReportEffective(QueryBenefithubReportEffectiveRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.queryBenefithubReportEffectiveEx(request, headers, runtime);
+    }
+
+    /**
+     * <b>description</b> :
+     * <p>Description: 渠道查询报告有效期
+     * Summary: 渠道查询报告有效期</p>
+     */
+    public QueryBenefithubReportEffectiveResponse queryBenefithubReportEffectiveEx(QueryBenefithubReportEffectiveRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "riskplus.benefithub.report.effective.query", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new QueryBenefithubReportEffectiveResponse());
     }
 
     /**
