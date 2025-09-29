e4532d914770339d30c5b83226dca04531163433
sdk-team <sdk-team@alibabacloud.com>
2025-09-29 11:18:46 +0000

    Generated SDK for BXPT_NEW.

--- bxpt_new/go/client/client.go
+++ bxpt_new/go/client/client.go
@@ -6,6 +6,7 @@ import (
 	util "github.com/alibabacloud-go/tea-utils/v2/service"
 	"github.com/alibabacloud-go/tea/tea"
 	antchainutil "github.com/antchain-openapi-sdk-go/antchain-util/service"
+	"io"
 )
 
 /**
@@ -145,6 +146,32 @@ func (s *Config) SetMaxRequests(v int) *Config {
 
 func (s *Config) SetMaxRequestsPerHost(v int) *Config {
 	s.MaxRequestsPerHost = &v
+	return s
+}
+
+// 键值对
+type XNameValuePair struct {
+	// 键名
+	Name *string `json:"name,omitempty" xml:"name,omitempty" require:"true"`
+	// 键值
+	Value *string `json:"value,omitempty" xml:"value,omitempty" require:"true"`
+}
+
+func (s XNameValuePair) String() string {
+	return tea.Prettify(s)
+}
+
+func (s XNameValuePair) GoString() string {
+	return s.String()
+}
+
+func (s *XNameValuePair) SetName(v string) *XNameValuePair {
+	s.Name = &v
+	return s
+}
+
+func (s *XNameValuePair) SetValue(v string) *XNameValuePair {
+	s.Value = &v
 	return s
 }
 
@@ -815,6 +842,176 @@ func (s *QueryDrrdataRiafdResponse) SetBizResult(v string) *QueryDrrdataRiafdRes
 	return s
 }
 
+type ExecMultimodalDataprodRequest struct {
+	// OAuth模式下的授权token
+	AuthToken *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
+	// 文件唯一id
+	// 待上传文件
+	FileObject io.Reader `json:"fileObject,omitempty" xml:"fileObject,omitempty"`
+	// 待上传文件名
+	FileObjectName *string `json:"fileObjectName,omitempty" xml:"fileObjectName,omitempty"`
+	FileId         *string `json:"file_id,omitempty" xml:"file_id,omitempty" require:"true"`
+	// 业务入参的json字符串
+	BizContent *string `json:"biz_content,omitempty" xml:"biz_content,omitempty"`
+	// 数据产品编码
+	ProductCode *string `json:"product_code,omitempty" xml:"product_code,omitempty" require:"true"`
+	// 请求唯一标识
+	RequestId *string `json:"request_id,omitempty" xml:"request_id,omitempty" require:"true"`
+}
+
+func (s ExecMultimodalDataprodRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ExecMultimodalDataprodRequest) GoString() string {
+	return s.String()
+}
+
+func (s *ExecMultimodalDataprodRequest) SetAuthToken(v string) *ExecMultimodalDataprodRequest {
+	s.AuthToken = &v
+	return s
+}
+
+func (s *ExecMultimodalDataprodRequest) SetFileObject(v io.Reader) *ExecMultimodalDataprodRequest {
+	s.FileObject = v
+	return s
+}
+
+func (s *ExecMultimodalDataprodRequest) SetFileObjectName(v string) *ExecMultimodalDataprodRequest {
+	s.FileObjectName = &v
+	return s
+}
+
+func (s *ExecMultimodalDataprodRequest) SetFileId(v string) *ExecMultimodalDataprodRequest {
+	s.FileId = &v
+	return s
+}
+
+func (s *ExecMultimodalDataprodRequest) SetBizContent(v string) *ExecMultimodalDataprodRequest {
+	s.BizContent = &v
+	return s
+}
+
+func (s *ExecMultimodalDataprodRequest) SetProductCode(v string) *ExecMultimodalDataprodRequest {
+	s.ProductCode = &v
+	return s
+}
+
+func (s *ExecMultimodalDataprodRequest) SetRequestId(v string) *ExecMultimodalDataprodRequest {
+	s.RequestId = &v
+	return s
+}
+
+type ExecMultimodalDataprodResponse struct {
+	// 请求唯一ID，用于链路跟踪和问题排查
+	ReqMsgId *string `json:"req_msg_id,omitempty" xml:"req_msg_id,omitempty"`
+	// 结果码，一般OK表示调用成功
+	ResultCode *string `json:"result_code,omitempty" xml:"result_code,omitempty"`
+	// 异常信息的文本描述
+	ResultMsg *string `json:"result_msg,omitempty" xml:"result_msg,omitempty"`
+	// 业务返回值json
+	BizResult *string `json:"biz_result,omitempty" xml:"biz_result,omitempty"`
+}
+
+func (s ExecMultimodalDataprodResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ExecMultimodalDataprodResponse) GoString() string {
+	return s.String()
+}
+
+func (s *ExecMultimodalDataprodResponse) SetReqMsgId(v string) *ExecMultimodalDataprodResponse {
+	s.ReqMsgId = &v
+	return s
+}
+
+func (s *ExecMultimodalDataprodResponse) SetResultCode(v string) *ExecMultimodalDataprodResponse {
+	s.ResultCode = &v
+	return s
+}
+
+func (s *ExecMultimodalDataprodResponse) SetResultMsg(v string) *ExecMultimodalDataprodResponse {
+	s.ResultMsg = &v
+	return s
+}
+
+func (s *ExecMultimodalDataprodResponse) SetBizResult(v string) *ExecMultimodalDataprodResponse {
+	s.BizResult = &v
+	return s
+}
+
+type NotifyMultimodalDataprodRequest struct {
+	// OAuth模式下的授权token
+	AuthToken *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
+	// 通知具体json内容
+	BizContent *string `json:"biz_content,omitempty" xml:"biz_content,omitempty"`
+	// 唯一请求id
+	RequestId *string `json:"request_id,omitempty" xml:"request_id,omitempty" require:"true"`
+	// 业务标识
+	BizNo *string `json:"biz_no,omitempty" xml:"biz_no,omitempty" require:"true"`
+}
+
+func (s NotifyMultimodalDataprodRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s NotifyMultimodalDataprodRequest) GoString() string {
+	return s.String()
+}
+
+func (s *NotifyMultimodalDataprodRequest) SetAuthToken(v string) *NotifyMultimodalDataprodRequest {
+	s.AuthToken = &v
+	return s
+}
+
+func (s *NotifyMultimodalDataprodRequest) SetBizContent(v string) *NotifyMultimodalDataprodRequest {
+	s.BizContent = &v
+	return s
+}
+
+func (s *NotifyMultimodalDataprodRequest) SetRequestId(v string) *NotifyMultimodalDataprodRequest {
+	s.RequestId = &v
+	return s
+}
+
+func (s *NotifyMultimodalDataprodRequest) SetBizNo(v string) *NotifyMultimodalDataprodRequest {
+	s.BizNo = &v
+	return s
+}
+
+type NotifyMultimodalDataprodResponse struct {
+	// 请求唯一ID，用于链路跟踪和问题排查
+	ReqMsgId *string `json:"req_msg_id,omitempty" xml:"req_msg_id,omitempty"`
+	// 结果码，一般OK表示调用成功
+	ResultCode *string `json:"result_code,omitempty" xml:"result_code,omitempty"`
+	// 异常信息的文本描述
+	ResultMsg *string `json:"result_msg,omitempty" xml:"result_msg,omitempty"`
+}
+
+func (s NotifyMultimodalDataprodResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s NotifyMultimodalDataprodResponse) GoString() string {
+	return s.String()
+}
+
+func (s *NotifyMultimodalDataprodResponse) SetReqMsgId(v string) *NotifyMultimodalDataprodResponse {
+	s.ReqMsgId = &v
+	return s
+}
+
+func (s *NotifyMultimodalDataprodResponse) SetResultCode(v string) *NotifyMultimodalDataprodResponse {
+	s.ResultCode = &v
+	return s
+}
+
+func (s *NotifyMultimodalDataprodResponse) SetResultMsg(v string) *NotifyMultimodalDataprodResponse {
+	s.ResultMsg = &v
+	return s
+}
+
 type QueryDatapromotionDecisionRequest struct {
 	// OAuth模式下的授权token
 	AuthToken *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
@@ -979,6 +1176,126 @@ func (s *PushDatapromotionTrafficResponse) SetBizResult(v string) *PushDatapromo
 	return s
 }
 
+type CreateAntcloudGatewayxFileUploadRequest struct {
+	// OAuth模式下的授权token
+	AuthToken *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
+	// 上传文件作用的openapi method
+	ApiCode *string `json:"api_code,omitempty" xml:"api_code,omitempty" require:"true"`
+	// 文件标签，多个标签;分割
+	FileLabel *string `json:"file_label,omitempty" xml:"file_label,omitempty" maxLength:"100"`
+	// 自定义的文件元数据
+	FileMetadata *string `json:"file_metadata,omitempty" xml:"file_metadata,omitempty" maxLength:"1000"`
+	// 文件名，不传则随机生成文件名
+	FileName *string `json:"file_name,omitempty" xml:"file_name,omitempty" maxLength:"100"`
+	// 文件的多媒体类型
+	MimeType *string `json:"mime_type,omitempty" xml:"mime_type,omitempty"`
+	// 产品方的api归属集群，即productInstanceId
+	ApiCluster *string `json:"api_cluster,omitempty" xml:"api_cluster,omitempty"`
+}
+
+func (s CreateAntcloudGatewayxFileUploadRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s CreateAntcloudGatewayxFileUploadRequest) GoString() string {
+	return s.String()
+}
+
+func (s *CreateAntcloudGatewayxFileUploadRequest) SetAuthToken(v string) *CreateAntcloudGatewayxFileUploadRequest {
+	s.AuthToken = &v
+	return s
+}
+
+func (s *CreateAntcloudGatewayxFileUploadRequest) SetApiCode(v string) *CreateAntcloudGatewayxFileUploadRequest {
+	s.ApiCode = &v
+	return s
+}
+
+func (s *CreateAntcloudGatewayxFileUploadRequest) SetFileLabel(v string) *CreateAntcloudGatewayxFileUploadRequest {
+	s.FileLabel = &v
+	return s
+}
+
+func (s *CreateAntcloudGatewayxFileUploadRequest) SetFileMetadata(v string) *CreateAntcloudGatewayxFileUploadRequest {
+	s.FileMetadata = &v
+	return s
+}
+
+func (s *CreateAntcloudGatewayxFileUploadRequest) SetFileName(v string) *CreateAntcloudGatewayxFileUploadRequest {
+	s.FileName = &v
+	return s
+}
+
+func (s *CreateAntcloudGatewayxFileUploadRequest) SetMimeType(v string) *CreateAntcloudGatewayxFileUploadRequest {
+	s.MimeType = &v
+	return s
+}
+
+func (s *CreateAntcloudGatewayxFileUploadRequest) SetApiCluster(v string) *CreateAntcloudGatewayxFileUploadRequest {
+	s.ApiCluster = &v
+	return s
+}
+
+type CreateAntcloudGatewayxFileUploadResponse struct {
+	// 请求唯一ID，用于链路跟踪和问题排查
+	ReqMsgId *string `json:"req_msg_id,omitempty" xml:"req_msg_id,omitempty"`
+	// 结果码，一般OK表示调用成功
+	ResultCode *string `json:"result_code,omitempty" xml:"result_code,omitempty"`
+	// 异常信息的文本描述
+	ResultMsg *string `json:"result_msg,omitempty" xml:"result_msg,omitempty"`
+	// 上传有效期
+	ExpiredTime *string `json:"expired_time,omitempty" xml:"expired_time,omitempty" pattern:"\\d{4}[-]\\d{1,2}[-]\\d{1,2}[T]\\d{2}:\\d{2}:\\d{2}([Z]|([\\.]\\d{1,9})?[\\+]\\d{2}[\\:]?\\d{2})"`
+	// 32位文件唯一id
+	FileId *string `json:"file_id,omitempty" xml:"file_id,omitempty"`
+	// 放入http请求头里
+	UploadHeaders []*XNameValuePair `json:"upload_headers,omitempty" xml:"upload_headers,omitempty" type:"Repeated"`
+	// 文件上传地址
+	UploadUrl *string `json:"upload_url,omitempty" xml:"upload_url,omitempty"`
+}
+
+func (s CreateAntcloudGatewayxFileUploadResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s CreateAntcloudGatewayxFileUploadResponse) GoString() string {
+	return s.String()
+}
+
+func (s *CreateAntcloudGatewayxFileUploadResponse) SetReqMsgId(v string) *CreateAntcloudGatewayxFileUploadResponse {
+	s.ReqMsgId = &v
+	return s
+}
+
+func (s *CreateAntcloudGatewayxFileUploadResponse) SetResultCode(v string) *CreateAntcloudGatewayxFileUploadResponse {
+	s.ResultCode = &v
+	return s
+}
+
+func (s *CreateAntcloudGatewayxFileUploadResponse) SetResultMsg(v string) *CreateAntcloudGatewayxFileUploadResponse {
+	s.ResultMsg = &v
+	return s
+}
+
+func (s *CreateAntcloudGatewayxFileUploadResponse) SetExpiredTime(v string) *CreateAntcloudGatewayxFileUploadResponse {
+	s.ExpiredTime = &v
+	return s
+}
+
+func (s *CreateAntcloudGatewayxFileUploadResponse) SetFileId(v string) *CreateAntcloudGatewayxFileUploadResponse {
+	s.FileId = &v
+	return s
+}
+
+func (s *CreateAntcloudGatewayxFileUploadResponse) SetUploadHeaders(v []*XNameValuePair) *CreateAntcloudGatewayxFileUploadResponse {
+	s.UploadHeaders = v
+	return s
+}
+
+func (s *CreateAntcloudGatewayxFileUploadResponse) SetUploadUrl(v string) *CreateAntcloudGatewayxFileUploadResponse {
+	s.UploadUrl = &v
+	return s
+}
+
 type Client struct {
 	Endpoint                *string
 	RegionId                *string
@@ -1101,7 +1418,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.2.21"),
+				"sdk_version":      tea.String("1.2.27"),
 				"_prod_code":       tea.String("BXPT_NEW"),
 				"_prod_channel":    tea.String("default"),
 			}
@@ -1364,6 +1681,104 @@ func (client *Client) QueryDrrdataRiafdEx(request *QueryDrrdataRiafdRequest, hea
 }
 
 /**
+ * Description: 提供多模态的数据产品执行，支持上传文本，图片、视频、音频等格式
+ * Summary: 提供多模态的数据产品执行
+ */
+func (client *Client) ExecMultimodalDataprod(request *ExecMultimodalDataprodRequest) (_result *ExecMultimodalDataprodResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &ExecMultimodalDataprodResponse{}
+	_body, _err := client.ExecMultimodalDataprodEx(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+/**
+ * Description: 提供多模态的数据产品执行，支持上传文本，图片、视频、音频等格式
+ * Summary: 提供多模态的数据产品执行
+ */
+func (client *Client) ExecMultimodalDataprodEx(request *ExecMultimodalDataprodRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *ExecMultimodalDataprodResponse, _err error) {
+	if !tea.BoolValue(util.IsUnset(request.FileObject)) {
+		uploadReq := &CreateAntcloudGatewayxFileUploadRequest{
+			AuthToken: request.AuthToken,
+			ApiCode:   tea.String("antcloud.bxptnew.multimodal.dataprod.exec"),
+			FileName:  request.FileObjectName,
+		}
+		uploadResp, _err := client.CreateAntcloudGatewayxFileUploadEx(uploadReq, headers, runtime)
+		if _err != nil {
+			return _result, _err
+		}
+
+		if !tea.BoolValue(antchainutil.IsSuccess(uploadResp.ResultCode, tea.String("ok"))) {
+			execMultimodalDataprodResponse := &ExecMultimodalDataprodResponse{
+				ReqMsgId:   uploadResp.ReqMsgId,
+				ResultCode: uploadResp.ResultCode,
+				ResultMsg:  uploadResp.ResultMsg,
+			}
+			_result = execMultimodalDataprodResponse
+			return _result, _err
+		}
+
+		uploadHeaders := antchainutil.ParseUploadHeaders(uploadResp.UploadHeaders)
+		_err = antchainutil.PutObject(request.FileObject, uploadHeaders, uploadResp.UploadUrl)
+		if _err != nil {
+			return _result, _err
+		}
+		request.FileId = uploadResp.FileId
+		request.FileObject = nil
+	}
+
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = &ExecMultimodalDataprodResponse{}
+	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("antcloud.bxptnew.multimodal.dataprod.exec"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+/**
+ * Description: 数据产品执行结果通知
+ * Summary: 数据产品执行结果通知
+ */
+func (client *Client) NotifyMultimodalDataprod(request *NotifyMultimodalDataprodRequest) (_result *NotifyMultimodalDataprodResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &NotifyMultimodalDataprodResponse{}
+	_body, _err := client.NotifyMultimodalDataprodEx(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+/**
+ * Description: 数据产品执行结果通知
+ * Summary: 数据产品执行结果通知
+ */
+func (client *Client) NotifyMultimodalDataprodEx(request *NotifyMultimodalDataprodRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *NotifyMultimodalDataprodResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = &NotifyMultimodalDataprodResponse{}
+	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("antcloud.bxptnew.multimodal.dataprod.notify"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+/**
  * Description: 保险数据营销决策查询
  * Summary: 保险数据营销决策查询
  */
@@ -1424,6 +1839,40 @@ func (client *Client) PushDatapromotionTrafficEx(request *PushDatapromotionTraff
 	}
 	_result = &PushDatapromotionTrafficResponse{}
 	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("antcloud.bxptnew.datapromotion.traffic.push"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+/**
+ * Description: 创建HTTP PUT提交的文件上传
+ * Summary: 文件上传创建
+ */
+func (client *Client) CreateAntcloudGatewayxFileUpload(request *CreateAntcloudGatewayxFileUploadRequest) (_result *CreateAntcloudGatewayxFileUploadResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &CreateAntcloudGatewayxFileUploadResponse{}
+	_body, _err := client.CreateAntcloudGatewayxFileUploadEx(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+/**
+ * Description: 创建HTTP PUT提交的文件上传
+ * Summary: 文件上传创建
+ */
+func (client *Client) CreateAntcloudGatewayxFileUploadEx(request *CreateAntcloudGatewayxFileUploadRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *CreateAntcloudGatewayxFileUploadResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = &CreateAntcloudGatewayxFileUploadResponse{}
+	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("antcloud.gatewayx.file.upload.create"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
 	if _err != nil {
 		return _result, _err
 	}
