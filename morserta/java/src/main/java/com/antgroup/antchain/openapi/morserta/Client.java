71fddb5d3da081b918b8f47d90ff4a8e98270426
sdk-team <sdk-team@alibabacloud.com>
2025-10-15 07:41:34 +0000

    Generated SDK for MORSERTA.

--- morserta/java/src/main/java/com/antgroup/antchain/openapi/morserta/Client.java
+++ morserta/java/src/main/java/com/antgroup/antchain/openapi/morserta/Client.java
@@ -126,7 +126,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "5.0.0"),
+                    new TeaPair("sdk_version", "6.0.0"),
                     new TeaPair("_prod_code", "MORSERTA"),
                     new TeaPair("_prod_channel", "default")
                 );
