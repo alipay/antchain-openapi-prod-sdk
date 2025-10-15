d9c0d91c6f29821a64971a0b8456e93d9566de70
sdk-team <sdk-team@alibabacloud.com>
2025-10-15 02:30:22 +0000

    Generated SDK for l4-MORSERTA.

--- l4-morserta/java/src/main/java/com/antgroup/antchain/openapi/morserta/Client.java
+++ l4-morserta/java/src/main/java/com/antgroup/antchain/openapi/morserta/Client.java
@@ -126,7 +126,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "6.0.0"),
+                    new TeaPair("sdk_version", "6.0.1"),
                     new TeaPair("_prod_code", "MORSERTA"),
                     new TeaPair("_prod_channel", "default")
                 );
@@ -291,5 +291,26 @@ public class Client {
     public OcpxAdDataResponse ocpxAdDataEx(OcpxAdDataRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "antcloud.morserta.ad.data.ocpx", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new OcpxAdDataResponse());
+    }
+
+    /**
+     * <b>description</b> :
+     * <p>Description: 实验效果数据拉取接口
+     * Summary: 实验效果数据拉取接口</p>
+     */
+    public DataAdDataExportExperimentResponse dataAdDataExportExperiment(DataAdDataExportExperimentRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.dataAdDataExportExperimentEx(request, headers, runtime);
+    }
+
+    /**
+     * <b>description</b> :
+     * <p>Description: 实验效果数据拉取接口
+     * Summary: 实验效果数据拉取接口</p>
+     */
+    public DataAdDataExportExperimentResponse dataAdDataExportExperimentEx(DataAdDataExportExperimentRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "antcloud.morserta.ad.data.export.experiment.data", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new DataAdDataExportExperimentResponse());
     }
 }
