0d8e4047514a4f79c396ebd2a26ba446506c0361
sdk-team <sdk-team@alibabacloud.com>
2025-10-16 02:31:02 +0000

    Generated SDK for BLOCKCHAIN.

--- blockchain/nodejs/src/client.ts
+++ blockchain/nodejs/src/client.ts
@@ -55269,7 +55269,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.28.58",
+          sdk_version: "1.28.59",
           _prod_code: "BLOCKCHAIN",
           _prod_channel: "undefined",
         };
