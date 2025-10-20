c43e17e4744d90c7aab79edd5a4f5cf60d2d2adc
sdk-team <sdk-team@alibabacloud.com>
2025-10-20 09:51:30 +0000

    Generated SDK for SDS.

--- sds/csharp/core/Properties/AssemblyInfo.cs
+++ sds/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a088f2a0-ad94-11f0-a9e0-0f0bc1f4d140")]
+[assembly: Guid("55b26990-ad9a-11f0-8287-d53561a3bce6")]
 
 // Version information for an assembly consists of the following four values:
 //
