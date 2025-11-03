622cb9e3b16e63425012913c6489556bfbe68069
sdk-team <sdk-team@alibabacloud.com>
2025-11-03 07:15:08 +0000

    Generated SDK for MORSERTA.

--- morserta/composer/src/Client.php
+++ morserta/composer/src/Client.php
@@ -174,7 +174,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '6.1.1',
+                    'sdk_version'      => '6.1.2',
                     '_prod_code'       => 'MORSERTA',
                     '_prod_channel'    => 'default',
                 ];
