7af1a1cb7dec4dcdcce3cb9c04fe4e9434a9025b
sdk-team <sdk-team@alibabacloud.com>
2025-11-04 07:21:58 +0000

    Generated SDK for BCCR.

--- bccr/go/client/client.go
+++ bccr/go/client/client.go
@@ -15475,7 +15475,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.19.75"),
+				"sdk_version":      tea.String("1.19.76"),
 				"_prod_code":       tea.String("BCCR"),
 				"_prod_channel":    tea.String("undefined"),
 			}
