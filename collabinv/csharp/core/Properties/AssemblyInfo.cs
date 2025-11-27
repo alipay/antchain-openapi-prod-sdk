792a87de2219195f6f16d0419aef64cf94c7695f
sdk-team <sdk-team@alibabacloud.com>
2025-11-27 09:45:00 +0000

    Generated SDK for COLLABINV.

--- collabinv/csharp/core/Properties/AssemblyInfo.cs
+++ collabinv/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("66fb8cf0-cb71-11f0-ac03-87089b553ef4")]
+[assembly: Guid("b9576510-cb75-11f0-8c89-eb5c8f5090da")]
 
 // Version information for an assembly consists of the following four values:
 //
