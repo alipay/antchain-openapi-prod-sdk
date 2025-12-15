dabf3b7117fea30318dd988a8a471bdfbb168566
sdk-team <sdk-team@alibabacloud.com>
2025-12-15 14:39:05 +0000

    Generated SDK for AITECH.

--- aitech/nodejs/src/client.ts
+++ aitech/nodejs/src/client.ts
@@ -3012,6 +3012,152 @@ export class SubmitAuditMeiyouResponse extends $tea.Model {
   }
 }
 
+export class SubmitAuditDocumentRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  // 待检测对象的URL，请确保该URL能通过公网访问到，且URL地址长度不超过2048个字符 URL地址中不能包含中文，且一次请求请确保仅传入1条URL
+  url: string;
+  // 审核场景码
+  scene: string;
+  // 检测对象对应的数据ID。 由大小写英文字母、数字、下划线（_）、短划线（-）、英文句号（.）组成，不超过128个字符，可以用于唯一标识您的业务数据
+  dataId: string;
+  // 客户业务ID。 由大小写英文字母、数字、下划线（_）、短划线（-）、英文句号（.）组成，不超过128个字符，可以用于唯一标识您的业务数据
+  businessId?: string;
+  // 结果通知地址，不指定时需要调用方主动查询结果
+  callback?: string;
+  // 传callback时必须指定，tenant + seed + auditResult做SHA256生成checksum，保证结果未被篡改（即数科官网控制台-账户信息中的「用户code」）
+  seed?: string;
+  // 审核时输入的额外信息
+  extendInfo?: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+      url: 'url',
+      scene: 'scene',
+      dataId: 'data_id',
+      businessId: 'business_id',
+      callback: 'callback',
+      seed: 'seed',
+      extendInfo: 'extend_info',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+      url: 'string',
+      scene: 'string',
+      dataId: 'string',
+      businessId: 'string',
+      callback: 'string',
+      seed: 'string',
+      extendInfo: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class SubmitAuditDocumentResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  // 任务ID
+  taskId?: string;
+  // 检测对象对应的数据ID。 如果在提交审核任务的请求参数中传入了dataId，则此处返回对应dataId
+  dataId?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+      taskId: 'task_id',
+      dataId: 'data_id',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+      taskId: 'string',
+      dataId: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class QueryAuditDocumentRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  // 任务ID
+  taskId: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+      taskId: 'task_id',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+      taskId: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class QueryAuditDocumentResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  // 文档审核结果
+  result?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+      result: 'result',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+      result: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class QueryAicoguardcloudAdbsinkRequest extends $tea.Model {
   // OAuth模式下的授权token
   authToken?: string;
@@ -5192,7 +5338,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.1.50",
+          sdk_version: "1.1.51",
           _prod_code: "AITECH",
           _prod_channel: "default",
         };
@@ -5808,6 +5954,44 @@ export default class Client {
   async submitAuditMeiyouEx(request: SubmitAuditMeiyouRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<SubmitAuditMeiyouResponse> {
     Util.validateModel(request);
     return $tea.cast<SubmitAuditMeiyouResponse>(await this.doRequest("1.0", "aitech.comm.audit.meiyou.submit", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new SubmitAuditMeiyouResponse({}));
+  }
+
+  /**
+   * Description: 文档人审入审
+   * Summary: 文档人审入审
+   */
+  async submitAuditDocument(request: SubmitAuditDocumentRequest): Promise<SubmitAuditDocumentResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.submitAuditDocumentEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 文档人审入审
+   * Summary: 文档人审入审
+   */
+  async submitAuditDocumentEx(request: SubmitAuditDocumentRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<SubmitAuditDocumentResponse> {
+    Util.validateModel(request);
+    return $tea.cast<SubmitAuditDocumentResponse>(await this.doRequest("1.0", "aitech.comm.audit.document.submit", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new SubmitAuditDocumentResponse({}));
+  }
+
+  /**
+   * Description: 文档人审查询
+   * Summary: 文档人审查询
+   */
+  async queryAuditDocument(request: QueryAuditDocumentRequest): Promise<QueryAuditDocumentResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.queryAuditDocumentEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 文档人审查询
+   * Summary: 文档人审查询
+   */
+  async queryAuditDocumentEx(request: QueryAuditDocumentRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<QueryAuditDocumentResponse> {
+    Util.validateModel(request);
+    return $tea.cast<QueryAuditDocumentResponse>(await this.doRequest("1.0", "aitech.comm.audit.document.query", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new QueryAuditDocumentResponse({}));
   }
 
   /**
