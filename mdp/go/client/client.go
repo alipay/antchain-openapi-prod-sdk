d612bc472a5c2577a27b2add9e795e39f71f2d59
sdk-team <sdk-team@alibabacloud.com>
2025-12-22 11:14:57 +0000

    Generated SDK for MDP.

--- mdp/go/client/client.go
+++ mdp/go/client/client.go
@@ -665,7 +665,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.0.1"),
+				"sdk_version":      tea.String("1.0.2"),
 				"_prod_code":       tea.String("MDP"),
 				"_prod_channel":    tea.String("default"),
 			}
