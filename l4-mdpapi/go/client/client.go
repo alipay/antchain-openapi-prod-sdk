0300ab816e9aac4e3a8b211cf1ca1e5cb950f06a
sdk-team <sdk-team@alibabacloud.com>
2025-12-22 05:48:19 +0000

    Generated SDK for l4-MDPAPI.

--- l4-mdpapi/go/client/client.go
+++ l4-mdpapi/go/client/client.go
@@ -497,7 +497,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.1.0"),
+				"sdk_version":      tea.String("1.1.1"),
 				"_prod_code":       tea.String("MDPAPI"),
 				"_prod_channel":    tea.String("default"),
 			}
