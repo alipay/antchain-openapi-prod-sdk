c8a3525e6f9985e4d42d2845cfafa83aa08ac6fb
sdk-team <sdk-team@alibabacloud.com>
2025-12-22 11:15:05 +0000

    Generated SDK for MDP.

--- mdp/nodejs/src/client.ts
+++ mdp/nodejs/src/client.ts
@@ -512,7 +512,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.1",
+          sdk_version: "1.0.2",
           _prod_code: "MDP",
           _prod_channel: "default",
         };
