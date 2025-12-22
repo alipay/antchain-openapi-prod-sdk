c8fbc8b798c3f7b029e48747b02030b5a9f30cb7
sdk-team <sdk-team@alibabacloud.com>
2025-12-22 05:48:12 +0000

    Generated SDK for l4-MDPAPI.

--- l4-mdpapi/nodejs/src/client.ts
+++ l4-mdpapi/nodejs/src/client.ts
@@ -378,7 +378,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.1.0",
+          sdk_version: "1.1.1",
           _prod_code: "MDPAPI",
           _prod_channel: "default",
         };
