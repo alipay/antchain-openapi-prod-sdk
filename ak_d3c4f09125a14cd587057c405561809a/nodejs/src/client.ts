3497a20bc1a19234b8c61b191119b0ba457e591f
sdk-team <sdk-team@alibabacloud.com>
2025-12-29 10:42:25 +0000

    Generated SDK for ak_d3c4f09125a14cd587057c405561809a.

--- ak_d3c4f09125a14cd587057c405561809a/nodejs/src/client.ts
+++ ak_d3c4f09125a14cd587057c405561809a/nodejs/src/client.ts
@@ -2109,7 +2109,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.1.1",
+          sdk_version: "1.1.2",
           _prod_code: "ak_d3c4f09125a14cd587057c405561809a",
           _prod_channel: "saas",
         };
