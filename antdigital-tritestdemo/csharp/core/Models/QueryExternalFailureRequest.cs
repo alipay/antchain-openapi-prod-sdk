fa99bb0df33bab61285803a6925126c67dced63f
sdk-team <sdk-team@alibabacloud.com>
2025-12-31 03:00:43 +0000

    Generated SDK for antdigital-TRITESTDEMO.

---
+++ antdigital-tritestdemo/csharp/core/Models/QueryExternalFailureRequest.cs
@@ -0,0 +1,24 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.TRITESTDEMO.Models
+{
+    public class QueryExternalFailureRequest : TeaModel {
+        // OAuth模式下的授权token
+        [NameInMap("auth_token")]
+        [Validation(Required=false)]
+        public string AuthToken { get; set; }
+
+        // 入参
+        [NameInMap("param")]
+        [Validation(Required=true)]
+        public string Param { get; set; }
+
+    }
+
+}
