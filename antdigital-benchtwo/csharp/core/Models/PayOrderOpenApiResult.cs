8cca641b22e2332d6964104610346ba24d21ff80
sdk-team <sdk-team@alibabacloud.com>
2026-01-04 02:53:01 +0000

    Generated SDK for antdigital-BENCHTWO.

---
+++ antdigital-benchtwo/csharp/core/Models/PayOrderOpenApiResult.cs
@@ -0,0 +1,20 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AntChain.SDK.BENCHTWO.Models
+{
+    // 测试
+    public class PayOrderOpenApiResult : TeaModel {
+        // 11
+        [NameInMap("test")]
+        [Validation(Required=true)]
+        public string Test { get; set; }
+
+    }
+
+}
