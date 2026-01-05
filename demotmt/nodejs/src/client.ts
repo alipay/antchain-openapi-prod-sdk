5283eafe98302cf47278228e05037bdf89d0627c
sdk-team <sdk-team@alibabacloud.com>
2026-01-05 08:14:12 +0000

    Generated SDK for DEMOTMT.

--- demotmt/nodejs/src/client.ts
+++ demotmt/nodejs/src/client.ts
@@ -597,7 +597,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.2",
+          sdk_version: "1.0.3",
           _prod_code: "DEMOTMT",
           _prod_channel: "default",
         };
