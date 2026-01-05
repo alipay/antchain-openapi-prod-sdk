6b39c639b1da87839489b5de56b8612d46cc6d77
sdk-team <sdk-team@alibabacloud.com>
2026-01-05 11:59:33 +0000

    Generated SDK for DEMOTMT.

--- demotmt/go/client/client.go
+++ demotmt/go/client/client.go
@@ -765,7 +765,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.0.3"),
+				"sdk_version":      tea.String("1.0.4"),
 				"_prod_code":       tea.String("DEMOTMT"),
 				"_prod_channel":    tea.String("default"),
 			}
