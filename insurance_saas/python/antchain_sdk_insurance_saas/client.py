dd6064693772cfa56d7da56e7e5ad999c76da63a
sdk-team <sdk-team@alibabacloud.com>
2026-01-09 12:39:55 +0000

    Generated SDK for INSURANCE_SAAS.

--- insurance_saas/python/antchain_sdk_insurance_saas/client.py
+++ insurance_saas/python/antchain_sdk_insurance_saas/client.py
@@ -135,7 +135,7 @@ class Client:
                     'req_msg_id': AntchainUtils.get_nonce(),
                     'access_key': self._access_key_id,
                     'base_sdk_version': 'TeaSDK-2.0',
-                    'sdk_version': '1.11.2',
+                    'sdk_version': '1.11.3',
                     '_prod_code': 'INSURANCE_SAAS',
                     '_prod_channel': 'undefined'
                 }
@@ -239,7 +239,7 @@ class Client:
                     'req_msg_id': AntchainUtils.get_nonce(),
                     'access_key': self._access_key_id,
                     'base_sdk_version': 'TeaSDK-2.0',
-                    'sdk_version': '1.11.2',
+                    'sdk_version': '1.11.3',
                     '_prod_code': 'INSURANCE_SAAS',
                     '_prod_channel': 'undefined'
                 }
@@ -2289,4 +2289,60 @@ class Client:
         return TeaCore.from_map(
             insurance__saas_models.CallbackMarketingPolicycancelResponse(),
             await self.do_request_async('1.0', 'antcloud.insurance.marketing.policycancel.callback', 'HTTPS', 'POST', f'/gateway.do', TeaCore.to_map(request), headers, runtime)
+        )
+
+    def query_lead_market(
+        self,
+        request: insurance__saas_models.QueryLeadMarketRequest,
+    ) -> insurance__saas_models.QueryLeadMarketResponse:
+        """
+        Description: 车险线索查询
+        Summary: 车险线索查询
+        """
+        runtime = util_models.RuntimeOptions()
+        headers = {}
+        return self.query_lead_market_ex(request, headers, runtime)
+
+    async def query_lead_market_async(
+        self,
+        request: insurance__saas_models.QueryLeadMarketRequest,
+    ) -> insurance__saas_models.QueryLeadMarketResponse:
+        """
+        Description: 车险线索查询
+        Summary: 车险线索查询
+        """
+        runtime = util_models.RuntimeOptions()
+        headers = {}
+        return await self.query_lead_market_ex_async(request, headers, runtime)
+
+    def query_lead_market_ex(
+        self,
+        request: insurance__saas_models.QueryLeadMarketRequest,
+        headers: Dict[str, str],
+        runtime: util_models.RuntimeOptions,
+    ) -> insurance__saas_models.QueryLeadMarketResponse:
+        """
+        Description: 车险线索查询
+        Summary: 车险线索查询
+        """
+        UtilClient.validate_model(request)
+        return TeaCore.from_map(
+            insurance__saas_models.QueryLeadMarketResponse(),
+            self.do_request('1.0', 'antcloud.insurance.lead.market.query', 'HTTPS', 'POST', f'/gateway.do', TeaCore.to_map(request), headers, runtime)
+        )
+
+    async def query_lead_market_ex_async(
+        self,
+        request: insurance__saas_models.QueryLeadMarketRequest,
+        headers: Dict[str, str],
+        runtime: util_models.RuntimeOptions,
+    ) -> insurance__saas_models.QueryLeadMarketResponse:
+        """
+        Description: 车险线索查询
+        Summary: 车险线索查询
+        """
+        UtilClient.validate_model(request)
+        return TeaCore.from_map(
+            insurance__saas_models.QueryLeadMarketResponse(),
+            await self.do_request_async('1.0', 'antcloud.insurance.lead.market.query', 'HTTPS', 'POST', f'/gateway.do', TeaCore.to_map(request), headers, runtime)
         )
--- insurance_saas/python/antchain_sdk_insurance_saas/models.py
+++ insurance_saas/python/antchain_sdk_insurance_saas/models.py
@@ -5635,3 +5635,102 @@ class CallbackMarketingPolicycancelResponse(TeaModel):
         return self
 
 
+class QueryLeadMarketRequest(TeaModel):
+    def __init__(
+        self,
+        auth_token: str = None,
+        product_code: str = None,
+        lead_id: str = None,
+        biz_content: str = None,
+    ):
+        # OAuth模式下的授权token
+        self.auth_token = auth_token
+        # 产品编码
+        self.product_code = product_code
+        # 请求时的线索唯一id
+        self.lead_id = lead_id
+        # 业务参数
+        self.biz_content = biz_content
+
+    def validate(self):
+        self.validate_required(self.product_code, 'product_code')
+        self.validate_required(self.lead_id, 'lead_id')
+
+    def to_map(self):
+        _map = super().to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.auth_token is not None:
+            result['auth_token'] = self.auth_token
+        if self.product_code is not None:
+            result['product_code'] = self.product_code
+        if self.lead_id is not None:
+            result['lead_id'] = self.lead_id
+        if self.biz_content is not None:
+            result['biz_content'] = self.biz_content
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('auth_token') is not None:
+            self.auth_token = m.get('auth_token')
+        if m.get('product_code') is not None:
+            self.product_code = m.get('product_code')
+        if m.get('lead_id') is not None:
+            self.lead_id = m.get('lead_id')
+        if m.get('biz_content') is not None:
+            self.biz_content = m.get('biz_content')
+        return self
+
+
+class QueryLeadMarketResponse(TeaModel):
+    def __init__(
+        self,
+        req_msg_id: str = None,
+        result_code: str = None,
+        result_msg: str = None,
+        biz_result: str = None,
+    ):
+        # 请求唯一ID，用于链路跟踪和问题排查
+        self.req_msg_id = req_msg_id
+        # 结果码，一般OK表示调用成功
+        self.result_code = result_code
+        # 异常信息的文本描述
+        self.result_msg = result_msg
+        # JSON字符串
+        self.biz_result = biz_result
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super().to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.req_msg_id is not None:
+            result['req_msg_id'] = self.req_msg_id
+        if self.result_code is not None:
+            result['result_code'] = self.result_code
+        if self.result_msg is not None:
+            result['result_msg'] = self.result_msg
+        if self.biz_result is not None:
+            result['biz_result'] = self.biz_result
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('req_msg_id') is not None:
+            self.req_msg_id = m.get('req_msg_id')
+        if m.get('result_code') is not None:
+            self.result_code = m.get('result_code')
+        if m.get('result_msg') is not None:
+            self.result_msg = m.get('result_msg')
+        if m.get('biz_result') is not None:
+            self.biz_result = m.get('biz_result')
+        return self
+
+
