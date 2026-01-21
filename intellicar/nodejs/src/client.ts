5db3fbd32170e73a3bf74470b473e61d86798798
sdk-team <sdk-team@alibabacloud.com>
2026-01-21 03:22:08 +0000

    Generated SDK for INTELLICAR.

--- intellicar/nodejs/src/client.ts
+++ intellicar/nodejs/src/client.ts
@@ -1165,7 +1165,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.8",
+          sdk_version: "1.0.9",
           _prod_code: "INTELLICAR",
           _prod_channel: "default",
         };
