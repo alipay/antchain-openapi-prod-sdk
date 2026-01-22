7d50fc10ec3eb29d7aa479dfe7605c4b5f04afd1
sdk-team <sdk-team@alibabacloud.com>
2026-01-22 07:20:38 +0000

    Generated SDK for antdigital-BENCHTWO.

--- antdigital-benchtwo/nodejs/src/client.ts
+++ antdigital-benchtwo/nodejs/src/client.ts
@@ -274,7 +274,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.2",
+          sdk_version: "1.0.3",
           _prod_code: "BENCHTWO",
           _prod_channel: "default",
         };
