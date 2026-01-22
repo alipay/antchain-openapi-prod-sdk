7d78837e4fe446366e2a3663b1fa81b68eb2df3a
sdk-team <sdk-team@alibabacloud.com>
2026-01-22 07:41:10 +0000

    Generated SDK for DEMOTMT.

--- demotmt/java/src/main/java/com/antgroup/antchain/openapi/demotmt/Client.java
+++ demotmt/java/src/main/java/com/antgroup/antchain/openapi/demotmt/Client.java
@@ -126,7 +126,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.0.4"),
+                    new TeaPair("sdk_version", "1.0.5"),
                     new TeaPair("_prod_code", "DEMOTMT"),
                     new TeaPair("_prod_channel", "default")
                 );
@@ -291,26 +291,5 @@ public class Client {
     public QueryTenantApiusageResponse queryTenantApiusageEx(QueryTenantApiusageRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "antchain.demotmt.tenant.apiusage.query", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new QueryTenantApiusageResponse());
-    }
-
-    /**
-     * <b>description</b> :
-     * <p>Description: 测试使用
-     * Summary: 测试使用</p>
-     */
-    public TestWorkbenchCreateResponse testWorkbenchCreate(TestWorkbenchCreateRequest request) throws Exception {
-        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
-        java.util.Map<String, String> headers = new java.util.HashMap<>();
-        return this.testWorkbenchCreateEx(request, headers, runtime);
-    }
-
-    /**
-     * <b>description</b> :
-     * <p>Description: 测试使用
-     * Summary: 测试使用</p>
-     */
-    public TestWorkbenchCreateResponse testWorkbenchCreateEx(TestWorkbenchCreateRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
-        com.aliyun.teautil.Common.validateModel(request);
-        return TeaModel.toModel(this.doRequest("1.0", "antchain.demotmt.workbench.create.test", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new TestWorkbenchCreateResponse());
     }
 }
