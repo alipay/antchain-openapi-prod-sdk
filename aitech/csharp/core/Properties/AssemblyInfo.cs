75b933cf8d78535de0db575c7fc26dd16345195d
sdk-team <sdk-team@alibabacloud.com>
2026-01-27 13:09:35 +0000

    Generated SDK for AITECH.

--- aitech/csharp/core/Properties/AssemblyInfo.cs
+++ aitech/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7e2142a0-fb80-11f0-a186-83495b9f759b")]
+[assembly: Guid("6598c8b0-fb81-11f0-8cf6-99ecc6ac51d4")]
 
 // Version information for an assembly consists of the following four values:
 //
