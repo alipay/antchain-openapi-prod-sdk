c3aec4dc0222ffe6cb4db9637b80336be6991282
sdk-team <sdk-team@alibabacloud.com>
2026-01-27 13:09:31 +0000

    Generated SDK for AITECH.

--- aitech/composer/src/Client.php
+++ aitech/composer/src/Client.php
@@ -280,7 +280,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.1.58',
+                    'sdk_version'      => '1.1.59',
                     '_prod_code'       => 'AITECH',
                     '_prod_channel'    => 'default',
                 ];
