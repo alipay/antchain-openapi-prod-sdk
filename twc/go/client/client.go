1ca331304703b305f0d76e09bc40a2ea5b2d2464
sdk-team <sdk-team@alibabacloud.com>
2026-01-29 06:33:19 +0000

    Generated SDK for TWC.

--- twc/go/client/client.go
+++ twc/go/client/client.go
@@ -25216,6 +25216,83 @@ func (s *UnbindContractZfbagreementResponse) SetUnsignSuccess(v bool) *UnbindCon
 	return s
 }
 
+type FinishContractFlowRequest struct {
+	// OAuth模式下的授权token
+	AuthToken         *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
+	ProductInstanceId *string `json:"product_instance_id,omitempty" xml:"product_instance_id,omitempty"`
+	// 流程id
+	FlowId *string `json:"flow_id,omitempty" xml:"flow_id,omitempty" require:"true"`
+	// 用户账号
+	AccountId *string `json:"account_id,omitempty" xml:"account_id,omitempty" require:"true"`
+}
+
+func (s FinishContractFlowRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s FinishContractFlowRequest) GoString() string {
+	return s.String()
+}
+
+func (s *FinishContractFlowRequest) SetAuthToken(v string) *FinishContractFlowRequest {
+	s.AuthToken = &v
+	return s
+}
+
+func (s *FinishContractFlowRequest) SetProductInstanceId(v string) *FinishContractFlowRequest {
+	s.ProductInstanceId = &v
+	return s
+}
+
+func (s *FinishContractFlowRequest) SetFlowId(v string) *FinishContractFlowRequest {
+	s.FlowId = &v
+	return s
+}
+
+func (s *FinishContractFlowRequest) SetAccountId(v string) *FinishContractFlowRequest {
+	s.AccountId = &v
+	return s
+}
+
+type FinishContractFlowResponse struct {
+	// 请求唯一ID，用于链路跟踪和问题排查
+	ReqMsgId *string `json:"req_msg_id,omitempty" xml:"req_msg_id,omitempty"`
+	// 结果码，一般OK表示调用成功
+	ResultCode *string `json:"result_code,omitempty" xml:"result_code,omitempty"`
+	// 异常信息的文本描述
+	ResultMsg *string `json:"result_msg,omitempty" xml:"result_msg,omitempty"`
+	// 是否成功. true-成功, false-不成功
+	Success *bool `json:"success,omitempty" xml:"success,omitempty"`
+}
+
+func (s FinishContractFlowResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s FinishContractFlowResponse) GoString() string {
+	return s.String()
+}
+
+func (s *FinishContractFlowResponse) SetReqMsgId(v string) *FinishContractFlowResponse {
+	s.ReqMsgId = &v
+	return s
+}
+
+func (s *FinishContractFlowResponse) SetResultCode(v string) *FinishContractFlowResponse {
+	s.ResultCode = &v
+	return s
+}
+
+func (s *FinishContractFlowResponse) SetResultMsg(v string) *FinishContractFlowResponse {
+	s.ResultMsg = &v
+	return s
+}
+
+func (s *FinishContractFlowResponse) SetSuccess(v bool) *FinishContractFlowResponse {
+	s.Success = &v
+	return s
+}
+
 type PushDigitalcontentUsageRequest struct {
 	// OAuth模式下的授权token
 	AuthToken         *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
@@ -50526,7 +50603,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.13.16"),
+				"sdk_version":      tea.String("1.13.20"),
 				"_prod_code":       tea.String("TWC"),
 				"_prod_channel":    tea.String("undefined"),
 			}
@@ -55545,6 +55622,40 @@ func (client *Client) UnbindContractZfbagreementEx(request *UnbindContractZfbagr
 	}
 	_result = &UnbindContractZfbagreementResponse{}
 	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("twc.notary.contract.zfbagreement.unbind"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+/**
+ * Description: 完成手动签署流程
+ * Summary: 完成手动签署流程
+ */
+func (client *Client) FinishContractFlow(request *FinishContractFlowRequest) (_result *FinishContractFlowResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &FinishContractFlowResponse{}
+	_body, _err := client.FinishContractFlowEx(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+/**
+ * Description: 完成手动签署流程
+ * Summary: 完成手动签署流程
+ */
+func (client *Client) FinishContractFlowEx(request *FinishContractFlowRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *FinishContractFlowResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = &FinishContractFlowResponse{}
+	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("twc.notary.contract.flow.finish"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
 	if _err != nil {
 		return _result, _err
 	}
