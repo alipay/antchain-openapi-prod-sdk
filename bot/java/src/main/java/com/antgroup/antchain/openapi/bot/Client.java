ea547e603bfbb198795f84a549327a1be0966029
sdk-team <sdk-team@alibabacloud.com>
2026-01-29 02:18:44 +0000

    Generated SDK for BOT.

--- bot/java/src/main/java/com/antgroup/antchain/openapi/bot/Client.java
+++ bot/java/src/main/java/com/antgroup/antchain/openapi/bot/Client.java
@@ -126,7 +126,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.12.62"),
+                    new TeaPair("sdk_version", "1.12.66"),
                     new TeaPair("_prod_code", "BOT"),
                     new TeaPair("_prod_channel", "undefined")
                 );
@@ -5963,6 +5963,27 @@ public class Client {
     public StartIotagentChatResponse startIotagentChatEx(StartIotagentChatRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "blockchain.bot.iotagent.chat.start", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new StartIotagentChatResponse());
+    }
+
+    /**
+     * <b>description</b> :
+     * <p>Description: 智能体流式物模型上报接口
+     * Summary: 智能体流式物模型上报接口</p>
+     */
+    public StartIotagentThingmodelResponse startIotagentThingmodel(StartIotagentThingmodelRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.startIotagentThingmodelEx(request, headers, runtime);
+    }
+
+    /**
+     * <b>description</b> :
+     * <p>Description: 智能体流式物模型上报接口
+     * Summary: 智能体流式物模型上报接口</p>
+     */
+    public StartIotagentThingmodelResponse startIotagentThingmodelEx(StartIotagentThingmodelRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "blockchain.bot.iotagent.thingmodel.start", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new StartIotagentThingmodelResponse());
     }
 
     /**
