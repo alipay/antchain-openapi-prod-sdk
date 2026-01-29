db118e292f2e81505395b9d25fb3e0e49d9f245d
sdk-team <sdk-team@alibabacloud.com>
2026-01-29 02:19:29 +0000

    Generated SDK for BOT.

--- bot/nodejs/src/client.ts
+++ bot/nodejs/src/client.ts
@@ -28188,6 +28188,65 @@ export class StartIotagentChatResponse extends $tea.Model {
   }
 }
 
+export class StartIotagentThingmodelRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  // 请求内容，内容为json字符串
+  agentChatRequest: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+      agentChatRequest: 'agent_chat_request',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+      agentChatRequest: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class StartIotagentThingmodelResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  // 物模型上报结果
+  chatCompletionObject?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+      chatCompletionObject: 'chat_completion_object',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+      chatCompletionObject: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class ExecThingsdidOneapiRequest extends $tea.Model {
   // OAuth模式下的授权token
   authToken?: string;
@@ -29570,7 +29629,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.12.62",
+          sdk_version: "1.12.66",
           _prod_code: "BOT",
           _prod_channel: "undefined",
         };
@@ -34847,6 +34906,25 @@ export default class Client {
   async startIotagentChatEx(request: StartIotagentChatRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<StartIotagentChatResponse> {
     Util.validateModel(request);
     return $tea.cast<StartIotagentChatResponse>(await this.doRequest("1.0", "blockchain.bot.iotagent.chat.start", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new StartIotagentChatResponse({}));
+  }
+
+  /**
+   * Description: 智能体流式物模型上报接口
+   * Summary: 智能体流式物模型上报接口
+   */
+  async startIotagentThingmodel(request: StartIotagentThingmodelRequest): Promise<StartIotagentThingmodelResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.startIotagentThingmodelEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 智能体流式物模型上报接口
+   * Summary: 智能体流式物模型上报接口
+   */
+  async startIotagentThingmodelEx(request: StartIotagentThingmodelRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<StartIotagentThingmodelResponse> {
+    Util.validateModel(request);
+    return $tea.cast<StartIotagentThingmodelResponse>(await this.doRequest("1.0", "blockchain.bot.iotagent.thingmodel.start", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new StartIotagentThingmodelResponse({}));
   }
 
   /**
