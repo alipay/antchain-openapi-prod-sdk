7ab33e2434cc6fb6ac2722da3f8571165a4333ae
sdk-team <sdk-team@alibabacloud.com>
2026-03-10 10:02:29 +0000

    Generated SDK for INTELLICAR.

--- intellicar/nodejs/src/client.ts
+++ intellicar/nodejs/src/client.ts
@@ -1129,6 +1129,69 @@ export class QueryUsedcarResponse extends $tea.Model {
   }
 }
 
+export class SubmitIonchiRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  // 线索唯一标识
+  submitId: string;
+  // 手机号md5
+  mobileMd5: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+      submitId: 'submit_id',
+      mobileMd5: 'mobile_md5',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+      submitId: 'string',
+      mobileMd5: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class SubmitIonchiResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  // 推送数科客户成功
+  pushSuccess?: boolean;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+      pushSuccess: 'push_success',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+      pushSuccess: 'boolean',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class CreateAntcloudGatewayxFileUploadRequest extends $tea.Model {
   // OAuth模式下的授权token
   authToken?: string;
@@ -1330,7 +1393,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.0.14",
+          sdk_version: "1.0.15",
           _prod_code: "INTELLICAR",
           _prod_channel: "default",
         };
@@ -1550,6 +1613,25 @@ export default class Client {
   async queryUsedcarEx(request: QueryUsedcarRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<QueryUsedcarResponse> {
     Util.validateModel(request);
     return $tea.cast<QueryUsedcarResponse>(await this.doRequest("1.0", "antdigital.intellicar.usedcar.query", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new QueryUsedcarResponse({}));
+  }
+
+  /**
+   * Description: 逸安启回调接口
+   * Summary: 逸安启回调接口
+   */
+  async submitIonchi(request: SubmitIonchiRequest): Promise<SubmitIonchiResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.submitIonchiEx(request, headers, runtime);
+  }
+
+  /**
+   * Description: 逸安启回调接口
+   * Summary: 逸安启回调接口
+   */
+  async submitIonchiEx(request: SubmitIonchiRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<SubmitIonchiResponse> {
+    Util.validateModel(request);
+    return $tea.cast<SubmitIonchiResponse>(await this.doRequest("1.0", "antdigital.intellicar.ionchi.submit", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new SubmitIonchiResponse({}));
   }
 
   /**
