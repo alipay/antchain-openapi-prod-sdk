a73111dc4e9a2c39d860b99a75c725e62edab36d
sdk-team <sdk-team@alibabacloud.com>
2026-03-25 06:09:11 +0000

    Generated SDK for BOT.

--- bot/nodejs/src/client.ts
+++ bot/nodejs/src/client.ts
@@ -29705,7 +29705,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.12.77",
+          sdk_version: "1.12.78",
           _prod_code: "BOT",
           _prod_channel: "undefined",
         };
