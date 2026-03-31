b897d36b3958f422324707cfad44d0f7d057e1d2
sdk-team <sdk-team@alibabacloud.com>
2026-03-31 14:32:51 +0000

    Generated SDK for INSURANCE_SAAS.

--- insurance_saas/csharp/core/Properties/AssemblyInfo.cs
+++ insurance_saas/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("872a3fb0-2d07-11f1-8d5e-732be7ea6211")]
+[assembly: Guid("75ef5fd0-2d0e-11f1-aad7-cfc3c7c46874")]
 
 // Version information for an assembly consists of the following four values:
 //
