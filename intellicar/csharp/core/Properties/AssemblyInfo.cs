5e00503283e040fa4db04f4706176ded64ed34f7
sdk-team <sdk-team@alibabacloud.com>
2026-04-01 09:51:41 +0000

    Generated SDK for INTELLICAR.

--- intellicar/csharp/core/Properties/AssemblyInfo.cs
+++ intellicar/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1b55a6a0-2c06-11f1-aded-97ed28c7de55")]
+[assembly: Guid("5aa28390-2db0-11f1-8f04-c98231daed55")]
 
 // Version information for an assembly consists of the following four values:
 //
