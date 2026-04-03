eee97608e14bacc5af8143b1aa6e5e73137cd563
sdk-team <sdk-team@alibabacloud.com>
2026-04-03 06:07:41 +0000

    Generated SDK for BOT.

--- bot/go/client/client.go
+++ bot/go/client/client.go
@@ -36996,6 +36996,118 @@ func (s *StartIotagentThingmodelResponse) SetChatCompletionObject(v string) *Sta
 	return s
 }
 
+type ImportIotagentClientRequest struct {
+	// OAuth模式下的授权token
+	AuthToken         *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
+	ProductInstanceId *string `json:"product_instance_id,omitempty" xml:"product_instance_id,omitempty"`
+	// 实例ID，由蚂蚁提供
+	InstanceId *string `json:"instance_id,omitempty" xml:"instance_id,omitempty" require:"true"`
+	// SKU名称，由蚂蚁提供
+	SkuName *string `json:"sku_name,omitempty" xml:"sku_name,omitempty" require:"true"`
+	// 模版智能体ID，由蚂蚁提供
+	TemplateAgentId *string `json:"template_agent_id,omitempty" xml:"template_agent_id,omitempty" require:"true"`
+	// 话题，由蚂蚁提供
+	AgentTopic *string `json:"agent_topic,omitempty" xml:"agent_topic,omitempty" require:"true"`
+	// 设备标识列表
+	UidList []*string `json:"uid_list,omitempty" xml:"uid_list,omitempty" require:"true" type:"Repeated"`
+}
+
+func (s ImportIotagentClientRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ImportIotagentClientRequest) GoString() string {
+	return s.String()
+}
+
+func (s *ImportIotagentClientRequest) SetAuthToken(v string) *ImportIotagentClientRequest {
+	s.AuthToken = &v
+	return s
+}
+
+func (s *ImportIotagentClientRequest) SetProductInstanceId(v string) *ImportIotagentClientRequest {
+	s.ProductInstanceId = &v
+	return s
+}
+
+func (s *ImportIotagentClientRequest) SetInstanceId(v string) *ImportIotagentClientRequest {
+	s.InstanceId = &v
+	return s
+}
+
+func (s *ImportIotagentClientRequest) SetSkuName(v string) *ImportIotagentClientRequest {
+	s.SkuName = &v
+	return s
+}
+
+func (s *ImportIotagentClientRequest) SetTemplateAgentId(v string) *ImportIotagentClientRequest {
+	s.TemplateAgentId = &v
+	return s
+}
+
+func (s *ImportIotagentClientRequest) SetAgentTopic(v string) *ImportIotagentClientRequest {
+	s.AgentTopic = &v
+	return s
+}
+
+func (s *ImportIotagentClientRequest) SetUidList(v []*string) *ImportIotagentClientRequest {
+	s.UidList = v
+	return s
+}
+
+type ImportIotagentClientResponse struct {
+	// 请求唯一ID，用于链路跟踪和问题排查
+	ReqMsgId *string `json:"req_msg_id,omitempty" xml:"req_msg_id,omitempty"`
+	// 结果码，一般OK表示调用成功
+	ResultCode *string `json:"result_code,omitempty" xml:"result_code,omitempty"`
+	// 异常信息的文本描述
+	ResultMsg *string `json:"result_msg,omitempty" xml:"result_msg,omitempty"`
+	// 成功导入的设备标识列表
+	SuccessUidList []*string `json:"success_uid_list,omitempty" xml:"success_uid_list,omitempty" type:"Repeated"`
+	// 已经存在的设备标识列表（不会导入）
+	ExistedUidList []*string `json:"existed_uid_list,omitempty" xml:"existed_uid_list,omitempty" type:"Repeated"`
+	// 不合法的设备标识列表（不会导入）
+	InvalidUidList []*string `json:"invalid_uid_list,omitempty" xml:"invalid_uid_list,omitempty" type:"Repeated"`
+}
+
+func (s ImportIotagentClientResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s ImportIotagentClientResponse) GoString() string {
+	return s.String()
+}
+
+func (s *ImportIotagentClientResponse) SetReqMsgId(v string) *ImportIotagentClientResponse {
+	s.ReqMsgId = &v
+	return s
+}
+
+func (s *ImportIotagentClientResponse) SetResultCode(v string) *ImportIotagentClientResponse {
+	s.ResultCode = &v
+	return s
+}
+
+func (s *ImportIotagentClientResponse) SetResultMsg(v string) *ImportIotagentClientResponse {
+	s.ResultMsg = &v
+	return s
+}
+
+func (s *ImportIotagentClientResponse) SetSuccessUidList(v []*string) *ImportIotagentClientResponse {
+	s.SuccessUidList = v
+	return s
+}
+
+func (s *ImportIotagentClientResponse) SetExistedUidList(v []*string) *ImportIotagentClientResponse {
+	s.ExistedUidList = v
+	return s
+}
+
+func (s *ImportIotagentClientResponse) SetInvalidUidList(v []*string) *ImportIotagentClientResponse {
+	s.InvalidUidList = v
+	return s
+}
+
 type ExecThingsdidOneapiRequest struct {
 	// OAuth模式下的授权token
 	AuthToken         *string `json:"auth_token,omitempty" xml:"auth_token,omitempty"`
@@ -38766,7 +38878,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.12.78"),
+				"sdk_version":      tea.String("1.12.79"),
 				"_prod_code":       tea.String("BOT"),
 				"_prod_channel":    tea.String("undefined"),
 			}
@@ -48193,6 +48305,40 @@ func (client *Client) StartIotagentThingmodelEx(request *StartIotagentThingmodel
 	}
 	_result = &StartIotagentThingmodelResponse{}
 	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("blockchain.bot.iotagent.thingmodel.start"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+/**
+ * Description: 智能体设备标识导入接口
+ * Summary: 智能体设备标识导入接口
+ */
+func (client *Client) ImportIotagentClient(request *ImportIotagentClientRequest) (_result *ImportIotagentClientResponse, _err error) {
+	runtime := &util.RuntimeOptions{}
+	headers := make(map[string]*string)
+	_result = &ImportIotagentClientResponse{}
+	_body, _err := client.ImportIotagentClientEx(request, headers, runtime)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = _body
+	return _result, _err
+}
+
+/**
+ * Description: 智能体设备标识导入接口
+ * Summary: 智能体设备标识导入接口
+ */
+func (client *Client) ImportIotagentClientEx(request *ImportIotagentClientRequest, headers map[string]*string, runtime *util.RuntimeOptions) (_result *ImportIotagentClientResponse, _err error) {
+	_err = util.ValidateModel(request)
+	if _err != nil {
+		return _result, _err
+	}
+	_result = &ImportIotagentClientResponse{}
+	_body, _err := client.DoRequest(tea.String("1.0"), tea.String("blockchain.bot.iotagent.client.import"), tea.String("HTTPS"), tea.String("POST"), tea.String("/gateway.do"), tea.ToMap(request), headers, runtime)
 	if _err != nil {
 		return _result, _err
 	}
