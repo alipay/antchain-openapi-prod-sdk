9f2d4e7a52f158c18204ced0e01afd3d1018da74
sdk-team <sdk-team@alibabacloud.com>
2026-04-24 11:16:28 +0000

    Generated SDK for ATO.

--- ato/composer/src/Client.php
+++ ato/composer/src/Client.php
@@ -772,7 +772,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.19.61',
+                    'sdk_version'      => '1.19.62',
                     '_prod_code'       => 'ATO',
                     '_prod_channel'    => 'undefined',
                 ];
