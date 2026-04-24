3954272f9d1a69ec20217cbaf889ddfaaa8acd3b
sdk-team <sdk-team@alibabacloud.com>
2026-04-24 06:03:30 +0000

    Generated SDK for INS_RISK.

--- ins_risk/csharp/core/Properties/AssemblyInfo.cs
+++ ins_risk/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("674d2150-3f8e-11f1-a96f-f319b159fd36")]
+[assembly: Guid("4a8d0110-3fa3-11f1-a178-2588d3e87d97")]
 
 // Version information for an assembly consists of the following four values:
 //
