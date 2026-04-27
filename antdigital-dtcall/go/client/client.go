59f1fe98e976e21a27652929e36785fac66b22fa
sdk-team <sdk-team@alibabacloud.com>
2026-04-27 09:02:44 +0000

    Generated SDK for antdigital-DTCALL.

--- antdigital-dtcall/go/client/client.go
+++ antdigital-dtcall/go/client/client.go
@@ -534,7 +534,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("1.0.0"),
+				"sdk_version":      tea.String("1.0.1"),
 				"_prod_code":       tea.String("DTCALL"),
 				"_prod_channel":    tea.String("default"),
 			}
