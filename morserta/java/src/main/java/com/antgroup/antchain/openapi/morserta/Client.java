23767c9ff7f9f6464c710190f4df946e44b4e72c
sdk-team <sdk-team@alibabacloud.com>
2026-05-07 08:56:13 +0000

    Generated SDK for MORSERTA.

--- morserta/java/src/main/java/com/antgroup/antchain/openapi/morserta/Client.java
+++ morserta/java/src/main/java/com/antgroup/antchain/openapi/morserta/Client.java
@@ -126,7 +126,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "6.1.2"),
+                    new TeaPair("sdk_version", "6.1.4"),
                     new TeaPair("_prod_code", "MORSERTA"),
                     new TeaPair("_prod_channel", "default")
                 );
@@ -333,6 +333,27 @@ public class Client {
     public ConversionAdDataAttributedResponse conversionAdDataAttributedEx(ConversionAdDataAttributedRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "antcloud.morserta.ad.data.attributed.conversion", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new ConversionAdDataAttributedResponse());
+    }
+
+    /**
+     * <b>description</b> :
+     * <p>Description: 新版转化回传接口
+     * Summary: 新版转化回传接口</p>
+     */
+    public SaveDataConversionResponse saveDataConversion(SaveDataConversionRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.saveDataConversionEx(request, headers, runtime);
+    }
+
+    /**
+     * <b>description</b> :
+     * <p>Description: 新版转化回传接口
+     * Summary: 新版转化回传接口</p>
+     */
+    public SaveDataConversionResponse saveDataConversionEx(SaveDataConversionRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "antcloud.morserta.data.conversion.save", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new SaveDataConversionResponse());
     }
 
     /**
