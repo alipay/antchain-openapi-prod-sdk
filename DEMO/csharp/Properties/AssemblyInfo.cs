14b6dad857e067a6edfce9fda0b01ac8347119af
antdigital2026 <antdigital2026@users.noreply.github.com>
2026-05-08 20:52:19 +0800

    Generated SDK for DEMO.

--- DEMO/csharp/Properties/AssemblyInfo.cs
+++ DEMO/csharp/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7d93bc10-4ac6-11f1-8d26-d1b11ca07b37")]
+[assembly: Guid("7f3b9320-4adb-11f1-8b65-1f08555ebf70")]
 
 // Version information for an assembly consists of the following four values:
 //
