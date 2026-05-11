be96cf5c5c72e8620dd5690ac864c4eec4675063
antdigital2026 <antdigital2026@users.noreply.github.com>
2026-05-11 11:16:49 +0800

    Generated SDK for antdigital-BENCHONE.

---
+++ antdigital-BENCHONE/java/src/main/java/com/antgroup/antchain/openapi/benchone/models/InitPack.java
@@ -0,0 +1,60 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.benchone.models;
+
+import com.aliyun.tea.*;
+
+public class InitPack extends TeaModel {
+    // 2022-11-07 14:48
+    /**
+     * <strong>example:</strong>
+     * <p>2022-11-07 14:48</p>
+     */
+    @NameInMap("time")
+    public String time;
+
+    // wanyi
+    /**
+     * <strong>example:</strong>
+     * <p>wanyi</p>
+     */
+    @NameInMap("operator")
+    public String operator;
+
+    // 1
+    /**
+     * <strong>example:</strong>
+     * <p>1</p>
+     */
+    @NameInMap("count")
+    public Long count;
+
+    public static InitPack build(java.util.Map<String, ?> map) throws Exception {
+        InitPack self = new InitPack();
+        return TeaModel.build(map, self);
+    }
+
+    public InitPack setTime(String time) {
+        this.time = time;
+        return this;
+    }
+    public String getTime() {
+        return this.time;
+    }
+
+    public InitPack setOperator(String operator) {
+        this.operator = operator;
+        return this;
+    }
+    public String getOperator() {
+        return this.operator;
+    }
+
+    public InitPack setCount(Long count) {
+        this.count = count;
+        return this;
+    }
+    public Long getCount() {
+        return this.count;
+    }
+
+}
