0f06d0198a460bd6b55efdae07a6d2cc5c630fe4
sdk-team <sdk-team@alibabacloud.com>
2026-05-19 15:13:48 +0000

    Generated SDK for DTAIAGT.

--- dtaiagt/go/client/client.go
+++ dtaiagt/go/client/client.go
@@ -6030,7 +6030,7 @@ func (client *Client) DoRequest(version *string, action *string, protocol *strin
 				"req_msg_id":       antchainutil.GetNonce(),
 				"access_key":       client.AccessKeyId,
 				"base_sdk_version": tea.String("TeaSDK-2.0"),
-				"sdk_version":      tea.String("4.0.2"),
+				"sdk_version":      tea.String("4.1.0"),
 				"_prod_code":       tea.String("DTAIAGT"),
 				"_prod_channel":    tea.String("default"),
 			}
