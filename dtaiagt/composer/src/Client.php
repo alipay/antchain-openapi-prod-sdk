acf26bd9f80e8f56c8d246601acf54c25b58ce3f
sdk-team <sdk-team@alibabacloud.com>
2026-05-19 15:13:37 +0000

    Generated SDK for DTAIAGT.

--- dtaiagt/composer/src/Client.php
+++ dtaiagt/composer/src/Client.php
@@ -224,7 +224,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '4.0.2',
+                    'sdk_version'      => '4.1.0',
                     '_prod_code'       => 'DTAIAGT',
                     '_prod_channel'    => 'default',
                 ];
