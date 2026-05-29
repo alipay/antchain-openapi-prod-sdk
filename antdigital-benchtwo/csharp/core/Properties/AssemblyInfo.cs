4da14450e44c666bc0be29cf15702a9b9b133a07
sdk-team <sdk-team@alibabacloud.com>
2026-05-29 02:29:26 +0000

    Generated SDK for antdigital-BENCHTWO.

--- antdigital-benchtwo/csharp/core/Properties/AssemblyInfo.cs
+++ antdigital-benchtwo/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e9b10090-233b-11f1-869d-99f9a262d422")]
+[assembly: Guid("2eabd2e0-5b06-11f1-8fec-3d744c1ea7b7")]
 
 // Version information for an assembly consists of the following four values:
 //
