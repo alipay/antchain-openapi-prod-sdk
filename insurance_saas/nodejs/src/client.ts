f387d5b265a8b713b6fe0deb6ee586e237310151
sdk-team <sdk-team@alibabacloud.com>
2026-06-01 07:29:29 +0000

    Generated SDK for INSURANCE_SAAS.

--- insurance_saas/nodejs/src/client.ts
+++ insurance_saas/nodejs/src/client.ts
@@ -4174,7 +4174,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.12.24",
+          sdk_version: "1.12.26",
           _prod_code: "INSURANCE_SAAS",
           _prod_channel: "undefined",
         };
