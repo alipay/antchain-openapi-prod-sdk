cd7fcaab752612ae270e485b18a2d351992e5618
sdk-team <sdk-team@alibabacloud.com>
2026-06-04 02:22:28 +0000

    Generated SDK for antdigital-AIGC.

--- antdigital-aigc/composer/src/Client.php
+++ antdigital-aigc/composer/src/Client.php
@@ -166,7 +166,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.1.2',
+                    'sdk_version'      => '1.1.3',
                     '_prod_code'       => 'AIGC',
                     '_prod_channel'    => 'default',
                 ];
