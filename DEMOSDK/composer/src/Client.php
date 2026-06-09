47e2a623308c4a4f3189cf607253afa9cba5283b
antdigital2026 <antdigital2026@users.noreply.github.com>
2026-06-09 17:58:47 +0800

    Generated SDK for DEMOSDK.

--- DEMOSDK/composer/src/Client.php
+++ DEMOSDK/composer/src/Client.php
@@ -216,7 +216,7 @@ class Client
                     'req_msg_id'       => UtilClient::getNonce(),
                     'access_key'       => $this->_accessKeyId,
                     'base_sdk_version' => 'TeaSDK-2.0',
-                    'sdk_version'      => '1.3.43',
+                    'sdk_version'      => '1.3.45',
                     '_prod_code'       => 'DEMOSDK',
                     '_prod_channel'    => 'default',
                 ];
