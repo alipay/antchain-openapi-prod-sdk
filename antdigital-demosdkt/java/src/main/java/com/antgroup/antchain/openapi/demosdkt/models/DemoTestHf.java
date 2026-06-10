37f8087c3bd2d74687a8629c9d874f1bb92aeb01
sdk-team <sdk-team@alibabacloud.com>
2026-06-10 02:44:25 +0000

    Generated SDK for antdigital-DEMOSDKT.

---
+++ antdigital-demosdkt/java/src/main/java/com/antgroup/antchain/openapi/demosdkt/models/DemoTestHf.java
@@ -0,0 +1,29 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.demosdkt.models;
+
+import com.aliyun.tea.*;
+
+public class DemoTestHf extends TeaModel {
+    // test
+    /**
+     * <strong>example:</strong>
+     * <p>undefined</p>
+     */
+    @NameInMap("info")
+    @Validation(required = true)
+    public ResultTest info;
+
+    public static DemoTestHf build(java.util.Map<String, ?> map) throws Exception {
+        DemoTestHf self = new DemoTestHf();
+        return TeaModel.build(map, self);
+    }
+
+    public DemoTestHf setInfo(ResultTest info) {
+        this.info = info;
+        return this;
+    }
+    public ResultTest getInfo() {
+        return this.info;
+    }
+
+}
