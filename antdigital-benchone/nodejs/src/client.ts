b43fb91e0901044fdb5294efa87abd753dc23707
sdk-team <sdk-team@alibabacloud.com>
2026-06-10 03:06:52 +0000

    Generated SDK for antdigital-BENCHONE.

---
+++ antdigital-benchone/nodejs/src/client.ts
@@ -0,0 +1,679 @@
+// This file is auto-generated, don't edit it
+import AntchainUtil from '@antchain/alipay-util';
+import Util, * as $Util from '@alicloud/tea-util';
+import RPCUtil from '@alicloud/rpc-util';
+import * as $tea from '@alicloud/tea-typescript';
+
+/**
+ * @remarks
+ * Model for initing client
+ */
+export class Config extends $tea.Model {
+  /**
+   * @remarks
+   * accesskey id
+   */
+  accessKeyId?: string;
+  /**
+   * @remarks
+   * accesskey secret
+   */
+  accessKeySecret?: string;
+  /**
+   * @remarks
+   * security token
+   */
+  securityToken?: string;
+  /**
+   * @remarks
+   * http protocol
+   * 
+   * @example
+   * http
+   */
+  protocol?: string;
+  /**
+   * @remarks
+   * read timeout
+   * 
+   * @example
+   * 10
+   */
+  readTimeout?: number;
+  /**
+   * @remarks
+   * connect timeout
+   * 
+   * @example
+   * 10
+   */
+  connectTimeout?: number;
+  /**
+   * @remarks
+   * http proxy
+   * 
+   * @example
+   * http://localhost
+   */
+  httpProxy?: string;
+  /**
+   * @remarks
+   * https proxy
+   * 
+   * @example
+   * https://localhost
+   */
+  httpsProxy?: string;
+  /**
+   * @remarks
+   * endpoint
+   * 
+   * @example
+   * cs.aliyuncs.com
+   */
+  endpoint?: string;
+  /**
+   * @remarks
+   * proxy white list
+   * 
+   * @example
+   * http://localhost
+   */
+  noProxy?: string;
+  /**
+   * @remarks
+   * max idle conns
+   * 
+   * @example
+   * 3
+   */
+  maxIdleConns?: number;
+  /**
+   * @remarks
+   * user agent
+   * 
+   * @example
+   * Alibabacloud/1
+   */
+  userAgent?: string;
+  /**
+   * @remarks
+   * socks5 proxy
+   */
+  socks5Proxy?: string;
+  /**
+   * @remarks
+   * socks5 network
+   * 
+   * @example
+   * TCP
+   */
+  socks5NetWork?: string;
+  /**
+   * @remarks
+   * 长链接最大空闲时长
+   */
+  maxIdleTimeMillis?: number;
+  /**
+   * @remarks
+   * 长链接最大连接时长
+   */
+  keepAliveDurationMillis?: number;
+  /**
+   * @remarks
+   * 最大连接数（长链接最大总数）
+   */
+  maxRequests?: number;
+  /**
+   * @remarks
+   * 每个目标主机的最大连接数（分主机域名的长链接最大总数
+   */
+  maxRequestsPerHost?: number;
+  static names(): { [key: string]: string } {
+    return {
+      accessKeyId: 'accessKeyId',
+      accessKeySecret: 'accessKeySecret',
+      securityToken: 'securityToken',
+      protocol: 'protocol',
+      readTimeout: 'readTimeout',
+      connectTimeout: 'connectTimeout',
+      httpProxy: 'httpProxy',
+      httpsProxy: 'httpsProxy',
+      endpoint: 'endpoint',
+      noProxy: 'noProxy',
+      maxIdleConns: 'maxIdleConns',
+      userAgent: 'userAgent',
+      socks5Proxy: 'socks5Proxy',
+      socks5NetWork: 'socks5NetWork',
+      maxIdleTimeMillis: 'maxIdleTimeMillis',
+      keepAliveDurationMillis: 'keepAliveDurationMillis',
+      maxRequests: 'maxRequests',
+      maxRequestsPerHost: 'maxRequestsPerHost',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      accessKeyId: 'string',
+      accessKeySecret: 'string',
+      securityToken: 'string',
+      protocol: 'string',
+      readTimeout: 'number',
+      connectTimeout: 'number',
+      httpProxy: 'string',
+      httpsProxy: 'string',
+      endpoint: 'string',
+      noProxy: 'string',
+      maxIdleConns: 'number',
+      userAgent: 'string',
+      socks5Proxy: 'string',
+      socks5NetWork: 'string',
+      maxIdleTimeMillis: 'number',
+      keepAliveDurationMillis: 'number',
+      maxRequests: 'number',
+      maxRequestsPerHost: 'number',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+// Demo类1
+export class DemoClass extends $tea.Model {
+  // 字符串测试
+  /**
+   * @example
+   * some string
+   */
+  someString: string;
+  // 日期测试
+  /**
+   * @example
+   * 3
+   */
+  someDate: string;
+  // Boolean测试
+  /**
+   * @example
+   * true
+   */
+  someBoolean: boolean;
+  // 整数测试
+  /**
+   * @example
+   * 3
+   */
+  someInt: number;
+  // 列表测试
+  someList: string[];
+  static names(): { [key: string]: string } {
+    return {
+      someString: 'some_string',
+      someDate: 'some_date',
+      someBoolean: 'some_boolean',
+      someInt: 'some_int',
+      someList: 'some_list',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      someString: 'string',
+      someDate: 'string',
+      someBoolean: 'boolean',
+      someInt: 'number',
+      someList: { 'type': 'array', 'itemType': 'string' },
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+// 返回结果测试类
+export class ResultTest extends $tea.Model {
+  // 姓名
+  /**
+   * @example
+   * 张三
+   */
+  name?: string;
+  // test
+  /**
+   * @example
+   * undefined
+   */
+  list: DemoClass;
+  static names(): { [key: string]: string } {
+    return {
+      name: 'name',
+      list: 'list',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      name: 'string',
+      list: DemoClass,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+// 返回结果测试类
+export class ResultTests extends $tea.Model {
+  // 姓名
+  /**
+   * @example
+   * 张三
+   */
+  name?: string;
+  // 年龄
+  /**
+   * @example
+   * 18
+   */
+  age: string;
+  static names(): { [key: string]: string } {
+    return {
+      name: 'name',
+      age: 'age',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      name: 'string',
+      age: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+// 1
+export class TestDemo extends $tea.Model {
+  // 1
+  /**
+   * @example
+   * 张三
+   */
+  name?: string;
+  // 1
+  /**
+   * @example
+   * 23
+   */
+  number?: number;
+  static names(): { [key: string]: string } {
+    return {
+      name: 'name',
+      number: 'number',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      name: 'string',
+      number: 'number',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+// test
+export class DemoTestHf extends $tea.Model {
+  // test
+  /**
+   * @example
+   * undefined
+   */
+  info: ResultTest;
+  static names(): { [key: string]: string } {
+    return {
+      info: 'info',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      info: ResultTest,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+// InitPack
+export class InitPack extends $tea.Model {
+  // 2022-11-07 14:48
+  /**
+   * @example
+   * 2022-11-07 14:48
+   */
+  time?: string;
+  // wanyi
+  /**
+   * @example
+   * wanyi
+   */
+  operator?: string;
+  // 1
+  /**
+   * @example
+   * 1
+   */
+  count?: number;
+  static names(): { [key: string]: string } {
+    return {
+      time: 'time',
+      operator: 'operator',
+      count: 'count',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      time: 'string',
+      operator: 'string',
+      count: 'number',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+// 测试实体
+export class SumBean extends $tea.Model {
+  // 元素名称
+  /**
+   * @example
+   * 张三
+   */
+  eName?: string;
+  // 数量
+  /**
+   * @example
+   * 23
+   */
+  num?: number;
+  // test
+  /**
+   * @example
+   * undefined
+   */
+  info: ResultTests;
+  static names(): { [key: string]: string } {
+    return {
+      eName: 'e_name',
+      num: 'num',
+      info: 'info',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      eName: 'string',
+      num: 'number',
+      info: ResultTests,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class ImportAntchainDemosdkCreateTestRequest extends $tea.Model {
+  // OAuth模式下的授权token
+  authToken?: string;
+  productInstanceId?: string;
+  static names(): { [key: string]: string } {
+    return {
+      authToken: 'auth_token',
+      productInstanceId: 'product_instance_id',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      authToken: 'string',
+      productInstanceId: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class ImportAntchainDemosdkCreateTestResponse extends $tea.Model {
+  // 请求唯一ID，用于链路跟踪和问题排查
+  reqMsgId?: string;
+  // 结果码，一般OK表示调用成功
+  resultCode?: string;
+  // 异常信息的文本描述
+  resultMsg?: string;
+  static names(): { [key: string]: string } {
+    return {
+      reqMsgId: 'req_msg_id',
+      resultCode: 'result_code',
+      resultMsg: 'result_msg',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      reqMsgId: 'string',
+      resultCode: 'string',
+      resultMsg: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+
+export default class Client {
+  _endpoint: string;
+  _regionId: string;
+  _accessKeyId: string;
+  _accessKeySecret: string;
+  _protocol: string;
+  _userAgent: string;
+  _readTimeout: number;
+  _connectTimeout: number;
+  _httpProxy: string;
+  _httpsProxy: string;
+  _socks5Proxy: string;
+  _socks5NetWork: string;
+  _noProxy: string;
+  _maxIdleConns: number;
+  _securityToken: string;
+  _maxIdleTimeMillis: number;
+  _keepAliveDurationMillis: number;
+  _maxRequests: number;
+  _maxRequestsPerHost: number;
+
+  /**
+   * @remarks
+   * Init client with Config
+   * 
+   * @param config - config contains the necessary information to create a client
+   */
+  constructor(config: Config) {
+    if (Util.isUnset(config)) {
+      throw $tea.newError({
+        code: "ParameterMissing",
+        message: "'config' can not be unset",
+      });
+    }
+
+    this._accessKeyId = config.accessKeyId;
+    this._accessKeySecret = config.accessKeySecret;
+    this._securityToken = config.securityToken;
+    this._endpoint = config.endpoint;
+    this._protocol = config.protocol;
+    this._userAgent = config.userAgent;
+    this._readTimeout = Util.defaultNumber(config.readTimeout, 20000);
+    this._connectTimeout = Util.defaultNumber(config.connectTimeout, 20000);
+    this._httpProxy = config.httpProxy;
+    this._httpsProxy = config.httpsProxy;
+    this._noProxy = config.noProxy;
+    this._socks5Proxy = config.socks5Proxy;
+    this._socks5NetWork = config.socks5NetWork;
+    this._maxIdleConns = Util.defaultNumber(config.maxIdleConns, 60000);
+    this._maxIdleTimeMillis = Util.defaultNumber(config.maxIdleTimeMillis, 5);
+    this._keepAliveDurationMillis = Util.defaultNumber(config.keepAliveDurationMillis, 5000);
+    this._maxRequests = Util.defaultNumber(config.maxRequests, 100);
+    this._maxRequestsPerHost = Util.defaultNumber(config.maxRequestsPerHost, 100);
+  }
+
+  /**
+   * @remarks
+   * Encapsulate the request and invoke the network
+   * 
+   * @param action - api name
+   * @param protocol - http or https
+   * @param method - e.g. GET
+   * @param pathname - pathname of every api
+   * @param request - which contains request params
+   * @param runtime - which controls some details of call api, such as retry times
+   * @returns the response
+   */
+  async doRequest(version: string, action: string, protocol: string, method: string, pathname: string, request: {[key: string]: any}, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<{[key: string]: any}> {
+    let _runtime: { [key: string]: any } = {
+      timeouted: "retry",
+      readTimeout: Util.defaultNumber(runtime.readTimeout, this._readTimeout),
+      connectTimeout: Util.defaultNumber(runtime.connectTimeout, this._connectTimeout),
+      httpProxy: Util.defaultString(runtime.httpProxy, this._httpProxy),
+      httpsProxy: Util.defaultString(runtime.httpsProxy, this._httpsProxy),
+      noProxy: Util.defaultString(runtime.noProxy, this._noProxy),
+      maxIdleConns: Util.defaultNumber(runtime.maxIdleConns, this._maxIdleConns),
+      maxIdleTimeMillis: this._maxIdleTimeMillis,
+      keepAliveDuration: this._keepAliveDurationMillis,
+      maxRequests: this._maxRequests,
+      maxRequestsPerHost: this._maxRequestsPerHost,
+      retry: {
+        retryable: runtime.autoretry,
+        maxAttempts: Util.defaultNumber(runtime.maxAttempts, 3),
+      },
+      backoff: {
+        policy: Util.defaultString(runtime.backoffPolicy, "no"),
+        period: Util.defaultNumber(runtime.backoffPeriod, 1),
+      },
+      ignoreSSL: runtime.ignoreSSL,
+    }
+
+    let _lastRequest = null;
+    let _now = Date.now();
+    let _retryTimes = 0;
+    while ($tea.allowRetry(_runtime['retry'], _retryTimes, _now)) {
+      if (_retryTimes > 0) {
+        let _backoffTime = $tea.getBackoffTime(_runtime['backoff'], _retryTimes);
+        if (_backoffTime > 0) {
+          await $tea.sleep(_backoffTime);
+        }
+      }
+
+      _retryTimes = _retryTimes + 1;
+      try {
+        let request_ = new $tea.Request();
+        request_.protocol = Util.defaultString(this._protocol, protocol);
+        request_.method = method;
+        request_.pathname = pathname;
+        request_.query = {
+          method: action,
+          version: version,
+          sign_type: "HmacSHA1",
+          req_time: AntchainUtil.getTimestamp(),
+          req_msg_id: AntchainUtil.getNonce(),
+          access_key: this._accessKeyId,
+          base_sdk_version: "TeaSDK-2.0",
+          sdk_version: "1.0.3",
+          _prod_code: "BENCHONE",
+          _prod_channel: "default",
+        };
+        if (!Util.empty(this._securityToken)) {
+          request_.query["security_token"] = this._securityToken;
+        }
+
+        request_.headers = {
+          host: Util.defaultString(this._endpoint, "openapi.antchain.antgroup.com"),
+          'user-agent': Util.getUserAgent(this._userAgent),
+          ...headers,
+        };
+        let tmp = Util.anyifyMapValue(RPCUtil.query(request));
+        request_.body = new $tea.BytesReadable(Util.toFormString(tmp));
+        request_.headers["content-type"] = "application/x-www-form-urlencoded";
+        let signedParam = {
+          ...request_.query,
+          ...RPCUtil.query(request),
+        };
+        request_.query["sign"] = AntchainUtil.getSignature(signedParam, this._accessKeySecret);
+        _lastRequest = request_;
+        let response_ = await $tea.doAction(request_, _runtime);
+
+        let raw = await Util.readAsString(response_.body);
+        let obj = Util.parseJSON(raw);
+        let res = Util.assertAsMap(obj);
+        let resp = Util.assertAsMap(res["response"]);
+        if (AntchainUtil.hasError(raw, this._accessKeySecret)) {
+          throw $tea.newError({
+            message: resp["result_msg"],
+            data: resp,
+            code: resp["result_code"],
+          });
+        }
+
+        return resp;
+      } catch (ex) {
+        if ($tea.isRetryable(ex)) {
+          continue;
+        }
+        throw ex;
+      }
+    }
+
+    throw $tea.newUnretryableError(_lastRequest);
+  }
+
+  /**
+   * @remarks
+   * Description: 用于个人工作台二期测试使用
+   * Summary: 用于个人工作台二期测试使用
+   */
+  async importAntchainDemosdkCreateTest(request: ImportAntchainDemosdkCreateTestRequest): Promise<ImportAntchainDemosdkCreateTestResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers : {[key: string ]: string} = { };
+    return await this.importAntchainDemosdkCreateTestEx(request, headers, runtime);
+  }
+
+  /**
+   * @remarks
+   * Description: 用于个人工作台二期测试使用
+   * Summary: 用于个人工作台二期测试使用
+   */
+  async importAntchainDemosdkCreateTestEx(request: ImportAntchainDemosdkCreateTestRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<ImportAntchainDemosdkCreateTestResponse> {
+    Util.validateModel(request);
+    return $tea.cast<ImportAntchainDemosdkCreateTestResponse>(await this.doRequest("1.0", "antchain.demosdk.create.test.import", "HTTPS", "POST", `/gateway.do`, $tea.toMap(request), headers, runtime), new ImportAntchainDemosdkCreateTestResponse({}));
+  }
+
+}
