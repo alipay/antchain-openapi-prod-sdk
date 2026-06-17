eb7f4aa3903ae98a8123e71da41d92ad49934905
sdk-team <sdk-team@alibabacloud.com>
2026-06-17 02:38:08 +0000

    Generated SDK for TWC.

--- twc/java/src/main/java/com/antgroup/antchain/openapi/twc/Client.java
+++ twc/java/src/main/java/com/antgroup/antchain/openapi/twc/Client.java
@@ -126,7 +126,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.13.26"),
+                    new TeaPair("sdk_version", "1.13.28"),
                     new TeaPair("_prod_code", "TWC"),
                     new TeaPair("_prod_channel", "undefined")
                 );
@@ -7895,5 +7895,26 @@ public class Client {
     public QueryEsignAccountResponse queryEsignAccountEx(QueryEsignAccountRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "twc.notary.esign.account.query", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new QueryEsignAccountResponse());
+    }
+
+    /**
+     * <b>description</b> :
+     * <p>Description: 区块链合同查询授权有效期
+     * Summary: 区块链合同查询授权有效期</p>
+     */
+    public QueryContractSignauthResponse queryContractSignauth(QueryContractSignauthRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.queryContractSignauthEx(request, headers, runtime);
+    }
+
+    /**
+     * <b>description</b> :
+     * <p>Description: 区块链合同查询授权有效期
+     * Summary: 区块链合同查询授权有效期</p>
+     */
+    public QueryContractSignauthResponse queryContractSignauthEx(QueryContractSignauthRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "twc.notary.contract.signauth.query", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new QueryContractSignauthResponse());
     }
 }
