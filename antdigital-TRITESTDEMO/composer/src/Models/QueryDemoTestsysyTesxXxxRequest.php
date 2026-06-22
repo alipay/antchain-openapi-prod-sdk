bb55c992c949beacb7b53675299a61cf91693eff
antdigital2026 <antdigital2026@users.noreply.github.com>
2026-06-22 11:39:07 +0800

    Generated SDK for antdigital-TRITESTDEMO.

---
+++ antdigital-TRITESTDEMO/composer/src/Models/QueryDemoTestsysyTesxXxxRequest.php
@@ -0,0 +1,73 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AntChain\TRITESTDEMO\Models;
+
+use AlibabaCloud\Tea\Model;
+
+class QueryDemoTestsysyTesxXxxRequest extends Model
+{
+    // OAuth模式下的授权token
+    /**
+     * @var string
+     */
+    public $authToken;
+
+    /**
+     * @var string
+     */
+    public $productInstanceId;
+
+    // 张三
+    /**
+     * @var string
+     */
+    public $name;
+    protected $_name = [
+        'authToken'         => 'auth_token',
+        'productInstanceId' => 'product_instance_id',
+        'name'              => 'name',
+    ];
+
+    public function validate()
+    {
+    }
+
+    public function toMap()
+    {
+        $res = [];
+        if (null !== $this->authToken) {
+            $res['auth_token'] = $this->authToken;
+        }
+        if (null !== $this->productInstanceId) {
+            $res['product_instance_id'] = $this->productInstanceId;
+        }
+        if (null !== $this->name) {
+            $res['name'] = $this->name;
+        }
+
+        return $res;
+    }
+
+    /**
+     * @param array $map
+     *
+     * @return QueryDemoTestsysyTesxXxxRequest
+     */
+    public static function fromMap($map = [])
+    {
+        $model = new self();
+        if (isset($map['auth_token'])) {
+            $model->authToken = $map['auth_token'];
+        }
+        if (isset($map['product_instance_id'])) {
+            $model->productInstanceId = $map['product_instance_id'];
+        }
+        if (isset($map['name'])) {
+            $model->name = $map['name'];
+        }
+
+        return $model;
+    }
+}
