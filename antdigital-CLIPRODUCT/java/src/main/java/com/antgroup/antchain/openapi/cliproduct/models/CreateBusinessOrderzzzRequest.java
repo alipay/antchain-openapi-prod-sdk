6b60275efe5a240c1e1c480c5cbe584deb8b869d
antdigital2026 <antdigital2026@users.noreply.github.com>
2026-07-08 17:15:06 +0800

    Generated SDK for antdigital-CLIPRODUCT.

---
+++ antdigital-CLIPRODUCT/java/src/main/java/com/antgroup/antchain/openapi/cliproduct/models/CreateBusinessOrderzzzRequest.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.antgroup.antchain.openapi.cliproduct.models;
+
+import com.aliyun.tea.*;
+
+public class CreateBusinessOrderzzzRequest extends TeaModel {
+    // OAuth模式下的授权token
+    @NameInMap("auth_token")
+    public String authToken;
+
+    @NameInMap("product_instance_id")
+    public String productInstanceId;
+
+    // 订单编号
+    @NameInMap("order_no")
+    @Validation(required = true)
+    public String orderNo;
+
+    public static CreateBusinessOrderzzzRequest build(java.util.Map<String, ?> map) throws Exception {
+        CreateBusinessOrderzzzRequest self = new CreateBusinessOrderzzzRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public CreateBusinessOrderzzzRequest setAuthToken(String authToken) {
+        this.authToken = authToken;
+        return this;
+    }
+    public String getAuthToken() {
+        return this.authToken;
+    }
+
+    public CreateBusinessOrderzzzRequest setProductInstanceId(String productInstanceId) {
+        this.productInstanceId = productInstanceId;
+        return this;
+    }
+    public String getProductInstanceId() {
+        return this.productInstanceId;
+    }
+
+    public CreateBusinessOrderzzzRequest setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+        return this;
+    }
+    public String getOrderNo() {
+        return this.orderNo;
+    }
+
+}
