e4564fd8050a8b1592fcbc969892fbbea97297c1
antdigital2026 <antdigital2026@users.noreply.github.com>
2026-07-14 20:08:57 +0800

    Generated SDK for antdigital-CLIPRODUCT.

--- antdigital-CLIPRODUCT/java/README-CN.md
+++ antdigital-CLIPRODUCT/java/README-CN.md
@@ -8,7 +8,7 @@
 <dependency>
    <groupId>com.antgroup.antchain.openapi</groupId>
    <artifactId>antdigital-cliproduct</artifactId>
-   <version>1.0.7</version>
+   <version>1.0.8</version>
 </dependency>
 ```
 
--- antdigital-CLIPRODUCT/java/src/main/java/com/antgroup/antchain/openapi/cliproduct/Client.java
+++ antdigital-CLIPRODUCT/java/src/main/java/com/antgroup/antchain/openapi/cliproduct/Client.java
@@ -126,7 +126,7 @@ public class Client {
                     new TeaPair("req_msg_id", com.antgroup.antchain.openapi.antchain.util.AntchainUtils.getNonce()),
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("base_sdk_version", "TeaSDK-2.0"),
-                    new TeaPair("sdk_version", "1.0.7"),
+                    new TeaPair("sdk_version", "1.0.8"),
                     new TeaPair("_prod_code", "CLIPRODUCT"),
                     new TeaPair("_prod_channel", "default")
                 );
@@ -522,6 +522,27 @@ public class Client {
     public CreateBusinessOrderjResponse createBusinessOrderjEx(CreateBusinessOrderjRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         return TeaModel.toModel(this.doRequest("1.0", "antdigital.cliproduct.business.orderj.create", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new CreateBusinessOrderjResponse());
+    }
+
+    /**
+     * <b>description</b> :
+     * <p>Description: 创建订单
+     * Summary: 创建订单</p>
+     */
+    public CreateBusinessOrderkResponse createBusinessOrderk(CreateBusinessOrderkRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.createBusinessOrderkEx(request, headers, runtime);
+    }
+
+    /**
+     * <b>description</b> :
+     * <p>Description: 创建订单
+     * Summary: 创建订单</p>
+     */
+    public CreateBusinessOrderkResponse createBusinessOrderkEx(CreateBusinessOrderkRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("1.0", "antdigital.cliproduct.business.orderk.create", "HTTPS", "POST", "/gateway.do", TeaModel.buildMap(request), headers, runtime), new CreateBusinessOrderkResponse());
     }
 
     /**
