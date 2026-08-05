ac9e927a4c4e858944b738f5b87280b3de9adee5
antdigital2026 <antdigital2026@users.noreply.github.com>
2026-08-05 14:23:50 +0800

    Generated SDK for antdigital-mscene.

--- antdigital-mscene/composer/src/Client.php
+++ antdigital-mscene/composer/src/Client.php
@@ -157,7 +157,7 @@ class Client {
                     "req_msg_id" => UtilClient::getNonce(),
                     "access_key" => $this->_accessKeyId,
                     "base_sdk_version" => "TeaSDK-2.0",
-                    "sdk_version" => "1.0.1",
+                    "sdk_version" => "1.0.3",
                     "_prod_code" => "MSCENE",
                     "_prod_channel" => "default"
                 ];
