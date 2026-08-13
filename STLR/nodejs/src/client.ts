c88187c4350af24cd81ee9596cf8986f7faa5c86
antdigital2026 <antdigital2026@users.noreply.github.com>
2026-08-13 15:56:18 +0800

    Generated SDK for STLR.

--- STLR/nodejs/src/client.ts
+++ STLR/nodejs/src/client.ts
@@ -10213,7 +10213,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "2.11.15",
+          sdk_version: "2.11.16",
           _prod_code: "STLR",
           _prod_channel: "undefined",
         };
