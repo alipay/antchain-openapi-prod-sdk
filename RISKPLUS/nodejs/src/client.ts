2b27b2fca397485b589fb98c8d3827feb3ecb8c5
antdigital2026 <antdigital2026@users.noreply.github.com>
2026-08-17 11:26:34 +0800

    Generated SDK for RISKPLUS.

--- RISKPLUS/nodejs/src/client.ts
+++ RISKPLUS/nodejs/src/client.ts
@@ -36274,7 +36274,7 @@ export default class Client {
           req_msg_id: AntchainUtil.getNonce(),
           access_key: this._accessKeyId,
           base_sdk_version: "TeaSDK-2.0",
-          sdk_version: "1.31.28",
+          sdk_version: "1.31.29",
           _prod_code: "RISKPLUS",
           _prod_channel: "undefined",
         };
