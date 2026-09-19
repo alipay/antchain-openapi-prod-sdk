9f645a850bd3241986cc8950801ec0343119433f
antdigital2026 <antdigital2026@users.noreply.github.com>
2026-09-19 19:59:06 +0800

    Generated SDK for antdigital-AIOSPRODUCT.

---
+++ antdigital-AIOSPRODUCT/composer/src/Models/SaveGwchildinsurancePlanningtargetsRequest.php
@@ -0,0 +1,78 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+namespace AntChain\AIOSPRODUCT\Models;
+
+use AlibabaCloud\Tea\Model;
+
+class SaveGwchildinsurancePlanningtargetsRequest extends Model {
+    protected $_name = [
+        'authToken' => 'auth_token',
+        'productInstanceId' => 'product_instance_id',
+        'tenantId' => 'tenant_id',
+        'requestData' => 'request_data',
+    ];
+    public function validate() {
+        Model::validateRequired('tenantId', $this->tenantId, true);
+        Model::validateRequired('requestData', $this->requestData, true);
+    }
+    public function toMap() {
+        $res = [];
+        if (null !== $this->authToken) {
+            $res['auth_token'] = $this->authToken;
+        }
+        if (null !== $this->productInstanceId) {
+            $res['product_instance_id'] = $this->productInstanceId;
+        }
+        if (null !== $this->tenantId) {
+            $res['tenant_id'] = $this->tenantId;
+        }
+        if (null !== $this->requestData) {
+            $res['request_data'] = $this->requestData;
+        }
+        return $res;
+    }
+    /**
+     * @param array $map
+     * @return SaveGwchildinsurancePlanningtargetsRequest
+     */
+    public static function fromMap($map = []) {
+        $model = new self();
+        if(isset($map['auth_token'])){
+            $model->authToken = $map['auth_token'];
+        }
+        if(isset($map['product_instance_id'])){
+            $model->productInstanceId = $map['product_instance_id'];
+        }
+        if(isset($map['tenant_id'])){
+            $model->tenantId = $map['tenant_id'];
+        }
+        if(isset($map['request_data'])){
+            $model->requestData = $map['request_data'];
+        }
+        return $model;
+    }
+    // OAuth模式下的授权token
+    /**
+     * @var string
+     */
+    public $authToken;
+
+    /**
+     * @var string
+     */
+    public $productInstanceId;
+
+    // 租户标识，沿用已发布 Profile API 的租户校验。
+    /**
+     * @var string
+     */
+    public $tenantId;
+
+    // JSON 字符串形式的业务请求；必须是单层 JSON 字符串，不能再次序列化成二层 JSON 字符串。
+    /**
+     * @var string
+     */
+    public $requestData;
+
+}
---
+++ antdigital-AIOSPRODUCT/composer/src/Models/SaveGwchildinsuranceSolutionkycResponse.php
@@ -0,0 +1,102 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+namespace AntChain\AIOSPRODUCT\Models;
+
+use AlibabaCloud\Tea\Model;
+
+class SaveGwchildinsuranceSolutionkycResponse extends Model {
+    protected $_name = [
+        'reqMsgId' => 'req_msg_id',
+        'resultCode' => 'result_code',
+        'resultMsg' => 'result_msg',
+        'data' => 'data',
+        'message' => 'message',
+        'success' => 'success',
+    ];
+    public function validate() {}
+    public function toMap() {
+        $res = [];
+        if (null !== $this->reqMsgId) {
+            $res['req_msg_id'] = $this->reqMsgId;
+        }
+        if (null !== $this->resultCode) {
+            $res['result_code'] = $this->resultCode;
+        }
+        if (null !== $this->resultMsg) {
+            $res['result_msg'] = $this->resultMsg;
+        }
+        if (null !== $this->data) {
+            $res['data'] = $this->data;
+        }
+        if (null !== $this->message) {
+            $res['message'] = $this->message;
+        }
+        if (null !== $this->success) {
+            $res['success'] = $this->success;
+        }
+        return $res;
+    }
+    /**
+     * @param array $map
+     * @return SaveGwchildinsuranceSolutionkycResponse
+     */
+    public static function fromMap($map = []) {
+        $model = new self();
+        if(isset($map['req_msg_id'])){
+            $model->reqMsgId = $map['req_msg_id'];
+        }
+        if(isset($map['result_code'])){
+            $model->resultCode = $map['result_code'];
+        }
+        if(isset($map['result_msg'])){
+            $model->resultMsg = $map['result_msg'];
+        }
+        if(isset($map['data'])){
+            $model->data = $map['data'];
+        }
+        if(isset($map['message'])){
+            $model->message = $map['message'];
+        }
+        if(isset($map['success'])){
+            $model->success = $map['success'];
+        }
+        return $model;
+    }
+    // 请求唯一ID，用于链路跟踪和问题排查
+    /**
+     * @var string
+     */
+    public $reqMsgId;
+
+    // 结果码，一般OK表示调用成功
+    /**
+     * @var string
+     */
+    public $resultCode;
+
+    // 异常信息的文本描述
+    /**
+     * @var string
+     */
+    public $resultMsg;
+
+    // 业务数据 JSON 字符串
+    /**
+     * @var string
+     */
+    public $data;
+
+    // 成功时允许为 null，失败时返回可理解原因
+    /**
+     * @var string
+     */
+    public $message;
+
+    // 业务处理是否成功
+    /**
+     * @var bool
+     */
+    public $success;
+
+}
