ac5bdbde02e6aa38f7d8607084cf44e9adf67891
antdigital2026 <antdigital2026@users.noreply.github.com>
2026-09-23 11:37:38 +0800

    Generated SDK for GESAAS.

--- GESAAS/composer/src/Client.php
+++ GESAAS/composer/src/Client.php
@@ -179,7 +179,7 @@ class Client {
                     "req_msg_id" => UtilClient::getNonce(),
                     "access_key" => $this->_accessKeyId,
                     "base_sdk_version" => "TeaSDK-2.0",
-                    "sdk_version" => "1.4.1",
+                    "sdk_version" => "1.6.0",
                     "_prod_code" => "GESAAS",
                     "_prod_channel" => "default"
                 ];
